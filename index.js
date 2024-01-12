3b9fd43fca9592b8c8bdce98d56dd446b3a946ee
mun-jihye <m84422583@gmail.com>
2024-01-13 02:02:04 +0900

    fix: text노드 삭제 후 다시 화면이 늘어났을 때 text노드 추가 로직 수정

--- index.js
+++ index.js
@@ -16,12 +16,35 @@ function applyChanges() {
       var img = container.querySelector("img");
       var title = container.querySelector(".title");
       var subtitle = container.querySelector(".subtitle");
+
       if (text && img && title && subtitle) {
         // title과 subtitle을 img와 같은 레벨로 이동
         img.parentNode.insertBefore(title, img.nextSibling);
         img.parentNode.insertBefore(subtitle, img.nextSibling);
 
         text.parentNode.removeChild(text);
+      }
+    });
+  } else {
+    // 웹 화면에서 실행할 코드
+    containers.forEach(function (container, index) {
+      var text = container.querySelector(".text");
+      var img = container.querySelector("img");
+      var title = container.querySelector(".title");
+      var subtitle = container.querySelector(".subtitle");
+      if (!text && img && title && subtitle) {
+        // text가 없으면 생성
+        var newText = document.createElement("div");
+        newText.classList.add("text");
+        if (index === 0 || index === 2) {
+          container.insertBefore(newText, img);
+        } else {
+          container.appendChild(newText);
+        }
+
+        // title과 subtitle을 text 아래로 이동
+        newText.appendChild(title);
+        newText.appendChild(subtitle);
       }
     });
   }
