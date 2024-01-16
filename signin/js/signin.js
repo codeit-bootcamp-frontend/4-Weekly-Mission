915e40e4ac837fb2b96c71b62209c5758605edcb
김재성 <bumpy@gimjaeseong-ui-MacBookAir.local>
2024-01-16 16:27:59 +0900

    fix: 코드리뷰 내용 추가 반영
    - 스크립트에서 alt보단 data사용
    - br을 이용해서 반응형 대응
    - img width 100% 사용하지 않고 스타일링

--- signin/js/signin.js
+++ signin/js/signin.js
@@ -4,13 +4,13 @@ pwdView.addEventListener('click', function () {
     let target = this.dataset.target;
     let targetInput = document.querySelector(`#${target}`);
     let eye = this.querySelector('img');
-    if (eye.getAttribute('alt') == 'off') {
-        eye.setAttribute('alt', 'on');
+    if (eye.dataset.onoff == 'off') {
+        eye.dataset.onoff = 'on';
         eye.setAttribute('src', '/images/eye-on.png');
 
         targetInput.setAttribute('type', 'text');
     } else {
-        eye.setAttribute('alt', 'off');
+        eye.dataset.onoff = 'off';
         eye.setAttribute('src', '/images/eye-off.png');
 
         targetInput.setAttribute('type', 'password');
--- signup/js/signup.js
+++ signup/js/signup.js
@@ -26,13 +26,13 @@ function pwdSwitch(arg) {
     let target = arg.dataset.target;
     let targetInput = document.querySelector(`#${target}`);
     let eye = arg.querySelector('img');
-    if (eye.getAttribute('alt') == 'off') {
-        eye.setAttribute('alt', 'on');
+    if (eye.dataset.onoff == 'off') {
+        eye.dataset.onoff = 'on';
         eye.setAttribute('src', '/images/eye-on.png');
 
         targetInput.setAttribute('type', 'text');
     } else {
-        eye.setAttribute('alt', 'off');
+        eye.dataset.onoff = 'off';
         eye.setAttribute('src', '/images/eye-off.png');
 
         targetInput.setAttribute('type', 'password');
