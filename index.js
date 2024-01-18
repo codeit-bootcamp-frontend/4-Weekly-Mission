83eb0a5005c307fba90239dd364e0cea409db089
whai2 <whai2125@gmail.com>
2024-01-18 19:28:53 +0900

    feat: 기본 요구 사항, 잘못된 이메일 입력 에러 메세지 구현

--- index.js
+++ index.js
@@ -3,18 +3,50 @@ let email = document.querySelector(".input-email");
 function noInputFocusOut() {
   let container = document.querySelector(".input-form-email")
 
-  // 엘리먼트 생성
-  let span = document.createElement("span");
-  container.appendChild(span);
-  span.classList.add('errorMessage');
-  span.style.color = 'red';
-  span.style.position = 'relative';
-  span.style.top = '-6px';
+  // 기존 오류 엘리먼트를 삭제
+  let oldErrorMessage = document.querySelector(".errorMessage");
+  if (oldErrorMessage) {
+      container.removeChild(oldErrorMessage);
+  }
 
   // 동작
-  let errorMessage = document.querySelector(".errorMessage");
   if (email.value.trim() === "") {
+    // 엘리먼트 생성
+    let span = document.createElement("span");
+    container.appendChild(span);
+    span.classList.add('errorMessage',"empty");
+    span.style.color = 'red';
+    span.style.position = 'relative';
+    span.style.top = '-6px';
+    // 텍스트 출력
+    let errorMessage = document.querySelector(".errorMessage");
     errorMessage.textContent = '이메일을 입력해주세요';
+  }
+}
+
+function notValidEmailInput() {
+  let container = document.querySelector(".input-form-email")
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // 기존 오류 엘리먼트를 삭제
+  let oldErrorMessage = document.querySelector(".errorMessage");
+  if (oldErrorMessage) {
+      container.removeChild(oldErrorMessage);
+  }
+
+  if (emailRegex.test(email)) {
+    return
+  } else {
+    // 엘리먼트 생성
+    let span = document.createElement("span");
+    container.appendChild(span);
+    span.classList.add('errorMessage');
+    span.style.color = 'red';
+    span.style.position = 'relative';
+    span.style.top = '-6px';
+    // 텍스트 출력
+    let errorMessage = document.querySelector(".errorMessage");
+    errorMessage.textContent = '올바른 이메일 주소가 아닙니다';
   }
 }
 
@@ -25,5 +57,13 @@ function focusIn() {
   }
 }
 
+function focusOut() {
+  let errorMessage = document.querySelector(".errorMessage");
+  if (errorMessage) {
+    errorMessage.remove();
+  }
+}
+
 email.addEventListener("focusout", noInputFocusOut);
+email.addEventListener("change", notValidEmailInput);
 email.addEventListener("focusin", focusIn);
