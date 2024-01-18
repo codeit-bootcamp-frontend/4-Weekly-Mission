371ed6d62a68d04e5da2fcf35951d1c81567ea3d
sihyonn <sihyeon982@naver.com>
2024-01-19 02:41:41 +0900

    feat: 비밀번호 input 폼 유효성 검사 추가 및 focusout 이벤트 등록

--- src/sign.js
+++ src/sign.js
@@ -2,9 +2,15 @@ import SIGN from './constants/VALIDATION.js';
 
 const signEmail = document.getElementById('email');
 const emailError = document.getElementById('email-error');
+const signPw = document.getElementById('password');
+const pwError = document.getElementById('password-error');
 
 const isValidEmail = (email) => {
   return SIGN.EMAIL_REGEX.test(email);
+};
+
+const isValidPw = (pw) => {
+  return SIGN.PW_REGEX.test(pw);
 };
 
 const validateEmail = () => {
@@ -15,4 +21,12 @@ const validateEmail = () => {
   else emailError.innerText = '';
 };
 
+const validatePw = () => {
+  const pwValue = signPw.value.trim();
+  if (pwValue === '') pwError.innerText = SIGN.REQUIRED_PASSWORD;
+  else if (!isValidPw(pwValue)) pwError.innerText = SIGN.INVALID_PW_FORMAT;
+  else pwError.innerText = '';
+};
+
 signEmail.addEventListener('focusout', validateEmail);
+signPw.addEventListener('focusout', validatePw);
