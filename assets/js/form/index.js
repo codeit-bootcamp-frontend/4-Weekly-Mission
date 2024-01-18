b94f4dcee5d8db2bbf47cb51af20f719c15174bf
d5ng <dong5ffice@gmail.com>
2024-01-18 15:02:25 +0900

    Revert "Create js file"
    This reverts commit 47cfdcaab212733b008f2a0459f2d52d05a53094.

--- assets/js/form/index.js
+++
@@ -1,7 +0,0 @@
-import FormInput from "./form-input.js"
-
-const inputEmail = document.querySelector(".input-email")
-const inputPassword = document.querySelector(".input-password")
-
-new FormInput("email", inputEmail)
-new FormInput("password", inputPassword)
