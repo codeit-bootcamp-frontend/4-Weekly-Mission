b584f9a25e881f3a4e8159c2d852052d14c840ce
박지윤 <tkrhkfld123@naver.com>
2024-01-18 23:34:36 +0900

    ✨ feat: 로그인 미입력, 올바르지 못한 형식에 대한 에러 문구출력 기능 구현

--- sign.js
+++ sign.js
@@ -1,19 +1,60 @@
-const emailInput = document.querySelector('.sign-input');
+// 아이디, 비밀번호 input 클래스 전부 가져오기
+const signInputValue = document.querySelector('.sign-input');
+// 비어있는 아이디로 focusout시 나타나는 에러 메세지
 const emptyEmailErrorMessage = document.querySelector('.empty-email-error');
+// 옳지 않은 형식의 아이디로 focusout시 나타나는 에러 메세지
 const invalidEmailFormatMessage = document.querySelector('.correct-email-error');
+// 비어있는 비밀번호로 focusout시 나타나는 에러 메세지
+const emptyPasswordErrorMessage = document.querySelector('.empty-password-error');
+// 아이디 input id 가져오기
+const signEmailInputValue = document.querySelector('#email-input'); 
+// 비밀번호 input id 가져오기
+const signPasswordInputValue = document.querySelector('#email-password')
+// 아이디 input value 값
+const emailValue = signEmailInputValue.value.trim();
 
-emailInput.addEventListener('focusout', function () {
-    let emailValue = emailInput.value.trim();
-    if (emailValue === '') {
-        emptyEmailErrorMessage.classList.add('show-error');
-        invalidEmailFormatMessage.classList.remove('show-error');
-    } else {
-        emptyEmailErrorMessage.classList.remove('show-error');
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(emailValue)) {
-          invalidEmailFormatMessage.classList.add('show-error');
-        } else {
-          invalidEmailFormatMessage.classList.remove('show-error');
-        }
-    }
+function emailRegexError(){
+  const emailValue = signEmailInputValue.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailValue)) {
+    invalidEmailFormatMessage.classList.add('show-error');
+    signEmailInputValue.classList.add('redBorder')
+    emptyEmailErrorMessage.classList.remove('show-error');
+  } else {
+    invalidEmailFormatMessage.classList.remove('show-error');
+    signEmailInputValue.classList.remove('redBorder')
+
+  }
+}
+function emailError(){
+  const emailValue = signEmailInputValue.value.trim();
+  if (emailValue === '') {
+    emptyEmailErrorMessage.classList.add('show-error');
+    signEmailInputValue.classList.add('redBorder')
+  } else {
+    emailRegexError()
+    emptyEmailErrorMessage.classList.remove('show-error');
+    signEmailInputValue.classList.remove('.redBorder');
+  }
+}
+function passwordError(){
+  let passwordValue = signPasswordInputValue.value.trim();
+  if(passwordValue === ''){
+    emptyPasswordErrorMessage.classList.add('show-error')
+  }
+}
+
+signInputValue.addEventListener('focusout', function() {
+  console.log('실행되나??');
+  emailError();
+  passwordError();
 });
+
+function submitLogin(){
+  let correctEmail =  'test@codeit.com'
+  let correctPassword = 'codeit101'
+  if(emailValue === correctEmail && passwordValue === correctPassword){
+    window.location.href = './folder'
+  }
+  console.log('correctEmail')
+}
