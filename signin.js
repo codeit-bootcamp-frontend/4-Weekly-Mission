f905a67971c9273e9bb146370613d69d856e39b9
jae6269 <jae6269@gmail.com>
2024-01-18 19:20:44 +0900

    feat(pw-onoff-img) : 눈모양 아이콘 클릭시 비밀번호 확인기능추가

--- signin.js
+++ signin.js
@@ -1,20 +1,39 @@
 const emailInput = document.getElementById('signin-email');
 const pwInput = document.getElementById('signin-pw');
 const pwOnOffImg = document.getElementById('pw-onoff');
+let pwSwitch = false;
+const signButton = document.getElementById('signin-button');
+const signForm = document.querySelector('form');
+
 //이메일 유효성 검사 메서드
 function emailCheck(email) {
   const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
   return email_regex.test(email);
 }
+
+/*파랑(focus), 빨강(error), 그레이(default) 색으로 border변경,
+  각 상태에 따라서 error msg block,none 결정 매서드 */
+function inputBorderBlue(element) {
+  element.style.outline = 'none';
+  element.style.border = '1px solid #6d6afe';
+}
+
+function inputBorderRed(inputElement, errorElement) {
+  inputElement.style.border = '1px solid var(--Linkbrary-red,#FF5B56)';
+  errorElement.style.display = 'block';
+}
+function inputBorderGray(inputElement, errorElement) {
+  inputElement.style.border = '1px solid var(--Linkbrary-gray20, #CCD5E3)';
+  errorElement.style.display = 'none';
+}
+
 /*이메일 , 비밀번호 input focus일때
   테두리 색상 변경 (css -> js)*/
 emailInput.addEventListener('focus', function () {
-  emailInput.style.outline = 'none';
-  emailInput.style.border = '1px solid #6d6afe';
+  inputBorderBlue(emailInput);
 });
 pwInput.addEventListener('focus', function () {
-  pwInput.style.outline = 'none';
-  pwInput.style.border = '1px solid #6d6afe';
+  inputBorderBlue(pwInput);
 });
 
 // 이메일 에러 검사 메서드
@@ -24,15 +43,12 @@ emailInput.addEventListener('focusout', function () {
   const emailError = document.getElementById('email__error-msg');
   if (!emailValue) {
     emailError.innerHTML = '이메일을 입력해 주세요.';
-    emailInput.style.border = '1px solid var(--Linkbrary-red,#FF5B56)';
-    emailError.style.display = 'block';
+    inputBorderRed(emailInput, emailError);
   } else if (emailCheck(emailValue) === false) {
     emailError.innerHTML = '올바른 이메일 주소가 아닙니다.';
-    emailInput.style.border = '1px solid var(--Linkbrary-red,#FF5B56)';
-    emailError.style.display = 'block';
+    inputBorderRed(emailInput, emailError);
   } else {
-    emailInput.style.border = '1px solid var(--Linkbrary-gray20, #CCD5E3)';
-    emailError.style.display = 'none';
+    inputBorderGray(emailInput, emailError);
   }
 });
 
@@ -43,11 +59,29 @@ pwInput.addEventListener('focusout', function () {
   if (!pwValue) {
     pwError.innerHTML = '비밀번호를 입력해 주세요.';
     pwOnOffImg.style.bottom = '2.9375rem';
-    pwInput.style.border = '1px solid var(--Linkbrary-red,#FF5B56)';
-    pwError.style.display = 'block';
+    inputBorderRed(pwInput, pwError);
   } else {
     pwOnOffImg.style.bottom = '1.1875rem';
-    pwInput.style.border = '1px solid var(--Linkbrary-gray20, #CCD5E3)';
-    pwError.style.display = 'none';
+    inputBorderGray(pwInput, pwError);
+  }
+});
+
+//비밀번호 확인 기능 매서드
+pwOnOffImg.addEventListener('click', function () {
+  if (!pwSwitch) {
+    pwOnOffImg.src = './icons/eyeson.png';
+    pwInput.type = 'none';
+    pwSwitch = true;
+  } else {
+    pwOnOffImg.src = './icons/eyesoff.png';
+    pwInput.type = 'password';
+    pwSwitch = false;
+  }
+});
+
+//로그인 기능 매서드
+signForm.addEventListener('submit', function () {
+  if (emailInput.value === 'test@codeit.com' && pwInput.value === '1') {
+    window.location.href('folder.html');
   }
 });
