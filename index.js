42053fd4d13a35809d1edf1bfedfeeb82f780230
whai2 <whai2125@gmail.com>
2024-01-19 23:11:31 +0900

    feat: 기본 사항, 비밀 번호 에러 메세지 구현

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let email = document.querySelector(".input-email");
+let password = document.querySelector(".input-password");
 
 function noInputFocusOut() {
-  let container = document.querySelector(".input-form-email")
+  let container = document.querySelector(".input-form-email");
 
   // 기존 오류 엘리먼트를 삭제
   let oldErrorMessage = document.querySelector(".errorMessage");
@@ -20,7 +21,31 @@ function noInputFocusOut() {
     span.style.top = '-6px';
     // 텍스트 출력
     let errorMessage = document.querySelector(".errorMessage");
-    errorMessage.textContent = '이메일을 입력해주세요';
+    errorMessage.textContent = '이메일을 입력해 주세요';
+  }
+}
+
+function noInputFocusOutPassword() {
+  let container = document.querySelector(".input-form-password");
+
+  // 기존 오류 엘리먼트를 삭제
+  let oldErrorMessage = document.querySelector(".errorMessage");
+  if (oldErrorMessage) {
+      container.removeChild(oldErrorMessage);
+  }
+
+  // 동작
+  if (email.value.trim() === "") {
+    // 엘리먼트 생성
+    let span = document.createElement("span");
+    container.appendChild(span);
+    span.classList.add('errorMessage',"empty");
+    span.style.color = 'red';
+    span.style.position = 'relative';
+    span.style.top = '-6px';
+    // 텍스트 출력
+    let errorMessage = document.querySelector(".errorMessage");
+    errorMessage.textContent = '비밀번호를 입력해 주세요';
   }
 }
 
@@ -66,15 +91,17 @@ function focusOut() {
 }
 
 email.addEventListener("focusout", noInputFocusOut);
+password.addEventListener("focusout", noInputFocusOutPassword);
 email.addEventListener("change", notValidEmailInput);
 email.addEventListener("focusin", focusIn);
+password.addEventListener("focusin", focusIn);
 
 // 로그인 시도
 function folderPage() {
   let folderEmail = "test@codeit.com";
   let folderPassword = "codeit101";
 
-  if (email.value === folderEmail && 입력된_비밀번호 === folderPassword) {
+  if (email.value === folderEmail && password.value === folderPassword) {
     // 로그인 성공 시 "/folder" 페이지로 이동
     window.location.href = "/folder";
   } else {
