941189446ae5041b428cd6ab85bbb7b863a85a55
whai2 <whai2125@gmail.com>
2024-01-19 22:05:21 +0900

    feat: 기본 사항, 폴더 페이지 이동 구현

--- index.js
+++ index.js
@@ -66,6 +66,19 @@ function focusOut() {
 }
 
 email.addEventListener("focusout", noInputFocusOut);
-//email.addEventListener("focusout", notValidEmailInput);
 email.addEventListener("change", notValidEmailInput);
 email.addEventListener("focusin", focusIn);
+
+// 로그인 시도
+function folderPage() {
+  let folderEmail = "test@codeit.com";
+  let folderPassword = "codeit101";
+
+  if (email.value === folderEmail && 입력된_비밀번호 === folderPassword) {
+    // 로그인 성공 시 "/folder" 페이지로 이동
+    window.location.href = "/folder";
+  } else {
+    // 로그인 실패 시 적절한 처리
+    console.log("로그인 실패. 올바른 이메일과 비밀번호를 입력하세요.");
+  }
+}
