e5368aeec39f3e49769b32f1c1da84b8f646ddea
soohwan93 <soohwan2im@gmail.com>
2024-01-19 12:00:37 +0900

    validation 빨간 테두리 추가

--- signin.js
+++ signin.js
@@ -1,13 +1,12 @@
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const loginBtn = document.querySelector('#loginBtn');
-const eyeIcon = document.querySelector('#eyeIcon')
+const eyeIcon = document.querySelector('#eyeIcon');
 
 
 //email focus out 이벤트
 emailInput.onblur = function(){
   //초기화
-  init(emailInput);
   init(emailInput);
 
   //이메일 유효성 함수 호출
@@ -27,10 +26,13 @@ loginBtn.onclick = function(){
   if(passwordInput.value === 'codeit101' && emailInput.value === 'test@codeit.com'){
     location.href = 'folder.html';
   }else {
-    errMsg(emailInput,'이메일을 확인해 주세요.')
-    errMsg(passwordInput,'비밀번호를 확인해 주세요.')
+    emailInput.classList.add('false');
+    passwordInput.classList.add('false');
+    errMsg(emailInput,'이메일을 확인해 주세요.');
+    errMsg(passwordInput,'비밀번호를 확인해 주세요.');
   }
 }
+
 
 
 //이메일 유효성 검사
@@ -54,12 +56,12 @@ function errMsg(inputVal ,txt) {
 
 //요소 초기화
 function init(element){
-    //div 요소 확인
+    //다음 요소 확인
     const divExist = element.nextElementSibling;
-
-    //div 요소 확인 될 시 요소 삭제
+    //다음 요소 확인 될 시 요소 삭제, 빨간 테두리 제거
     if(divExist){
       divExist.remove();
+      element.classList.remove('false');
     }
 }
 
@@ -80,15 +82,18 @@ eyeIcon.onclick = function() {
 //이메일 유효성 검사
 function emailValidationChk(){
   if(!emailInput.value){
-    errMsg(emailInput,'이메일을 입력해 주세요.')
+    emailInput.classList.add('false');//테두리 색 변경
+    errMsg(emailInput,'이메일을 입력해 주세요.');
   }else if(!emailValidChk(emailInput.value)){
-    errMsg(emailInput, '올바른 이메일 주소가 아닙니다.')
+    emailInput.classList.add('false');
+    errMsg(emailInput, '올바른 이메일 주소가 아닙니다.');
   }
 }
 
 //비밀번호 유효성 검사
 function passwordValidationChk(){
   if(!passwordInput.value){
-    errMsg(passwordInput,'비밀번호를 입력해 주세요.')
+    passwordInput.classList.add('false');
+    errMsg(passwordInput,'비밀번호를 입력해 주세요.');
   }
 }
