a62e22ce86fa8ad545286cc28f7426e0feb342c0
suzinxix <suzinxix@gmail.com>
2024-01-19 11:33:49 +0900

    feat: implement password toggle button

--- signin.js
+++ signin.js
@@ -4,14 +4,14 @@ const btn = document.querySelector(".btn-submit");
 const showErrorMsg = (e) => {
   const message = e.target.value;
 
-  if (e.target.type === "email") {
+  if (e.target.name === "email") {
     const emailMsgBox = document.querySelector(".error-email");
     message
       ? emailValid(e.target.value)
         ? (emailMsgBox.innerHTML = "")
         : (emailMsgBox.innerHTML = "올바른 이메일 주소가 아닙니다.")
       : (emailMsgBox.innerHTML = "이메일을 입력해 주세요.");
-  } else if (e.target.type === "password") {
+  } else if (e.target.name === "password") {
     const passwordMsgBox = document.querySelector(".error-pw");
     message ? (passwordMsgBox.innerHTML = "") : (passwordMsgBox.innerHTML = "비밀번호를 확인해 주세요.");
   }
@@ -37,9 +37,18 @@ const enter = (e) => {
     }
   }
 
+const showPassword = (e) => {
+  const password = document.querySelector(".input-pw");
+  if(e.target.classList.contains('img-eye')){
+    e.target.classList.toggle('active');
+    password.type = password.type === 'password' ? 'text' : 'password';
+  }
+}
+
 form.addEventListener("focusout", showErrorMsg);
 btn.addEventListener("click", moveFolderPage);
 form.addEventListener("keydown", enter);
+form.addEventListener("click", showPassword);
 
 function emailValid(email) {
   regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
