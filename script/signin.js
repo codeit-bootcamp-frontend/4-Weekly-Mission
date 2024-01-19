fcac6fd315f00926715f467a88409dffa09a0f11
Jules <dali1999@naver.com>
2024-01-20 03:52:25 +0900

    feat: emailInput focusout 이벤트 추가

---
+++ script/signin.js
@@ -0,0 +1,23 @@
+// input
+const emailInput = document.querySelector(".sign-email");
+const passwordInput = document.querySelector(".sign-password input");
+
+// error Message
+const emailErrorMessage = document.querySelector(".sign-email-error");
+const passwordErrorMessage = document.querySelector(".sign-password-error");
+
+const loginBtn = document.querySelector(".sign-btn");
+
+// focusout events
+emailInput.addEventListener("focusout", () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailInput.value === "") {
+    emailErrorMessage.textContent = "이메일을 입력해 주세요";
+  } else if (!emailRegex.test(emailInput.value)) {
+    emailErrorMessage.textContent = "올바른 이메일 주소가 아닙니다.";
+  } else {
+    emailErrorMessage.textContent = "";
+  }
+});
+
+
