b768dc4af0cd5567c1ae137cb9e994b06d321ff6
yohan11 <yipco77@naver.com>
2024-01-20 03:37:57 +0900

    feat: 주어진 계정으로 로그인 시도 시 페이지 이동 및 이외 로그인 시도 시 에러 표시 구현

--- src/js/signin.js
+++ src/js/signin.js
@@ -1,9 +1,16 @@
 const inputEmail = document.querySelector("#email");
 const inputPassword = document.querySelector("#password");
+const btnSigninSubmit = document.querySelector("#btn_signin_submit");
+const form = document.querySelector("form");
+
+const correctEmail = "test@codeit.com";
+const correctPassword = "codeit101";
 
 const checkEmailIsValid = (e) => {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   const errorMessage = e.target.nextElementSibling;
+
+  let isValid = false;
 
   if (e.target.value === "") {
     errorMessage.textContent = "이메일을 입력해 주세요.";
@@ -14,19 +21,53 @@ const checkEmailIsValid = (e) => {
   } else {
     errorMessage.textContent = "";
     e.target.classList.remove("error_box");
+    isValid = true;
   }
+  return isValid;
 };
 
 const checkPasswordIsValid = (e) => {
   const errorMessage = e.target.nextElementSibling.nextElementSibling;
+  let isValid = false;
   if (e.target.value === "") {
     errorMessage.textContent = "비밀번호를 입력해 주세요.";
     e.target.classList.add("error_box");
   } else {
     errorMessage.textContent = "";
     e.target.classList.remove("error_box");
+    isValid = true;
+  }
+  return isValid;
+};
+
+const compareEmail = () => {
+  if (inputEmail.value !== correctEmail) {
+    inputEmail.nextElementSibling.textContent = "이메일을 확인해 주세요.";
+    inputEmail.classList.add("error_box");
+  }
+  return inputEmail.value === correctEmail;
+};
+
+const comparePassword = () => {
+  if (inputPassword.value !== correctPassword) {
+    inputPassword.nextElementSibling.nextElementSibling.textContent =
+      "비밀번호를 확인해 주세요.";
+    inputPassword.classList.add("error_box");
+  }
+  return inputPassword.value === correctPassword;
+};
+
+const compareUser = (e) => {
+  e.preventDefault();
+  const isCorrectEmail = compareEmail();
+  const isCorrectPassword = comparePassword();
+
+  if (isCorrectEmail && isCorrectPassword) {
+    form.submit();
   }
 };
 
 inputEmail.addEventListener("focusout", checkEmailIsValid);
 inputPassword.addEventListener("focusout", checkPasswordIsValid);
+
+btnSigninSubmit.addEventListener("click", compareUser);
