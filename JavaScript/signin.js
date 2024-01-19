b541358f747d5ae22ed31536711cac3da432b004
Rhajiit <ksd9638@gmail.com>
2024-01-19 16:14:10 +0900

    Fixed not errorDesign to Input
    Fix errorDesign not disapear

--- JavaScript/signin.js
+++ JavaScript/signin.js
@@ -18,11 +18,13 @@ function submitLoginData(event)  {
          if (inputtedLoginPasswordCheck.value == registeredEmails.password[i]) {
             userVerification = true
             window.location.replace("folder.html");
-
          }
       } 
    }
+
    if (!userVerification) {
+      inputtedLoginEmailCheck.classList.add('errorInputDesign')
+      inputtedLoginPasswordCheck.classList.add('errorInputDesign')
       inputtedLoginEmailCheck.parentElement.lastElementChild.textContent = '이메일을 확인해 주세요.'
       inputtedLoginPasswordCheck.parentElement.parentElement.lastElementChild.textContent = '비밀번호를 확인해 주세요.' 
    }
@@ -33,12 +35,15 @@ orderCheckEmail.addEventListener('click' , submitLoginData)
 // 이메일 포커스
 function emailFocusOut(event) {
    if (event.target.value == '') {
+      event.target.classList.add('errorInputDesign')
       event.target.parentElement.lastElementChild.textContent = '이메일을 입력해 주세요.'
    } else if (!event.target.value.match(mailFormat)) {
+      event.target.classList.add('errorInputDesign')
       event.target.parentElement.lastElementChild.textContent = '올바른 이메일 주소가 아닙니다.'
    }
 }
 function emailFocusIn(event) {
+   inputtedLoginEmailCheck.classList.remove('errorInputDesign')
    event.target.parentElement.lastElementChild.textContent = ''
 }
 inputtedLoginEmailCheck.addEventListener('focus' , emailFocusIn)
@@ -48,10 +53,12 @@ inputtedLoginEmailCheck.addEventListener('blur' , emailFocusOut)
 // 패스워드 포커스
 function passwordFocusOut(event) {
    if (event.target.value == '') {
+      event.target.classList.add('errorInputDesign')
       event.target.parentElement.parentElement.lastElementChild.textContent = '비밀번호를 입력해 주세요.' 
    }
 }
 function passwordFocusIn(event) {
+   inputtedLoginPasswordCheck.classList.remove('errorInputDesign')
    event.target.parentElement.parentElement.lastElementChild.textContent = ''
    
 }
