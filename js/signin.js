8389f06d58a453a6c298d473831b5a8ac117c7ae
nali-kang <softnali92@gmail.com>
2024-01-20 01:18:44 +0900

    fix(signin): 주석 추가, 소스정리

--- js/signin.js
+++ js/signin.js
@@ -1,20 +1,24 @@
+// valid input 관련 element
 const emailInput = document.querySelector("#signin_email");
 const emailValidText = document.querySelector("#email_valid_text");
 const pwInput = document.querySelector("#signin_password");
 const pwValidText = document.querySelector("#pw_valid_text");
 
+// 심화 : password eye button
 const pwHiddenBtn = document.querySelector("#pw_hidden_btn");
 pwHiddenBtn.innerHTML = '<img src="./images/eye-off.svg" />';
 
+// signin form
 const signinForm = document.querySelector("#signin_form");
-const signinBtn = document.querySelector("#signin_button");
 
+// email 정규식
 const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
-let isValidId = false;
-let isValidPw = false;
+// let isValidId = false;
+// let isValidPw = false;
 let isToggleEyes = false;
 
+// focus on
 emailInput.addEventListener("focus", function () {
   inputFocusOn(emailInput, emailValidText);
 });
@@ -22,6 +26,7 @@ pwInput.addEventListener("focus", function () {
   inputFocusOn(pwInput, pwValidText);
 });
 
+// focus out
 emailInput.addEventListener("focusout", function () {
   if (emailInput.value === "") {
     inputFocusOutError(emailInput, emailValidText, "이메일을 입력해 주세요.");
@@ -36,7 +41,6 @@ emailInput.addEventListener("focusout", function () {
     inputFocusOutNone(emailInput, emailValidText);
   }
 });
-
 pwInput.addEventListener("focusout", function () {
   if (pwInput.value === "") {
     inputFocusOutError(pwInput, pwValidText, "비밀번호를 입력해 주세요.");
@@ -46,6 +50,7 @@ pwInput.addEventListener("focusout", function () {
   }
 });
 
+// form submit (enter, submit click)
 signinForm.addEventListener("submit", function (e) {
   e.preventDefault();
   if (emailInput.value === "test@codeit.com" && pwInput.value === "codeit101") {
@@ -53,6 +58,19 @@ signinForm.addEventListener("submit", function (e) {
   } else {
     inputFocusOutError(emailInput, emailValidText, "이메일을 확인해 주세요.");
     inputFocusOutError(pwInput, pwValidText, "비밀번호를 확인해 주세요.");
+  }
+});
+
+// 심화 : pw eye button 클릭 이벤트 추가 > type - password, text toggle event
+pwHiddenBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  isToggleEyes = !isToggleEyes;
+  if (isToggleEyes) {
+    pwHiddenBtn.innerHTML = '<img src="./images/eye-on.svg" />';
+    pwInput.type = "text";
+  } else {
+    pwHiddenBtn.innerHTML = '<img src="./images/eye-off.svg" />';
+    pwInput.type = "password";
   }
 });
 
@@ -71,15 +89,3 @@ function inputFocusOutError(input, textElm, text) {
   input.style.outline = "none";
   textElm.textContent = text;
 }
-
-pwHiddenBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  isToggleEyes = !isToggleEyes;
-  if (isToggleEyes) {
-    pwHiddenBtn.innerHTML = '<img src="./images/eye-on.svg" />';
-    pwInput.type = "text";
-  } else {
-    pwHiddenBtn.innerHTML = '<img src="./images/eye-off.svg" />';
-    pwInput.type = "password";
-  }
-});
