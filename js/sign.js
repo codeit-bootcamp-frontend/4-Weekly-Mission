860d7762200f97713df586ccd675fc1eb1547417
LeeSeungHoon <ysh73900@gmail.com>
2024-01-20 11:52:52 +0900

    feat: 비밀번호 유효성 검사 함수

--- js/sign.js
+++ js/sign.js
@@ -1,16 +1,26 @@
 const email = document.querySelector("#email");
-const error = document.querySelector(".error");
+const password = document.querySelector("#password");
+const emailError = document.querySelector(".email-error");
+const passwordError = document.querySelector(".password-error");
 
 /* 이메일 유효성 검사 */
 function isValidEmail() {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (email.value === "") {
-    error.innerHTML = "이메일을 입력해 주세요.";
+    emailError.innerHTML = "이메일을 입력해 주세요.";
   } else if (!emailRegex.test(email.value)) {
-    error.innerHTML = "올바른 이메일 주소가 아닙니다.";
+    emailError.innerHTML = "올바른 이메일 주소가 아닙니다.";
   } else {
-    error.innerHTML = "";
+    emailError.innerHTML = "";
+  }
+}
+
+/* 비밀번호 유효성 검사 */
+function isValidPassword() {
+  if (password.value === "") {
+    passwordError.innerHTML = "비밀번호를 입력해주세요.";
   }
 }
 
 email.addEventListener("blur", isValidEmail);
+password.addEventListener("blur", isValidPassword);
