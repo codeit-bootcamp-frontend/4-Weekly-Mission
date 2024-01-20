caa51da7b467ef419f93c5c8f941a7231d304200
hongjaehoun <ehswkfn123@naver.com>
2024-01-20 20:29:05 +0900

    비밀번호 체크기능 추가

--- linkbrary/script/signin.js
+++ linkbrary/script/signin.js
@@ -1,8 +1,8 @@
 let email_input = document.querySelector(".email_input")
 let email_error = document.querySelector(".email_error") 
 let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
-console.log(1)
+let pw_input = document.querySelector('.pw_input')
+let password_error  = document.querySelector(".password_error")
 function checkNullValue(e) {
     if(e.target.value == "") {
         email_error.innerHTML = "이메일을 입력해주세요"
@@ -22,12 +22,33 @@ function checkNullValue(e) {
     }
 }
 
-function inputFocus() {
+function emailInputFocus() {
     email_error.classList.remove('view_error')
     email_input.classList.add('input_focus')
+    
+
 }
-email_input.addEventListener('focus', inputFocus)
+email_input.addEventListener('focus', emailInputFocus)
 email_input.addEventListener('blur', checkNullValue)
 
 
-console.log(regExp.test(email_input.value))
+function passwordCheck(e) {
+    if(e.target.value == "") {
+        password_error.classList.add('view_error')
+        pw_input.classList.add('input_error')
+
+    } else {
+        pw_input.classList.remove('input_error')
+    }
+       pw_input.classList.remove('input_focus')
+
+    
+}
+function passWordInputFocus() {
+    password_error.classList.remove('view_error')
+    pw_input.classList.add('input_focus')
+    
+
+}
+pw_input.addEventListener('focus', passWordInputFocus)
+pw_input.addEventListener('blur', passwordCheck)
