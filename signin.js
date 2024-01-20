be1fcad874ba4b6503999662572a7e1781cc8d6d
suzinxix <suzinxix@gmail.com>
2024-01-20 23:56:30 +0900

    refactor(mentor): fix function of showErrorMsg to be readable

--- signin.js
+++ signin.js
@@ -10,15 +10,15 @@ const emailInput = document.querySelector(".input-email");
 const passwordInput = document.querySelector(".input-pw");
 
 const showErrorMsg = (e) => {
-  const message = e.target.value;
-  if (e.target.name === "email") {
-    message
-      ? emailValid(e.target.value)
-        ? (emailMsgBox.innerHTML = "")
-        : (emailMsgBox.innerHTML = "올바른 이메일 주소가 아닙니다.")
-      : (emailMsgBox.innerHTML = "이메일을 입력해 주세요.");
-  } else if (e.target.name === "password") {
-    message ? (passwordMsgBox.innerHTML = "") : (passwordMsgBox.innerHTML = "비밀번호를 확인해 주세요.");
+  const { value, name } = e.target;
+  let errorMsg = "";
+  if (name === "email") {
+    if (!value) errorMsg = "이메일을 입력해 주세요.";
+    else if (!emailValid(value)) errorMsg = "올바른 이메일 주소가 아닙니다.";
+    emailMsgBox.innerHTML = errorMsg;
+  } else if (name === "password") {
+    errorMsg = value ? "" : "비밀번호를 확인해 주세요.";
+    passwordMsgBox.innerHTML = errorMsg;
   }
   changeInputColor();
 };
