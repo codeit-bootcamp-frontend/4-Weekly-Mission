e8e13b5cc3bee843daa617b48a6b78b59bf02b29
Namju So <skawn1228@gmail.com>
2024-01-20 19:17:25 +0900

    Feat: 로그인 시 email 유효성 확인 추가

--- login/index.js
+++ login/index.js
@@ -1,7 +1,9 @@
 const inputEmail = document.querySelector('#login-email');
 const inputPassword = document.querySelector('#login-password');
+const loginButton = document.querySelector('#login-button')
 
 inputEmail.addEventListener('focusout', noInput);
+inputEmail.addEventListener('focusout', emailCheck);
 
 inputPassword.addEventListener('focusout', noInput);
 
@@ -17,6 +19,30 @@ function noInput(e) {
     }
     else if (e.target.classList.contains('login-password')) {
       e.target.nextElementSibling.textContent = '비밀번호를 입력해 주세요.';
+    }
+
+  }
+}
+
+
+// 이메일 유효성 확인
+function emailValidation(input) {
+  let reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+  if (!reg_email.test(input)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function emailCheck(e) {
+  let value = e.target.value;
+
+  if (value !== '') {
+
+    if (!emailValidation(e.target.value)) {
+      e.target.nextElementSibling.textContent = '올바른 이메일 주소가 아닙니다.';
     }
 
   }
