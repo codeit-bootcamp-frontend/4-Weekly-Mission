30727ac87ff6ee6fb022025d4a4f31d4273ee815
김보경 <rlaqhrud6263@naver.com>
2024-01-20 20:36:11 +0900

    signin-HTML save & re commit

--- signin/signin.js
+++ signin/signin.js
@@ -1,34 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
+const emailSignIn = document.querySelector('.signin__email');
+const emailInput = document.querySelector('#email');
+const passSignIn = document.querySelector('.signin__pass');
+const passInput = document.querySelector('#pass');
 
-  const emailSignIn = document.querySelector('.signin__email');
-  const emailInput = document.querySelector('#email');
-  const passSignIn = document.querySelector('.signin__pass');
-  const passInput = document.querySelector('#pass');
+// 유효성 검사
+function checkEmail (email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 
-  // 유효성 검사
-  function checkEmail (email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+// 에러 메세지 출력
+function showErrorMassage() {
+  const error = document.createElement('div');
+  error.classList.add('errorMessage');
+
+  if (emailInput.value.trim() === '') {
+    error.textContent = '이메일을 입력해 주세요.';
   }
-
-  // 에러 메세지 출력
-  function showErrorMassage() {
-    const error = document.createElement('div');
-    error.classList.add('errorMessage');
-
-    if (emailInput.value.trim() === '') {
-      error.textContent = '이메일을 입력해 주세요.';
-    }
-    else if (!checkEmail(emailInput.value)) {
-      error.textContent = '올바른 이메일 주소가 아닙니다.';
-    }
-    else {
-      error.textContent = '';
-    }
-
-    emailSignIn.appendChild(error);
+  else if (!checkEmail(emailInput.value)) {
+    error.textContent = '올바른 이메일 주소가 아닙니다.';
   }
+  else {
+    error.textContent = '';
+  }
+  emailSignIn.appendChild(error);
+}
 
+// 로그인시 folder 페이지로 이동
 
+emailInput.addEventListener('focusout', showErrorMassage);
 
-});
