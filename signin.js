79fdb051a03b410696e40851595e8a12cc18de9e
yeona813 <aya813@naver.com>
2024-01-20 19:01:29 +0900

    feat : 로그인 실행 구현 및 이메일, 비밀번호 에러 메시지 구현

--- signin.js
+++ signin.js
@@ -1,5 +1,6 @@
 const emailInput = document.getElementById("emailInput");
 const passwordInput = document.getElementById("passwordInput");
+const loginButton = document.getElementById("loginButton");
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 function createMessage(message) {
@@ -43,4 +44,56 @@ emailInput.addEventListener("blur", function () {
 
 passwordInput.addEventListener("blur", function () {
   handleBlur(passwordInput, createMessage("비밀번호를 입력해 주세요."));
+});
+
+function signIn(event) {
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  // 이메일, 비밀번호 에러 메시지 초기화
+  const emailContainer = emailInput.parentElement;
+  const passwordContainer = passwordInput.parentElement;
+
+  const existingEmailMessage = emailContainer.querySelector(".empty-message");
+  const existingPasswordMessage =
+    passwordContainer.querySelector(".empty-message");
+
+  if (existingEmailMessage) {
+    existingEmailMessage.remove();
+  }
+
+  if (existingPasswordMessage) {
+    existingPasswordMessage.remove();
+  }
+
+  if (email === "test@codeit.com" && password === "codeit101") {
+    // 로그인 성공
+    console.log("한원준 사랑해");
+    window.location.href = "/folder.html";
+  } else {
+    // 로그인 실패 - 에러 메시지 표시
+    if (email !== "test@codeit.com") {
+      const emailMessage = createMessage("이메일을 확인해 주세요.");
+      const parentElement = document.querySelector(".sign-input-box");
+      parentElement.appendChild(emailMessage);
+    }
+
+    if (password !== "codeit101") {
+      const passwordMessage = createMessage("비밀번호를 확인해 주세요.");
+      const parentElement = document.querySelector(".sign-password");
+      parentElement.appendChild(passwordMessage);
+    }
+  }
+}
+
+// 로그인 버튼 클릭 시 signIn 함수 호출
+loginButton.addEventListener("click", (event) => {
+  signIn(event);
+});
+
+// Enter 키 눌렀을 때 signIn 함수 호출
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    signIn(event);
+  }
 });
