d9676fae3cb8521ac4fbab986e0454615ba9452a
JA_X <envijs93@gmail.com>
2024-01-20 11:52:37 +0900

    feat: 이메일 인풋 이벤트 핸들러 추가

--- js/sign.js
+++ js/sign.js
@@ -1,3 +1,8 @@
+import validateEmailInput from './validate.js';
+
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+
 const addErrorMessage = (parentElement, errorMessage) => {
   const errorTag = document.createElement('p');
   errorTag.innerText = errorMessage;
@@ -10,3 +15,26 @@ const removeErrorMessage = parentElement => {
     errorTag.remove();
   }
 };
+
+emailInput.addEventListener('blur', e => {
+  const signInputBox = e.target.parentElement;
+  const emailValue = e.target.value;
+  const existingError = signInputBox.querySelector('p');
+
+  if (emailValue) {
+    if (validateEmailInput(emailValue)) {
+      signInputBox.classList.remove('error');
+      removeErrorMessage(signInputBox);
+    } else if (existingError) {
+      existingError.innerText = '올바른 이메일을 입력해 주세요.';
+    } else {
+      signInputBox.classList.add('error');
+      addErrorMessage(signInputBox, '올바른 이메일을 입력해 주세요.');
+    }
+  } else if (existingError) {
+    existingError.innerText = '이메일을 입력해 주세요.';
+  } else {
+    signInputBox.classList.add('error');
+    addErrorMessage(signInputBox, '이메일을 입력해 주세요.');
+  }
+});
