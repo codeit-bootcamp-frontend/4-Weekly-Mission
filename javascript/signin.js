d747a9746a9dae1934d38dd04442d4c1719eeb0b
HarryK <yjkang1223@naver.com>
2024-01-21 00:53:30 +0900

    feature: Print error message when email is not valid

--- javascript/signin.js
+++ javascript/signin.js
@@ -9,23 +9,46 @@ document.addEventListener('DOMContentLoaded', function () {
     const email = emailInput.value.trim();
     clearErrorStyle(emailInput);
 
-    if (errorText.parentNode === )
-  }
-
-  function passwordCheck () {
-    const password = passwordInput.value.trim();
-    clearErrorStyle(passwordInput);
-  }
-
-  emailInput.addEventListener('focusout', emailCheck());
-  form.addEventListener('submit', function (event) {
-    const isEmailValid = emailCheck();
-    const isPasswordValid = passwordCheck();
-
-    if (isEmailValid && isPasswordValid) {
-
+    // 에러 메세지 삭제
+    if (errorText.parentNode === emailInput.parentNode) {
+      emailInput.parentNode.removeChild(errorText);
     }
-  });
+    // 이메일 미입력 에러
+    if (email == '') {
+      showError(errorText, '이메일을 입력해 주세요.', emailInput);
+      return false;
+    }
+    // 이메일 형식 확인
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      displayErrorMessage(errorTextEmail, '올바른 이메일 주소가 아닙니다.', emailInput);
+      return false;
+    }
+    return true;
+  }
 
- 
+  // 에러 스타일 빼기
+  function clearErrorStyle(inputElement) {
+    const parentContainer = inputElement.parentNode;
+    const errorElement = parentContainer.querySelector('.error-message');
+
+    parentContainer.classList.remove('error');
+    if (errorElement == true) {
+      parentContainer.removeChild(errorText);
+    }
+  }
+
+  // 에러 메세지 띄우기
+  function showError(errorText, message, inputElement) {
+    errorText.textContent = message;
+    errorText.classList.add('error-message');
+    inputElement.parentNode.appendChild(errorText);
+
+    inputElement.parentNode.classList.add('error');
+  }
+
+  //이벤트 리스너 부여
+  emailInput.addEventListener('focusout', function () {
+    emailCheck();
+  });
 });
