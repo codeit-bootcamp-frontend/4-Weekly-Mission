ffa14caa5bcd2a86d98b846e35276cb578457bbb
박지윤 <tkrhkfld123@naver.com>
2024-01-20 20:31:52 +0900

    ♻️ refactor: 상수 대문자 변경 및 addClassList, removeClassList함수 생성

--- sign.js
+++ sign.js
@@ -2,22 +2,25 @@
 const get = (selector) => document.querySelector(selector);
 
 // 아이디, 비밀번호 input 요소들
-const signInputValue = get('.sign-input');
-const signEmailInputValue = get('#email-input');
-const signPasswordInputValue = get('#password-input');
+const SIGN_EMAIL_INPUT = get('#email-input');
+const SIGN_PASSWORD_INPUT = get('#password-input');
 
 // 에러 메시지 요소들
-const emptyEmailErrorMessage = get('.empty-email-error'); //이메일을 입력해 주세요
-const invalidEmailFormatMessage = get('.correct-email-error');//올바른 이메일 주소가 아닙니다
-const emptyPasswordErrorMessage = get('.empty-password-error');//비밀번호를 입력해 주세요
-const emailValidationMessage = get('.email-validation-message');//이메일을 확인해 주세요
-const passwordValidationMessage = get('.password-validation-message');//비밀번호를 확인해 주세요
+const EMPTY_EMAIL_ERROR_MESSAGE = get('.empty-email-error'); //이메일을 입력해 주세요
+const INVALID_EMAIL_FORMAT_MESSAGE = get('.correct-email-error');//올바른 이메일 주소가 아닙니다
+const EMPTY_PASSWORD_ERROR_MESSAGE = get('.empty-password-error');//비밀번호를 입력해 주세요
+const EMAIL_VALIDATION_MESSAGE = get('.email-validation-message');//이메일을 확인해 주세요
+const PASSWORD_VALIDATION_MESSAGE = get('.password-validation-message');//비밀번호를 확인해 주세요
 
 // 눈 아이콘 버튼
-const eyeButton = get('.eye-button-icon');
+const EYE_BUTTON = get('.eye-button-icon');
+
+//클래스 이름 상수화
+const SHOW_ERROR_CLASS_NAME = 'show-error';
+const RED_BORDER_CLASS_NAME = 'red-border';
 
 function isEmailRegexError(){
-  const emailValue = signEmailInputValue.value.trim();
+  const emailValue = SIGN_EMAIL_INPUT.value.trim();
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(emailValue)) {
     return true;
@@ -25,69 +28,81 @@ function isEmailRegexError(){
   return false; 
 }
 function isEmailFormatError(){
-  const emailValue = signEmailInputValue.value.trim();
+  const emailValue = SIGN_EMAIL_INPUT.value.trim();
   if (emailValue === '') {
     return true;
   }
   return false;
 }
 function isPasswordFormatError(){
-  const passwordValue = signPasswordInputValue.value.trim();
+  const passwordValue = SIGN_PASSWORD_INPUT.value.trim();
   if(passwordValue === ''){
     return true
   }
   return false
 }
 
-// TODO: addClassList 함수 분리하기 
-// TODO: removeClassList 함수 분리하기
+// target에 className에 해당하는 class를 추가한다.
+function addClassList(target, className){
+  target.classList.add(className)
+}
 
+// target에 className에 해당하는 class를 제거한다.
+function removeClassList(target, className){
+  target.classList.remove(className)
+}
 
-signEmailInputValue.addEventListener('focusout', function() {
-  // TODO: initEmailErrorStatus 함수분리하기
-  emptyEmailErrorMessage.classList.remove('show-error');
-  signEmailInputValue.classList.remove('redBorder');
-  invalidEmailFormatMessage.classList.remove('show-error');
-  emailValidationMessage.classList.remove('show-error');
+function initEmailErrorStatus(){
+  removeClassList(EMPTY_EMAIL_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
+  removeClassList(SIGN_EMAIL_INPUT, 'RED_BORDER_CLASS_NAME')
+  removeClassList(INVALID_EMAIL_FORMAT_MESSAGE, SHOW_ERROR_CLASS_NAME)
+  removeClassList(EMAIL_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
+}
+
+function initPasswordErrorStatus(){
+  removeClassList(EMPTY_PASSWORD_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
+  removeClassList(SIGN_PASSWORD_INPUT, 'RED_BORDER_CLASS_NAME')
+  removeClassList(PASSWORD_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
+}
+
+SIGN_EMAIL_INPUT.addEventListener('focusout', function() {
+  initEmailErrorStatus()
   if(isEmailFormatError()) {
-    emptyEmailErrorMessage.classList.add('show-error');
-    signEmailInputValue.classList.add('redBorder')
-    invalidEmailFormatMessage.classList.remove('show-error');
-    emailValidationMessage.classList.remove('show-error')
+    addClassList(EMPTY_EMAIL_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(SIGN_EMAIL_INPUT, 'RED_BORDER_CLASS_NAME')
+    removeClassList(INVALID_EMAIL_FORMAT_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    removeClassList(EMAIL_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
   } else if(isEmailRegexError()) {
-    invalidEmailFormatMessage.classList.add('show-error');
-    signEmailInputValue.classList.add('redBorder')
-    emptyEmailErrorMessage.classList.remove('show-error');
+    addClassList(INVALID_EMAIL_FORMAT_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(SIGN_EMAIL_INPUT, 'RED_BORDER_CLASS_NAME')
+    removeClassList(EMPTY_EMAIL_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
   }
 });
 
-signPasswordInputValue.addEventListener('focusout', function() {
-  // TODO: initPasswordErrorStatus 함수분리하기
-  emptyPasswordErrorMessage.classList.remove('show-error')
-  signPasswordInputValue.classList.remove('redBorder')
-  passwordValidationMessage.classList.remove('show-error')
+SIGN_PASSWORD_INPUT.addEventListener('focusout', function() {
+  initPasswordErrorStatus()
   if(isPasswordFormatError()) {
-    emptyPasswordErrorMessage.classList.add('show-error')
-    signPasswordInputValue.classList.add('redBorder')
-    passwordValidationMessage.classList.remove('show-error')
+    addClassList(EMPTY_PASSWORD_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(SIGN_PASSWORD_INPUT, 'RED_BORDER_CLASS_NAME')
+    removeClassList(PASSWORD_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
   }
 });
 
 function submitLogin(){
-  const emailValue = signEmailInputValue.value.trim();
-  const passwordValue = signPasswordInputValue.value.trim();
+  const emailValue = SIGN_EMAIL_INPUT.value.trim();
+  const passwordValue = SIGN_PASSWORD_INPUT.value.trim();
   let correctEmail = 'test@codeit.com'
   let correctPassword = 'codeit101'
   if(emailValue === correctEmail && passwordValue === correctPassword){
     location.href = 'folder.html';
   }else{
-    emailValidationMessage.classList.add('show-error')
-    signEmailInputValue.classList.add('redBorder')
-    invalidEmailFormatMessage.classList.remove('show-error')
-    emptyEmailErrorMessage.classList.remove('show-error')
-    passwordValidationMessage.classList.add('show-error')
-    signPasswordInputValue.classList.add('redBorder')
-    emptyPasswordErrorMessage.remove('show-error')
+    addClassList(EMAIL_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(SIGN_EMAIL_INPUT, 'RED_BORDER_CLASS_NAME')
+    removeClassList(INVALID_EMAIL_FORMAT_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    removeClassList(EMPTY_EMAIL_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(PASSWORD_VALIDATION_MESSAGE, SHOW_ERROR_CLASS_NAME)
+    addClassList(SIGN_PASSWORD_INPUT, 'RED_BORDER_CLASS_NAME')
+    removeClassList(EMPTY_PASSWORD_ERROR_MESSAGE, SHOW_ERROR_CLASS_NAME)
   }
 }
 document.onkeyup = function (event) {
@@ -97,7 +112,7 @@ document.onkeyup = function (event) {
 }
 
 function togglePasswordVisibility() {
-  const passwordFieldType = signPasswordInputValue.type;
-  signPasswordInputValue.type = passwordFieldType === 'password' ? 'text' : 'password';
-  eyeButton.src = `./images/eye-${passwordFieldType === 'password' ? 'on' : 'off'}.svg`;
+  const passwordFieldType = SIGN_PASSWORD_INPUT.type;
+  SIGN_PASSWORD_INPUT.type = passwordFieldType === 'password' ? 'text' : 'password';
+  EYE_BUTTON.src = `./images/eye-${passwordFieldType === 'password' ? 'on' : 'off'}.svg`;
 };
