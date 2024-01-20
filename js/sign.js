d4f07f39ed63e9bdf3c08902bfc599bff5079993
HaeJungg <gpwnd0120@gmail.com>
2024-01-20 21:36:21 +0900

    ✨feat : 엔터, 버튼 눌렀을때 로그인. 이후 folder.html로 이동

--- js/sign.js
+++ js/sign.js
@@ -1,5 +1,6 @@
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
+const loginButton = document.querySelector('.submit');
 
 //이메일 형식 검증 function
 function validationEmail(email) {
@@ -21,10 +22,8 @@ function validateEmail() {
 
   if (emailInput.value === "") {
     createErrorMessage("이메일을 입력해 주세요.", emailInput);
-  } else if (!validateEmail(emailInput.value)) {
+  } else if (!validationEmail(emailInput.value)) {
     createErrorMessage("올바른 이메일 주소가 아닙니다.", emailInput);
-  } else {
-    loginValidation();
   }
 }
 
@@ -39,24 +38,49 @@ function validatePassword() {
 
   if (passwordInput.value === "") {
     createErrorMessage("비밀번호를 입력해 주세요.", passwordInput);
-  } else {
-    loginValidation();
   }
 }
 
-function loginValidation() {
-  const validEmail = "test@codeit.com";
-  const validPassword = "codeit101";
+// 로그인 버튼 클릭 시 로그인 실행
+loginButton.addEventListener("click", login);
 
-  if (emailInput.value === validEmail && passwordInput.value === validPassword) {
-    // 로그인 페이지 이동
-    window.location.href = '/folder.html';
+// 로그인 함수
+function login() {
+  const enteredEmail = emailInput.value;
+  const enteredPassword = passwordInput.value;
+  
+  function newPage() {
+    let link = '/folder.html'
+    location.href = link;
+  }
+
+  // 기존 에러 메시지 삭제
+  const emailError = emailInput.parentElement.querySelector(".error-message");
+  if (emailError) {
+    emailError.remove();
+  }
+
+  const passwordError = passwordInput.parentElement.querySelector(".error-message");
+  if (passwordError) {
+    passwordError.remove();
+  }
+
+  if (enteredEmail === 'test@codeit.com' && enteredPassword === 'codeit101') {
+    newPage();
   } else {
-    // 로그인 실패 시 에러 메시지 표시
-    createErrorMessage("이메일을 확인해 주세요", emailInput);
+    createErrorMessage("이메일을 확인해 주세요.", emailInput);
     createErrorMessage("비밀번호를 확인해 주세요.", passwordInput);
   }
 }
+
+//엔터키로 로그인 실행
+document.addEventListener('keydown', function(e) {
+  let pressedKey = e.key
+  if (pressedKey === 'Enter') {
+    e.preventDefault();
+    login();
+  }
+});
 
 // 공통된 에러 메시지 생성 함수
 function createErrorMessage(message, inputElement) {
