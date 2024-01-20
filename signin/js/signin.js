c4e04ef9089774dc83db12a348cf0d4c864518b6
김재성 <bumpy@gimjaeseong-ui-MacBookAir.local>
2024-01-20 15:06:11 +0900

    feat: 4주차 기본&심화 요구사항 적용

--- signin/js/signin.js
+++ signin/js/signin.js
@@ -1,18 +1,40 @@
-let pwdView = document.querySelector('.pwd-view');
+const PWD_VIEW = document.querySelector('.pwd-view');
+const INPUT_EMAIL = document.querySelector('#email');
+const INPUT_PASSWORD = document.querySelector('#password');
+const FORM = document.querySelector('#login-form-box');
 
-pwdView.addEventListener('click', function () {
-    let target = this.dataset.target;
-    let targetInput = document.querySelector(`#${target}`);
-    let eye = this.querySelector('img');
-    if (eye.dataset.onoff == 'off') {
-        eye.dataset.onoff = 'on';
-        eye.setAttribute('src', '/images/eye-on.png');
+PWD_VIEW.addEventListener('click', function () {
+    pwdSwitch(this);
+});
+INPUT_EMAIL.addEventListener('focusin', function () {
+    errorBoxDisable(this);
+});
+INPUT_EMAIL.addEventListener('focusout', function () {
+    if (this.value == '') {
+        errorBoxEnable(this, '이메일을 입력해 주세요.');
+    } else if (!emailValidChk(this.value)) {
+        errorBoxEnable(this, '올바른 이메일 주소가 아닙니다.');
+    }
+});
 
-        targetInput.setAttribute('type', 'text');
+INPUT_PASSWORD.addEventListener('focusin', function () {
+    errorBoxDisable(this);
+});
+INPUT_PASSWORD.addEventListener('focusout', function () {
+    if (this.value == '') {
+        errorBoxEnable(this, '비밀번호를 입력해 주세요.');
+    }
+});
+
+FORM.addEventListener('submit', function (e) {
+    let email = this.querySelector('#email');
+    let password = this.querySelector('#password');
+
+    if (email.value === 'test@codeit.com' && password.value === 'codeit101') {
+        this.submit();
     } else {
-        eye.dataset.onoff = 'off';
-        eye.setAttribute('src', '/images/eye-off.png');
-
-        targetInput.setAttribute('type', 'password');
+        e.preventDefault();
+        errorBoxEnable(email, '이메일을 확인해 주세요.');
+        errorBoxEnable(password, '비밀번호를 확인해 주세요.');
     }
 });
--- signup/js/signup.js
+++ signup/js/signup.js
@@ -1,40 +1,26 @@
-let pwdViews = document.querySelectorAll('.pwd-view');
-let pwdCheck = document.querySelector('.pwd-confirm');
+const PWD_VIEWS = document.querySelectorAll('.pwd-view');
+const PWD_CHECK = document.querySelector('.pwd-confirm');
 
-pwdViews.forEach((target) => {
+PWD_VIEWS.forEach((target) => {
     target.addEventListener('click', () => {
         pwdSwitch(target);
     });
 });
-
-pwdCheck.addEventListener('keyup', function () {
+/*
+PWD_CHECK.addEventListener('keyup', function () {
     let pwd = document.querySelector('#password');
-    let text = document.querySelector('.different-text');
+    let text = document.querySelector('.error-text');
 
     if (pwd.value != this.value) {
-        if (!this.classList.contains('different')) {
-            this.classList.add('different');
+        if (!this.classList.contains('error-box')) {
+            this.classList.add('error-box');
+            text.innerHTML = '비밀번호가 다릅니다.';
             text.style.display = 'block';
         }
     } else {
-        this.classList.remove('different');
+        this.classList.remove('error-box');
+        text.innerHTML = '';
         text.style.display = 'none';
     }
 });
-
-function pwdSwitch(arg) {
-    let target = arg.dataset.target;
-    let targetInput = document.querySelector(`#${target}`);
-    let eye = arg.querySelector('img');
-    if (eye.dataset.onoff == 'off') {
-        eye.dataset.onoff = 'on';
-        eye.setAttribute('src', '/images/eye-on.png');
-
-        targetInput.setAttribute('type', 'text');
-    } else {
-        eye.dataset.onoff = 'off';
-        eye.setAttribute('src', '/images/eye-off.png');
-
-        targetInput.setAttribute('type', 'password');
-    }
-}
+*/
