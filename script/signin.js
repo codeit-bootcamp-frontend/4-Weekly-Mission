87628e27fc9c9081944ad4bfae5d779701030cf1
Taeuk471 <asky990223@gmail.com>
2024-01-20 22:28:38 +0900

    :construction: Show error when login access denied

--- script/signin.js
+++ script/signin.js
@@ -17,3 +17,34 @@ emailInput.addEventListener('focusout', function(){
     emailInput.classList.remove('error-box');
   }
 });
+
+/*이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 
+"올바른 이메일 주소가 아닙니다" 라고 에러 메세지를 보냅니다. */
+
+const emailFormatErrorMessage = document.createElement('div');
+emailFormatErrorMessage.className = 'error-message';
+emailFormatErrorMessage.classList.add('text-medium');
+emailFormatErrorMessage.innerText = '올바른 이메일 주소가 아닙니다.';
+
+emailInput.addEventListener('focusout', function () {
+    const emailValue = emailInput.value.trim(); 
+    if (emailValue && !isValidEmail(emailValue)) { //빈문자열이 아니면서 이메일 형식과 맞지 않을 때 
+        emailFormatErrorMessage.style.display = 'block'; //화면에 보여줌
+        emailInput.parentElement.lastElementChild.after(emailFormatErrorMessage);
+    } else {
+        emailFormatErrorMessage.style.display = 'none';
+    }
+});
+
+function isValidEmail(email) { //isValidEmail function을 제작하면됨. 음 .com. .kr등의 pop했을 때 양식을 만들어준다? 정도로 가자 질문!!!
+    let spotcount = atsigncount = 0;
+
+    for(let word of email) {
+        if(word === '.') spotcount++;
+        else if (word ==='@') atsigncount++;
+    }
+    
+    if(spotcount === 1 && atsigncount ===1) return true;
+    else return false;
+}
+
