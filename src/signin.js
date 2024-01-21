566559e25bcce2adc0fbda48271d48354e5c29b2
thgus5335 <thgus5335@naver.com>
2024-01-22 04:53:40 +0900

    :sparkles: Add: change eye icon event
    Add change eye icon event (toggle)

--- src/signin.js
+++ src/signin.js
@@ -3,6 +3,7 @@ import { ERROR_MSG, REGEX_EMAIL, USER_INFO } from './constant.js';
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const signinForm = document.querySelector('.sign-signin-form');
+const eyeIcon = document.querySelector('.eye');
 
 //Check email: 입력 여부 확인, 메일 형식 확인-------------------------------
 const errorEmail = document.createElement('div');
@@ -48,6 +49,21 @@ function submitForm(e) {
   }
 }
 
+//Change icon: eye-on/off----------------------------------------------
+const eyeOnIcon = '/assets/icon/eye-on.svg';
+const eyeOffIcon = '/assets/icon/eye-off.svg';
+
+function changeEyeIcon() {
+  eyeIcon.classList.toggle('on');
+  if (eyeIcon.classList.contains('on')) {
+    password.removeAttribute('type');
+    changeImage(eyeIcon, eyeOnIcon);
+  } else {
+    password.setAttribute('type', 'password');
+    changeImage(eyeIcon, eyeOffIcon);
+  }
+}
+
 //Util----------------------------------------------------------------
 function showInputError(id, element, message) {
   element.textContent = message;
@@ -55,6 +71,11 @@ function showInputError(id, element, message) {
   id.after(element);
 }
 
+function changeImage(brfore, after) {
+  brfore.setAttribute('src', after);
+}
+
 email.addEventListener('focusout', checkEmail);
 password.addEventListener('focusout', checkPassword);
 signinForm.addEventListener('submit', submitForm);
+eyeIcon.addEventListener('click', changeEyeIcon);
