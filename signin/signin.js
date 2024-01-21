2ddcff75ce459dfbc5541e64d89ae1d83298c081
kong33 <kge021114@naver.com>
2024-01-21 11:32:45 +0900

    유효성 검사

--- signin/signin.js
+++ signin/signin.js
@@ -42,4 +42,50 @@ document
     errorMessage.style.display = "none";
   });
 
-const loginForm = document.getElementById();
+//유효성 검사
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("동작");
+  const loginForm = document.getElementsByClassName("sign-form")[0];
+  const patternMap = {
+    emailValidation: (value) =>
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value),
+    rightEmail: (value) => value === "test@codeit.com",
+    rightPassword: (value) => value === "codeit101",
+  };
+
+  loginForm.addEventListener("submit", function (e) {
+    console.log("제출");
+    const emailInput = document.getElementById("email-input").value;
+    const passwordInput = document.getElementById("passWord-input").value;
+    console.log(emailInput);
+    console.log(passwordInput);
+    let isValid = true;
+
+    if (!patternMap.emailValidation(emailInput)) {
+      console.log("첫번째");
+      document.getElementById("errorMessage-email").textContent =
+        "유효하지 않은 이메일 주소입니다.";
+      document.getElementById("errorMessage-email").style.display = "block";
+      isValid = false;
+    } else if (!patternMap.rightEmail(emailInput)) {
+      document.getElementById("errorMessage-email").textContent =
+        "이메일을 확인해주세요.";
+      document.getElementById("errorMessage-email").style.display = "block";
+      isValid = false;
+    }
+
+    if (!patternMap.rightPassword(passwordInput)) {
+      document.getElementById("errorMessage-password").textContent =
+        "비밀번호를 확인해주세요.";
+      document.getElementById("errorMessage-password").style.display = "block";
+      isValid = false;
+    }
+
+    if (!isValid) {
+      e.preventDefault(e);
+    } else {
+      document.getElementById("errorMessage-email").style.display = "none";
+      document.getElementById("errorMessage-password").style.display = "none";
+    }
+  });
+});
