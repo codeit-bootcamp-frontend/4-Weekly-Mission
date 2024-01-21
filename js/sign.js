9d51a876b5013a7902b916dc4298c5b7aea6a982
LeeSeungHoon <ysh73900@gmail.com>
2024-01-21 20:20:41 +0900

    feat: 특정 로그인 시 페이지 이동 함수

--- js/sign.js
+++ js/sign.js
@@ -2,9 +2,10 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const emailError = document.querySelector(".email-error");
 const passwordError = document.querySelector(".password-error");
+const loginButton = document.querySelector(".login-button");
 
 /* 이메일 유효성 검사 */
-function isValidEmail() {
+const isValidEmail = () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (email.value === "") {
     emailError.innerHTML = "이메일을 입력해 주세요.";
@@ -13,14 +14,28 @@ function isValidEmail() {
   } else {
     emailError.innerHTML = "";
   }
-}
+};
 
 /* 비밀번호 유효성 검사 */
-function isValidPassword() {
+const isValidPassword = () => {
   if (password.value === "") {
     passwordError.innerHTML = "비밀번호를 입력해주세요.";
+  } else {
+    passwordError.innerHTML = "";
   }
-}
+};
+
+/* 특정 아이디 및 비밀번호로 로그인 시도 시 페이지 이동 */
+const redirectToFolderPage = () => {
+  if (email.value === "test@codeit.com" && password.value === "codeit101") {
+    const link = "./folder.html";
+    location.href = link;
+  } else {
+    emailError.innerHTML = "이메일을 확인해 주세요.";
+    passwordError.innerHTML = "비밀번호를 확인해 주세요.";
+  }
+};
 
 email.addEventListener("blur", isValidEmail);
 password.addEventListener("blur", isValidPassword);
+loginButton.addEventListener("click", redirectToFolderPage);
