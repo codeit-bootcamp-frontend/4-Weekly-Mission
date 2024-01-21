07a57ee0c769dd144ac5cb52a1a0622cbae3d6c3
Dodam0719 <blueoh719@gmail.com>
2024-01-21 22:46:20 +0900

    Feat: Add password error message

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
 
   emailInput.addEventListener("blur", validateEmail);
+  passwordInput.addEventListener("blur", validatePassword);
 
   function validateEmail() {
     const emailValue = emailInput.value;
@@ -18,6 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     document.querySelector("emailError").textContent = "";
     emailInput.classList.remove("invalid");
+    return true;
+  }
+
+  function validatePassword() {
+    const passwordValue = passwordInput.value;
+    if (!passwordValue) {
+      document.querySelector("#passwordError").textContent =
+        "비밀번호를 입력해 주세요.";
+      return false;
+    }
+    document.querySelector("passwordError").textContent = "";
     return true;
   }
 
