d5e1a3e9e772c8cc0d2c11529b25eeff055164b2
codingaring <sksms2104@naver.com>
2024-01-21 10:28:11 +0900

    [Feat] tryLogin 기능 구현 완료, 엔터키로 tryLogin 함수가 실행될 수 있게 함

--- js/signin.js
+++ js/signin.js
@@ -32,7 +32,7 @@ function checkPassword(event) {
   // input에 입력된 값, errorMessage가 출력될 태그, input 태그 선택
   const passwordValue = event.target.value;
   const errorMessageBox = document.querySelector("#pw-error-message");
-
+  // 공백일 경우
   if (passwordValue === "") {
     errorMessageBox.innerHTML = "비밀번호를 입력해주세요.";
     pwInputBox.classList.add("error-box");
@@ -49,9 +49,11 @@ function tryLogin() {
   const errorMessage = document.querySelector("#email-error-message");
   const errorMessageBox = document.querySelector("#pw-error-message");
 
+  // 입력값이 모두 맞는 경우
   if (emailValue === "test@codeit.com" && passwordValue === "codeit101") {
-    location.href = "./folder.html";
-  } else {
+    location.href = "folder.html";
+  } else if (emailValue != "test@codeit.com" || passwordValue != "codeit101") {
+    // 아이디가 틀리거나, 비밀번호가 틀리는 경우
     errorMessage.innerHTML = "이메일을 확인해주세요.";
     emailInputBox.classList.add("error-box");
     errorMessageBox.innerHTML = "비밀번호를 확인해주세요";
@@ -59,7 +61,21 @@ function tryLogin() {
   }
 }
 
-// 이벤트 등록
+// 엔터키로 로그인 시도
+function loginByEnter(event) {
+  // event.key 값이 "Enter"일 때만 tryLogin 함수 호출
+  if (event.key === "Enter") {
+    tryLogin();
+  }
+}
+
+/** 이벤트 등록 */
+// 이메일, 비밀번호 올바른지 확인
 emailInputBox.addEventListener("focusout", checkEmail);
 pwInputBox.addEventListener("focusout", checkPassword);
+
+// 로그인하거나, "확인해주세요" 띄우기
 loginBtn.addEventListener("click", tryLogin);
+
+// 엔터키로 로그인하기
+document.addEventListener("keydown", loginByEnter);
