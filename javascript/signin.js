889c6a837efd16d11dcfd431b29c0bde8b5ec662
HarryK <yjkang1223@naver.com>
2024-01-21 11:24:41 +0900

    feature: Add page movement when email & password is correct <br/> feature: Print error text when email & password is wrong

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,27 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.sign-box');
   const emailInput = document.querySelector('#signin-email');
-  const errorText = document.createElement('div');
+  const passwordInput = document.querySelector('#signin-password');
+  const errorTextEmail = document.createElement('div');
+  const errorTextPassword = document.createElement('div');
 
-  // 함수 정의
+  //////////////// 함수 정의 ////////////////////
+
   // 이메일 확인 함수
   function emailCheck() {
     const email = emailInput.value.trim();
     clearErrorStyle(emailInput);
 
-    // 에러 메세지 삭제
-    if (errorText.parentNode === emailInput.parentNode) {
-      emailInput.parentNode.removeChild(errorText);
+    // 기존 에러 메세지 삭제
+    if (errorTextEmail.parentNode === emailInput.parentNode) {
+      emailInput.parentNode.removeChild(errorTextEmail);
     }
     // 이메일 미입력 에러
     if (email == '') {
-      showError(errorText, '이메일을 입력해 주세요.', emailInput);
+      showError(errorTextEmail, '이메일을 입력해 주세요.', emailInput);
       return false;
     }
     // 이메일 형식 확인
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       displayErrorMessage(errorTextEmail, '올바른 이메일 주소가 아닙니다.', emailInput);
+      return false;
+    }
+    return true;
+  }
+
+  // 비번 확인 함수
+  function passwordCheck() {
+    const password = passwordInput.value.trim();
+    clearErrorStyle(passwordInput);
+    // 기존 에러 메세지 삭제
+    if (errorTextPassword.parentNode === passwordInput.parentNode) {
+      passwordInput.parentNode.removeChild(errorTextPassword);
+    }
+    // 비밀번호 미입력 에러
+    if (password == '') {
+      showError(errorTextPassword, '비밀번호를 입력해 주세요.', passwordInput);
       return false;
     }
     return true;
@@ -34,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     parentContainer.classList.remove('error');
     if (errorElement == true) {
-      parentContainer.removeChild(errorText);
+      parentContainer.removeChild(errorTextEmail);
     }
   }
 
@@ -43,12 +62,33 @@ document.addEventListener('DOMContentLoaded', function () {
     errorText.textContent = message;
     errorText.classList.add('error-message');
     inputElement.parentNode.appendChild(errorText);
-
     inputElement.parentNode.classList.add('error');
   }
 
-  //이벤트 리스너 부여
+  //////////////// 함수 사용////////////////////
+
+  // 이메일 이벤트 리스너 부여
   emailInput.addEventListener('focusout', function () {
     emailCheck();
+  });
+
+  // 비번 이벤트 리스너 부여
+  passwordInput.addEventListener('focusout', function () {
+    passwordCheck();
+  });
+
+  // 로그인 버튼 입력 시 이메일 비번 확인 후 이동
+  form.addEventListener('submit', function (event) {
+    const codeit = { email: 'test@codeit.com', password: 'codeit101' };
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (email === codeit.email && password === codeit.password) {
+      window.location.href = '/folder.html';
+    } else {
+      showError(errorTextEmail, '이메일을 확인해 주세요.', emailInput);
+      showError(errorTextPassword, '비밀번호를 확인해 주세요.', passwordInput);
+      event.preventDefault();
+    }
   });
 });
