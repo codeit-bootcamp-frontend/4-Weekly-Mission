a94f8362d99ed54991030eb468c554d18d09414e
jaebeen95 <nin2348@gmail.com>
2024-01-21 17:16:08 +0900

    4주차 기본요구사항 4번 완료 - 비밀번호 입력 안 했을 시 에러메시지

--- week4/src/sign.js
+++ week4/src/sign.js
@@ -3,7 +3,9 @@ const passwordInput = document.querySelector(".sign-input[type='password']");
 const loginButton = document.querySelector(".cta");
 const errorMessage = document.createElement("p");
 
+//
 // 이메일 관리
+//
 
 errorMessage.classList.add("error-message");
 
@@ -11,7 +13,7 @@ function isEmailValid(email) {
   return email.includes("@") && email.includes(".");
 }
 
-function focusoutError() {
+function emailError() {
   const emailValue = signInput.value.trim();
 
   if (emailValue === "") {
@@ -25,15 +27,39 @@ function focusoutError() {
   }
 }
 
-function focusinErrorRemove() {
+function emailErrorRemove() {
   signInput.classList.remove("input-border-red");
   errorMessage.remove();
 }
 
-signInput.addEventListener("focusout", focusoutError);
-signInput.addEventListener("focusin", focusinErrorRemove);
+signInput.addEventListener("focusout", emailError);
+signInput.addEventListener("focusin", emailErrorRemove);
 
-// 비밀번호 관리 & 로그인 관리
+//
+// 비밀번호 관리
+//
+
+function passwordError() {
+  const passwordValue = passwordInput.value.trim();
+
+  if (passwordValue === "") {
+    errorMessage.textContent = "비밀번호를 입력해주세요";
+    passwordInput.classList.add("input-border-red");
+    passwordInput.after(errorMessage);
+  }
+}
+
+function passwordErrorRemove() {
+  passwordInput.classList.remove("input-border-red");
+  errorMessage.remove();
+}
+
+passwordInput.addEventListener("focusout", passwordError);
+passwordInput.addEventListener("focusin", passwordErrorRemove);
+
+//
+// 로그인 관리
+//
 
 function isLoginValid(email, password) {
   return email === "test@codeit.com" && password === "codeit101";
