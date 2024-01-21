52b2737f192981ec2c453ad5a333b0ed12a6b455
yb3143 <yb3143@gmail.com>
2024-01-21 18:44:24 +0900

    js 수정

--- login/login.js
+++ login/login.js
@@ -19,3 +19,21 @@ function notSameEMAIL() {
 
 EMAIL.addEventListener("click", nullEMAIL);
 EMAIL.addEventListener("blur", notSameEMAIL);
+
+// const EMAIL = document.getElementById("email");
+// const emailError = document.getElementsByClassName("email_error");
+
+// const PASSWORD = document.getElementById("password");
+// const ERROREPASSWORD = document.createElement("div");
+
+// ERROREPASSWORD.textContent = "비밀번호를 입력해주세요.";
+
+// function nullEmail() {
+//   emailError.innerText = "이메일을 입력해주세요.";
+// }
+// function notSameEmail() {
+//   alert("올바른 이메일을 입력해주세요.");
+// }
+
+// EMAIL.addEventListener("focus", nullEmail);
+// EMAIL.addEventListener("blur", notSameEmail);
