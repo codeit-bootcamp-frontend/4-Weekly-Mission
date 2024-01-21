74921d5d1a8d459fdc9e49bd80ba3461665c94d9
naeun14 <84865501+naeun14@users.noreply.github.com>
2024-01-21 13:58:50 +0900

    week4 weekly mission

---
+++ inputform_change.js
@@ -0,0 +1,92 @@
+const loginEmailInput = document.querySelector('#login__email-form');
+const loginPwInput = document.querySelector('#login__password-form')
+const loginEmailError = document.querySelector('.login__email-form--error');
+const loginPwError = document.querySelector('.login__password-form--error');
+
+const loginButton = document.querySelector('.login__info--loginBt');
+const passwordIcon = document.querySelector('.password__toggle-icon');
+
+loginEmailInput.addEventListener('focusout', loginEmailFocusOut);
+loginPwInput.addEventListener('focusout', loginpwFocusOut);
+passwordIcon.addEventListener('click', passwordToggleIcon);
+loginButton.addEventListener('click', handleLoginButtonClick);
+
+//이메일 입력 확인
+function loginEmailFocusOut(event) {
+  emailHandleInputFocusOut(loginEmailInput,loginEmailError);
+}
+
+function loginpwFocusOut(event) {
+  passwordHandleFocusOut(loginPwInput,loginPwError);
+}
+
+//비밀번호 아이콘 및 타입 변경
+function passwordToggleIcon(event) {
+  togglePasswordType(loginPwInput);
+}
+
+//이메일 입력 확인
+function emailHandleInputFocusOut(inputElement, errorElement) {
+  // focusout 이벤트가 발생했을 때 실행되는 코드
+  if (!inputElement.value) {
+    errorElement.textContent = '이메일을 입력해 주세요.';
+    errorElement.style.display = 'block';
+    inputElement.style.borderColor = '#FF5B56'; // 테두리 색상 변경
+  } else if(!isValidEmail(inputElement.value)) {
+    errorElement.textContent = '올바른 이메일 주소가 아닙니다.';
+    errorElement.style.display = 'block';
+    inputElement.style.borderColor = '#FF5B56'; // 테두리 색상 변경  
+  } else {
+    errorElement.style.display = 'none';
+    errorElement.textContent = '';
+    inputElement.style.borderColor = '#CCD5E3';
+  }
+}
+
+//비밀번호 입력 확인
+function passwordHandleFocusOut(inputElement, errorElement) {
+  if (!inputElement.value) {
+    errorElement.textContent = '비밀번호를 입력해 주세요.';
+    errorElement.style.display = 'block';
+    inputElement.style.borderColor = '#FF5B56'; 
+  } else {
+    errorElement.style.display = 'none';
+    errorElement.textContent = '';
+    inputElement.style.borderColor = '#CCD5E3';
+  }
+}
+
+function isValidEmail(email) {
+  // 간단한 이메일 유효성 검사를 위한 정규 표현식 사용
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+//아이콘 변경
+function togglePasswordType(passwordInput) {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordIcon.classList.remove('fa-eye');
+    passwordIcon.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    passwordIcon.classList.remove('fa-eye-slash');
+    passwordIcon.classList.add('fa-eye');
+  }
+}
+
+//로그인 버튼 클릭 시
+function handleLoginButtonClick(event) {
+  if (loginEmailInput.value === 'test@codeit.com' && loginPwInput.value === 'codeit101') {
+    alert('로그인 성공! 페이지 이동: /folder');
+    window.location.href = "/folder";
+  }
+  else {
+    loginPwError.textContent = '비밀번호를 확인해 주세요.';
+    loginPwError.style.display = 'block';
+    loginPwInput.style.borderColor = '#FF5B56'; 
+    loginEmailError.textContent = '이메일을 확인해 주세요.';
+    loginEmailError.style.display = 'block';
+    loginEmailInput.style.borderColor = '#FF5B56'; 
+  }
+}
