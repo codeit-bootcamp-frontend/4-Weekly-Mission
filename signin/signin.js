485dbf1ed6bd006318407ba1e7d0dbfc852724db
kimgaeun <kimgaeun21@naver.com>
2024-01-21 22:47:00 +0900

    email check 기능 구현

---
+++ signin/signin.js
@@ -0,0 +1,20 @@
+﻿let regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+let text = ""
+
+function email_check(){
+    const email = document.querySelector("#email");
+    console.log(!regex.test(email.value))
+    if (!email.value){
+        document.querySelector('.email_result').innerText = "이메일을 입력해주세요"
+        email.classList.add('focus_red');
+    }
+    else if (!regex.test(email.value)){
+        document.querySelector('.email_result').innerText = "올바른 이메일 주소가 아닙니다."
+        email.classList.add('focus_red');
+    }
+    else{
+        document.querySelector('.email_result').innerText = ""
+        email.classList.remove('focus_red');
+    }
+    console.log(email.value)
+}
