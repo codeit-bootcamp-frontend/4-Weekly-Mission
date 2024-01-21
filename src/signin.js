52f8bdcd01754d817ab0c82fbe2702c27a3f46ea
thgus5335 <thgus5335@naver.com>
2024-01-22 03:52:41 +0900

    :art: Fix: format of the code
    Fix format of the code

--- src/signin.js
+++ src/signin.js
@@ -3,10 +3,10 @@ import { ERROR_MSG, REGEX_EMAIL } from './constant.js';
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+//Check email: 입력 여부 확인, 메일 형식 확인-------------------------------
 const errorEmail = document.createElement('div');
 errorEmail.className = 'error-text';
 
-//Check email: 입력 여부 확인, 메일 형식 확인-------------------------------
 function checkEmail() {
   if (!email.value) {
     showInputError(email, errorEmail, ERROR_MSG.INPUT_EMAIL);
@@ -23,25 +23,24 @@ function validateEmail(email_address) {
   else return true;
 }
 
-function showInputError(id, element, message) {
-  element.textContent = message;
-  id.classList.add('error-input');
-  id.after(element);
-}
-
 //Check password: 입력 여부 확인-----------------------------------------
 const errorPassword = document.createElement('div');
 errorPassword.className = 'error-text';
 
 function checkPassword() {
   if (!password.value) {
-    errorPassword.textContent = ERROR_MSG.INPUT_PASSWORD;
-    password.classList.add('error-input');
-    password.after(errorPassword);
+    showInputError(password, errorPassword, ERROR_MSG.INPUT_PASSWORD);
   } else {
     errorPassword.remove();
     password.classList.remove('error-input');
   }
+}
+
+//Util----------------------------------------------------------------
+function showInputError(id, element, message) {
+  element.textContent = message;
+  id.classList.add('error-input');
+  id.after(element);
 }
 
 email.addEventListener('focusout', checkEmail);
