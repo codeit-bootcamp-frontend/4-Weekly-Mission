a5b2642816ac818423166549fcca867a58772579
thgus5335 <thgus5335@naver.com>
2024-01-21 20:34:28 +0900

    :sparkles: Create: constant.js
    Create constant.js

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const ERROR_MSG = {
+  INPUT_EMAIL: '이메일을 입력해 주세요.',
+  INPUT_PASSWORD: '비밀번호를 입력해 주세요.',
+  INPUT_EMAIL_ADDRESS: '올바른 이메일 주소가 아닙니다.',
+
+  CHECK_EMAIL: '이메일을 확인해 주세요.',
+  CHECK_PASSWORD: '비밀번호를 확인해 주세요.',
+};
