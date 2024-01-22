b99485f62e4c4679eec8a5861933ecf6a9e4a62f
jae6269 <jae6269@gmail.com>
2024-01-23 01:29:25 +0900

    Refactor(signin.js) : 로그인 관련 함수 다른 파일로 분리 2024.01.23 signup.html 페이지에서도 몇몇 함수들을 재사용하기위해서 함수들은 signFuctions.js 로 전부 분리, signin과 signup 파일에서는 각각 요소 변수 선언과 이벤트 리스너 할당만 해줄 예정

---
+++ signFunctions.js
@@ -0,0 +1,75 @@
+//이메일 유효성 검사 메서드
+export function emailFormatCheck(email) {
+  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  return email_regex.test(email);
+}
+
+/*파랑(focus), 빨강(error), 그레이(default) 색으로 border변경,
+  각 상태에 따라서 error msg block,none 결정 매서드 */
+export function inputBorderBlue(element) {
+  element.style.outline = 'none';
+  element.style.border = '1px solid #6d6afe';
+}
+export function inputBorderRed(inputElement, errorElement) {
+  inputElement.style.border = '1px solid var(--Linkbrary-red,#FF5B56)';
+  errorElement.style.display = 'block';
+}
+export function inputBorderGray(inputElement, errorElement) {
+  inputElement.style.border = '1px solid var(--Linkbrary-gray20, #CCD5E3)';
+  errorElement.style.display = 'none';
+}
+
+//로그인 기능 매서드
+export function signIn(emailInput, pwInput, emailError, pwError, pwOnOffImg) {
+  if (emailInput.value == 'test@codeit.com' && pwInput.value == 'codeit101') {
+    window.location.href = './folder.html';
+  } else {
+    emailError.innerHTML = '이메일을 확인해 주세요.';
+    pwError.innerHTML = '비밀번호를 확인해 주세요.';
+    inputBorderRed(emailInput, emailError);
+    pwOnOffImg.style.bottom = '2.9375rem';
+    inputBorderRed(pwInput, pwError);
+  }
+}
+
+// 이메일 에러 검사 메서드
+// 작성 되었는지, 유효한 이메일인지 검사
+export function emailErrorCheck(inputElement, errorElement) {
+  const emailValue = inputElement.value;
+  if (!emailValue) {
+    errorElement.innerHTML = '이메일을 입력해 주세요.';
+    inputBorderRed(inputElement, errorElement);
+  } else if (emailFormatCheck(emailValue) === false) {
+    errorElement.innerHTML = '올바른 이메일 주소가 아닙니다.';
+    inputBorderRed(inputElement, errorElement);
+  } else {
+    inputBorderGray(inputElement, errorElement);
+  }
+}
+
+//로그인시 비밀번호 에러 확인 메서드
+export function signinPasswordErrorCheck(pwInput, pwError, pwOnOffImg) {
+  const pwValue = pwInput.value;
+
+  if (!pwValue) {
+    pwError.innerHTML = '비밀번호를 입력해 주세요.';
+    pwOnOffImg.style.bottom = '2.9375rem';
+    inputBorderRed(pwInput, pwError);
+  } else {
+    pwOnOffImg.style.bottom = '1.1875rem';
+    inputBorderGray(pwInput, pwError);
+  }
+}
+
+//비밀번호 보여주기 스위치 메서드
+export function passwordVisibleSwitch(isPasswordVisible, pwOnOffImg, pwInput) {
+  if (!isPasswordVisible[0]) {
+    pwOnOffImg.src = './icons/eyeson.png';
+    pwInput.type = 'none';
+    isPasswordVisible[0] = true;
+  } else {
+    pwOnOffImg.src = './icons/eyesoff.png';
+    pwInput.type = 'password';
+    isPasswordVisible[0] = false;
+  }
+}
