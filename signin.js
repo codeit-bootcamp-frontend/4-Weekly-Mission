26bfc7765be069867ab59be1e05dafad83e2dae9
suzinxix <suzinxix@gmail.com>
2024-01-23 19:40:37 +0900

    feat: implement error message when email is already in use

--- signin.js
+++ signin.js
@@ -6,19 +6,33 @@ const btn = document.querySelector(".btn-submit");
 
 const emailMsgBox = document.querySelector(".error-email");
 const passwordMsgBox = document.querySelector(".error-pw");
+const passwordCheckMsg = document.querySelector(".error-pw-check");
 const emailInput = document.querySelector(".input-email");
 const passwordInput = document.querySelector(".input-pw");
+const passwordCheck = document.querySelector(".input-pw-check");
 
 const showErrorMsg = (e) => {
-  const { value, name } = e.target;
+  const { value, name, classList } = e.target;
   let errorMsg = "";
   if (name === "email") {
     if (!value) errorMsg = "이메일을 입력해 주세요.";
     else if (!isValidEmail(value)) errorMsg = "올바른 이메일 주소가 아닙니다.";
+    else if (value === EMAIL) errorMsg = "이미 사용 중인 이메일입니다.";
     emailMsgBox.innerHTML = errorMsg;
-  } else if (name === "password") {
+  } else if (classList.contains("input-pw")) {
     errorMsg = value ? "" : "비밀번호를 확인해 주세요.";
     passwordMsgBox.innerHTML = errorMsg;
+  } else if (classList.contains("input-pw-check")) {
+    if(value){
+      if (passwordInput.value === value) {
+        passwordCheckMsg.classList.add("hidden");
+      } else {
+        passwordCheckMsg.classList.remove("hidden");
+      }
+    }else{
+      passwordCheckMsg.classList.add("hidden");
+    }
+    
   }
   changeInputColor();
 };
@@ -36,7 +50,7 @@ const moveFolderPage = (e) => {
   changeInputColor();
 };
 
-const enter = ({key}) => {
+const enter = ({ key }) => {
   if (key === "Enter") {
     moveFolderPage();
   }
@@ -59,4 +73,5 @@ form.addEventListener("click", showPassword);
 function changeInputColor() {
   emailMsgBox.innerHTML ? emailInput.classList.add("error") : emailInput.classList.remove("error");
   passwordMsgBox.innerHTML ? passwordInput.classList.add("error") : passwordInput.classList.remove("error");
+  passwordCheckMsg.classList.contains('hidden') ? passwordCheck.classList.remove("error") : passwordCheck.classList.add("error");
 }
