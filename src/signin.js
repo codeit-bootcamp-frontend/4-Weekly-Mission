4beb712f72da25fe2a5543f4401a53c4c9131fcd
김보경 <rlaqhrud6263@naver.com>
2024-01-23 19:09:17 +0900

    Fix signin.js - 4week Mission finish

--- src/signin.js
+++ src/signin.js
@@ -5,31 +5,36 @@ const signinForm = document.querySelector('.signin');
 //이메일 유효성 검사
 function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+  return emailRegex.test(email.value);
 }
 
 //에러 메세지 함수
 function errorMessage (input, message) {
     //에러메세지 중복 삭제
     if (input.parentElement.lastChild.className === 'errorMessage') {
-      emailInput.parentElement.remove(lastChild);
+      input.parentElement.lastChild.remove();
     }
-  const Error = document.createElement('div');
+  const Error = document.createElement('p');
   Error.classList.add('errorMessage');
   Error.style.color = '#FF5B56';
   Error.textContent = message;
   input.parentElement.appendChild(Error);
+
+  //inputbox 색상 변경
+    if (input.parentElement.lastChild.className === 'errorMessage') {
+      input.style.borderColor = '#FF5B56';
+    }
 }
 
 //이메일 에러 메세지 출력
 function emailErrorMessage (e) {
   //에러메세지 - 값이 없을 경우
   if (emailInput.value.trim() === '') {
-    return errorMessage(emailInput, '이메일을 입력해주세요.');
+    errorMessage(emailInput, '이메일을 입력해주세요.');
   }
   //에러 메세지 - 형식에 맞지 않을 경우
   else if (!validateEmail(email)) {
-    return errorMessage(emailInput, '올바른 이메일 주소가 아닙니다.')
+    errorMessage(emailInput, '올바른 이메일 주소가 아닙니다.')
   }
 }
 
@@ -37,7 +42,7 @@ function emailErrorMessage (e) {
 function passErrorMessage (e) {
   //에러메세지
   if(passInput.value.trim() === '') {
-    return errorMessage(passInput, '비밀번호를 입력해주세요.');
+    errorMessage(passInput, '비밀번호를 입력해주세요.');
   }
 }
 
@@ -59,5 +64,4 @@ passInput.addEventListener('focusout', passErrorMessage);
 signinForm.addEventListener('submit', submitForm);
 
 // 잘 안된거
-// 메세지 유형이 하나만 출력됨
 // 비밀번호 에러메세지 갭 차이
