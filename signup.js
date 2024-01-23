db8832005d45fe5ad795909d04ab3d46b930ced7
jae6269 <jae6269@gmail.com>
2024-01-23 17:45:03 +0900

    feat(signup.js) : 회원가입 input focus event 등록

---
+++ signup.js
@@ -0,0 +1,27 @@
+import * as signFunctions from './signFunctions.js';
+
+const emailInput = document.querySelector('#signup-email');
+const pwInput = document.querySelector('#signup-pw');
+const pwInputRepeat = document.querySelector('#signup-pw-repeat');
+const pwOnOffImg = document.querySelectorAll('#pw-onoff');
+let isPasswordVisible = [false];
+const signButton = document.getElementById('signup-button');
+const emailError = document.getElementById('email__error-msg');
+const pwError = document.getElementById('pw__error-msg');
+const pwRepeatError = document.getElementById('pw-repeat__error-msg');
+
+//focus in event
+emailInput.addEventListener('focus', function () {
+  signFunctions.inputBorderBlue(emailInput);
+});
+pwInput.addEventListener('focus', function () {
+  signFunctions.inputBorderBlue(pwInput);
+});
+pwInputRepeat.addEventListener('focus', function () {
+  signFunctions.inputBorderBlue(pwInputRepeat);
+});
+
+//focus out event
+emailInput.addEventListener('focusout', function () {
+  signFunctions.emailErrorCheck(emailInput, emailError);
+});
