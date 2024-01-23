8c180c992e7b22bd950022177ef0848f37323230
LeeSeungHoon <ysh73900@gmail.com>
2024-01-23 20:42:25 +0900

    refactor: 에러메시지 상수로 관리

--- js/sign.js
+++ js/sign.js
@@ -15,14 +15,17 @@ const setValidStyle = (element) => {
 
 /* 이메일 유효성 검사 */
 const handleEmailValidation = () => {
+  const EMAIL_CHECK_MESSAGE = "이메일을 입력해 주세요.";
+  const EMAIL_ERROR_MESSAGE = "올바른 이메일 주소가 아닙니다.";
   const emailRegex =
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
   if (email.value === "") {
     setInvalidStyle(email);
-    emailError.innerHTML = "이메일을 입력해 주세요.";
+    emailError.innerHTML = EMAIL_CHECK_MESSAGE;
   } else if (!emailRegex.test(email.value)) {
     setInvalidStyle(email);
-    emailError.innerHTML = "올바른 이메일 주소가 아닙니다.";
+    emailError.innerHTML = EMAIL_ERROR_MESSAGE;
   } else {
     setValidStyle(email);
     emailError.innerHTML = "";
@@ -31,9 +34,11 @@ const handleEmailValidation = () => {
 
 /* 비밀번호 유효성 검사 */
 const handlePasswordValidation = () => {
+  const PASSWORD_ERROR_MESSAGE = "비밀번호를 입력해주세요.";
+
   if (password.value === "") {
     setInvalidStyle(password);
-    passwordError.innerHTML = "비밀번호를 입력해주세요.";
+    passwordError.innerHTML = PASSWORD_ERROR_MESSAGE;
   } else {
     setValidStyle(password);
     passwordError.innerHTML = "";
@@ -42,14 +47,17 @@ const handlePasswordValidation = () => {
 
 /* 특정 아이디 및 비밀번호로 로그인 시도 시 페이지 이동 */
 const redirectToFolderPage = () => {
+  const EMAIL_ERROR_MESSAGE = "이메일을 확인해 주세요.";
+  const PASSWORD_ERROR_MESSAGE = "비밀번호를 확인해 주세요.";
+
   if (email.value === "test@codeit.com" && password.value === "codeit101") {
     const link = "./folder.html";
     location.href = link;
   } else {
     setInvalidStyle(email);
     setInvalidStyle(password);
-    emailError.innerHTML = "이메일을 확인해 주세요.";
-    passwordError.innerHTML = "비밀번호를 확인해 주세요.";
+    emailError.innerHTML = EMAIL_ERROR_MESSAGE;
+    passwordError.innerHTML = PASSWORD_ERROR_MESSAGE;
   }
 };
 
