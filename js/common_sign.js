c7036a544fd99f8b420ff65028f5d8fd7f331bf8
jinhok96 <jinhok96a@gmail.com>
2024-01-24 17:22:14 +0900

    refactor: 파일 이름 변경 chore: 필요없는 <script> 제거 	renamed:    js/utility.js -> js/common_sign.js 	modified:   signin/index.html 	modified:   signin/index.js

--- js/common_sign.js
+++ js/common_sign.js
@@ -97,40 +97,42 @@ function hidePassword() {
 function checkEmailError() {
   if (isEmpty(emailInput.value)) {
     return showError(emailInput, emailErrorMessageElement, "이메일을 입력해 주세요.");
-  } else if (!isValidEmail()) {
-    return showError(emailInput, emailErrorMessageElement, "올바른 이메일 주소가 아닙니다.");
-  } else {
-    return hideError(emailInput, emailErrorMessageElement);
   }
+
+  if (!isValidEmail()) {
+    return showError(emailInput, emailErrorMessageElement, "올바른 이메일 주소가 아닙니다.");
+  }
+
+  return hideError(emailInput, emailErrorMessageElement);
 }
 
 //비밀번호 에러 검사
 function checkPasswordError() {
   if (isEmpty(passwordInput.value)) {
     return showError(passwordInput, passwordErrorMessageElement, "비밀번호를 입력해 주세요.");
-  } else {
-    return hideError(passwordInput, passwordErrorMessageElement);
   }
+
+  return hideError(passwordInput, passwordErrorMessageElement);
 }
 
 //로그인 성공/실패
 function checkLogin() {
   if (isValidUser(emailInput, passwordInput)) {
     return (location.href = "../folder/index.html");
-  } else {
-    showError(emailInput, emailErrorMessageElement, "이메일을 확인해 주세요.");
-    showError(passwordInput, passwordErrorMessageElement, "비밀번호를 확인해 주세요.");
-    return;
   }
+
+  showError(emailInput, emailErrorMessageElement, "이메일을 확인해 주세요.");
+  showError(passwordInput, passwordErrorMessageElement, "비밀번호를 확인해 주세요.");
+  return;
 }
 
 //eyeBtn 비밀번호 토글
 function togglePassword() {
   if (passwordInput.type === "password") {
     return showPassword();
-  } else {
-    return hidePassword();
   }
+
+  return hidePassword();
 }
 
 /********************
