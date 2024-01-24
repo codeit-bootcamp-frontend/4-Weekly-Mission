befd40cd35675b81e963fa56276ef2ad8b9684d1
Rhajiit <ksd9638@gmail.com>
2024-01-24 13:17:51 +0900

    Refactor : Rename passwordToggleButton to passToggleBtn

--- JavaScript/passwordHide.js
+++ JavaScript/passwordHide.js
@@ -1,8 +1,8 @@
-const passwordBeHidden = document.querySelectorAll('.passwordSeenButton')
+const passToggleBtn = document.querySelectorAll('.passToggleBtn')
 
 
 // 비밀번호 숨김 구현
-function togglePassword(event) {
+function passwordToggle(event) {
    if (event.target.classList.contains('notHidden')) {
      //비밀번호를 숨김
       event.target.classList.remove('notHidden')
@@ -13,6 +13,7 @@ function togglePassword(event) {
       event.target.previousElementSibling.setAttribute('type','text')
    }
 }
-for (let i = 0 ; i < passwordBeHidden.length ; i++) {
-   passwordBeHidden[i].addEventListener ('click', togglePassword)
+
+for (let i = 0 ; i < passToggleBtn.length ; i++) {
+   passToggleBtn[i].addEventListener ('click', passwordToggle)
 }
