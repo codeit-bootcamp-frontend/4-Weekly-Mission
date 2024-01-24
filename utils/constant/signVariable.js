184c41327a3b175add4033a9aacd5ef4ee6f68c4
박지윤 <tkrhkfld123@naver.com>
2024-01-24 23:51:39 +0900

    style: 에러메세지 모듈 추가

---
+++ utils/constant/signVariable.js
@@ -0,0 +1,9 @@
+export const EMAIL_ERROR_MESSAGE = {
+  empty: '이메일을 입력해 주세요.',
+  invalid: '올바른 이메일 주소가 아닙니다.',
+  validation: '이메일 주소를 확인해 주세요.'
+};
+export const PASSWORD_ERROR_MESSAGE = {
+  empty: '비밀번호를 입력해 주세요.',
+  validation: '비밀번호를 확인해 주세요.'
+};
