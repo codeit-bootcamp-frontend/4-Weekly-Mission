f5291b52b49e6149f9e9b4c043bbb84ca56b614b
이진호 <tead7@naver.com>
2024-01-24 17:26:29 +0900

    피드백 수정 완료?

--- js/signin.js
+++ js/signin.js
@@ -1,86 +1,141 @@
+const form = document.querySelector('.sign-form');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const form = document.getElementById('sign-form');
-const emailE =document.getElementById('error-email');
-const passwordE = document.getElementById('error-password');
-const eyebutton = document.getElementById('eye-button');
-const eye = document.getElementById('eye');
+const emailErrorMessage =document.getElementById('error-email');
+const passwordErrorMessage = document.getElementById('error-password');
+const passwordRepeat = document.getElementById("passwordR");
+const passwordRepeatErrorMessage = document.getElementById('error-passwordR');
+
+const errorMessages = {
+    emailRequired: "이메일을 입력해주세요.",
+    invalidEmail: "올바른 이메일 주소가 아닙니다.",
+    duplicateEmail: "이미 사용중인 이메일입니다.",
+    passwordRequired: "비밀번호를 입력해주세요.",
+    passwordRepeatRequired: "비밀번호 재확인칸을 입력해주세요.",
+    invalidPassword: "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+    passwordMismatch: "비밀번호가 일치하지 않아요."
+};
+
+let isCheckingEmail= false;
+let isCheckingPassword= false;
+let isCheckingPasswordRepeatEmail= false;
 
 //포커싱시 파란색 테두리 출력
-function focusing(){
-    emailE.innerHTML="";
-    email.style.border= "0.1rem solid var(--primary)";
-}
-function focusing2(){
-    passwordE.innerHTML="";
-    password.style.border= "0.1rem solid var(--primary)";
+function focusing(errorName,styleName){
+    errorName.innerHTML="";
+    styleName.style.border= "0.1rem solid var(--primary)";
 }
 
+//눈 비밀번호 표시 / 미표시
+function eyeBlink(input,icon){
+    if(input.type=="password"){
+        input.type="text";
+        icon.src="../images/eye-on.svg"
+
+    }else{
+        input.type="password";
+        icon.src="../images/eye-off.svg"
+    }
+}
+ 
 //이메일
-function checkingE(){
+function checkingEmail(type){
 
     //이메일 체크를 위한 정규식
-    let checkE = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    const checkEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
     //이메일 체크
     if(email.value.trim()==""){
-        emailE.innerHTML="이메일을 입력해주세요.";
+        emailErrorMessage.innerHTML=emailRequired;
         email.style.border= "0.1rem solid var(--red)";
-    }else if(checkE.test(email.value.trim())){
-        emailE.innerHTML="";
+    }else if(checkEmail.test(email.value.trim())){
+        emailErrorMessage.innerHTML="";
         email.style.border= "0.1rem solid var(--gray20)";
     }else{
-        emailE.innerHTML="올바른 이메일 주소가 아닙니다.";
+        emailErrorMessage.innerHTML=invalidEmail;
         email.style.border= "0.1rem solid var(--red)";
+    }
+
+    if(type=='signup'){
+        if(email.value.trim()===`test@codeit.com`){
+            emailErrorMessage.innerHTML=duplicateEmail;
+            email.style.border= "0.1rem solid var(--red)";
+        }
     }
 }
 
 //비밀번호
-function checkingP(){
+function checkingPassword(){
+
+    //비밀번호 체크를 위한 정규식
+    const checkPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,30}$/
+
     //비밀번호 체크
     if(password.value.trim()==""){
-        passwordE.innerHTML="비밀번호를 입력해주세요.";
+        passwordErrorMessage.innerHTML=passwordRequired;
         password.style.border= "0.1rem solid var(--red)";
-    }else{
-        passwordE.innerHTML="";
+        isCheckingPassword= false;
+    }else if(checkPassword.test(password.value.trim())){
+        passwordErrorMessage.innerHTML="";
         password.style.border= "0.1rem solid var(--gray20)";
+        isCheckingPassword= true;
+    }else{
+        passwordErrorMessage.innerHTML=invalidPassword;
+        password.style.border= "0.1rem solid var(--red)";
+        isCheckingPassword= false;
     }
 }
+
+function checkingPasswordRepeat(){
+    if(passwordRepeat.value.trim()==""){
+        passwordRepeatErrorMessage.innerHTML=passwordRepeatRequired;
+        passwordRepeat.style.border= "0.1rem solid var(--red)";
+        isCheckingPasswordRepeatEmail= false;
+    }else if(password.value.trim()==passwordRepeat.value.trim()){
+        passwordRepeatErrorMessage.innerHTML="";
+        passwordRepeat.style.border= "0.1rem solid var(--gray20)";
+        isCheckingPasswordRepeatEmail= true;
+    }else{
+        passwordRepeatErrorMessage.innerHTML=passwordMismatch;
+        passwordRepeat.style.border= "0.1rem solid var(--red)";
+        isCheckingPasswordRepeatEmail= false;
+    }
+}
+
 
 
 //addEventListener를 이용한 체크 시스템
 form.addEventListener("submit",(e)=>{
 
-    //반복 방지
+
+    //새로고침 방지
     e.preventDefault();
 
-    //특정 아이디 확인
-    if(email.value.trim()=="test@codeit.com" && password.value.trim()=="codeit101"){
-        //폴더 페이지 미완성으로 인한 임시
-        location.href='/folder.html'
-    }else if(email.value.trim()!="test@codeit.com" && password.value.trim()=="codeit101"){
-        emailE.innerHTML="이메일을 확인해주세요.";
-        email.style.border= "0.1rem solid var(--red)";
-    }else if(email.value.trim()=="test@codeit.com" && password.value.trim()!="codeit101"){
-        passwordE.innerHTML="비밀번호를 확인해주세요.";
-        password.style.border= "0.1rem solid var(--red)";
-    }else{
-        emailE.innerHTML="이메일을 확인해주세요.";
-        email.style.border= "0.1rem solid var(--red)";
-        passwordE.innerHTML="비밀번호를 확인해주세요.";
-        password.style.border= "0.1rem solid var(--red)";
+    console.log(e.target.id);
+
+    if(e.target.id ==='signin'){
+        if(email.value.trim()=="test@codeit.com" && password.value.trim()=="codeit101"){
+            //폴더 페이지 미완성으로 인한 임시
+            location.href='/folder.html';
+        }else if(email.value.trim()!="test@codeit.com" && password.value.trim()=="codeit101"){
+            emailErrorMessage.innerHTML="이메일을 확인해주세요.";
+            email.style.border= "0.1rem solid var(--red)";
+        }else if(email.value.trim()=="test@codeit.com" && password.value.trim()!="codeit101"){
+            passwordErrorMessage.innerHTML="비밀번호를 확인해주세요.";
+            password.style.border= "0.1rem solid var(--red)";
+        }else{
+            emailErrorMessage.innerHTML="이메일을 확인해주세요.";
+            email.style.border= "0.1rem solid var(--red)";
+            passwordErrorMessage.innerHTML="비밀번호를 확인해주세요.";
+            password.style.border= "0.1rem solid var(--red)";
+        }
+    }else if(e.target.id ==='signup'){
+        if(isCheckingEmail == true && isCheckingPasswordRepeatEmail==true && isCheckingPassword == true){
+            location.href='/folder.html';
+        }else{
+            checkingEmail();
+            checkingPassword();
+            checkingPasswordRepeat();
+        }
     }
 });
-
-// onclick을 이용한 눈 비밀번호 표시 / 미표시
-
-eyebutton.onclick= (()=>{
-    if(password.type=="password"){
-        password.type="text";
-        eye.src="../images/eye-on.svg"
-
-    }else{
-        password.type="password";
-        eye.src="../images/eye-off.svg"
-    }
-})
