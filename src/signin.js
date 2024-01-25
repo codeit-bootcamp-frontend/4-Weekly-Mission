7496ff6f0295c587ff523cd08d08d767c25f4fb7
thgus5335 <thgus5335@naver.com>
2024-01-26 05:17:06 +0900

    :art: Fix: format of the function
    Fix format of the function to arrow function

--- src/signin.js
+++ src/signin.js
@@ -9,37 +9,38 @@ const eyeIcon = document.getElementById('eyeIcon');
 const errorEmail = document.createElement('div');
 errorEmail.className = 'error-text';
 
-function checkEmail() {
+email.addEventListener('focusout', function () {
   if (!email.value) {
     showInputError(email, errorEmail, ERROR_MESSAGE.EMPTY_EMAIL);
-  } else if (validateEmail(email.value)) {
-    showInputError(email, errorEmail, ERROR_MESSAGE.INVALID_EMAIL_ADDRESS);
-  } else {
-    errorEmail.remove();
-    email.classList.remove('error-input');
+    return;
   }
-}
+  if (!validateEmail(email.value)) {
+    showInputError(email, errorEmail, ERROR_MESSAGE.INVALID_EMAIL_ADDRESS);
+    return;
+  }
+  errorEmail.remove();
+  email.classList.remove('error-input');
+});
 
 function validateEmail(email_address) {
-  if (email_address.match(REGEX_EMAIL)) return false;
-  else return true;
+  return email_address.match(REGEX_EMAIL);
 }
 
 //Check password: 입력 여부 확인-----------------------------------------
 const errorPassword = document.createElement('div');
 errorPassword.className = 'error-text';
 
-function checkPassword() {
+password.addEventListener('focusout', function () {
   if (!password.value) {
     showInputError(password, errorPassword, ERROR_MESSAGE.EMPTY_PASSWORD);
   } else {
     errorPassword.remove();
     password.classList.remove('error-input');
   }
-}
+});
 
 //Login: submit form--------------------------------------------------
-function submitForm(e) {
+signinForm.addEventListener('submit', function (e) {
   e.preventDefault();
   if (email.value === USER_INFO.ID && password.value === USER_INFO.PASSWORD) {
     email.value = '';
@@ -48,13 +49,13 @@ function submitForm(e) {
     showInputError(email, errorEmail, ERROR_MESSAGE.INVAILD_EMAIL);
     showInputError(password, errorPassword, ERROR_MESSAGE.INVAILD_PASSWORD);
   }
-}
+});
 
 //Change icon: eye-on/off----------------------------------------------
 const eyeOnIcon = '/assets/icon/eye-on.svg';
 const eyeOffIcon = '/assets/icon/eye-off.svg';
 
-function changeEyeIcon() {
+eyeIcon.addEventListener('click', function () {
   eyeIcon.classList.toggle('on');
   if (eyeIcon.classList.contains('on')) {
     password.removeAttribute('type');
@@ -63,7 +64,7 @@ function changeEyeIcon() {
     password.setAttribute('type', 'password');
     changeImage(eyeIcon, eyeOffIcon);
   }
-}
+});
 
 //Util----------------------------------------------------------------
 function showInputError(id, element, message) {
@@ -75,8 +76,3 @@ function showInputError(id, element, message) {
 function changeImage(brfore, after) {
   brfore.setAttribute('src', after);
 }
-
-email.addEventListener('focusout', checkEmail);
-password.addEventListener('focusout', checkPassword);
-signinForm.addEventListener('submit', submitForm);
-eyeIcon.addEventListener('click', changeEyeIcon);
