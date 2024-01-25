17bec014ed063eeb55b1f15cf50112f58929bcce
LimDongHyeon <ilhai3@naver.com>
2024-01-25 16:53:24 +0900

    4주차 위클리 수정사항 수정

--- .github/resource/js/login.js
+++ .github/resource/js/login.js
@@ -13,51 +13,59 @@ const password = document.querySelector('#password');
 const hostname = '/folder';
 
 // 비밀번호 아이콘
-const pwd_icon = document.querySelector('.pwd_icon')
+const pwd_icon = document.querySelector('.pwd_icon');
+
+// 에러 메시지
+const ERROR_MESSAGE = {
+    email_empty: '이메일을 입력해 주세요.',
+    email_invalid: '올바른 이메일 주소가 아닙니다.',
+    pwd_empty: '비밀번호를 입력해주세요.'
+}
+
+const showError = (targetEl, errorEl, errorType) => {
+    errorEl.style.display = 'block';
+    targetEl.classList.add('error');
+    errorEl.innerText = ERROR_MESSAGE[errorType];
+}
+
+// 에러 메시지 삭제
+const deleteError = (targetEl, errorEl) => {
+    targetEl.classList.remove('error');
+    errorEl.style.display = 'none';
+}
 
 email.addEventListener('focusout', function(e) {
-    if(email.value == '') {
-        error.style.display = 'block';
-        email.classList.add('error');
-        error.innerText = "이메일을 입력해 주세요";
+    if(email.value === '') {
+        showError(email, error, 'email_empty');
     } else if(!emailRegex.test(email.value)) {
-        error.style.display = 'block';
-        email.classList.add('error');
-        error.innerText = "올바른 이메일 주소가 아닙니다.";
+        showError(email, error, 'email_invalid');
     } else if(emailRegex.test(email.value) && email.value !== '') {
-        error.style.display = 'none';
-        email.classList.remove('error');
+        deleteError(email, error);
     }
 })
 
 password.addEventListener('focusout', function(e) {
-    if(password.value == '') {
-        error_pwd.style.display = 'block';
-        password.classList.add('error');
-        error_pwd.innerText = "비밀번호를 입력해주세요.";
-    } else if(email.value !== '') {
-        error_pwd.style.display = 'none';
-        password.classList.remove('error');
+    if(password.value === '') {
+        showError(password, error_pwd, 'pwd_empty');
+    } else if(password.value !== '') {
+        deleteError(password, error_pwd);
     }
 })
 
 form.onsubmit = function(e) {
-    if(email.value.trim() == 'test@codeit.com' && password.value == 'codeit101') {
+    if(email.value.trim() === 'test@codeit.com' && password.value === 'codeit101') {
         e.preventDefault();
         window.location.href = hostname;
     } else {
-        error.style.display = 'block';
-        email.classList.add('error');
-        error.innerText = "이메일을 확인해 주세요";
-        error_pwd.style.display = 'block';
-        password.classList.add('error');
-        error_pwd.innerText = "비밀번호를 확인해 주세요.";
+        e.preventDefault()
+        showError(email, error, 'email_empty');
+        showError(password, error_pwd, 'pwd_empty');
     }
 }
 
 pwd_icon.addEventListener('click', function() {
     pwd_icon.classList.toggle('hide');
-    if(password.type == 'password') {
+    if(password.type === 'password') {
         password.setAttribute('type', 'text');
     } else {
         password.setAttribute('type', 'password');
