3654388bbab2393f9aa8609420794a3d6d3f4bc6
Jin-Chanyong <cksdyd324@gmail.com>
2024-01-25 15:33:21 +0900

    (로그인) 변수 이름 소문자로 수정. 클래스네임 수정.

--- signin/index.js
+++ signin/index.js
@@ -2,25 +2,25 @@
         Account
 *********************/
 
-class creatUser {
+class User {
   constructor(email, password) {
     this.email = email;
     this.password = password;
   }
 }
 
-const USER_1 = new creatUser('test@codeit.com', 'codeit101')
+const user = new User('test@codeit.com', 'codeit101')
 
 /*********************
       UI Constant
 *********************/
 
-const SIGN_IN_FORM = document.querySelector('.signin-form');
-const INPUT_EMAIL = document.querySelector('#signin-email');
-const INPUT_PASSWORD = document.querySelector('#signin-password');
-const ERROR_MESSAGE_EMAIL = document.querySelector('.errorMessage-email');
-const ERROR_MESSAGE_PASSWORD = document.querySelector('.errorMessage-password');
-const EYE_IMG = document.querySelector('.eye-img');
+const signInForm = document.querySelector('.signin-form');
+const inputEmail = document.querySelector('#signin-email');
+const inputPassword = document.querySelector('#signin-password');
+const errorMessageEmail = document.querySelector('.errorMessage-email');
+const errorMessagePassword = document.querySelector('.errorMessage-password');
+const eyeImg = document.querySelector('.eye-img');
 
 /*********************
        Function
@@ -31,7 +31,7 @@ function isEmpty(text) { return text.length === 0 };
 function isEmailValid(text) { return text.includes('@') };
 
 function isAccountValid(email, password) {
-  return (USER_1.email === email && USER_1.password === password);
+  return (user.email === email && user.password === password);
 };
 
 function showErrorMessage(targetElement, errorMessage) {
@@ -55,51 +55,51 @@ function changePlaceholderFocusOut(e) {
 };
 
 function emailError() {
-  if (isEmpty(INPUT_EMAIL.value)) {
-    showErrorMessage(ERROR_MESSAGE_EMAIL, '이메일을 입력해 주세요.');
-    INPUT_EMAIL.classList.add('red-border')
-  } else if (!isEmailValid(INPUT_EMAIL.value)) {
-    showErrorMessage(ERROR_MESSAGE_EMAIL, '올바른 이메일 주소가 아닙니다.')
-    INPUT_EMAIL.classList.add('red-border')
+  if (isEmpty(inputEmail.value)) {
+    showErrorMessage(errorMessageEmail, '이메일을 입력해 주세요.');
+    inputEmail.classList.add('red-border')
+  } else if (!isEmailValid(inputEmail.value)) {
+    showErrorMessage(errorMessageEmail, '올바른 이메일 주소가 아닙니다.')
+    inputEmail.classList.add('red-border')
   }
 };
 
 function passwordError() {
-  if (isEmpty(INPUT_PASSWORD.value)) {
-    showErrorMessage(ERROR_MESSAGE_PASSWORD, '비밀번호를 입력해 주세요.')
-    INPUT_PASSWORD.classList.add('red-border')
+  if (isEmpty(inputPassword.value)) {
+    showErrorMessage(errorMessagePassword, '비밀번호를 입력해 주세요.')
+    inputPassword.classList.add('red-border')
   };
 };
 
 function deleteError(e) {
-  if (e.target === INPUT_EMAIL) {
-    hideErorrMessage(ERROR_MESSAGE_EMAIL);
-    INPUT_EMAIL.classList.remove('red-border');
+  if (e.target === inputEmail) {
+    hideErorrMessage(errorMessageEmail);
+    inputEmail.classList.remove('red-border');
   };
-  if (e.target === INPUT_PASSWORD) {
-    hideErorrMessage(ERROR_MESSAGE_PASSWORD);
-    INPUT_PASSWORD.classList.remove('red-border');
+  if (e.target === inputPassword) {
+    hideErorrMessage(errorMessagePassword);
+    inputPassword.classList.remove('red-border');
   };
 };
 
 function passwordToggle() {
-  if (EYE_IMG.classList.contains('eye-open')) {
-    EYE_IMG.classList.toggle('eye-open');
-    EYE_IMG.classList.toggle('eye-closed'); 
-    INPUT_PASSWORD.setAttribute('type', 'password');
+  if (eyeImg.classList.contains('eye-open')) {
+    eyeImg.classList.toggle('eye-open');
+    eyeImg.classList.toggle('eye-closed'); 
+    inputPassword.setAttribute('type', 'password');
   } else {
-    EYE_IMG.classList.toggle('eye-open');
-    EYE_IMG.classList.toggle('eye-closed'); 
-    INPUT_PASSWORD.setAttribute('type', 'text');
+    eyeImg.classList.toggle('eye-open');
+    eyeImg.classList.toggle('eye-closed'); 
+    inputPassword.setAttribute('type', 'text');
   };
 };
 
 function login(e) {
   e.preventDefault();
 
-  if (!isAccountValid(INPUT_EMAIL.value, INPUT_PASSWORD.value)) {
-    showErrorMessage(ERROR_MESSAGE_EMAIL, '이메일을 확인해 주세요.');
-    showErrorMessage(ERROR_MESSAGE_PASSWORD, '비밀번호를 확인해 주세요.');
+  if (!isAccountValid(inputEmail.value, inputPassword.value)) {
+    showErrorMessage(errorMessageEmail, '이메일을 확인해 주세요.');
+    showErrorMessage(errorMessagePassword, '비밀번호를 확인해 주세요.');
     return alert('계정을 확인해주세요.')
   };
 
@@ -110,13 +110,13 @@ function login(e) {
       EventHandler
 *********************/
 
-SIGN_IN_FORM.addEventListener('focusin', changePlaceholderFocusIn);
-SIGN_IN_FORM.addEventListener('focusout', changePlaceholderFocusOut);
-SIGN_IN_FORM.addEventListener('submit', login);
-SIGN_IN_FORM.addEventListener('focusin', deleteError);
+signInForm.addEventListener('focusin', changePlaceholderFocusIn);
+signInForm.addEventListener('focusout', changePlaceholderFocusOut);
+signInForm.addEventListener('submit', login);
+signInForm.addEventListener('focusin', deleteError);
 
-INPUT_EMAIL.addEventListener('focusout', emailError);
+inputEmail.addEventListener('focusout', emailError);
 
-INPUT_PASSWORD.addEventListener('focusout', passwordError);
+inputPassword.addEventListener('focusout', passwordError);
 
-EYE_IMG.addEventListener('click', passwordToggle);
+eyeImg.addEventListener('click', passwordToggle);
