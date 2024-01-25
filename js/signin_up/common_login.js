9a1b53e53cb10af4995941b92dfecd2cf1066612
Kim Min Jeong <minjeong9919@naver.com>
2024-01-26 00:19:24 +0900

    Refactor: 로그인, 회원가입 페이지 공통 로직 모듈화

---
+++ js/signin_up/common_login.js
@@ -0,0 +1,86 @@
+import {ERROR_MESSAGE } from "../../constant.js";
+import { emailCheck, passwordCheck } from "../validation.js"; 
+import { inputAddNode, inputDeleteNode } from "../node.js";
+
+export const emailDiv = document.querySelector('#email');
+export const emailInput = document.querySelector('.email-input');
+export const pwdInput = document.querySelector('.password-input');
+export const signinBtn = document.querySelector('#signinBtn');
+export const pwdEyeIcon = document.querySelector('.PwdEyeIcon');
+
+let [emailVal, pwdVal] = ["", ""];
+
+// 에러 메세지 출력 함수
+export function errorMsg(errorCase) {
+  let newNode = document.createElement('div');
+
+  switch(errorCase) {
+    case "NoEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.empty, errorCase);
+      break;
+    case "wrongEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.invalid, errorCase);
+      break;
+    case "inUseEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.inUse, errorCase);
+    case "NoPwd" :
+      inputAddNode('password', ERROR_MESSAGE.password.empty, errorCase);
+      break;
+    case "wrongPwd" :
+      inputAddNode('password', ERROR_MESSAGE.password.invalid, errorCase);
+      break;
+    case "Other": 
+      inputAddNode('email', ERROR_MESSAGE.email.check, errorCase);
+      inputAddNode('password', ERROR_MESSAGE.password.check, errorCase);
+      break;
+  }
+ }
+
+// 이메일 input 핸들러
+export function emailHandlerFunc(email) {
+  if(email) {
+    emailCheck(email) ? inputDeleteNode('email') : errorMsg("wrongEmail")
+  } else {
+    errorMsg("NoEmail");
+  }
+  emailVal = email; 
+}
+
+// 비밀번호 input 핸들러 함수
+export function passwordHandlerFuc(password) {
+  if(password) {
+    passwordCheck(password) ? inputDeleteNode('password') : errorMsg("wrongPwd")
+  } else {
+    errorMsg("NoPwd");
+  }
+  pwdVal = password;
+}
+
+// 로그인 시도 함수
+export function trySignin(email,password) {
+  if(email === "test@codeit.com" && password === "codeit101") {
+    signinBtn.parentElement.setAttribute('href',"/folder.html");
+  } else {
+    errorMsg("Other"); 
+  }
+}
+
+
+// enter키 입력으로 로그인 실행 
+export function EnterLogin(key) {
+  if(key === 'Enter') {
+    trySignin();
+  }
+}
+
+// password input 눈모양 클릭 실행 함수
+export function EyePwd(EyeStatus) {
+  if(EyeStatus.classList.contains('off')) {
+    pwdEyeIcon.setAttribute('src',"assets/icons/eye-off.png");
+    pwdInput.setAttribute('type', 'password');
+
+  } else {
+    pwdEyeIcon.setAttribute('src',"assets/icons/eye-on.png");
+    pwdInput.setAttribute('type', 'text');
+  }
+}
