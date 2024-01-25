40e72c22c600b915e277c1fb98c9463aeaf2e683
thgus5335 <thgus5335@naver.com>
2024-01-26 08:06:08 +0900

    :art: Fix: add element in HTML & function
    Add element in HTML & function

--- src/signin.js
+++ src/signin.js
@@ -5,38 +5,31 @@ const password = document.getElementById('password');
 const signinForm = document.getElementById('signinForm');
 const eyeIcon = document.getElementById('eyeIcon');
 
-//Check email: 입력 여부 확인, 메일 형식 확인-------------------------------
-const errorEmail = document.createElement('div');
-errorEmail.className = 'error-text';
+const emailErrorMessage = document.getElementById('emailErrorMessage');
+const passwordErrorMessage = document.getElementById('passwordErrorMessage');
 
+//Check email: 입력 여부 확인, 메일 형식 확인-------------------------------
 email.addEventListener('focusout', function () {
   if (!email.value) {
-    showInputError(email, errorEmail, ERROR_MESSAGE.EMPTY_EMAIL);
+    showInputError(email);
+    showErrorMessage(emailErrorMessage, ERROR_MESSAGE.EMPTY_EMAIL);
     return;
   }
   if (!validateEmail(email.value)) {
-    showInputError(email, errorEmail, ERROR_MESSAGE.INVALID_EMAIL_ADDRESS);
+    showErrorMessage(emailErrorMessage, ERROR_MESSAGE.INVALID_EMAIL_ADDRESS);
     return;
   }
-  errorEmail.remove();
-  email.classList.remove('error-input');
+  hideInputError(email, emailErrorMessage);
 });
 
-function validateEmail(email_address) {
-  return email_address.match(REGEX_EMAIL);
-}
-
 //Check password: 입력 여부 확인-----------------------------------------
-const errorPassword = document.createElement('div');
-errorPassword.className = 'error-text';
-
 password.addEventListener('focusout', function () {
   if (!password.value) {
-    showInputError(password, errorPassword, ERROR_MESSAGE.EMPTY_PASSWORD);
-  } else {
-    errorPassword.remove();
-    password.classList.remove('error-input');
+    showInputError(password);
+    showErrorMessage(passwordErrorMessage, ERROR_MESSAGE.EMPTY_PASSWORD);
+    return;
   }
+  hideInputError(password, passwordErrorMessage);
 });
 
 //Login: submit form--------------------------------------------------
@@ -45,34 +38,48 @@ signinForm.addEventListener('submit', function (e) {
   if (email.value === USER_INFO.ID && password.value === USER_INFO.PASSWORD) {
     email.value = '';
     window.location.href = '/pages/folder.html';
-  } else {
-    showInputError(email, errorEmail, ERROR_MESSAGE.INVAILD_EMAIL);
-    showInputError(password, errorPassword, ERROR_MESSAGE.INVAILD_PASSWORD);
+    return;
   }
+  showInputError(email);
+  showErrorMessage(emailErrorMessage, ERROR_MESSAGE.INVALID_EMAIL_ADDRESS);
+  showInputError(password);
+  showErrorMessage(passwordErrorMessage, ERROR_MESSAGE.EMPTY_PASSWORD);
 });
 
 //Change icon: eye-on/off----------------------------------------------
 const eyeOnIcon = '/assets/icon/eye-on.svg';
 const eyeOffIcon = '/assets/icon/eye-off.svg';
 
-eyeIcon.addEventListener('click', function () {
+eyeIcon.addEventListener('click', () => {
   eyeIcon.classList.toggle('on');
   if (eyeIcon.classList.contains('on')) {
     password.removeAttribute('type');
     changeImage(eyeIcon, eyeOnIcon);
-  } else {
-    password.setAttribute('type', 'password');
-    changeImage(eyeIcon, eyeOffIcon);
+    return;
   }
+  password.setAttribute('type', 'password');
+  changeImage(eyeIcon, eyeOffIcon);
 });
 
 //Util----------------------------------------------------------------
-function showInputError(id, element, message) {
-  element.textContent = message;
-  id.classList.add('error-input');
-  id.after(element);
+function validateEmail(email_address) {
+  return email_address.match(REGEX_EMAIL);
 }
 
-function changeImage(brfore, after) {
-  brfore.setAttribute('src', after);
+function showInputError(inputElement) {
+  inputElement.classList.add('error-input');
+}
+
+function showErrorMessage(messageElement, message) {
+  messageElement.classList.remove('hidden');
+  messageElement.textContent = message;
+}
+
+function hideInputError(inputElement, messageElement) {
+  inputElement.classList.remove('error-input');
+  messageElement.classList.add('hidden');
+}
+
+function changeImage(beforeImageElement, after) {
+  beforeImageElement.setAttribute('src', after);
 }
