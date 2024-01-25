39d6784cbb42d32d0a2f4b9d7caffb5f2a46ba91
ingssg <js2121good@gmail.com>
2024-01-26 02:08:22 +0900

    refactor: html에 에러메시지 p 태그 추가, 해당 p태그로 에러 메시지 컨트롤하게 수정

--- js/login.js
+++ js/login.js
@@ -31,88 +31,48 @@ const isBlank = (e) => e.target.value === '';
 // 비어있는 인풋에 에러메시지 추가
 const addErrorToBlankInput = (e) => {
   const blankInput = e.target;
-  const errorMsg = $('.js-input-profile-error-blank-msg', blankInput.parentNode);
+  const errorMsg = $('.input-error-msg', blankInput.parentNode);
   if(isBlank(e)) {
-    // 에러메시지가 존재하지 않는 최초의 경우에는 추가
     blankInput.classList.add('js-input-profile-error');
-    if(!errorMsg) {
-      const errorInputMsg = document.createElement('p');      
-      // input이 id인지 email인지 체크
-      if(blankInput.getAttribute('id') === 'email')
-        errorInputMsg.textContent = ERROR_MSG.BLANK.EMAIL;
-      else 
-        errorInputMsg.textContent = ERROR_MSG.BLANK.PASSWORD;
-
-      errorInputMsg.className = 'js-input-profile-error-blank-msg';
-      blankInput.after(errorInputMsg);
-    }
-    else { // 에러메시지는 있는데 인풋을 다시 지워서 빈 경우
-      errorMsg.classList.remove('js-display-none');
-    }
+    errorMsg.classList.remove('js-display-none');
+    // input이 id인지 email인지 체크
+    if(blankInput.getAttribute('id') === 'email')
+      errorMsg.textContent = ERROR_MSG.BLANK.EMAIL;
+    else 
+      errorMsg.textContent = ERROR_MSG.BLANK.PASSWORD;
   }
-  else {  // 에러메시지가 있고 인풋이 비어있지 않은 경우
-    if(errorMsg) {
-      blankInput.classList.remove('js-input-profile-error');
-      errorMsg.classList.add('js-display-none');
-    }
+  else {  // 인풋이 비어있지 않은 경우
+    blankInput.classList.remove('js-input-profile-error');
+    errorMsg.classList.add('js-display-none');
   }
 }
 
 const addErrorToInvalidInput = (e) => {
   const invalidEmailInput = e.target;
-  const errorMsg = $('.js-input-profile-error-invalid-msg', invalidEmailInput.parentNode);
-  if(!isBlank(e) && !isEmail(invalidEmailInput.value)){
-    invalidEmailInput.classList.add('js-input-profile-error');
-    if(!errorMsg) {  // 에러메시지 최초 생성
-      const errorInputMsg = document.createElement('p');
-      errorInputMsg.textContent = ERROR_MSG.INVALID.EMAIL;
-      errorInputMsg.className = 'js-input-profile-error-invalid-msg';
-      errorInputMsg.classList.add('js-input-profile-error-msg-order');
-      invalidEmailInput.after(errorInputMsg);
+  const errorMsg = $('.input-error-msg', invalidEmailInput.parentNode);
+  if(!isBlank(e)) {
+    if(!isEmail(invalidEmailInput.value)){
+      invalidEmailInput.classList.add('js-input-profile-error');
+      errorMsg.classList.remove('js-display-none');
+      errorMsg.textContent = ERROR_MSG.INVALID.EMAIL;
     }
     else {
-      errorMsg.classList.remove('js-display-none');
+      invalidEmailInput.classList.remove('js-input-profile-error');
+      errorMsg.classList.add('js-display-none');
     }
-  }
-  else if(isBlank(e)) {
-    if(errorMsg) errorMsg.classList.add('js-display-none');
-  }
-  else {
-    invalidEmailInput.classList.remove('js-input-profile-error');
-    if(errorMsg) errorMsg.classList.add('js-display-none');
   }
 }
 
 const addErrorToIncorrectInput = (input) => {
   const incorrectInput = input;
-  console.log(incorrectInput)
-  const errorMsg = $('.js-input-profile-error-incorrect-msg', incorrectInput.parentNode);
+  const errorMsg = $('.input-error-msg', incorrectInput.parentNode);
   incorrectInput.classList.add('js-input-profile-error');
-  if(!errorMsg) {
-    const errorInputMsg = document.createElement('p');      
-    // input이 id인지 email인지 체크
-    if(incorrectInput.getAttribute('id') === 'email')
-      errorInputMsg.textContent = ERROR_MSG.INCORRECT.EMAIL;
-    else 
-      errorInputMsg.textContent = ERROR_MSG.INCORRECT.PASSWORD;
-  
-    errorInputMsg.className = 'js-input-profile-error-incorrect-msg';
-    incorrectInput.after(errorInputMsg);
-  }
-  else {
-    errorMsg.classList.remove('js-display-none');
-  }
-}
-
-const removeErrorToIncorrectInput = (e) => {
-  const incorrectInput = e.target;
-  console.log('제거');
-  console.log(incorrectInput);
-  console.log('끗');
-  const errorMsg = $('.js-input-profile-error-incorrect-msg', incorrectInput.parentNode);
-  if(errorMsg) {
-    errorMsg.classList.add('js-display-none');
-  }
+  errorMsg.classList.remove('js-display-none');     
+  // input이 id인지 email인지 체크
+  if(incorrectInput.getAttribute('id') === 'email')
+    errorMsg.textContent = ERROR_MSG.INCORRECT.EMAIL;
+  else 
+    errorMsg.textContent = ERROR_MSG.INCORRECT.PASSWORD;
 }
 
 const loginHandler = (e) => {
@@ -152,7 +112,6 @@ const togglePassword = () => {
 $('#email').addEventListener('blur', addErrorToBlankInput);
 $('#password').addEventListener('blur', addErrorToBlankInput);
 $('#email').addEventListener('blur', addErrorToInvalidInput);
-$('.login-form').addEventListener('focusin', removeErrorToIncorrectInput);
 $('.login-form').addEventListener('submit', noRefreshOnSubmit);
 $('.login-form').addEventListener('submit', loginHandler);
 $('.eye-icon').addEventListener('click', togglePassword);
