1b3e45063494f8dc4d52b7c846a23ebe0a62e6ec
Taeuk471 <asky990223@gmail.com>
2024-01-26 00:18:34 +0900

    :art: Group functions and simplified Code

---
+++ script/functions/signFunction.js
@@ -0,0 +1,24 @@
+import {EMAIL_REGEX} from '../constant.js'
+
+//빈 문자열인지 체크
+export function isEmptyString(value) {
+  return typeof value === 'string' && value.trim() === '';
+}
+//이메일의 유효성 검사
+export function isValidEmail(email) { 
+  return EMAIL_REGEX.test(email);
+}
+
+
+//해당 Element가 보이는 상태인지 아닌지를 판별
+export function isVisiable(element) {
+  return element.classList.contains('hidden');
+}
+
+export function makeVisiable(element) {
+  element.classList.remove('hidden');
+}
+
+export function makeInvisiable(element) {
+  element.classList.add('hidden');
+}
--- script/signin.js
+++ script/signin.js
@@ -1,127 +1,65 @@
-/* 이메일 input에서 focus out 할 때
-값이 없을 경우 아래에 "이메일을 입력해 주세요." 에러 메시지를 보입니다 */
+import {EMAIL_REGEX, USERS, VISIABLE_STATE} from './constant.js'
+import { isEmptyString, isVisiable, makeInvisiable, makeVisiable, isValidEmail } from './functions/signFunction.js';
 
+
+// ERROR_MESSAGE for email
+const emailErrorMessage = document.getElementById('emailErrorMessage');
+const emailFormatErrorMessage = document.getElementById('emailFormatErroMessage');
+const emailCheckErrorMessage = document.getElementById('emailCheckErrorMessage');
+
+// ERROR_MESSAGE for password
+const passwordErrorMessage = document.getElementById('passwordErrorMessage');
+const passwordCheckEMessage = document.getElementById('passwordCheckMessage');
+
+//INPUT section
 const emailInput = document.getElementById('email');
-const emailErrorMessage = document.createElement('div');
-emailErrorMessage.className = 'error-message';
-emailErrorMessage.classList.add('text-medium');
-emailErrorMessage.innerText = '이메일을 입력해 주세요'
+const passwordInput = document.getElementById('password');
 
-function isEmptyString(value) {
-    return typeof value === 'string' && value.trim() === '';
-  }
+//Button section
+const signInButton = document.getElementById('login-button');
+const watchPassword = document.getElementById('eye-button');
+
+const goToFolderhtml = '../folder.html';
+
 
 emailInput.addEventListener('focusout', function(){
-  if(isEmptyString(emailInput.value)) { 
-    emailErrorMessage.classList.remove('hidden'); 
-    emailInput.classList.add('error-box');
-    emailInput.parentElement.lastElementChild.after(emailErrorMessage);
-  } else {
-    emailErrorMessage.classList.add('hidden');
-    emailInput.classList.remove('error-box');
-  }
+    const emailValue = emailInput.value.trim(); 
+    if(!isEmptyString(emailValue)) {makeInvisiable(emailErrorMessage);return;}
+    makeVisiable(emailErrorMessage);
 });
 
-/*이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 
-"올바른 이메일 주소가 아닙니다" 라고 에러 메세지를 보냅니다. */
-
-const emailFormatErrorMessage = document.createElement('div');
-emailFormatErrorMessage.className = 'error-message';
-emailFormatErrorMessage.classList.add('text-medium');
-emailFormatErrorMessage.innerText = '올바른 이메일 주소가 아닙니다.';
+passwordInput.addEventListener('focusout', function () {
+    const passwordValue = passwordInput.value.trim();
+    if(!isEmptyString(passwordValue)) {makeInvisiable(passwordErrorMessage);return;} 
+    makeVisiable(passwordErrorMessage);
+});
 
 emailInput.addEventListener('focusout', function () {
-    const emailValue = emailInput.value.trim(); 
-    if (emailValue && !isValidEmail(emailValue)) { 
-        emailFormatErrorMessage.classList.remove('hidden'); 
-        emailInput.after(emailFormatErrorMessage);
-        return;
-    } 
-    emailFormatErrorMessage.classList.add('hidden');
+    const emailValue = emailInput.value.trim();
+    if (!isEmptyString(emailValue) && isValidEmail(emailValue)) {
+        makeInvisiable(emailFormatErrorMessage); return;} 
+    if (isEmptyString(emailValue)) {makeInvisiable(emailFormatErrorMessage); return;}
+    makeVisiable(emailFormatErrorMessage);
 });
-
-const EMAIL_REGEX = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-function isValidEmail(email) { 
-    return EMAIL_REGEX.test(email);
-}
-
-/*이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, 
-“/folder” 페이지로 이동합니다.*/
-
-/*이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해 주세요.”,
-비밀번호 input 아래에 “비밀번호를 확인해 주세요.” 에러 메세지를 보입니다.*/ 
-
-// 2가지 모두 같은 동작으로 진행되어서 묶어서 처리합니다..
-// 만약 해당 출력이 맞지 않는다면 이를 분해해서 if-else문 2개로 처리하면 됩니다.
-
-const signInButton = document.getElementById('login-button');
-const passwordInput = document.getElementById('password'); // emailValue는 위에서 가져왔기 때문에 PWValue만 참조
-
-const emailCheckErrorMessage = document.createElement('div');
-emailCheckErrorMessage.className = 'error-message';
-emailCheckErrorMessage.classList.add('text-medium');
-emailCheckErrorMessage.innerText = '이메일을 확인해 주세요.';
-
-const passwordCheckMessage = document.createElement('div');
-passwordCheckMessage.className = 'error-message';
-passwordCheckMessage.classList.add('text-medium')
-passwordCheckMessage.innerText = '비밀번호를 확인해 주세요.';
-
-const USERS = [{id: 'test@codeit.com', password : 'codeit101'}];
 
 signInButton.addEventListener('click', function (event) {
     event.preventDefault();
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
-
     if (USERS[0].id === emailValue && USERS[0].password === passwordValue) {
-        window.location.href = '../folder.html'; // 데이터베이스 접근이 예상되지만, 정상적 실행 확인을 위해 html 사이트로 연결
-    } else {
-        emailInput.parentElement.lastElementChild.after(emailCheckErrorMessage);
-        passwordInput.parentElement.lastElementChild.after(passwordCheckMessage);
+        window.location.href = goToFolderhtml;
     }
+    makeVisiable(emailCheckErrorMessage);
+    makeVisiable(passwordCheckMessage);
 });
-
-/*비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 
-에러 메세지를 보입니다.*/
-
-const passwordErrorMessage = document.createElement('div');
-passwordErrorMessage.className = 'error-message';
-passwordErrorMessage.classList.add('text-medium')
-passwordErrorMessage.innerText = '비밀번호를 입력해 주세요.';
-
-passwordInput.addEventListener('focusout', function () {
-    if (!passwordInput.value.trim()) {
-        passwordErrorMessage.classList.remove('hidden');
-        passwordInput.classList.add('error-box')
-        passwordInput.parentElement.lastElementChild.after(passwordErrorMessage);
-    } else {
-        passwordErrorMessage.classList.add('hidden');
-        passwordInput.classList.remove('error-box');
-    }
-});
-
-/*눈 모양 아이콘 클릭시 비밀번호의 문자열이 보이기도 하고, 가려지기도 합니다.
-비밀번호의 문자열이 가려질 때는 눈 모양 아이콘에는 사선이 그어져있고, 
-비밀번호의 문자열이 보일 때는 사선이 없는 눈 모양 아이콘이 보이도록 합니다.*/ 
-
-const watchPassword = document.querySelector('.eye-button');
-const INPUT_BOX = watchPassword.previousElementSibling;
-const state = {
-    shouldPasswordVisible: watchPassword.previousElementSibling.type === 'text',
-    EYE_OFF_IMAGE: './assets/logo/eye-off.svg',
-    EYE_ON_IMAGE: './assets/logo/eye-on.svg'
-};
 
 watchPassword.addEventListener('click', function () {
-    state.shouldPasswordVisible = !state.shouldPasswordVisible;
-
-    if (!state.shouldPasswordVisible) {
-        watchPassword.firstElementChild.src = state.EYE_OFF_IMAGE;
-        INPUT_BOX.type = 'password';
+    if (passwordInput.type === 'text' ? true : false) {
+        watchPassword.firstElementChild.src = VISIABLE_STATE.invisiable;
+        passwordInput.type = 'password';
     } else {
-        watchPassword.firstElementChild.src = state.EYE_ON_IMAGE;
-        INPUT_BOX.type = 'text';
+        watchPassword.firstElementChild.src = VISIABLE_STATE.visiable;
+        passwordInput.type = 'text';
     }
 });
 
