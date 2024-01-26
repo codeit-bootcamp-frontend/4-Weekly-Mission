88f9a43110aed981ac8bc576ea9d6c99a9dd089d
ayoung-iya <rhaehfdl1879@naver.com>
2024-01-27 00:07:41 +0900

    refactor: 화살표 함수로 변경

--- scripts/index.js
+++ scripts/index.js
@@ -6,24 +6,22 @@ const $passwordInput = document.querySelector("#password");
 const $btnEye = document.querySelector(".ico-eye");
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-function findErrorMessage(node) {
-  return node.parentElement.nextElementSibling;
-}
+const findErrorMessage = (node) => node.parentElement.nextElementSibling;
 
-function removeError($input) {
+const removeError = ($input) => {
   const $msg = findErrorMessage($input);
   $input.classList.remove("error");
   $msg.classList.add("hidden");
-}
+};
 
-function showError($input, errorMessage) {
+const showError = ($input, errorMessage) => {
   const $msg = findErrorMessage($input);
   $msg.textContent = errorMessage;
   $input.classList.add("error");
   $msg.classList.remove("hidden");
-}
+};
 
-function checkEmail() {
+const checkEmail = () => {
   if (emailRegex.test($emailInput.value)) {
     removeError($emailInput);
     return;
@@ -33,25 +31,25 @@ function checkEmail() {
     ? "올바른 이메일 주소가 아닙니다."
     : "이메일을 입력해주세요.";
   showError($emailInput, message);
-}
+};
 
-function checkPassword() {
+const checkPassword = () => {
   if ($passwordInput.value) {
     removeError($passwordInput);
     return;
   }
 
   showError($passwordInput, "비밀번호를 입력해주세요.");
-}
+};
 
-function togglePassoword(e) {
+const togglePassoword = (e) => {
   e.target.classList.toggle("on");
   e.target.previousElementSibling.type = e.target.classList.contains("on")
     ? "text"
     : "password";
-}
+};
 
-function validateEmailAndPassword(e) {
+const validateEmailAndPassword = (e) => {
   if (
     $emailInput.value !== RIGHT_EMAIL ||
     $passwordInput.value !== RIGHT_PASSWORD
@@ -60,7 +58,7 @@ function validateEmailAndPassword(e) {
     showError($emailInput, "이메일을 확인해주세요.");
     showError($passwordInput, "비밀번호를 확인해주세요.");
   }
-}
+};
 
 $emailInput.addEventListener("focusout", checkEmail);
 $passwordInput.addEventListener("focusout", checkPassword);
