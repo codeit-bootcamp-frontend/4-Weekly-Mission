b91df467d0fddcc159e737cf013474339f4c1131
LeeSeungHoon <ysh73900@gmail.com>
2024-01-26 11:11:20 +0900

    feat: 회원가입 비밀번호 유효성 검사 함수

--- js/signUp.js
+++ js/signUp.js
@@ -18,4 +18,16 @@ const handleEmailUsedValidation = () => {
   }
 };
 
+/* 회원가입 비밀번호 유효성 검사 */
+const handlePasswordFormatValidation = () => {
+  const PASSWORD_FORMAT_MESSAGE =
+    " 비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+  if (!passwordRegex.test(email.value)) {
+    setInvalidStyle(password);
+    passwordError.innerHTML = PASSWORD_FORMAT_MESSAGE;
+  }
+};
+
 email.addEventListener("blur", handleEmailUsedValidation);
+password.addEventListener("blur", handlePasswordFormatValidation);
