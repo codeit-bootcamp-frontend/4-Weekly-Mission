f6a5a25a4fc743f02c0f0fc0435c5edaa77f75f7
Rhajiit <ksd9638@gmail.com>
2024-01-26 21:44:34 +0900

    Fix : isRegistered condition to work well
    Rename input.Js to signFunction. because input is too meaningless
    add semicolon
    inputBlur function add some function can perform additional condition
    meaningless blurSection Check deleted
    meaningless div was deleted. now inputBlur and focus accept message Section.

--- JavaScript/signUp.js
+++ JavaScript/signUp.js
@@ -1,81 +1,63 @@
-import {inputFocus, inputBlur, printError, errorMessage} from './input.js'
-import {accountCheck, formatCheck, passFormatCheck} from './accountData.js'
+import {inputFocus, inputBlur, printError, errorType} from './signFunction.js';
+import {accountCheck, formatCheck, passFormatCheck, passCheckCorrect} from './accountData.js';
 import passToggleReset from './passwordToggle.js';
 
 // 비밀번호 토글 초기화
 passToggleReset();
 
-
-const signupEmailInput = document.querySelector('#signupEmailInput')
-const signupPasswordInput = document.querySelector('#signupPasswordInput')
-const signupPasswordCheckInput = document.querySelector('#signupPasswordCheckInput')
+const signupEmailInput = document.querySelector('#signupEmailInput');
+const signupPasswordInput = document.querySelector('#signupPasswordInput');
+const signupPasswordCheckInput = document.querySelector('#signupPasswordCheckInput');
 const confirmBtn = document.querySelector('#confirmBtn');
 
-const emailErrorSection = signupEmailInput.parentElement.parentElement.lastElementChild
-const passwordErrorSection = signupPasswordInput.parentElement.parentElement.lastElementChild
+
+//에러메시지 출력 장소
+const emailErrorSection = signupEmailInput.parentElement.lastElementChild;
+const passwordErrorSection = signupPasswordInput.parentElement.parentElement.lastElementChild;
+const passCheckErrorSection = signupPasswordCheckInput.parentElement.parentElement.lastElementChild;
 
 
-// 이메일 리스너
-const signupBlur = function () {
-   const isAccountRegistered = accountCheck(signupEmailInput.value)
-   inputBlur(signupEmailInput, errorMessage.email)
-
-   if (isAccountRegistered) {printError(signupEmailInput, emailErrorSection,errorMessage.registeredEmail)}
-}
-
-signupEmailInput.addEventListener('focus' , inputFocus)
-signupEmailInput.addEventListener('blur' , signupBlur)
-
-// 패스워드 리스너
-const passwordBlur = function () {
-   inputBlur(signupPasswordInput, errorMessage.passwordNotValid, passFormatCheck(signupPasswordInput.value))
-   inputBlur(signupPasswordInput, errorMessage.password)
-}
-
-signupPasswordInput.addEventListener('focus' , inputFocus)
-signupPasswordInput.addEventListener('blur' , passwordBlur)
+// 이메일 리스너 - 등록된 이메일 검증 조건 추가
+signupEmailInput.addEventListener('focus' , () => inputFocus(emailErrorSection));
+signupEmailInput.addEventListener('blur' , () => inputBlur(errorType.email, emailErrorSection, !accountCheck(signupEmailInput.value), errorType.registeredEmail));
 
 
-// 패스워드체크 리스너
-const passCheck = () => (signupPasswordInput.value == signupPasswordCheckInput.value)
-const passBlur = function () {
-   inputBlur(signupPasswordCheckInput, errorMessage.mismatchPasswordCheck, passCheck())
-   inputBlur(signupPasswordCheckInput, errorMessage.password)
-}
+// 패스워드 리스너 - 패스워드 형식 검증 조건 추가
+signupPasswordInput.addEventListener('focus' , () => inputFocus(passwordErrorSection));
+signupPasswordInput.addEventListener('blur' , () => inputBlur(errorType.password, passwordErrorSection, passFormatCheck(signupPasswordInput.value), errorType.passwordNotValid));
 
-signupPasswordCheckInput.addEventListener('focus' , inputFocus)
-signupPasswordCheckInput.addEventListener('blur' , passBlur)
+
+// 패스워드체크 리스너 - 패스워드체크 검증 조건 추가
+signupPasswordCheckInput.addEventListener('focus' , () => inputFocus(passCheckErrorSection));
+signupPasswordCheckInput.addEventListener('blur' , () => inputBlur(errorType.password, passCheckErrorSection, passCheckCorrect(signupPasswordInput.value, signupPasswordCheckInput.value), errorType.mismatchPasswordCheck));
 
 
 // 회원가입 함수
-const submitSignupData = function (event) {
+function submitSignupData() {
    event.preventDefault();
-   
-   //입력받은 값
-   const emailValue = signupEmailInput.value
-   const passwordValue = signupPasswordInput.value
-   const passwordCheck = signupPasswordCheckInput.value
 
-   //에러메시지 출력 장소
-   const passCheckErrorSection = signupPasswordCheckInput.parentElement.parentElement.lastElementChild
+   const emailInput = signupEmailInput.value;
+   const passInput = signupPasswordInput.value;
+   const passCheckInput = signupPasswordCheckInput.value;
 
    // 계정정보 유효 확인
-   const isAccountSigned = accountCheck(emailValue)
-   const passCheckCorrect = passwordValue === passwordCheck
+   const isAccountRegistered = accountCheck(emailInput);
+   const passCheckCorrect = passInput === passCheckInput;
 
-   let signupValid = true
+   let signupValid = true;
+
    // 오류가 초기화 됐을 때도 제출할 시 모든 오류가 표시되도록 모든 경우의 수 고려 필요
-   if (!emailValue) {printError(signupEmailInput, emailErrorSection, errorMessage.email); signupValid = false;} // 이메일 없음!
-   if (!formatCheck(emailValue)) {printError(signupEmailInput, emailErrorSection, errorMessage.emailNotValid); signupValid = false;} // 이메일 형식 안맞음!
-   if (!passwordValue) {printError(signupPasswordInput, passwordErrorSection,errorMessage.password); signupValid = false;} // 패스워드 없음!
-   if (!passFormatCheck(passwordValue)) {printError(signupPasswordInput, passwordErrorSection,errorMessage.passwordNotValid); signupValid = false;} // 패스워드 형식이 맞지 않음!!
-   if (!passwordCheck) {printError(signupPasswordCheckInput, passCheckErrorSection,errorMessage.password); signupValid = false;} // 패스워드 체크 없음!
-   if (!passCheckCorrect) {printError(signupPasswordCheckInput, passCheckErrorSection, errorMessage.mismatchPasswordCheck); signupValid = false;} // 패스워드 체크가 다름!
-   if (isAccountSigned) {printError(signupEmailInput, emailErrorSection, errorMessage.registeredEmail); signupValid = false;} // 이미 사용중인 이메일!   
+   if (!formatCheck(emailInput)) {printError(signupEmailInput, emailErrorSection, errorType.emailNotValid); signupValid = false;}; // 이메일 형식 안맞음!
+   if (!passFormatCheck(passInput)) {printError(signupPasswordInput, passwordErrorSection,errorType.passwordNotValid); signupValid = false;}; // 패스워드 형식이 맞지 않음!!
+   if (!passCheckCorrect) {printError(signupPasswordCheckInput, passCheckErrorSection, errorType.mismatchPasswordCheck); signupValid = false;}; // 패스워드 체크가 다름!
+   if (isAccountRegistered) {printError(signupEmailInput, emailErrorSection, errorType.registeredEmail); signupValid = false;}; // 이미 사용중인 이메일!   
+   if (!emailInput) {printError(signupEmailInput, emailErrorSection, errorType.email); signupValid = false;}; // 이메일 없음!
+   if (!passInput) {printError(signupPasswordInput, passwordErrorSection,errorType.password); signupValid = false;}; // 패스워드 없음!
+   if (!passCheckInput) {printError(signupPasswordCheckInput, passCheckErrorSection,errorType.password); signupValid = false;}; // 패스워드 체크 없음!
 
    if (signupValid) {
-      window.location.href = './folder.html'
+      window.location.href = './folder.html';
    } 
 }
 
-confirmBtn.addEventListener('click' , submitSignupData)
+confirmBtn.addEventListener('click' , () => submitSignupData());
--- JavaScript/signin.js
+++ JavaScript/signin.js
@@ -1,5 +1,5 @@
-import {inputFocus, inputBlur, printError, errorMessage} from './input.js'
-import {loginCheck} from './accountData.js'
+import {inputFocus, inputBlur, printError, errorType} from './signFunction.js'
+import {loginCheck} from './accountData.js';
 import passToggleReset from './passwordToggle.js';
 
 // 비밀번호 토글 초기화 및 함수 등록
@@ -9,15 +9,19 @@ const loginEmailInput = document.querySelector('#loginEmailInput');
 const loginPasswordInput = document.querySelector('#loginPasswordInput');
 const confirmBtn = document.querySelector('#confirmBtn');
 
+// 에러 메시지 출력 장소
+const emailErrorSection = loginEmailInput.parentElement.lastElementChild;
+const passwordErrorSection = loginPasswordInput.parentElement.parentElement.lastElementChild;
+
 
 // 이메일 리스너
-loginEmailInput.addEventListener('focus' , inputFocus)
-loginEmailInput.addEventListener('blur' , () => inputBlur(loginEmailInput, errorMessage.email))
+loginEmailInput.addEventListener('focus' , () => inputFocus(emailErrorSection));
+loginEmailInput.addEventListener('blur' , () => inputBlur(errorType.email, emailErrorSection));
 
 
 // 패스워드 리스너
-loginPasswordInput.addEventListener('focus' , inputFocus)
-loginPasswordInput.addEventListener('blur' , () => inputBlur(loginPasswordInput, errorMessage.password))
+loginPasswordInput.addEventListener('focus' , () => inputFocus(passwordErrorSection));
+loginPasswordInput.addEventListener('blur' , () => inputBlur(errorType.password, passwordErrorSection));
 
 
 // 로그인 정보 제출
@@ -25,22 +29,18 @@ const submitLoginData = function (event)  {
    event.preventDefault();
    
    //입력받은 값
-   const emailValue = loginEmailInput.value
-   const passwordValue = loginPasswordInput.value
+   const emailValue = loginEmailInput.value;
+   const passwordValue = loginPasswordInput.value;
    
    // 계정정보 유효 확인
-   const isAccountSigned = loginCheck(emailValue, passwordValue)
-
-   //에러메시지 출력 장소
-   const emailErrorSection = loginEmailInput.parentElement.parentElement.lastElementChild
-   const passwordErrorSection = loginPasswordInput.parentElement.parentElement.lastElementChild
+   const isAccountSigned = loginCheck(emailValue, passwordValue);
 
    if (!isAccountSigned) {
-      printError(loginEmailInput, emailErrorSection, errorMessage.mismatchEmail)
-      printError(loginPasswordInput, passwordErrorSection, errorMessage.mismatchPassword)
+      printError(loginEmailInput, emailErrorSection, errorType.mismatchEmail);
+      printError(loginPasswordInput, passwordErrorSection, errorType.mismatchPassword);
    } else {
-      window.location.href = './folder.html'
+      window.location.href = './folder.html';
    }
 }
 
-confirmBtn.addEventListener('click' , submitLoginData)
+confirmBtn.addEventListener('click' , submitLoginData);
