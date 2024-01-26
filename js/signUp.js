ffd7fb8d19d3b6e3d18e5468bdf7ce94083002b6
LeeSeungHoon <ysh73900@gmail.com>
2024-01-26 12:55:03 +0900

    feat: 에러메시지 모듈화, fix: 유효한 회원가입 시도 시 페이지 이동 함수

--- js/signUp.js
+++ js/signUp.js
@@ -5,6 +5,7 @@ import {
   passwordError,
   setInvalidStyle,
   setValidStyle,
+  errorMessage,
   handleEmailValidation,
 } from "./modules/AuthUtils.js";
 
@@ -14,10 +15,9 @@ const signUpButton = document.querySelector(".signup-button");
 
 /* 회원가입 이메일 유효성 검사 */
 const handleEmailUsedValidation = () => {
-  const EMAIL_USED_MESSAGE = "이미 사용 중인 이메일입니다.";
   if (email.value === "test@codeit.com") {
     setInvalidStyle(email);
-    emailError.innerHTML = EMAIL_USED_MESSAGE;
+    emailError.innerHTML = errorMessage.EMAIL_USED_MESSAGE;
   } else {
     handleEmailValidation();
   }
@@ -25,12 +25,10 @@ const handleEmailUsedValidation = () => {
 
 /* 회원가입 비밀번호 유효성 검사 */
 const handlePasswordFormatValidation = () => {
-  const PASSWORD_FORMAT_MESSAGE =
-    " 비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
   const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
   if (!passwordRegex.test(password.value)) {
     setInvalidStyle(password);
-    passwordError.innerHTML = PASSWORD_FORMAT_MESSAGE;
+    passwordError.innerHTML = errorMessage.PASSWORD_FORMAT_MESSAGE;
   } else {
     setValidStyle(password);
     passwordError.innerHTML = "";
@@ -39,11 +37,9 @@ const handlePasswordFormatValidation = () => {
 
 /* 비밀번호 일치 유효성 검사 */
 const handlePasswordMatchValidation = () => {
-  const PASSWORD_MATCH_MESSAGE = "비밀번호가 일치하지 않아요.";
-
   if (passwordRepeat.value !== password.value) {
     setInvalidStyle(passwordRepeat);
-    passwordRepeatError.innerHTML = PASSWORD_MATCH_MESSAGE;
+    passwordRepeatError.innerHTML = errorMessage.PASSWORD_MATCH_MESSAGE;
   } else {
     setValidStyle(passwordRepeat);
     passwordRepeatError.innerHTML = "";
@@ -53,24 +49,18 @@ const handlePasswordMatchValidation = () => {
 /* 유효한 회원가입 시도 시 페이지 이동 */
 const redirectToFolderPage = () => {
   const link = "./folder.html";
-  const EMAIL_CHECK_MESSAGE = "이메일을 입력해 주세요.";
-  const PASSWORD_ERROR_MESSAGE = "비밀번호를 입력해주세요.";
 
   if (email.value === "test@codeit.com") {
-    handleEmailUsedValidation();
+    setInvalidStyle(email);
+    emailError.innerHTML = errorMessage.EMAIL_USED_MESSAGE;
+  } else if (email.value === "") {
+    setInvalidStyle(email);
+    emailError.innerHTML = errorMessage.EMAIL_INPUT_MESSAGE;
+  } else if (password.value === "") {
+    setInvalidStyle(password);
+    passwordError.innerHTML = errorMessage.PASSWORD_FORMAT_MESSAGE;
   } else if (passwordRepeat.value !== password.value) {
     handlePasswordMatchValidation();
-  } else if (
-    email.value === "" ||
-    password.value === "" ||
-    passwordRepeat.word === ""
-  ) {
-    setInvalidStyle(email);
-    setInvalidStyle(password);
-    setInvalidStyle(passwordRepeat);
-    emailError.innerHTML = EMAIL_CHECK_MESSAGE;
-    passwordError.innerHTML = PASSWORD_ERROR_MESSAGE;
-    passwordRepeatError.innerHTML = PASSWORD_ERROR_MESSAGE;
   } else {
     location.href = link;
   }
