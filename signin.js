464abeca19afa85f21d75ea707ecd509a8628c2a
suzinxix <suzinxix@gmail.com>
2024-01-26 10:28:36 +0900

    style: clean up code

--- signin.js
+++ signin.js
@@ -4,6 +4,6 @@ import { showErrorMsgSignin as showErrorMsg } from "./functions/showErrorMsg.js"
 import { moveFolderSignin as moveFolderPage } from "./functions/moveFolderPage.js";
 
 form.addEventListener("focusout", showErrorMsg);
+form.addEventListener("click", showPassword);
 btn.addEventListener("click", moveFolderPage);
 form.addEventListener("keydown", ({ key }) => key === "Enter" && moveFolderPage());
-form.addEventListener("click", showPassword);
--- signup.js
+++ signup.js
@@ -1,9 +1,9 @@
 import { form, btn } from "./elements.js";
+import { showPassword } from "./functions/showPassword.js";
 import { showErrorMsgSignup as showErrorMsg } from "./functions/showErrorMsg.js";
 import { moveFolderSignup as moveFolderPage } from "./functions/moveFolderPage.js";
-import { showPassword } from "./functions/showPassword.js";
 
 form.addEventListener("focusout", showErrorMsg);
+form.addEventListener("click", showPassword);
 btn.addEventListener("click", moveFolderPage);
 form.addEventListener("keydown", ({ key }) => key === "Enter" && moveFolderPage());
-form.addEventListener("click", showPassword);
