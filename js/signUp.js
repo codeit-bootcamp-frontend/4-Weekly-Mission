a3d450b391fa8749a144eab1d5c3f5eca4805a1f
LeeSeungHoon <ysh73900@gmail.com>
2024-01-26 11:38:06 +0900

    feat: 비밀번호 일치 유효성 검사 함수

--- js/signUp.js
+++ js/signUp.js
@@ -4,8 +4,12 @@ import {
   setInvalidStyle,
   setValidStyle,
   handleEmailValidation,
+  password,
   passwordError,
 } from "./modules/AuthUtils.js";
+
+const passwordRepeat = document.querySelector("#password-repeat");
+const passwordRepeatError = document.querySelector(".passwordRepeat-error");
 
 /* 회원가입 이메일 유효성 검사 */
 const handleEmailUsedValidation = () => {
@@ -23,11 +27,26 @@ const handlePasswordFormatValidation = () => {
   const PASSWORD_FORMAT_MESSAGE =
     " 비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
   const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
-  if (!passwordRegex.test(email.value)) {
+  if (!passwordRegex.test(password.value)) {
     setInvalidStyle(password);
     passwordError.innerHTML = PASSWORD_FORMAT_MESSAGE;
+  } else {
+    setValidStyle(password);
+    passwordError.innerHTML = "";
+  }
+};
+
+/* 비밀번호 일치 유효성 검사 */
+const handlePasswordMatchValidation = () => {
+  const PASSWORD_MATCH_MESSAGE = "비밀번호가 일치하지 않아요.";
+
+  if (passwordRepeat.value !== password.value) {
+    passwordRepeatError.innerHTML = PASSWORD_MATCH_MESSAGE;
+  } else {
+    passwordRepeatError.innerHTML = "";
   }
 };
 
 email.addEventListener("blur", handleEmailUsedValidation);
 password.addEventListener("blur", handlePasswordFormatValidation);
+passwordRepeat.addEventListener("blur", handlePasswordMatchValidation);
