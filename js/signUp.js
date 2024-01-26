8215930358ff6405b2a249b2dffd11eada29dbe1
LeeSeungHoon <ysh73900@gmail.com>
2024-01-26 12:12:22 +0900

    feat: 유효한 회원가입 시도 시 페이지 이동 함수

--- js/signUp.js
+++ js/signUp.js
@@ -1,15 +1,16 @@
 import {
   email,
   emailError,
+  password,
+  passwordError,
   setInvalidStyle,
   setValidStyle,
   handleEmailValidation,
-  password,
-  passwordError,
 } from "./modules/AuthUtils.js";
 
 const passwordRepeat = document.querySelector("#password-repeat");
 const passwordRepeatError = document.querySelector(".passwordRepeat-error");
+const signUpButton = document.querySelector(".signup-button");
 
 /* 회원가입 이메일 유효성 검사 */
 const handleEmailUsedValidation = () => {
@@ -41,12 +42,40 @@ const handlePasswordMatchValidation = () => {
   const PASSWORD_MATCH_MESSAGE = "비밀번호가 일치하지 않아요.";
 
   if (passwordRepeat.value !== password.value) {
+    setInvalidStyle(passwordRepeat);
     passwordRepeatError.innerHTML = PASSWORD_MATCH_MESSAGE;
   } else {
+    setValidStyle(passwordRepeat);
     passwordRepeatError.innerHTML = "";
+  }
+};
+
+const redirectToFolderPage = () => {
+  const link = "./folder.html";
+  const EMAIL_CHECK_MESSAGE = "이메일을 입력해 주세요.";
+  const PASSWORD_ERROR_MESSAGE = "비밀번호를 입력해주세요.";
+
+  if (email.value === "test@codeit.com") {
+    handleEmailUsedValidation();
+  } else if (passwordRepeat.value !== password.value) {
+    handlePasswordMatchValidation();
+  } else if (
+    email.value === "" ||
+    password.value === "" ||
+    passwordRepeat.word === ""
+  ) {
+    setInvalidStyle(email);
+    setInvalidStyle(password);
+    setInvalidStyle(passwordRepeat);
+    emailError.innerHTML = EMAIL_CHECK_MESSAGE;
+    passwordError.innerHTML = PASSWORD_ERROR_MESSAGE;
+    passwordRepeatError.innerHTML = PASSWORD_ERROR_MESSAGE;
+  } else {
+    location.href = link;
   }
 };
 
 email.addEventListener("blur", handleEmailUsedValidation);
 password.addEventListener("blur", handlePasswordFormatValidation);
 passwordRepeat.addEventListener("blur", handlePasswordMatchValidation);
+signUpButton.addEventListener("click", redirectToFolderPage);
