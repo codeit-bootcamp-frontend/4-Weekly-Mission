26347c349f2bdb564c2d5b0052bf6a287040821d
LeeSeungHoon <ysh73900@gmail.com>
2024-01-26 11:06:39 +0900

    feat: 회원가입 이메일 유효성 검사 함수

--- js/signUp.js
+++ js/signUp.js
@@ -1,3 +1,21 @@
-import * as AuthUtils from "./modules/AuthUtils.js";
+import {
+  email,
+  emailError,
+  setInvalidStyle,
+  setValidStyle,
+  handleEmailValidation,
+  passwordError,
+} from "./modules/AuthUtils.js";
 
-email.addEventListener("blur", AuthUtils.handleEmailValidation);
+/* 회원가입 이메일 유효성 검사 */
+const handleEmailUsedValidation = () => {
+  const EMAIL_USED_MESSAGE = "이미 사용 중인 이메일입니다.";
+  if (email.value === "test@codeit.com") {
+    setInvalidStyle(email);
+    emailError.innerHTML = EMAIL_USED_MESSAGE;
+  } else {
+    handleEmailValidation();
+  }
+};
+
+email.addEventListener("blur", handleEmailUsedValidation);
