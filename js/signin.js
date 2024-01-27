1d326347aee22126b93faed96fe1b42163363afa
armd482 <armd482@naver.com>
2024-01-27 09:57:42 +0900

    Change signup.js

--- js/signin.js
+++ js/signin.js
@@ -5,31 +5,29 @@ import {
   checkPassword,
   visiblePassword,
   InputFocusEvent,
-} from "/js/login.js";
+} from '/js/login.js';
 
-const emailInput = document.querySelector("#email-input");
-const passwordInput = document.querySelector("#password-input");
-
-const emailWarningText = document.querySelector("#email-warning-text");
-const passwordWarningText = document.querySelector("#password-warning-text");
-
-const loginBtn = document.querySelector("#login-button");
-const eyeBtn = document.querySelector("#password-eye-button");
+const emailInput = document.querySelector('#email-input');
+const passwordInput = document.querySelector('#password-input');
+const emailWarningText = document.querySelector('#email-warning-text');
+const passwordWarningText = document.querySelector('#password-warning-text');
+const loginBtn = document.querySelector('#login-button');
+const eyeBtn = document.querySelector('#password-eye-button');
 
 const loginBtnOnclick = () => {
   if (
-    emailInput.value === "test@codeit.com" &&
-    passwordInput.value === "codeit101"
+    emailInput.value === 'test@codeit.com' &&
+    passwordInput.value === 'codeit101'
   ) {
-    window.location.href = "/page/folder";
+    window.location.href = '/page/folder';
   } else {
-    viewWarningText(emailWarningText, "이메일을 확인해 주세요.");
-    viewWarningText(passwordWarningText, "비밀번호를 확인해 주세요.");
+    viewWarningText(emailWarningText, '이메일을 확인해 주세요.');
+    viewWarningText(passwordWarningText, '비밀번호를 확인해 주세요.');
   }
 };
 
-const loginBtnEnter = (e) => {
-  if (e.key === "Enter") {
+const loginBtnEnter = e => {
+  if (e.key === 'Enter') {
     loginBtnOnclick();
     e.target.blur();
   }
@@ -39,11 +37,11 @@ const emailInputFocustIn = () => {
   hiddenWarningText(emailWarningText);
 };
 
-const emailInputFocustOut = (e) => {
+const emailInputFocustOut = e => {
   const status = checkEmail(e.target.value);
-  if (status === 0) viewWarningText(emailWarningText, "이메일을 입력해주세요.");
+  if (status === 0) viewWarningText(emailWarningText, '이메일을 입력해주세요.');
   else if (status === 1)
-    viewWarningText(emailWarningText, "올바른 이메일 주소가 아닙니다.");
+    viewWarningText(emailWarningText, '올바른 이메일 주소가 아닙니다.');
   else if (status === 2) hiddenWarningText(emailWarningText);
 };
 
@@ -51,10 +49,10 @@ const passwordInputFocustIn = () => {
   hiddenWarningText(passwordWarningText);
 };
 
-const passwordInputFocusOut = (e) => {
+const passwordInputFocusOut = e => {
   const status = checkPassword(e.target.value);
   if (status === 0)
-    viewWarningText(passwordWarningText, "비밀번호를  입력해주세요");
+    viewWarningText(passwordWarningText, '비밀번호를  입력해주세요');
   else if (status === 2) hiddenWarningText(passwordWarningText);
 };
 
@@ -64,6 +62,6 @@ const eyeBtnOnclick = () => {
 
 InputFocusEvent(emailInput, emailInputFocustIn, emailInputFocustOut);
 InputFocusEvent(passwordInput, passwordInputFocustIn, passwordInputFocusOut);
-loginBtn.addEventListener("click", loginBtnOnclick);
-document.addEventListener("keyup", loginBtnEnter);
-eyeBtn.addEventListener("click", eyeBtnOnclick);
+loginBtn.addEventListener('click', loginBtnOnclick);
+document.addEventListener('keyup', loginBtnEnter);
+eyeBtn.addEventListener('click', eyeBtnOnclick);
