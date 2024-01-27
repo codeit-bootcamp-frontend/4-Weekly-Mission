4872192a91495d4004fa97ac8882bd34d0db985e
HarryK <yjkang1223@naver.com>
2024-01-27 16:22:27 +0900

    feature: Show error message when password doesn't fit designated form

--- javascript/constants.js
+++ javascript/constants.js
@@ -3,4 +3,9 @@
  */
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-export { emailRegex }
+/**
+ * 비밀번호 정규 상수
+ */
+const passwordRegex = /(?=.*\d)(?=.*[a-z]).{8,}/
+
+export { emailRegex, passwordRegex }
--- javascript/signup.js
+++ javascript/signup.js
@@ -1,7 +1,10 @@
 const emailInput = document.querySelector('#signup-email')
 const errorEmail = document.querySelector('#errorEmail')
+const passwordInput = document.querySelector('#signup-password')
+const errorPassword = document.querySelector('#errorPassword')
 
 import { isEmailValid, isInputEmpty, showError, clearError } from './sign-error.js'
+import { passwordRegex } from './constants.js'
 
 /////////////////////////////////////
 
@@ -13,6 +16,17 @@ import { isEmailValid, isInputEmpty, showError, clearError } from './sign-error.
 const isEmailUsed = (input) => {
     const codeit = { email: 'test@codeit.com' }
     return input === codeit.email
+}
+
+/**
+ * 비밀번호 유효성 확인
+ * 8자 이상이며 문자와 숫자의 혼용 여부 확인
+ * @param {*} input 확인할 비밀번호
+ * @returns 위 사항을 동시에 만족하는지에 대한 불린값
+ */
+const isPasswordValid = (input) => {
+    const minPasswordLength = 8
+    return passwordRegex.test(input) && input.length >= minPasswordLength
 }
 
 /**이메일 확인 함수
@@ -36,14 +50,35 @@ const checkEmail = () => {
     }
 }
 
+/**
+ * 비번 확인 함수
+ * 빈 input 시 error 메세지 출력
+ */
+const checkPassword = () => {
+    const password = passwordInput.value.trim()
+    if (!isPasswordValid(password)) {
+        showError(
+            passwordInput,
+            errorPassword,
+            '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'
+        )
+    }
+}
+
 /////////핸들러 함수///////
 
 const handleEmailFocusout = () => {
     clearError(emailInput, errorEmail)
     checkEmail()
 }
+const handlePasswordFocusout = () => {
+    clearError(passwordInput, errorPassword)
+    checkPassword()
+}
 
 //////////////// 함수 사용////////////////////
 
 // 이메일 이벤트 리스너 부여
 emailInput.addEventListener('focusout', handleEmailFocusout)
+// 비번 이벤트 리스너 부여
+passwordInput.addEventListener('focusout', handlePasswordFocusout)
