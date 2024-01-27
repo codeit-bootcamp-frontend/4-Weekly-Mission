dbdf8efb971c292223ed0552fd46a339736cab3f
HarryK <yjkang1223@naver.com>
2024-01-27 15:23:50 +0900

    feature: Show error message when using used email address

--- javascript/signup.js
+++ javascript/signup.js
@@ -5,8 +5,18 @@ import { isEmailValid, isInputEmpty, showError, clearError } from './sign-error.
 
 /////////////////////////////////////
 
+/**
+ * 이메일 중복 확인 함수
+ * @param {*} input 확인할 이메일
+ * @returns 중복 여부 불린값
+ */
+const isEmailUsed = (input) => {
+    const codeit = { email: 'test@codeit.com' }
+    return input === codeit.email
+}
+
 /**이메일 확인 함수
- * 이메일 형식과 빈 input 확인 후 error 메세지 출력
+ * 이메일 확인 후 error 메세지 출력
  */
 const checkEmail = () => {
     const email = emailInput.value.trim()
@@ -18,6 +28,11 @@ const checkEmail = () => {
     // 이메일 형식 확인
     if (!isEmailValid(email)) {
         showError(emailInput, errorEmail, '올바른 이메일 주소가 아닙니다.')
+        return
+    }
+    // 이메일 중복 확인
+    if (isEmailUsed(email)) {
+        showError(emailInput, errorEmail, '이미 사용 중인 이메일입니다.')
     }
 }
 
