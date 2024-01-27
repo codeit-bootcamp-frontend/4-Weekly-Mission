475c605bdf664c6ca2822715f855973dfa25fcd9
박지윤 <tkrhkfld123@naver.com>
2024-01-28 02:20:08 +0900

    ♻️ refactor: 전역변수 모듈 패턴으로 분류

---
+++ src/signGlobals.js
@@ -0,0 +1,31 @@
+import * as DOM from './lib/DOM.js'
+
+export const signGlobals = (function() {
+  const signEmailSection = DOM.selectElement('.sign-email');
+  const signPasswordSection = DOM.selectElement('.sign-password');
+  const signPasswordCheckSecion = DOM.selectElement('#sign-password-check');
+  const signEmailInput = DOM.selectElement('#email-input');
+  const signPasswordInput = DOM.selectElement('#password-input');
+  DOM.createTagAndclassWithText(signEmailSection, 'email-error', '');
+  DOM.createTagAndclassWithText(signPasswordSection, 'password-error', '');
+  const emailError = signEmailSection.lastChild;
+  const passwordError = signPasswordSection.lastChild;
+  const eyeImg = DOM.selectElement('.eye-button-icon');
+  const loginButton = DOM.selectElement('.login-button');
+  const signUpButton = DOM.selectElement('.signup-button');
+  const currentPageURL = window.location.href;
+
+  return {
+    signEmailSection,
+    signPasswordSection,
+    signPasswordCheckSecion,
+    signEmailInput,
+    signPasswordInput,
+    emailError,
+    passwordError,
+    eyeImg,
+    loginButton,
+    signUpButton,
+    currentPageURL
+  };
+})();
