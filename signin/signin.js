f984aa07d32932d9895147566b4231398e86d686
JeongSabibi <jsbb528@naver.com>
2024-01-27 17:23:40 +0900

    refactor: 공통된 로직 분리

--- signin/signin.js
+++ signin/signin.js
@@ -1,4 +1,6 @@
-const form = document.querySelector('#form');
+import { isEmail } from '../src/common/sign.js';
+
+const form = document.querySelector('form');
 
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
@@ -10,50 +12,21 @@ const eyeIcon = document.querySelector('.icon');
 const eyeIconOn = document.querySelector('.eye-icon-on');
 const eyeIconOff = document.querySelector('.eye-icon-off');
 
-function onSubmit(e) {
+const TEST_EMAIL = 'test@codeit.com';
+const TEST_PASSWORD = 'codeit101';
+
+const onSubmit = (e) => {
   e.preventDefault();
 
-  if (email.value === 'test@codeit.com' && password.value === 'codeit101') {
+  if (email.value === TEST_EMAIL && password.value === TEST_PASSWORD) {
     location.href = '/folder/folder.html';
   } else {
     emailError.textContent = '이메일을 확인해 주세요.';
     passwordError.textContent = '비밀번호를 확인해 주세요.';
   }
-}
+};
 
-function isEmail(asValue) {
-  let regExp =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-
-  return regExp.test(asValue);
-}
-
-email.addEventListener('focusout', function (e) {
-  if (e.target.value.length <= 0) {
-    emailError.textContent = '이메일을 입력해 주세요.';
-    e.target.classList.add('inputError');
-  } else {
-    if (!isEmail(email.value)) {
-      emailError.textContent = '올바른 이메일 주소가 아닙니다.';
-      e.target.classList.add('inputError');
-    } else {
-      emailError.textContent = '';
-      e.target.classList.remove('inputError');
-    }
-  }
-});
-
-password.addEventListener('focusout', function (e) {
-  if (e.target.value.length <= 0) {
-    passwordError.textContent = '비밀번호를 입력해 주세요.';
-    e.target.classList.add('inputError');
-  } else {
-    passwordError.textContent = '';
-    e.target.classList.remove('inputError');
-  }
-});
-
-function togglePasswordVisibility() {
+const togglePasswordVisibility = () => {
   if (password.type === 'password') {
     password.type = 'text';
     eyeIconOff.style.display = 'none';
@@ -63,12 +36,41 @@ function togglePasswordVisibility() {
     eyeIconOff.style.display = 'inline-block';
     eyeIconOn.style.display = 'none';
   }
-}
+};
 
-form.addEventListener('submit', onSubmit);
+const emailInputCheck = (e) => {
+  if (e.target.value.length <= 0) {
+    emailError.textContent = '이메일을 입력해 주세요.';
+    e.target.classList.add('inputError');
+  } else if (!isEmail(email.value)) {
+    emailError.textContent = '올바른 이메일 주소가 아닙니다.';
+    e.target.classList.add('inputError');
+  } else {
+    emailError.textContent = '';
+    e.target.classList.remove('inputError');
+  }
+};
 
-eyeIcon.addEventListener('click', function (e) {
+const passwordInputCheck = (e) => {
+  if (e.target.value.length <= 0) {
+    passwordError.textContent = '비밀번호를 입력해 주세요.';
+    e.target.classList.add('inputError');
+  } else {
+    passwordError.textContent = '';
+    e.target.classList.remove('inputError');
+  }
+};
+
+const eyeIconClick = (e) => {
   if (e.target.classList.contains('eye-icon')) {
     togglePasswordVisibility();
   }
-});
+};
+
+form.addEventListener('submit', onSubmit);
+
+email.addEventListener('focusout', emailInputCheck);
+
+password.addEventListener('focusout', passwordInputCheck);
+
+eyeIcon.addEventListener('click', eyeIconClick);
