063e6994ce9e5265e5674172163eba5d3192d30a
hongjaehoun <ehswkfn123@naver.com>
2024-01-27 23:51:19 +0900

    5주차 기본 요구사항 완료

--- linkbrary/script/sign_up.js
+++ linkbrary/script/sign_up.js
@@ -50,13 +50,30 @@ email_input.addEventListener('focus', () => {remove_error_msg(email_error_msg)})
 email_input.addEventListener("blur", (e) => {checkInputNull(e, email_error_msg, "이메일을")})
 email_input.addEventListener("blur", (e) => {validateEmailInput(e, email_regExp)})
 
+// 비밀번호 유효성 확인 
+
+function validatePasswordInput(e, regExp, error_msg) {
+    if(regExp.test(e.target.value) == false && e.target.value !== "") {
+        error_msg.innerHTML = "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요"
+        error_msg.classList.add('view_error')
+        e.target.classList.add('input_error')
+    }
+}
 
 const pw_input = document.querySelector('.pw_input')
 const password_error_msg = document.querySelector('.password_error_msg')
+
+// 비밀번호 정규식 8자 이상 하나 이상의 문자 , 하나 이상의 숫자 포함
+const password_reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
 pw_input.addEventListener('focus', activeInput)
 pw_input.addEventListener('blur', blurInput)
 pw_input.addEventListener('focus', () => {remove_error_msg(password_error_msg)})
 pw_input.addEventListener("blur", (e) => {checkInputNull(e, password_error_msg, "비밀번호를")})
+pw_input.addEventListener('blur', (e) => {validatePasswordInput(e, password_reg, password_error_msg)})
+
+
+
 
 // 비밀번호 확인 함수
 
@@ -81,6 +98,7 @@ repeat_pw_input.addEventListener('blur', blurInput)
 repeat_pw_input.addEventListener('focus', () => {remove_error_msg(repeat_password_error_msg)})
 const signUpBtn = document.querySelector(".sign_up_btn")
 
+
 // 눈모양 아이콘
 
 function showPasswordInput(e, open, input) {
@@ -103,3 +121,40 @@ pw_icon_close.addEventListener('click', (e) => {showPasswordInput(e, pw_icon_ope
 pw_icon_open.addEventListener('click', (e) => {coverPasswordInput(e, pw_icon_close, pw_input)})
 pw_repeat_icon_close.addEventListener('click', (e) => {showPasswordInput(e, pw_repeat_icon_open, repeat_pw_input)})
 pw_repeat_icon_open.addEventListener('click', (e) => {coverPasswordInput(e, pw_repeat_icon_close, repeat_pw_input)})
+
+
+function checkDuplicate(e, error_msg) {
+    if(e.target.value == "test@codeit.com") {
+        console.log(e.target.value)
+        e.target.classList.add("input_error")
+        error_msg.classList.add('view_error')
+        error_msg.innerHTML = "이미 사용 중인 이메일입니다"
+    }
+}
+
+email_input.addEventListener('blur', (e) => {checkDuplicate(e, email_error_msg)})
+
+
+// 회원가입 실행 함수
+
+
+function checkEmailPwSignUp(e, email_input, pw_input, repeat_pw_input) {
+    e.preventDefault()
+
+    let arr = [email_input.value, pw_input.value, repeat_pw_input.value]
+    
+    console.log(arr.includes(""))
+    if(email_input.classList.contains("input_error") == false &&
+       pw_input.classList.contains("input_error") == false &&
+       repeat_pw_input.classList.contains("input_error") == false &&
+       arr.includes("") == false &&
+       arr[1] === arr[2]   
+    ) {
+        document.location.href = "/pages/folder.html"
+    }
+}
+
+
+const sign_up_btn = document.querySelector('.sign_up_btn')
+sign_up_btn.addEventListener('click' , (e) => {
+    checkEmailPwSignUp(e, email_input, pw_input, repeat_pw_input)})
