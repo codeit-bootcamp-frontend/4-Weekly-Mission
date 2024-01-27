795f4a4839922cd82de76f43eea631eb9f28257b
codingaring <sksms2104@naver.com>
2024-01-27 10:24:25 +0900

    [Docs] 주석 수정

--- js/functions/passwordTypeCheck.js
+++ js/functions/passwordTypeCheck.js
@@ -1,3 +1,4 @@
+/** 현재 속성이 password인지 확인하는 함수 */
 const passwordTypeCheck = function (passwordInput) {
   return passwordInput.getAttribute("type") === "password";
 };
--- js/signin.js
+++ js/signin.js
@@ -11,19 +11,14 @@ const signinPasswordInput = querySelector("#signin-password-input");
 // 로그인 버튼 선택
 const loginBtn = querySelector("#login-button");
 
-// 눈 모양 아이콘 선택
-const passwordEyeIcon = querySelector("#password-eye-icon");
-
 // 로그인 폼 선택
 const loginForm = querySelector("#login-form");
 
 function checkEmail(event) {
-  // errorMessage가 출력될 태그, input 태그 선택
   const emailValue = event.target.value.trim();
   const errorMessage = querySelector("#email-error-message");
 
   if (emailValue === "") {
-    // 공백일 경우
     updateErrorMessage(
       errorMessage,
       "이메일을 입력해주세요.",
@@ -31,10 +26,8 @@ function checkEmail(event) {
       true
     );
   } else if (isVaildEmail(emailValue)) {
-    // 유효한 경우, .error-box 클래스 제거
     updateErrorMessage(errorMessage, "", emailInputBox, false);
   } else {
-    // 유효하지 않은 경우
     updateErrorMessage(
       errorMessage,
       "올바른 이메일 주소가 아닙니다.",
@@ -45,10 +38,9 @@ function checkEmail(event) {
 }
 
 function checkPassword(event) {
-  // input에 입력된 값, errorMessage가 출력될 태그, input 태그 선택
   const passwordValue = event.target.value;
   const errorMessageBox = querySelector("#password-error-message");
-  // 공백일 경우
+
   if (passwordValue === "") {
     updateErrorMessage(
       errorMessageBox,
@@ -61,21 +53,19 @@ function checkPassword(event) {
   }
 }
 
-// 로그인 시도
+/** 로그인 시도 */
 function tryLogin() {
   const emailValue = emailInputBox.value;
   const passwordValue = signinPasswordInput.value;
   const errorMessage = querySelector("#email-error-message");
   const errorMessageBox = querySelector("#password-error-message");
 
-  // 입력값이 모두 맞는 경우
   if (emailValue === "test@codeit.com" && passwordValue === "codeit101") {
     location.href = "folder.html";
   } else if (
     emailValue !== "test@codeit.com" ||
     passwordValue !== "codeit101"
   ) {
-    // 아이디가 틀리거나, 비밀번호가 틀리는 경우
     updateErrorMessage(
       errorMessage,
       "이메일을 확인해주세요.",
@@ -91,31 +81,25 @@ function tryLogin() {
   }
 }
 
-// 엔터키로 로그인 시도
+/** enter키로 로그인 */
 function loginByEnter(event) {
-  // event.key 값이 "Enter"일 때만 tryLogin 함수 호출
   if (event.key === "Enter") {
     tryLogin();
   }
 }
 
-/** [심화] 눈 모양 아이콘 클릭시, 비밀번호 타입 변경하기 **/
+/** 비밀번호 보기 & 숨기기 **/
 function passwordShowHidden() {
+  const passwordEyeIcon = querySelector("#password-eye-icon"); // 눈 모양 아이콘 선택
+
   passwordTypeCheck(signinPasswordInput)
     ? typeToText(passwordEyeIcon, signinPasswordInput)
     : typeToPassword(passwordEyeIcon, signinPasswordInput);
 }
 
 /** 이벤트 등록 */
-// 이메일, 비밀번호 올바른지 확인
-emailInputBox.addEventListener("focusout", checkEmail);
-signinPasswordInput.addEventListener("focusout", checkPassword);
-
-// 로그인하거나, "확인해주세요" 띄우기
-loginBtn.addEventListener("click", tryLogin);
-
-// 엔터키로 로그인하기
-loginForm.addEventListener("keydown", loginByEnter);
-
-// 눈 모양 아이콘 클릭시
-passwordEyeIcon.addEventListener("click", passwordShowHidden);
+emailInputBox.addEventListener("focusout", checkEmail); // 이메일 입력이 유효한지
+signinPasswordInput.addEventListener("focusout", checkPassword); //비밀번호를 입력했는지
+loginBtn.addEventListener("click", tryLogin); // 로그인 시도
+loginForm.addEventListener("keydown", loginByEnter); // 엔터키로 로그인하기
+passwordEyeIcon.addEventListener("click", passwordShowHidden); // 비밀번호 보기 & 숨기기
