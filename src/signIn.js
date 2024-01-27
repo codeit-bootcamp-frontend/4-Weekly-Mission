90c1d1d9579090a590b79c49e069f46055e8ba72
sihyonn <sihyeon982@naver.com>
2024-01-27 20:51:15 +0900

    feat: signIn.js 생성, signIn.html에 연결

---
+++ src/signIn.js
@@ -0,0 +1,38 @@
+import { MESSAGE, TEST_AUTH } from './constants/SIGN.js';
+import {
+  emailInput,
+  emailError,
+  pwInput,
+  pwError,
+  pwToggle,
+  applyError,
+  validateEmail,
+  validatePw,
+  handleClickPwToggle,
+  handleEnterKey,
+} from './utils/auth.js';
+
+const signBtn = document.querySelector('.sign-normal__btn');
+
+const handleLoginFailure = () => {
+  applyError(emailError, MESSAGE.CHECK_EMAIL, emailInput);
+  applyError(pwError, MESSAGE.CHECK_PASSWORD, pwInput);
+};
+
+// 로그인 로직 수정필요
+const handleLogin = () => {
+  if (emailInput.value === TEST_AUTH.EMAIL && pwInput.value === TEST_AUTH.PW) {
+    window.location.href = 'folder.html';
+  } else {
+    handleLoginFailure();
+  }
+};
+
+emailInput.addEventListener('focusout', validateEmail);
+emailInput.addEventListener('keydown', (e) => handleEnterKey(e, handleLogin));
+
+pwInput.addEventListener('focusout', validatePw);
+pwInput.addEventListener('keydown', (e) => handleEnterKey(e, handleLogin));
+pwToggle.addEventListener('click', () => handleClickPwToggle(pwInput, pwToggle));
+
+signBtn.addEventListener('click', handleLogin);
