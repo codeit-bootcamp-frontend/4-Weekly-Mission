7c4fec53f79def1673d565e30480b8d9c34443b4
seoyeon <ysynop7@namver.com>
2024-01-28 00:21:27 +0900

    fix: Fix emailCheck function to emailValidCheck fuction

--- json/signin.js
+++ json/signin.js
@@ -4,12 +4,8 @@ const confirmBtn = document.querySelector('#confirm-button');
 const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 
 /*email 형식인지 체크해주는 함수*/
-function emailCheck(email_address){     
-	if (!email_regex.test(email_address)) { 
-		return false; 
-	} else {
-		return true;
-	}
+function emailValidCheck(email_address){     
+	return email_regex.test(email_address);
 }
 
 /*입력받은 email 값을 판별하고 조건에 맞는 에러메시지를 출력하는 함수*/
@@ -18,7 +14,7 @@ function checkInputEmail(e) {
   errorMessage.classList.add('error-message');
   emailInput.classList.add('error-border');
   e.target.value === '' ? errorMessage.textContent = '이메일을 입력해 주세요.'
-  : !emailCheck(event.target.value) ? errorMessage.textContent = '올바른 이메일 주소가 아닙니다.'
+  : !emailValidCheck(e.target.value) ? errorMessage.textContent = '올바른 이메일 주소가 아닙니다.'
   : emailInput.classList.remove('error-border');
 
   emailInput.after(errorMessage);
