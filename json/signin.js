b4be709ee61ab0ba04af80258e9cf35ccefde69f
seoyeon <ysynop7@namver.com>
2024-01-27 23:53:57 +0900

    fix: Fix functions

--- json/signin.js
+++ json/signin.js
@@ -13,28 +13,26 @@ function emailCheck(email_address){
 }
 
 /*입력받은 email 값을 판별하고 조건에 맞는 에러메시지를 출력하는 함수*/
-function checkInputEmail(event) {
+function checkInputEmail(e) {
   const errorMessage = document.createElement('div');
   errorMessage.classList.add('error-message');
   emailInput.classList.add('error-border');
-  if (event.target.value === '') {
-    errorMessage.innerHTML = '<p>이메일을 입력해 주세요.</p>';
-    emailInput.after(errorMessage);
-  } else if (!emailCheck(event.target.value)) {
-    errorMessage.innerHTML = '<p>올바른 이메일 주소가 아닙니다.</p>';
-    emailInput.after(errorMessage);
-  }
+  e.target.value === '' ? errorMessage.textContent = '이메일을 입력해 주세요.'
+  : !emailCheck(event.target.value) ? errorMessage.textContent = '올바른 이메일 주소가 아닙니다.'
+  : emailInput.classList.remove('error-border');
+
+  emailInput.after(errorMessage);
 }
 
 /*입력받은 password 값을 판별하고 조건에 맞는 에러메시지를 출력하는 함수*/
-function checkInputPassword(event) {
+function checkInputPassword(e) {
   const errorMessage = document.createElement('div');
   errorMessage.classList.add('error-message');
   passwordInput.classList.add('error-border');
-  if (event.target.value === '') {
-    errorMessage.innerHTML = '<p>비밀번호를 입력해 주세요.</p>';
-    passwordInput.after(errorMessage);
-  }
+  e.target.value === '' ? errorMessage.textContent = '비밀번호를 입력해 주세요.'
+  : passwordInput.classList.remove('error-border');
+
+  passwordInput.after(errorMessage);
 }
 
 /*에러메시지 초기화하는 함수*/
@@ -46,28 +44,27 @@ function removeError(e) {
 
 /*로그인을 시도할 때 일어나는 경우에 대한 함수*/
 function tryLogin(event) {
+  event.preventDefault();
   if (emailInput.value === 'test@codeit.com' && passwordInput.value === 'codeit101') {
-    event.preventDefault();
     window.location.href = "folder.html";
   } else if (emailInput.value !== 'test@codeit.com' && passwordInput.value === 'codeit101') {
-    event.preventDefault();
+    emailInput.classList.add('error-border');
     emailInput.nextElementSibling.textContent = '이메일을 확인해 주세요.';
   } else if (emailInput.value === 'test@codeit.com' && passwordInput.value !== 'codeit101') {
-    event.preventDefault();
+    passwordInput.classList.add('error-border');
     passwordInput.nextElementSibling.textContent = '비밀번호를 확인해 주세요.';
   } else {
+    emailInput.classList.add('error-border');
+    passwordInput.classList.add('error-border');
     emailInput.nextElementSibling.textContent = '이메일을 확인해 주세요.';
     passwordInput.nextElementSibling.textContent = '비밀번호를 확인해 주세요.';
   }
 }
 
 
-
 /*이벤트 핸들러 등록*/
 emailInput.addEventListener('focusin', removeError);
 emailInput.addEventListener('focusout', checkInputEmail);
-emailInput.addEventListener('keyup', keyCode => {13});
 passwordInput.addEventListener('focusin', removeError);
 passwordInput.addEventListener('focusout', checkInputPassword);
-passwordInput.addEventListener('keyup', keyCode => {13});
 confirmBtn.addEventListener('click', tryLogin);
