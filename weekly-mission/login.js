a88de8015cd13c9b9fc8bcd14d7abeaac554698c
parkhyeonwoo <pq120wal39@gmail.com>
2024-01-27 23:28:39 +0900

    longin.js 에러  수정

--- weekly-mission/login.js
+++ weekly-mission/login.js
@@ -1,13 +1,11 @@
-import {returnInputValue, addErrorClass, eyeIconClickEvent} from './recycle.js'
+import { returnInputValue, addErrorClass, eyeIconClickEvent } from './recycle.js'
 const errorMessageEmail = document.querySelector('.error-message[data-about=email]');
-const passwordInput = document.querySelector('#password');
 
 
 //이메일 형식 체크
-function checkEmailInput(event){
+function checkEmailInput(event) {
     const userInput = returnInputValue(event);
-    console.log(userInput)
-    let message = emailInput.value === '' ? '이메일을 입력해주세요.' : /@/g.test(emailInput.value) === false ? '올바른 이메일 형식이 아닙니다.' : '';
+    let message = userInput === '' ? '이메일을 입력해주세요.' : /@/g.test(emailInput.value) === false ? '올바른 이메일 형식이 아닙니다.' : '';
     errorMessageEmail.textContent = message;
     addErrorClass(event.target, message);
 }
@@ -15,19 +13,21 @@ function checkEmailInput(event){
 
 
 //폼 제출 이벤트
-function submitFormEvent(e){
+function submitFormEvent(e) {
     e.preventDefault();
-    if(emailInput.value === 'test@codeit.com' && passwordInput.value === 'codeit101'){
+    const passwordInput = document.querySelector('#password');
+    const emailInput = document.querySelector('#email');
+    if (emailInput.value === 'test@codeit.com' && passwordInput.value === 'codeit101') {
         window.location = '/folder'
-    }else{
+    } else {
         alert('이메일, 비밀번호를 확인해주세요.');
-    } 
+    }
 }
 
 document.querySelector('#email').addEventListener('focusout', checkEmailInput);
 
-document.querySelector('#form-signin').addEventListener('submit',submitFormEvent );
+document.querySelector('#form-signin').addEventListener('submit', submitFormEvent);
 
-document.querySelector('.show-password > img').addEventListener('click',eyeIconClickEvent );
+document.querySelector('.show-password > img').addEventListener('click', eyeIconClickEvent);
 
 
--- weekly-mission/signup.js
+++ weekly-mission/signup.js
@@ -1,4 +1,4 @@
-import {returnInputValue, addErrorClass, eyeIconClickEvent} from './recycle.js'
+import { returnInputValue, addErrorClass, eyeIconClickEvent } from './recycle.js'
 const errorMessageEmail = document.querySelector('.error-message[data-about=email]');
 const userInputPassword = document.querySelector('#password');
 const errorMessgePassword = document.querySelector('.error-message[data-about=password]');
@@ -8,7 +8,7 @@ const errorMessageBoxes = document.querySelectorAll('.error-message');
 
 
 //이메일 체크
-function checkEmailInput(event){
+function checkEmailInput(event) {
     const userInput = returnInputValue(event);
     let message = userInput === '' ? '이메일을 입력해주세요.' : /@/g.test(userInput) === false ? '올바른 이메일 주소가 아닙니다.' : userInput === 'test@codeit.com' ? '이미 사용 중인 이메일 입니다.' : '';
     errorMessageEmail.textContent = message;
@@ -17,17 +17,17 @@ function checkEmailInput(event){
 }
 
 //비밀번호 조합 체크
-function checkPasswordLength(event){
+function checkPasswordLength(event) {
     const userInput = returnInputValue(event);
-    let message = userInput.length < 8 || /[\d][a-z]/ig.test(userInput) === false? '비밀번호는 영문, 숫자 조합8자 이상 입력해 주세요.' : '';
+    let message = userInput.length < 8 || /[\d][a-z]/ig.test(userInput) === false ? '비밀번호는 영문, 숫자 조합8자 이상 입력해 주세요.' : '';
     errorMessgePassword.textContent = message;
     addErrorClass(event.target, message);
     return
 }
 
 //비밀번호 같은지 체크
-function samePasswordCheck(event){
-    const password =  userInputPassword.value;
+function samePasswordCheck(event) {
+    const password = userInputPassword.value;
     const passwordCheckInput = returnInputValue(event);
     let message = password === passwordCheckInput ? '' : '비밀번호가 일치하지 않아요.'
     errorMessageCheck.textContent = message;
@@ -36,32 +36,32 @@ function samePasswordCheck(event){
 }
 
 //폼제출 이벤트 함수
-function submitSignupFormEvent(e){
+function submitSignupFormEvent(e) {
     e.preventDefault();
     checkEmptyInput();
 
 }
 
 //폼제출시 input태그가 비어있으면 알림 함수
-function checkEmptyInput(){
+function checkEmptyInput() {
     const inputes = document.querySelectorAll('input');
-    if(inputes[0].value === '') alert(`${inputes[0].dataset['value']}을 입력해주세요.`)
-    else if (inputes[1].value === '')alert(`${inputes[1].dataset['value']}를 입력해주세요.`)
-    else{
+    if (inputes[0].value === '') alert(`${inputes[0].dataset['value']}을 입력해주세요.`)
+    else if (inputes[1].value === '') alert(`${inputes[1].dataset['value']}를 입력해주세요.`)
+    else {
         let check = '';
-        errorMessageBoxes.forEach(v=>check += v.textContent);
+        errorMessageBoxes.forEach(v => check += v.textContent);
         check === '' ? window.location = '/folder' : repeatAnimation();
     }
 }
 
 //에러메세지 강조 함수
-function repeatAnimation(){
-    errorMessageBoxes.forEach(v=>{
-        if(v.textContent !== ''){
+function repeatAnimation() {
+    errorMessageBoxes.forEach(v => {
+        if (v.textContent !== '') {
             v.classList.add('active');
-            setTimeout(()=>{
+            setTimeout(() => {
                 v.classList.remove('active')
-            },550)
+            }, 550)
         }
     })
 }
@@ -72,4 +72,4 @@ document.querySelector('#email').addEventListener('focusout', checkEmailInput);
 userInputPassword.addEventListener('focusout', checkPasswordLength);
 document.querySelector('#password-check').addEventListener('focusout', samePasswordCheck);
 document.querySelector('#form-signup').addEventListener('submit', submitSignupFormEvent);
-document.querySelectorAll('.show-password > img').forEach(v=> v.addEventListener('click',eyeIconClickEvent ));
+document.querySelectorAll('.show-password > img').forEach(v => v.addEventListener('click', eyeIconClickEvent));
