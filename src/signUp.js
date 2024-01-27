21411199f0b05469c3a1172a79b42438d174fb52
sihyonn <sihyeon982@naver.com>
2024-01-27 21:00:53 +0900

    feat: 회원가입 버튼 클릭 이벤트 등록

--- src/signUp.js
+++ src/signUp.js
@@ -9,12 +9,12 @@ import {
   validateEmail,
   validatePw,
   handleClickPwToggle,
-  handleEnterKey,
 } from './utils/auth.js';
 
 const pwCheckInput = document.getElementById('password-check');
 const pwCheckError = document.getElementById('password-check-error');
 const pwCheckToggle = document.querySelector('.password-check-eye');
+const signUpBtn = document.querySelector('#signup-btn');
 
 const handlePasswordMatch = () => {
   if (pwCheckInput.value !== pwInput.value) applyError(pwCheckError, MESSAGE.NOT_MATCH_PASSWORD, pwCheckInput);
@@ -32,10 +32,10 @@ const handleSignUp = () => {
 };
 
 emailInput.addEventListener('focusout', handleDuplicateEmail);
-emailInput.addEventListener('keydown', (e) => handleEnterKey(e, handleLogin));
 
 pwInput.addEventListener('focusout', validatePw);
-pwInput.addEventListener('keydown', (e) => handleEnterKey(e, handleLogin));
 pwCheckInput.addEventListener('focusout', handlePasswordMatch);
 pwToggle.addEventListener('click', () => handleClickPwToggle(pwInput, pwToggle));
 pwCheckToggle.addEventListener('click', () => handleClickPwToggle(pwCheckInput, pwCheckToggle));
+
+signUpBtn.addEventListener('click', handleSignUp);
