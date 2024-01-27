0735e8fe03871bd8534a7c74e3a12518b8784fb3
민영 <minyoung_kim@gimmin-yeong-ui-MacBookAir.local>
2024-01-27 20:43:25 +0900

    4주차 피드백 수정 및 기능 구현

--- js/signin.js
+++ js/signin.js
@@ -4,7 +4,11 @@ const emailError = document.querySelector('#signinEmailError');
 const pwError = document.querySelector('#signinPwError');
 const loginBtn = document.querySelector('#loginButton');
 
-document.addEventListener("DOMContentLoaded", function() {
+const testEmail = "test@codeit.com";
+const testPw = "codeit101";
+
+const emailCheck = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_\.-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
   document.addEventListener('keydown', function(event) {
     if (event.key === "Enter") {
       login();
@@ -12,44 +16,43 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   loginBtn.addEventListener('click', login);
-
   email.addEventListener('focusout', emailF);
   password.addEventListener('focusout', passwordF);
 
-});
 
 function login() {
-  const testEmail = "test@codeit.com";
-  const testPw = "codeit101";
-
   if (email.value === testEmail && password.value === testPw) {
-    window.location.href = '../folder.html'
+    window.location.href = './folder.html'
   }
 
   if (email.value !== testEmail) {
     emailError.textContent = '이메일을 확인해 주세요';
   }
 
-  if (password !== testPw) {
+  if (password.val !== testPw) {
     pwError.textContent = '비밀번호를 확인해 주세요';
   }
 }
 
 function emailF() {
-  let emailCheck = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_\.-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-
   if (!email.value) {
+    email.classList.add('inputError');
     emailError.textContent = "이메일을 입력해 주세요";
+  } else if (!emailCheck.test(email.value)) {
+    email.classList.add('inputError');
+    emailError.textContent = "올바른 이메일 주소가 아닙니다.";
   } else {
-    if (!emailCheck.test(email.value)) {
-      emailError.textContent = "올바른 이메일 주소가 아닙니다.";
-    }
+    email.classList.remove('inputError');
+    emailError.textContent = '';
   }
 }
 
 function passwordF() {
   if (!password.value) {
+    password.classList.add('inputError');
     pwError.textContent = "비밀번호를 입력해 주세요";
+  } else {
+    password.classList.remove('inputError');
+    pwError.textContent = '';
   }
 }
-
