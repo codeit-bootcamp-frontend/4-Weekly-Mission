6246143754eb2a1434ca3a8a4955b515241c9ad6
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-28 03:59:26 +0900

    feat: Add variable.js
    to export variables

---
+++ js/variable.js
@@ -0,0 +1,19 @@
+export function getEmailInput(login) {
+  return document.getElementById(`input-email-${login}`);
+}
+
+export function getPasswordInput(login) {
+  return document.getElementById(`input-password-${login}`);
+}
+
+export function getPasswordCheck() {
+  return document.getElementById(`input-password-signup-check`);
+}
+
+export function getLoginForm(login) {
+  return document.getElementById(`${login}-form`);
+}
+
+export const emailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
