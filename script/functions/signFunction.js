82ee32f1d6304a5cfa23a69166959bf261a0d34e
Taeuk471 <asky990223@gmail.com>
2024-01-27 16:34:48 +0900

    :sparkle: Make isValidPassword function

--- script/functions/signFunction.js
+++ script/functions/signFunction.js
@@ -1,4 +1,4 @@
-import {EMAIL_REGEX} from '../constant.js'
+import {EMAIL_REGEX, PASSWORD_REGEX} from '../constant.js'
 
 //빈 문자열인지 체크
 export function isEmptyString(value) {
@@ -9,6 +9,10 @@ export function isValidEmail(email) {
   return EMAIL_REGEX.test(email);
 }
 
+//비밀번호의 유효성 검사
+export function isValidPassword(email) { 
+  return PASSWORD_REGEX.test(email);
+}
 
 //해당 Element가 보이는 상태인지 아닌지를 판별
 export function isVisiable(element) {
