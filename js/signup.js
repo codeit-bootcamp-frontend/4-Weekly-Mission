a733f889d23a49be3bcd344d7fac85c50f879493
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-28 04:04:27 +0900

    feat: Add signup.js

---
+++ js/signup.js
@@ -0,0 +1,75 @@
+import { getEmailInput, getPasswordInput, getLoginForm, emailValidate, getPasswordCheck, passwordPattern } from "./variable.js";
+import { showError, hideError, pathTo } from "./func.js";
+
+const emailInput = getEmailInput("signup");
+const passwordInput = getPasswordInput("signup");
+const passwordCheck = getPasswordCheck();
+const loginForm = getLoginForm("signup");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+  const errorMessage = document.getElementsByClassName("error-message");
+
+  const isEmptyErrorMessages = Array.from(errorMessage).every((el) => el.textContent === "");
+
+  //조건이 허용될 때 folder path로 이동되는 부분 수정하기
+  if (emailValue !== "test@codeit.com" && emailValue && passwordValue && passwordCheck) {
+    pathTo("folder");
+  } else {
+    showError(emailInput, "이메일을 확인해 주세요.");
+    showError(passwordInput, "비밀번호를 확인해 주세요.");
+    showError(passwordCheck, "비밀번호를 확인해 주세요.");
+  }
+});
+
+emailInput.addEventListener("focusout", function () {
+  isValidateEmail();
+});
+
+passwordInput.addEventListener("focusout", function () {
+  validatePassword(passwordInput);
+});
+
+passwordCheck.addEventListener("focusout", function () {
+  validatePasswordCheck();
+});
+
+function isValidateEmail() {
+  const emailValue = emailInput.value.trim();
+
+  if (emailValue === "") {
+    showError(emailInput, "이메일을 입력해주세요.");
+  } else if (!emailValidate.test(emailValue)) {
+    showError(emailInput, "올바른 이메일 주소가 아닙니다.");
+  } else if (emailValue === "test@codeit.com") {
+    showError(emailInput, "이미 사용 중인 이메일입니다.");
+  } else {
+    hideError(emailInput);
+  }
+}
+
+function validatePassword(password) {
+  const passwordValue = password.value.trim();
+
+  if (passwordValue === "") {
+    showError(password, "비밀번호를 입력해주세요.");
+  } else if (!passwordPattern.test(passwordValue)) {
+    showError(password, "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+  } else {
+    hideError(password);
+  }
+}
+
+function validatePasswordCheck() {
+  const passwordValue = passwordInput.value.trim();
+  const passwordValueCheck = passwordCheck.value.trim();
+
+  if (passwordValue !== passwordValueCheck && passwordValueCheck !== "") {
+    showError(passwordCheck, "비밀번호가 일치하지 않아요.");
+  } else {
+    validatePassword(passwordCheck);
+  }
+}
