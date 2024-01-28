5e85395b8e442da6e2bd185562c8b72a6901c95b
김동현 <dtc03003@naver.com>
2024-01-29 00:48:52 +0900

    refactor: 반복코드 제거

--- src/script/sign.js
+++ src/script/sign.js
@@ -1,7 +1,7 @@
 const email_input = document.querySelector("#signin_email");
 const password_input = document.querySelector("#signin_password");
-const error_email = document.querySelector("#error_email");
-const error_password = document.querySelector("#error_password");
+const email_error = document.querySelector("#email_error");
+const password_error = document.querySelector("#password_error");
 const eyes = document.querySelector(".eye_button");
 const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 
@@ -9,20 +9,17 @@ const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 function validateEmail() {
     // 이메일 값이 없는 경우
     if (email_input.value.trim() === "") {
-        changeBorder(email_input, "red");
-        changeError(error_email, "이메일을 입력해 주세요.");
+        createError(email_input, email_error, "이메일을 입력해 주세요.");
         return;
     }
     // 올바른 이메일 주소가 아닐 경우
     else if (!isValidEmail(email_input.value)) {
-        changeBorder(email_input, "red");
-        changeError(error_email, "올바른 이메일 주소가 아닙니다.");
+        createError(email_input, email_error, "올바른 이메일 주소가 아닙니다.");
         return;
     }
     // 이메일 값이 있는 경우
     else {
-        changeBorder(email_input);
-        clearError(error_email);
+        clearError(email_input, email_error);
     }
 }
 
@@ -30,18 +27,16 @@ function validateEmail() {
 function validatePassword() {
     // 비밀번호 값이 없는 경우
     if (password_input.value.trim() === "") {
-        changeBorder(password_input, "red");
-        changeError(error_password, "비밀번호를 입력해 주세요.");
+        createError(password_input, password_error, "비밀번호를 입력해 주세요.");
         return;
     }
     // 비밀번호 값이 있는 경우
     else {
-        changeBorder(password_input);
-        clearError(error_password);
+        clearError(password_input, password_error);
     }
 }
 
-//이메일 유효성 검사
+// 이메일 유효성 검사
 function isValidEmail(email) {
     return emailRegex.test(email);
 }
@@ -52,13 +47,11 @@ function login(event) {
     if (email_input.value === "test@codeit.com" && password_input.value === "codeit101") {
         window.location.href = "/folder";
     } else {
-        if (error_email.style.display === "none") {
-            changeBorder(email_input, "red");
-            changeError(error_email, "이메일을 확인해 주세요.");
+        if (email_error.style.display === "none") {
+            createError(email_input, email_error, "이메일을 확인해 주세요.");
         }
-        if (error_password.style.display === "none") {
-            changeBorder(email_ipassword_inputnput, "red");
-            changeError(error_password, "비밀번호를 확인해 주세요.");
+        if (password_error.style.display === "none") {
+            createError(password_input, password_error, "비밀번호를 확인해 주세요.");
         }
     }
 }
@@ -69,18 +62,30 @@ function toggleEye() {
     password_input.type = inputType;
 }
 
+// 에러 발생
+function createError(input, error, str) {
+    changeBorder(input);
+    addErrorMessage(error, str);
+}
+
+// 에러 해결
+function clearError(input, error) {
+    changeBorder(input, "gray20");
+    deleteErrorMessage(error);
+}
+
 // 테두리 색 바꾸기
-function changeBorder(value, color = "gray20") {
+function changeBorder(value, color = "red") {
     value.style.border = `1px solid var(--Linkbrary-${color})`;
 }
 
 // 에러 메세지 추가
-function changeError(value, str) {
+function addErrorMessage(value, str) {
     value.innerText = str;
     value.style.display = "block";
 }
 
 // 에러 메세지 삭제
-function clearError(value) {
+function deleteErrorMessage(value) {
     value.style.display = "none";
 }
