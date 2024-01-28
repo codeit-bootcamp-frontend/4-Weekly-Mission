3f1f7194199e9468c266e694652d94fc1254a059
Taeuk471 <asky990223@gmail.com>
2024-01-28 21:32:51 +0900

    :art: Make error message to object

--- script/constant.js
+++ script/constant.js
@@ -11,3 +11,20 @@ export const VISIABLE_STATE = {
   invisiable : './assets/logo/eye-off.svg',
   visiable : './assets/logo/eye-on.svg'
 };
+
+export const EMAIL_ERROR_MESSAGE = {
+  isEmpty : '이메일을 입력해 주세요.',
+  isNotRightFormat : '올바른 이메일 주소가 아닙니다.',
+  haveToCheck : '이메일을 확인해 주세요.',
+  isUsing : '이미 사용중인 이메일 입니다.'
+}
+
+export const PASSWORD_ERROR_MESSAGE = {
+  isEmpty : '비밀번호를 입력해 주세요.',
+  haveToCheck : '비밀번호를 확인해 주세요.',
+  isNotRightFormat : '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'
+}
+
+export const PASSWORD_CONFIRM_ERROR_MESSAGE = {
+  isNotMatch : '비밀번호가 일치하지 않아요.'
+}
