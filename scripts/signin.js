cc7dae8228cc65d5fb2b17e7a9869171db263f8c
JI-SOOLEE <elenfl0122@gmail.com>
2024-01-28 21:29:50 +0900

    refactor: 4th code review

--- scripts/signin.js
+++ scripts/signin.js
@@ -2,30 +2,25 @@ let email = document.querySelector('#email');
 let PW = document.querySelector('#pw');
 let login = document.querySelector('.login-btn');
 
+// 유효한 이메일 형식인지 확인
 function isValidEmail(email) {
   // const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   // return regex.test(email.value);
   return email.value.includes('@') && email.value.includes('.');
 }
 
+// 에러메세지가 이미 존재하는지 확인
 function alertExist(email) {
   return email.classList.contains('red-box');
 }
 
 email.addEventListener('focusout', (e) => {
-  if (email.value === '') {
+  // 논리 연산자 or 사용해서 중복 제거
+  if (email.value === ' ' || !isValidEmail(email)) {
     if (!alertExist(email)) {
       email.classList.add('red-box');
       let alert = document.createElement('p');
       alert.textContent = '이메일을 입력해 주세요.';
-      alert.classList.add('red');
-      document.querySelector('.email').append(alert);
-    }
-  } else if (!isValidEmail(email)) {
-    if (!alertExist(email)) {
-      email.classList.add('red-box');
-      let alert = document.createElement('p');
-      alert.textContent = '올바른 이메일 주소가 아닙니다.';
       alert.classList.add('red');
       document.querySelector('.email').append(alert);
     }
@@ -42,5 +37,6 @@ email.addEventListener('focusin', (e) => {
 
 login.addEventListener('click', (e) => {
   if (email.value === 'test@codeit.com' && PW.value === 'codeit101') {
+    login.addEventListener('click', (window.location.href = './'));
   }
 });
