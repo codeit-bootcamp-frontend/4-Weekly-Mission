1f53c8970b602433cf10e8f6b84d79fd058c235e
Taeuk471 <asky990223@gmail.com>
2024-01-28 21:27:09 +0900

    :sparkles: Make domhelper.js

---
+++ script/dom/domhelper.js
@@ -0,0 +1,11 @@
+export function getElementById(id) {
+  return document.getElementById(id);
+}
+
+export function addClass(element, className) {
+  element.classList.add(className);
+}
+
+export function removeClass(element, className) {
+  element.classList.remove(className);
+}
