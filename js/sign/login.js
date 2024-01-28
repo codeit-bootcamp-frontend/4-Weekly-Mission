708c3bd6e640d06ff41f4c6bf008bb19c58cf5f9
kimdohyeon <jd105@naver.com>
2024-01-28 20:25:43 +0900

    5주차 위클리 과제

---
+++ js/sign/login.js
@@ -0,0 +1,52 @@
+import { ERROR_MESSAGE } from "../../constant.js";
+import { inputAddNode } from "../node.js";
+import { pwdInput, pwdEyeIcon} from "../declaration.js";
+
+// enter키 입력으로 로그인 실행 
+export function EnterLogin(key, func) {
+  if(key === 'Enter') {
+    func();
+  }
+}
+
+// 에러 메세지 출력 함수
+export function errorMsg(errorCase) {
+  let newNode = document.createElement('div');
+
+  switch(errorCase) {
+    case "NoEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.empty, errorCase);
+      break;
+    case "wrongEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.invalid, errorCase);
+      break;
+    case "inUseEmail":
+      inputAddNode('email', ERROR_MESSAGE.email.inUse, errorCase);
+      break;
+    case "NoPwd" :
+      inputAddNode('password', ERROR_MESSAGE.password.empty, errorCase);
+      break;
+    case "wrongPwd" :
+      inputAddNode('password', ERROR_MESSAGE.password.invalid, errorCase);
+      break;
+    case "noMatchPwd" : 
+      inputAddNode("password2", ERROR_MESSAGE.password.recheck, errorCase);
+      break;
+    case "Other": 
+      inputAddNode('email', ERROR_MESSAGE.email.check, errorCase);
+      inputAddNode('password', ERROR_MESSAGE.password.check, errorCase);
+      break;
+  }
+ }
+
+
+// password input 눈모양 클릭 실행 함수
+export function EyePwd(EyeStatus) {
+  EyeStatus.classList.contains('off') ? (
+    pwdEyeIcon.setAttribute('src',"assets/icons/eye_off.png"),
+    pwdInput.setAttribute('type', 'password')
+  ):(
+    pwdEyeIcon.setAttribute('src',"assets/icons/eye_on.png"),
+    pwdInput.setAttribute('type', 'text')
+  )
+}
---
+++ js/sign/signup.js
@@ -0,0 +1,58 @@
+import * as common from "./login.js";
+import { emailCheck } from "../validation.js"; 
+import { inputDeleteNode } from "../node.js";
+import { emailInput, pwdInput, signupBtn, pwdEyeIcon} from "../declaration.js";
+
+let emailVal = "", pwdVal = "";
+
+// 이메일 input 핸들러
+function emailHandlerFunc(email) {
+  if(email) {
+    emailCheck(email) ? inputDeleteNode('email') : common.errorMsg("wrongEmail")
+  } else {
+    common.errorMsg("NoEmail");
+  }
+  emailVal = email; 
+}
+
+// 로그인 시도 함수
+function trySignup() {
+  if(emailVal === "test@codeit.com" && pwdVal === "codeit101") {
+    return location.assign("folder.html");
+  } else {
+    return common.errorMsg("Other"); 
+  }
+}
+
+function passwordHandlerFuc(password) {
+  password ? inputDeleteNode('password') : common.errorMsg("NoPwd");
+  pwdVal = password;
+}
+
+// email input 이벤트 함수 등록
+emailInput.addEventListener('focusout', function(e) {
+  emailHandlerFunc(e.target.value);
+});
+emailInput.addEventListener('keypress', function(e) {
+  common.EnterLogin(e.key,trySignup);
+});
+
+// password input 이벤트 함수 등록
+pwdInput.addEventListener('focusout', function(e) {
+  passwordHandlerFuc(e.target.value);
+});
+pwdInput.addEventListener('keypress', function(e) {
+  common.EnterLogin(e.key, trySignup);
+});
+
+// 로그인 버튼 이벤트 함수 등록
+signupBtn.addEventListener('click', function(e) {
+  trySignup(emailVal, pwdVal);
+ });
+
+
+ // 눈모양 아이콘 이벤트 함수 등록
+pwdEyeIcon.addEventListener('click', function(e) {
+  e.target.classList.toggle('off');
+  common.EyePwd(e.target);
+ })
