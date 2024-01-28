36402a2ff205b6a7709541bd342260a2c9164195
ggjiny <jnss599@gmail.com>
2024-01-28 20:41:20 +0900

    feat: singIn, signUp 전체적인  수정, sign 모듈에 함수 분리

--- src/sign.js
+++ src/sign.js
@@ -1,64 +1,114 @@
-import { ERROR_MESSAGE, TEST_USER, emailCheck } from "./constant.js";
+import {
+  ERROR_MESSAGE,
+  TEST_USER,
+  emailCheck,
+  passwordCheck,
+} from "./constant.js";
 
-const formElement = document.querySelector(".sign-form");
-const emailElement = document.getElementById("email");
-const passwordElement = document.getElementById("password");
-const eyeImg = document.querySelector(".eye");
+const setErrorMessage = (currentElement, errorMsg) => {
+  currentElement.classList.add("errorInput");
+  currentElement.nextElementSibling.textContent = errorMsg;
+};
+const removeErrorMessage = (currentElement) => {
+  currentElement.classList.remove("errorInput");
+  currentElement.nextElementSibling.textContent = "";
+};
 
-const focusOutHandler = (e) => {
-  const currentInput = e.target;
-  const currentValue = currentInput.value;
-  const currentClassList = currentInput.classList;
-  const errorMsgElement = currentInput.nextElementSibling;
-
-  const isEmail = currentInput.type === "email";
-  const noInputText = isEmail
-    ? ERROR_MESSAGE.NO_INPUT_EMAIL
-    : ERROR_MESSAGE.NO_INPUT_PASSWORD;
-
-  if (!currentValue) {
-    currentClassList.add("errorInput");
-    errorMsgElement.textContent = noInputText;
-  } else if (isEmail && !emailCheck(currentValue)) {
-    currentClassList.add("errorInput");
-    errorMsgElement.textContent = ERROR_MESSAGE.INVALID_EMAIL;
-  } else {
-    currentClassList.remove("errorInput");
-    errorMsgElement.textContent = "";
+const noInputValue = (currentElement) => {
+  const noInputText =
+    currentElement.type === "email"
+      ? ERROR_MESSAGE.NO_INPUT_EMAIL
+      : ERROR_MESSAGE.NO_INPUT_PASSWORD;
+  if (!currentElement.value) {
+    setErrorMessage(currentElement, noInputText);
   }
 };
 
-const submitForm = (e) => {
-  const emailInput = e.target[0];
-  const passwordInput = e.target[1];
+const isValidEmail = (currentElement) => {
+  if (!emailCheck(currentElement.value)) {
+    setErrorMessage(currentElement, ERROR_MESSAGE.INVALID_EMAIL);
+  } else {
+    removeErrorMessage(currentElement);
+  }
+};
+const isValidPassword = (currentElement) => {
+  if (!passwordCheck(currentElement.value)) {
+    setErrorMessage(currentElement, ERROR_MESSAGE.INVALID_PASSWORD);
+  } else {
+    removeErrorMessage(currentElement);
+  }
+};
 
-  e.preventDefault();
-  console.log();
-  if (
-    emailInput.value === TEST_USER.ID &&
-    passwordInput.value === TEST_USER.PASSWORD
-  ) {
+const confirmEmail = (currentElement) => {
+  if (currentElement.value === TEST_USER.ID) {
+    setErrorMessage(currentElement, ERROR_MESSAGE.REGISTERED_EMAIL);
+  }
+};
+const confirmPasswordMatch = (currentElement) => {
+  const passwordElement = document.getElementById("password");
+
+  if (currentElement.value !== passwordElement.value) {
+    setErrorMessage(currentElement, ERROR_MESSAGE.DO_NOT_MATCH_PASSWORD);
+  } else {
+    removeErrorMessage(currentElement);
+  }
+};
+const confirmUserLogin = (currentElement) => {
+  const email = currentElement[0];
+  const password = currentElement[1];
+
+  if (email.value === TEST_USER.ID && password.value === TEST_USER.PASSWORD) {
     window.location.href = "/folder";
   } else {
-    emailInput.nextElementSibling.textContent = ERROR_MESSAGE.CONFIRM_EMAIL;
-    passwordInput.nextElementSibling.textContent =
-      ERROR_MESSAGE.CONFIRM_PASSWORD;
-    emailInput.classList.add("errorInput");
-    passwordInput.classList.add("errorInput");
+    setErrorMessage(email, ERROR_MESSAGE.CONFIRM_EMAIL);
+    setErrorMessage(password, ERROR_MESSAGE.CONFIRM_PASSWORD);
+  }
+};
+const confirmForm = (currentForm) => {
+  const emailElement = currentForm[0];
+  const passwordElement = currentForm[1];
+  const passwordConfirmElement = currentForm[2];
+
+  if (!emailCheck(emailElement.value) || emailElement.value === TEST_USER.ID) {
+    setErrorMessage(emailElement, ERROR_MESSAGE.CONFIRM_EMAIL);
+  }
+  if (!passwordCheck(passwordElement.value)) {
+    setErrorMessage(password, ERROR_MESSAGE.CONFIRM_PASSWORD);
+  }
+  if (passwordElement.value !== passwordConfirmElement.value) {
+    setErrorMessage(passwordConfirmElement, ERROR_MESSAGE.CONFIRM_PASSWORD);
   }
 };
 
-const eyeClickHandler = () => {
-  eyeImg.classList.toggle("on");
-  if (eyeImg.classList.contains("on")) {
-    password.removeAttribute("type");
+const confirmUserRegister = (currentElement) => {
+  confirmForm(currentElement);
+  const elementsArr = [...currentElement];
+  const satisfy = elementsArr.some((element) =>
+    element.classList.contains("errorInput")
+  );
+  if (!satisfy) {
+    window.location.href = "/folder";
+  }
+};
+
+const eyeClickHandler = (e) => {
+  const currentPassword = e.target.parentElement.children[1];
+  e.target.classList.toggle("on");
+  if (e.target.classList.contains("on")) {
+    currentPassword.removeAttribute("type");
   } else {
-    password.setAttribute("type", "password");
+    currentPassword.setAttribute("type", "password");
   }
 };
 
-emailElement.addEventListener("focusout", focusOutHandler);
-passwordElement.addEventListener("focusout", focusOutHandler);
-
-formElement.addEventListener("submit", submitForm);
-eyeImg.addEventListener("click", eyeClickHandler);
+export {
+  noInputValue,
+  isValidEmail,
+  isValidPassword,
+  confirmEmail,
+  confirmPasswordMatch,
+  confirmUserLogin,
+  confirmUserRegister,
+  eyeClickHandler,
+  removeErrorMessage,
+};
