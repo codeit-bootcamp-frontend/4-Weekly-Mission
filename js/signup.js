4f589cf6020d1eb94ce0deefd26452a6cc0b6d20
yukyoung kim <sksms2104@naver.com>
2024-01-28 16:18:56 +0900

    [Feat] signup trySignup 함수 추가 -> 모든 값을 유효하게 입력했을 때만 folder.html로 이동할 수 있도록 했음

--- js/signup.js
+++ js/signup.js
@@ -5,6 +5,10 @@ import { updateErrorMessage } from "./functions/updateErrorMessage.js";
 import { typeToText, typeToPassword } from "./functions/passwordShowHidden.js";
 import { checkValidLogin, validEmail } from "./functions/CheckValidLogin.js";
 import { isVaildPassword } from "../utils/isValidPassword.js";
+import {
+  checkSignupEmail,
+  checkSignupPassword,
+} from "./functions/checkValidSignup.js";
 
 // 이벤트 등록을 위한 변수 설정
 const emailInputBox = querySelector("#signup-email-input");
@@ -48,7 +52,7 @@ function checkEmail(event) {
 
 /** 비밀번호 입력 값이 유효한지 확인하는 함수 */
 function checkPassword(event) {
-  const passwordValue = event.target.value;
+  const passwordValue = event.target.value.trim();
   const errorMessageBox = querySelector("#password-error-message");
 
   if (passwordValue === "") {
@@ -70,25 +74,45 @@ function checkPassword(event) {
   }
 }
 
-/** 비밀번호 확인 */
-function checkPasswordRepeat(event) {
-  const passwordValue = event.target.value;
-  const errorMessageBox = querySelector("#password-repeat-error-message");
-
-  updateErrorMessage(
-    errorMessageBox,
-    passwordValue === ""
-      ? "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
-      : "",
-    signupPasswordInput,
-    passwordValue === ""
+/** 비밀번호 입력값과 확인 값이 맞는지 확인 */
+function checkPasswordRepeat() {
+  const passwordValue = querySelector("#signup-password-input").value;
+  const passwordRepeatValue = querySelector(
+    "#signup-password-repeat-input"
+  ).value;
+  const passwordRepeatErrorMessage = querySelector(
+    "#password-repeat-error-message"
   );
+
+  passwordRepeatValue === passwordValue && passwordRepeatValue !== ""
+    ? updateErrorMessage(
+        passwordRepeatErrorMessage,
+        "",
+        signupPasswordRepeatInput,
+        false
+      )
+    : updateErrorMessage(
+        passwordRepeatErrorMessage,
+        "비밀번호가 일치하지 않아요.",
+        signupPasswordRepeatInput,
+        true
+      );
+}
+
+function trySignup() {
+  if (
+    emailInputBox.classList.contains("error-box") === false &&
+    signupPasswordInput.classList.contains("error-box") === false &&
+    signupPasswordRepeatInput.classList.contains("error-box") === false
+  ) {
+    location.href = "folder.html";
+  }
 }
 
 /** 이벤트 등록 */
 emailInputBox.addEventListener("focusout", checkEmail); // 이메일 입력이 유효한지
 signupPasswordInput.addEventListener("focusout", checkPassword); //비밀번호를 입력했는지
 signupPasswordRepeatInput.addEventListener("focusout", checkPasswordRepeat); // 비밀번호 확인
-signupBtn.addEventListener("click", tryLogin); // 로그인 시도
-signupForm.addEventListener("keydown", loginByEnter); // 엔터키로 로그인하기
-passwordEyeIcon.addEventListener("click", passwordShowHidden); // 비밀번호 보기 & 숨기기
+signupBtn.addEventListener("click", trySignup); // 회원가입 시도
+// signupForm.addEventListener("keydown", signupByEnter); // 엔터키로 로그인하기
+// passwordEyeIcon.addEventListener("click", passwordShowHidden); // 비밀번호 보기 & 숨기기
