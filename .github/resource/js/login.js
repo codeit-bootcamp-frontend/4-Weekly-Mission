fc8ed1c17ba11570069eebb930ec4eac3e1d39e4
LimDongHyeon <ilhai3@naver.com>
2024-01-28 22:57:17 +0900

    5주차 위클리 미션 완료

--- .github/resource/js/login.js
+++ .github/resource/js/login.js
@@ -1,27 +1,24 @@
-const form = document.querySelector('#login_form');
-
-// 이메일 input
-const email = document.querySelector('#email');
+const email = document.querySelector('.email');
 const error = document.querySelector('.error_text');
 const error_pwd = document.querySelector('.password_text');
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-// 비밀번호 input
-const password = document.querySelector('#password');
-
-// 링크
+const password = document.querySelector('.password');
 const hostname = '/folder';
-
-// 비밀번호 아이콘
-const pwd_icon = document.querySelector('.pwd_icon');
+const pwd_icon = document.querySelectorAll('.pwd_icon');
 
 // 에러 메시지
 const ERROR_MESSAGE = {
     email_empty: '이메일을 입력해 주세요.',
     email_invalid: '올바른 이메일 주소가 아닙니다.',
-    pwd_empty: '비밀번호를 입력해주세요.'
+    email_check: '이메일을 확인해 주세요.',
+    email_use: '이미 사용 중인 이메일입니다.',
+    pwd_empty: '비밀번호를 입력해주세요.',
+    pwd_check: '비밀번호를 확인해 주세요.',
+    pwd_wrong: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
+    pwd_inc: '비밀번호가 일치하지 않아요.'
 }
 
+// 에러 메시지 송출
 const showError = (targetEl, errorEl, errorType) => {
     errorEl.style.display = 'block';
     targetEl.classList.add('error');
@@ -34,40 +31,27 @@ const deleteError = (targetEl, errorEl) => {
     errorEl.style.display = 'none';
 }
 
-email.addEventListener('focusout', function(e) {
+// 이메일 값 비교
+const email_form = () => {
     if(email.value === '') {
         showError(email, error, 'email_empty');
     } else if(!emailRegex.test(email.value)) {
         showError(email, error, 'email_invalid');
-    } else if(emailRegex.test(email.value) && email.value !== '') {
-        deleteError(email, error);
-    }
-})
-
-password.addEventListener('focusout', function(e) {
-    if(password.value === '') {
-        showError(password, error_pwd, 'pwd_empty');
-    } else if(password.value !== '') {
-        deleteError(password, error_pwd);
-    }
-})
-
-form.onsubmit = function(e) {
-    if(email.value.trim() === 'test@codeit.com' && password.value === 'codeit101') {
-        e.preventDefault();
-        window.location.href = hostname;
-    } else {
-        e.preventDefault()
-        showError(email, error, 'email_empty');
-        showError(password, error_pwd, 'pwd_empty');
     }
 }
+email.addEventListener('focusout', email_form);
 
-pwd_icon.addEventListener('click', function() {
-    pwd_icon.classList.toggle('hide');
-    if(password.type === 'password') {
-        password.setAttribute('type', 'text');
+// 비밀번호 눈 아이콘 토글
+const pwd_check = (e) => {
+    e.target.classList.toggle('hide');
+    if(e.target.previousElementSibling.type === 'password') {
+        e.target.previousElementSibling.setAttribute('type', 'text');
     } else {
-        password.setAttribute('type', 'password');
+        e.target.previousElementSibling.setAttribute('type', 'password');
     }
-})
+}
+pwd_icon.forEach((e) => {
+    e.addEventListener('click', pwd_check);
+});
+
+export { email, error, error_pwd, emailRegex, password, hostname, showError, deleteError, email_form};
