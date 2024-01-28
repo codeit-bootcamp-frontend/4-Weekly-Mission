9f2f9e29ca681d37b09ffc36673e9c7d83083f5c
hong <hsw5971@naver.com>
2024-01-28 15:25:12 +0900

    modified: week4 코드리뷰 반영-> 함수추가

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
+const USER_EMAIL = "test@codeit.com";
+const USER_PASSWORD = "codeit101";
+
 // email input
 const inputEmail = document.querySelector(".sign-input");
 const inputBoxEmail = document.querySelector(".sign-input-box");
@@ -11,99 +14,73 @@ const eyeButton = document.querySelector(".eye-button");
 const eyeImg = document.querySelector("#eye-id");
 
 function isValidEmail(emailAdress) {
-    let exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
     return exptext.test(emailAdress);
+}
+
+function showErrorMessage(element, message) {
+    const inputChild = element === inputBoxEmail ? element.children[1] : element.children[2];
+    console.log(inputChild);
+    const lastChild = element.lastElementChild;
+
+    if(lastChild === inputChild) {
+        const newErrorTag = document.createElement("p");
+        newErrorTag.textContent = message;
+        
+        element.append(newErrorTag);
+        inputChild.classList.add('error-border');
+        newErrorTag.classList.add('error-text');
+    } else {
+        lastChild.textContent = message;
+    }
+}
+function removeMessage(element) {
+    const inputChild = element === inputBoxEmail ? element.children[1] : element.children[2];
+    const lastChild = element.lastElementChild;
+    if(lastChild !== inputChild) {
+        lastChild.remove();
+        inputChild.classList.remove('error-border');
+    }
 }
 // focus-out email
 function eventFocusOutEmail() {
     // no email value
     if(inputEmail.value === "") {
-        if(inputBoxEmail.lastElementChild === inputEmail) {
-            const nothingInput = document.createElement("p");
-            nothingInput.textContent = "이메일을 입력해주세요";
-            inputBoxEmail.append(nothingInput);
-
-            inputEmail.classList.add('error-border');
-            nothingInput.classList.add('error-text');
-        } else {
-            inputBoxEmail.lastElementChild.textContent = "이메일을 입력해주세요";
-        }
+        showErrorMessage(inputBoxEmail, "이메일을 입력해주세요");
     }
     // not email form
     else if(!isValidEmail(inputEmail.value)) {
-        if(inputBoxEmail.lastElementChild === inputEmail) {
-            const invalidInput = document.createElement("p");
-            invalidInput.textContent = "올바른 이메일 주소가 아닙니다.";
-            inputBoxEmail.append(invalidInput);
-
-            inputEmail.classList.add('error-border');
-            invalidInput.classList.add('error-text');
-        } else {
-            inputBoxEmail.lastElementChild.textContent = "올바른 이메일 주소가 아닙니다.";
-        }
+        showErrorMessage(inputBoxEmail, "올바른 이메일 주소가 아닙니다.");
     }
     // error message remove
     else if(isValidEmail(inputEmail.value)) {
-        if(inputBoxEmail.lastElementChild !== inputEmail) {
-            inputBoxEmail.lastElementChild.remove();
-            inputEmail.classList.remove('error-border');
-        }
+        removeMessage(inputBoxEmail);
     }
 }
 // focus-out password
 function eventFocusOutPassword() {
     // no password value
     if(inputPassword.value === "") {
-        if(inputBoxPassword.lastElementChild === inputPassword) {
-            const nothingInput = document.createElement("p");
-            nothingInput.textContent = "비밀번호를 입력해주세요.";
-            inputBoxPassword.append(nothingInput);
-
-            inputPassword.classList.add('error-border');
-            nothingInput.classList.add('error-text');
-        } else {
-            inputBoxPassword.lastElementChild.textContent = "비밀번호를 입력해주세요.";
-        }
+        showErrorMessage(inputBoxPassword, "비밀번호를 입력해주세요")
     }
     else {
-        if(inputBoxPassword.lastElementChild !== inputPassword) {
-            inputBoxPassword.lastElementChild.remove();
-            inputPassword.classList.remove('error-border');
-        }
+        removeMessage(inputBoxPassword);
     }
 }
 // login button click
 function eventClickBtn() {
-    if(inputEmail.value === "test@codeit.com" && inputPassword.value === "codeit101") {
+    if(inputEmail.value === USER_EMAIL && inputPassword.value === USER_PASSWORD) {
         window.location.href = "/folder"
     }
-    if(inputEmail.value !== "test@codeit.com") {
-        if(inputBoxEmail.lastElementChild === inputEmail) {
-            const invalidInput = document.createElement("p");
-            invalidInput.textContent = "아이디를 확인해주세요.";
-            inputBoxEmail.append(invalidInput);
-
-            inputEmail.classList.add('error-border');
-            invalidInput.classList.add('error-text');
-        } else {
-            inputBoxEmail.lastElementChild.textContent = "아이디를 확인해주세요.";
-        }
+    if(inputEmail.value !== USER_EMAIL) {
+        showErrorMessage(inputBoxEmail, "아이디를 확인해주세요.");
     }
-    if(inputPassword.value !== "codeit101") {
-        if(inputBoxPassword.lastElementChild === inputPassword) {
-            const nothingInput = document.createElement("p");
-            nothingInput.textContent = "비밀번호를 확인해주세요.";
-            inputBoxPassword.append(nothingInput);
-
-            inputPassword.classList.add('error-border');
-            nothingInput.classList.add('error-text');
-        } else {
-            inputBoxPassword.lastElementChild.textContent = "비밀번호를 확인해주세요.";
-        }
+    if(inputPassword.value !== USER_PASSWORD) {
+        showErrorMessage(inputBoxPassword, "비밀번호를 확인해주세요.");
     }
 }
 // press Enter
-function eventPressEnter(e) {
+function eventKeyUpEnter(e) {
     if(e.keyCode == 13) {
         eventClickBtn();
     }
@@ -123,5 +100,5 @@ function eventClickEye() {
 inputEmail.addEventListener('focusout', eventFocusOutEmail);
 inputPassword.addEventListener('focusout', eventFocusOutPassword);
 loginButton.addEventListener('click', eventClickBtn);
-document.addEventListener('keyup', eventPressEnter);
+document.addEventListener('keyup', eventKeyUpEnter);
 eyeButton.addEventListener('click', eventClickEye);
