c63251b1c1db20c0afe2a4c87bf0432f88bda9a7
Yeojin Yoon <roseiris1001@gmail.com>
2024-01-28 20:58:37 +0900

    5주차 과제

--- js/common.js
+++ js/common.js
@@ -1,2 +1,8 @@
 const $ = (select, el = document) => el.querySelector(select);
-export { $ };
+
+const SIGN_INFO_REGEX = {
+  email: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/,
+  password: /^[A-Za-z0-9]{8,15}$/,
+};
+
+export { $, SIGN_INFO_REGEX };
--- js/signin.js
+++ js/signin.js
@@ -1,121 +1,82 @@
-import { $ } from './common.js';
-// const $ = (select, el = document) => el.querySelector(select);
-// console.log(a);
-const $emailInput = $('#email-input');
-const $emptyEmailErrorMsg = $('.empty-email-error-msg');
-const $wrongEmailErrorMsg = $('.wrong-email-error-msg');
+import { $, SIGN_INFO_REGEX } from './common.js';
+import {
+  setEmailErrorStyle,
+  resetEmailStyle,
+  setPasswordErrorStyle,
+  resetPasswordStyle,
+  styleLoginError,
+} from './signErrorStyle.js';
+import { hidePassword, showPassword } from './passwordShowStyle.js';
 
-$emailInput.addEventListener('blur', (e) => {
-  const EMAIL_REGEX = {
-    email: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/,
-  };
+const $jsEmailInput = $('.js-email-input');
+const $jsPasswordInput = $('.js-password-input');
 
+$jsEmailInput.addEventListener('blur', (e) => {
   resetEmailStyle();
 
   if (e.target.value === '') {
-    setEmptyEmailStyle();
+    setEmailErrorStyle('이메일을 입력해주세요.');
     return;
   }
-  if (!EMAIL_REGEX.email.test(e.target.value)) {
-    setWrongEmailStyle();
+
+  if (!SIGN_INFO_REGEX.email.test(e.target.value)) {
+    setEmailErrorStyle('올바른 이메일 주소가 아닙니다.');
+    return;
   }
 });
 
-const setEmptyEmailStyle = () => {
-  $emptyEmailErrorMsg.style.display = 'block';
-  $emailInput.style.border = '1px solid var(--red)';
-};
-
-const setWrongEmailStyle = () => {
-  $wrongEmailErrorMsg.style.display = 'block';
-  $emailInput.style.border = '1px solid var(--red)';
-};
-
-const $passwordInput = $('#passwordInput');
-const $emptyPasswordErrorMsg = $('.empty-password-error-msg');
-
-$passwordInput.addEventListener('blur', (e) => {
+$jsPasswordInput.addEventListener('blur', (e) => {
   resetPasswordStyle();
 
   if (e.target.value === '') {
-    setEmptyPasswordStyle('password');
+    setPasswordErrorStyle('비밀번호를 입력해주세요.');
+    return;
+  }
+
+  if (!SIGN_INFO_REGEX.password.test(e.target.value)) {
+    setPasswordErrorStyle('비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
   }
 });
 
-const setEmptyPasswordStyle = () => {
-  $emptyPasswordErrorMsg.style.display = 'block';
-  $passwordInput.style.border = '1px solid var(--red)';
-};
+const ENTER_KEY = 'Enter';
 
-const resetPasswordStyle = () => {
-  $passwordInput.style.border = '1px solid var(--primary)';
-  $emptyPasswordErrorMsg.style.display = 'none';
-  $checkPasswordErrorMsg.style.display = 'none';
-};
+$jsEmailInput.addEventListener('keydown', (e) => {
+  e.key === ENTER_KEY && $jsPasswordInput.focus();
+});
 
-const resetEmailStyle = () => {
-  $emptyEmailErrorMsg.style.display = 'none';
-  $wrongEmailErrorMsg.style.display = 'none';
-  $checkEmailErrorMsg.style.display = 'none';
-  $emailInput.style.border = '1px solid var(--primary)';
-};
+$jsPasswordInput.addEventListener('keydown', (e) => {
+  const ENTER_KEY = 'Enter';
+  e.key === ENTER_KEY && $loginButton.click();
+});
+
+const $btnPasswordIcon = $('.btn-password-icon');
+
+$btnPasswordIcon.addEventListener('click', () => {
+  if ($jsPasswordInput.type === 'text') {
+    hidePassword($jsPasswordInput);
+    return;
+  }
+  if ($jsPasswordInput.type === 'password') {
+    showPassword($jsPasswordInput);
+  }
+});
 
 const $loginButton = $('.js-btn-login');
-const $checkEmailErrorMsg = $('.check-email-error-msg');
-const $checkPasswordErrorMsg = $('.check-password-error-msg');
+const TEST_EMAIL_ADDRESS = 'test@codeit.com';
 
-const loginBtnClick = () => {
+$loginButton.addEventListener('click', () => {
   resetEmailStyle();
   resetPasswordStyle();
 
-  const TEST_EMAIL_ADDRESS = 'test@codeit.com';
   const TEST_PASSWORD = 'codeit101';
 
   if (
-    $emailInput.value !== TEST_EMAIL_ADDRESS ||
-    $passwordInput.value !== TEST_PASSWORD
+    $jsEmailInput.value !== TEST_EMAIL_ADDRESS ||
+    $jsPasswordInput.value !== TEST_PASSWORD
   ) {
     styleLoginError('check');
     return;
   }
 
   location.href = './folder.html';
-};
-
-const styleLoginError = () => {
-  $checkEmailErrorMsg.style.display = 'block';
-  $checkPasswordErrorMsg.style.display = 'block';
-  $emailInput.style.border = '1px solid var(--red)';
-  $passwordInput.style.border = '1px solid var(--red)';
-};
-
-$emailInput.addEventListener('keydown', (e) => {
-  const ENTER_KEY = 'Enter';
-  e.key === ENTER_KEY && $('#passwordInput').focus();
 });
-
-const $btnPasswordIcon = $('.btn-password-icon');
-const $iconEyeOpen = $('.icon-eye-open');
-const $iconEyeClose = $('.icon-eye-close');
-
-$btnPasswordIcon.addEventListener('click', () => {
-  if ($passwordInput.type === 'text') {
-    hidePassword();
-    return;
-  }
-  if ($passwordInput.type === 'password') {
-    showPassword();
-  }
-});
-
-const hidePassword = () => {
-  $passwordInput.type = 'password';
-  $iconEyeOpen.style.display = 'block';
-  $iconEyeClose.style.display = 'none';
-};
-
-const showPassword = () => {
-  $passwordInput.type = 'text';
-  $iconEyeOpen.style.display = 'none';
-  $iconEyeClose.style.display = 'block';
-};
