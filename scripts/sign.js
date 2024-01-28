713ed6c3d979c79eb8ece94cec01321b0784ad45
Junyoung Park <9851248@gmail.com>
2024-01-28 17:16:52 +0900

    refactor: Modify variable name(emailInput, passwordInput to emailElement, passwordElement)

--- scripts/sign.js
+++ scripts/sign.js
@@ -9,22 +9,22 @@ import {
   loginAction
 } from './constant/signConfig.js';
 
-const emailInputId = 'login-email';
-const passwordInputId = 'login-password';
+const emailElementId = 'login-email';
+const passwordElementId = 'login-password';
 const emailErrorId = 'email-error';
 const passwordErrorId = 'password-error';
 const loginBtnId = 'login-btn';
 
 /** @type {HTMLInputElement | null} emailInput*/
-const emailInput = DOMHandler.getById(emailInputId);
-/** @type {HTMLInputElement} passwordInput*/
-const passwordInput = DOMHandler.getById(passwordInputId);
+const emailElement = DOMHandler.getById(emailElementId);
+/** @type {HTMLInputElement} passwordElement*/
+const passwordElement = DOMHandler.getById(passwordElementId);
 /** @type {HTMLButtonElement} loginBtn*/
 const loginBtn = DOMHandler.getById(loginBtnId);
 /** @type {HTMLImageElement} eyeImg*/
 const eyeImg = DOMHandler.getById('eye-img');
-DOMHandler.addPAfterElement(emailInput, emailErrorId, 'error-text');
-DOMHandler.addPAfterElement(passwordInput, passwordErrorId, 'error-text');
+DOMHandler.addPAfterElement(emailElement, emailErrorId, 'error-text');
+DOMHandler.addPAfterElement(passwordElement, passwordErrorId, 'error-text');
 /** @type {HTMLElement} emailError*/
 const emailError = DOMHandler.getById(emailErrorId);
 /** @type {HTMLElement} passwordError*/
@@ -42,68 +42,72 @@ const showErrorMessage = (inputElement, textElement, text) => {
 };
 
 const handleEmailFocusIn = () => {
-  emailInput?.classList.remove('red-box');
+  emailElement?.classList.remove('red-box');
 };
 
 const handlePasswordFocusIn = () => {
-  passwordInput?.classList.remove('red-box');
+  passwordElement?.classList.remove('red-box');
 };
 
 const handleEmailFocusOut = () => {
-  if (InputHandler.isMatchRegEx(emailInput, EMAIL_REGEX)) {
+  if (InputHandler.isMatchRegEx(emailElement, EMAIL_REGEX)) {
     emailError?.classList.add('hidden');
     return;
   }
-  if (InputHandler.isEmptyValue(emailInput)) {
-    showErrorMessage(emailInput, emailError, EMAIL_MESSAGE.empty);
+  if (InputHandler.isEmptyValue(emailElement)) {
+    showErrorMessage(emailElement, emailError, EMAIL_MESSAGE.empty);
     return;
   }
-  showErrorMessage(emailInput, emailError, EMAIL_MESSAGE.invalid);
+  showErrorMessage(emailElement, emailError, EMAIL_MESSAGE.invalid);
 };
 
 const handlePasswordFocusOut = () => {
-  if (InputHandler.isEmptyValue(passwordInput)) {
-    showErrorMessage(passwordInput, passwordError, PASSWORD_MESSAGE.empty);
+  if (InputHandler.isEmptyValue(passwordElement)) {
+    showErrorMessage(passwordElement, passwordError, PASSWORD_MESSAGE.empty);
     return;
   }
   passwordError?.classList.add('hidden');
+};
+
+const isUserSignedUp = (USERS, emailElement, passwordElement) => {
+  return USERS.some(
+    user =>
+      InputHandler.isMatchValue(emailElement, user.id) && InputHandler.isMatchValue(passwordElement, user.password)
+  );
 };
 
 /**
  * @param {Event} event*/
 const handleLogin = event => {
   event?.preventDefault();
-  emailInput?.blur();
-  passwordInput?.blur();
+  emailElement?.blur();
+  passwordElement?.blur();
 
-  const isUserSignedUp = USERS.some(
-    user => InputHandler.isMatchValue(emailInput, user.id) && InputHandler.isMatchValue(passwordInput, user.password)
-  );
-
-  if (isUserSignedUp) {
+  const isUser = isUserSignedUp(USERS, emailElement, passwordElement);
+  if (isUser) {
     loginAction();
     return;
   }
-  showErrorMessage(emailInput, emailError, EMAIL_MESSAGE.fail);
-  showErrorMessage(passwordInput, passwordError, PASSWORD_MESSAGE.fail);
+  showErrorMessage(emailElement, emailError, EMAIL_MESSAGE.fail);
+  showErrorMessage(passwordElement, passwordError, PASSWORD_MESSAGE.fail);
 };
 
 const handleImgeClick = () => {
-  if (!passwordInput || !eyeImg) return;
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
+  if (!passwordElement || !eyeImg) return;
+  if (passwordElement.type === 'password') {
+    passwordElement.type = 'text';
     eyeImg.src = SHOW_PASSWORD_ICON.visible.src;
     eyeImg.alt = SHOW_PASSWORD_ICON.visible.alt;
   } else {
-    passwordInput.type = 'password';
+    passwordElement.type = 'password';
     eyeImg.src = SHOW_PASSWORD_ICON.invisible.src;
     eyeImg.alt = SHOW_PASSWORD_ICON.invisible.alt;
   }
 };
 
-emailInput?.addEventListener('focusout', handleEmailFocusOut);
-emailInput?.addEventListener('focusin', handleEmailFocusIn);
-passwordInput?.addEventListener('focusout', handlePasswordFocusOut);
-passwordInput?.addEventListener('focusin', handlePasswordFocusIn);
+emailElement?.addEventListener('focusout', handleEmailFocusOut);
+emailElement?.addEventListener('focusin', handleEmailFocusIn);
+passwordElement?.addEventListener('focusout', handlePasswordFocusOut);
+passwordElement?.addEventListener('focusin', handlePasswordFocusIn);
 eyeImg?.addEventListener('click', handleImgeClick);
 loginBtn?.addEventListener('click', handleLogin);
