6be3b0ad1ef1d0d10946fffd9102b39ce30df0ea
jinhok96 <59686080+jinhok96@users.noreply.github.com>
2024-01-29 11:43:47 +0900

    [강진호] Week5 (#279)
    * refactor: js파일 구조 변경
    - utility.js 추가
    - signin, signup 페이지 공통 스크립트를 utility.js로 이동
    	new file:   js/utility.js
    	modified:   signin/index.html
    	modified:   signin/index.js
    
    * refactor: 파일 이름 변경
    chore: 필요없는 <script> 제거
    	renamed:    js/utility.js -> js/common_sign.js
    	modified:   signin/index.html
    	modified:   signin/index.js
    
    * refactor: ACTIVE FUNCTION 위치 재조정
    	renamed:    js/common_sign.js -> js/utility.js
    	modified:   signin/index.js
    	modified:   signup/index.html
    	new file:   signup/index.js
    
    * chore: isValidEmail -> emailInput.value 전달 파라미터 추가
    	modified:   js/utility.js
    	modified:   signin/index.js
    	modified:   signup/index.js
    
    * chore: 이전 커밋과 동일
    	modified:   signup/index.js
    
    * feat: 비밀번호 양식 검사 기능 추가
    feat: 이메일 중복 검사 기능 추가
    	modified:   js/utility.js
    	modified:   signup/index.js
    
    * chore: 잘못된 함수 적용 수정
    	modified:   signup/index.js
    
    * refactor: UTILITY FUNCTION을 utility.js로 이동
    	modified:   js/utility.js
    	modified:   signup/index.js
    
    * feat: 비밀번호 확인 유효성 검사 기능 추가
    	modified:   js/utility.js
    	modified:   signup/index.js
    
    * fix: 잘못된 텍스트 수정
    	modified:   signup/index.html
    
    * feat: 회원가입 로직 추가
    	modified:   signup/index.js
    
    * chore: eyeBtn 쿼리 수정, eyeBtnCheck 추가
    	modified:   js/utility.js
    	modified:   signin/index.html
    	modified:   signup/index.html
    	modified:   signup/index.js
    
    * fix: 비밀번호 양식 로직 수정
    fix: 로그인, 회원가입 함수 파라미터 수정
    	modified:   js/utility.js
    	modified:   signin/index.js
    	modified:   signup/index.js

--- signin/index.js
+++ signin/index.js
@@ -1,93 +1,18 @@
-/********************
- * VARIABLE DEFINTION
- ********************/
-
-//이메일, 비밀번호 input
-const emailInput = document.querySelector("#input-email");
-const passwordInput = document.querySelector("#input-password");
-
-//이메일, 비밀번호 error-message
-const emailErrorMessageElement = document.querySelector("#error-email");
-const passwordErrorMessageElement = document.querySelector("#error-password");
-
-//로그인 버튼
-const loginBtn = document.querySelector(".btn-login");
-
-//비밀번호 btn-eye
-const eyeBtn = document.querySelector(".btn-eye");
-
-//유효한 로그인 정보
-const VALID_USER = {
-  email: "test@codeit.com",
-  password: "codeit101",
-};
-
-/********************
- * UTILITY FUNCTION
- ********************/
-
-//인풋이 비어있는지 검사
-function isEmpty(inputValue) {
-  if (inputValue === "") {
-    return true;
-  }
-  return false;
-}
-
-//이메일 양식
-function validateEmail(emailString) {
-  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  return regex.test(emailString);
-}
-
-//이메일 양식 유효성 검사
-function isValidEmail() {
-  if (validateEmail(emailInput.value)) {
-    return true;
-  }
-  return false;
-}
-
-//인풋 에러 켜기 -> 에러 테두리 스타일 추가, 에러 메시지 출력
-function showError(input, errorElement, message) {
-  input.classList.add("sign-input-error");
-  errorElement.textContent = message;
-  errorElement.classList.remove("hidden");
-}
-
-//인풋 에러 끄기 -> 에러 테두리 스타일 제거, 에러 메시지 숨기기
-function hideError(input, errorElement) {
-  input.classList.remove("sign-input-error");
-  errorElement.textContent = "";
-  errorElement.classList.add("hidden");
-}
-
-//유효한 유저 정보 반환
-function getUserByLoginInfo(emailInputValue, passwordInputValue) {
-  if (emailInputValue === VALID_USER.email && passwordInputValue === VALID_USER.password) {
-    return VALID_USER;
-  }
-  return null;
-}
-
-//로그인 유효성 검사
-function isValidUser(emailInput, passwordInput) {
-  return getUserByLoginInfo(emailInput.value, passwordInput.value);
-}
-
-//eyeBtn 비밀번호 보이게
-function showPassword() {
-  const imgElement = document.querySelector(".icon-eye");
-  passwordInput.type = "text";
-  imgElement.src = "../public/icon/eye-on.svg";
-}
-
-//eyeBtn 비밀번호 안보이게
-function hidePassword() {
-  const imgElement = document.querySelector(".icon-eye");
-  passwordInput.type = "password";
-  imgElement.src = "../public/icon/eye-off.svg";
-}
+import {
+  emailInput,
+  passwordInput,
+  emailErrorMessageElement,
+  passwordErrorMessageElement,
+  loginBtn,
+  eyeBtn,
+  isEmpty,
+  isValidEmail,
+  showError,
+  hideError,
+  isValidUser,
+  showPassword,
+  hidePassword,
+} from "../js/utility.js";
 
 /********************
  * ACTIVE FUNCTION
@@ -97,40 +22,42 @@ function hidePassword() {
 function checkEmailError() {
   if (isEmpty(emailInput.value)) {
     return showError(emailInput, emailErrorMessageElement, "이메일을 입력해 주세요.");
-  } else if (!isValidEmail()) {
-    return showError(emailInput, emailErrorMessageElement, "올바른 이메일 주소가 아닙니다.");
-  } else {
-    return hideError(emailInput, emailErrorMessageElement);
   }
+
+  if (!isValidEmail(emailInput.value)) {
+    return showError(emailInput, emailErrorMessageElement, "올바른 이메일 주소가 아닙니다.");
+  }
+
+  return hideError(emailInput, emailErrorMessageElement);
 }
 
 //비밀번호 에러 검사
 function checkPasswordError() {
   if (isEmpty(passwordInput.value)) {
     return showError(passwordInput, passwordErrorMessageElement, "비밀번호를 입력해 주세요.");
-  } else {
-    return hideError(passwordInput, passwordErrorMessageElement);
   }
+
+  return hideError(passwordInput, passwordErrorMessageElement);
 }
 
 //로그인 성공/실패
 function checkLogin() {
-  if (isValidUser(emailInput, passwordInput)) {
+  if (isValidUser(emailInput.value, passwordInput.value)) {
     return (location.href = "../folder/index.html");
-  } else {
-    showError(emailInput, emailErrorMessageElement, "이메일을 확인해 주세요.");
-    showError(passwordInput, passwordErrorMessageElement, "비밀번호를 확인해 주세요.");
-    return;
   }
+
+  showError(emailInput, emailErrorMessageElement, "이메일을 확인해 주세요.");
+  showError(passwordInput, passwordErrorMessageElement, "비밀번호를 확인해 주세요.");
+  return;
 }
 
 //eyeBtn 비밀번호 토글
 function togglePassword() {
   if (passwordInput.type === "password") {
     return showPassword();
-  } else {
-    return hidePassword();
   }
+
+  return hidePassword();
 }
 
 /********************
---
+++ signup/index.js
@@ -0,0 +1,124 @@
+import {
+  emailInput,
+  passwordInput,
+  passwordCheckInput,
+  emailErrorMessageElement,
+  passwordErrorMessageElement,
+  passwordCheckErrorMessageElement,
+  loginBtn,
+  eyeBtn,
+  eyeBtnCheck,
+  isEmpty,
+  isValidEmail,
+  isExistingEmail,
+  isValidPassword,
+  isSamePassword,
+  showError,
+  hideError,
+  isValidSignUp,
+  showPassword,
+  hidePassword,
+  showPasswordCheck,
+  hidePasswordCheck,
+} from "../js/utility.js";
+
+/********************
+ * ACTIVE FUNCTION
+ ********************/
+
+//이메일 에러 검사
+function checkEmailError() {
+  if (isEmpty(emailInput.value)) {
+    return showError(emailInput, emailErrorMessageElement, "이메일을 입력해 주세요.");
+  }
+
+  if (!isValidEmail(emailInput.value)) {
+    return showError(emailInput, emailErrorMessageElement, "올바른 이메일 주소가 아닙니다.");
+  }
+
+  if (isExistingEmail(emailInput.value)) {
+    return showError(emailInput, emailErrorMessageElement, "이미 사용 중인 이메일입니다.");
+  }
+
+  return hideError(emailInput, emailErrorMessageElement);
+}
+
+//비밀번호 에러 검사
+function checkPasswordError() {
+  if (isEmpty(passwordInput.value)) {
+    return showError(passwordInput, passwordErrorMessageElement, "비밀번호를 입력해 주세요.");
+  }
+
+  if (!isValidPassword(passwordInput.value)) {
+    return showError(passwordInput, passwordErrorMessageElement, "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+  }
+
+  return hideError(passwordInput, passwordErrorMessageElement);
+}
+
+//비밀번호 확인 에러 검사
+function checkPasswordCheckError() {
+  if (isEmpty(passwordCheckInput.value)) {
+    return showError(passwordCheckInput, passwordCheckErrorMessageElement, "비밀번호를 입력해 주세요.");
+  }
+
+  if (!isSamePassword(passwordInput.value, passwordCheckInput.value)) {
+    return showError(passwordCheckInput, passwordCheckErrorMessageElement, "비밀번호가 일치하지 않아요.");
+  }
+
+  return hideError(passwordCheckInput, passwordCheckErrorMessageElement);
+}
+
+//회원가입 성공/실패
+function checkSignUp() {
+  console.log(isValidSignUp(emailInput.value, passwordInput.value, passwordCheckInput.value));
+  if (isValidSignUp(emailInput.value, passwordInput.value, passwordCheckInput.value)) {
+    return (location.href = "../folder/index.html");
+  }
+
+  checkEmailError();
+  checkPasswordError();
+  checkPasswordCheckError();
+  return;
+}
+
+//eyeBtn 비밀번호 토글
+function togglePassword() {
+  if (passwordInput.type === "password") {
+    return showPassword();
+  }
+
+  return hidePassword();
+}
+
+//eyeBtnCheck 비밀번호 확인 토글
+function togglePasswordCheck() {
+  if (passwordCheckInput.type === "password") {
+    return showPasswordCheck();
+  }
+
+  return hidePasswordCheck();
+}
+
+/********************
+ * KEY BINDING
+ ********************/
+
+//엔터 키로 checkSignUp 호출
+function checkSignUpByEnter(event) {
+  if (event.key === "Enter") {
+    checkSignUp();
+  }
+}
+
+/********************
+ * EVENT HANDLER
+ ********************/
+
+emailInput.addEventListener("focusout", checkEmailError);
+passwordInput.addEventListener("focusout", checkPasswordError);
+passwordCheckInput.addEventListener("focusout", checkPasswordCheckError);
+loginBtn.addEventListener("click", checkSignUp);
+document.body.addEventListener("keypress", checkSignUpByEnter);
+eyeBtn.addEventListener("click", togglePassword);
+eyeBtnCheck.addEventListener("click", togglePasswordCheck);
