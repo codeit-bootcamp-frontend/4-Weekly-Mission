a5dd1183d3ec5b8aa3dd12617887598958e4bb87
이서인 <iseoin@iseoin-ui-MacBookAir.local>
2024-01-30 15:21:36 +0900

    Refector: 에러 메시지 함수 선언

--- source/js/signin.js
+++ source/js/signin.js
@@ -8,27 +8,32 @@ const passwordInput = document.getElementById('password');
 
 //email 에러 메시지 호출
 emailInput.addEventListener('focusout', handleEmailCheck);
+function checkError(input, error, message) {
+  input.classList.add('invalid');
+  error.innerHTML = message;
+  return;
+}
 function handleEmailCheck() {
   const isEmailValid = validateEmail(emailInput.value);
   const emailError = document.getElementById('emailError');
+  const emailErrorMessage = emailInput.value.trim() ? '올바른 이메일 주소가 아닙니다.' : '이메일을 입력해 주세요.';
   if (!isEmailValid) {
-    emailInput.classList.add('invalid');
-    emailError.innerHTML = emailInput.value.trim() ? '올바른 이메일 주소가 아닙니다.' : '이메일을 입력해 주세요.';
-    return;
+    checkError(emailInput, emailError, emailErrorMessage);
+  } else {
+    emailInput.classList.remove('invalid');
+    emailError.innerHTML = '';
   }
-  emailInput.classList.remove('invalid');
-  emailError.innerHTML = '';
 }
 
 //password 에러 메시지 호출
 passwordInput.addEventListener('focusout', validatePasswordError);
 function validatePasswordError() {
   const passwordError = document.getElementById('passwordError');
+  const passwordErrorMessage = '비밀번호를 입력해 주세요.';
   if (!passwordInput.value.trim()) {
-    passwordInput.classList.add('invalid');
-    passwordError.innerHTML = '비밀번호를 입력해 주세요.';
-    return;
+    checkError(passwordInput, passwordError, passwordErrorMessage);
+  } else {
+    passwordInput.classList.remove('invalid');
+    passwordError.innerHTML = '';
   }
-  passwordInput.classList.remove('invalid');
-  passwordError.innerHTML = '';
 }
