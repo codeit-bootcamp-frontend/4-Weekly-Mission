f95a8ab9d77f952c5421888d3dab6b443857bffa
이서인 <iseoin@iseoin-ui-MacBookAir.local>
2024-01-30 10:56:41 +0900

    Refector: emailInput 전역변수 지역변수로  수정

--- signin/signin.js
+++ signin/signin.js
@@ -3,14 +3,14 @@ import emailValidate from '../utils/emailValidate.js';
 
 login;
 
-const emailInput = document.querySelector('#email');
-const emailError = document.querySelector('#emailError');
 const passwordInput = document.querySelector('#password');
 
 //email 에러 메시지 호출
 emailInput.addEventListener('focusout', handleEmailCheck);
 function handleEmailCheck() {
   const isEmailValid = emailValidate(emailInput.value);
+  const emailInput = document.querySelector('#email');
+  const emailError = document.querySelector('#emailError');
   if (!isEmailValid) {
     emailInput.classList.add('invalid');
     emailError.innerHTML = emailInput.value.trim() ? '올바른 이메일 주소가 아닙니다.' : '이메일을 입력해 주세요.';
