a3ccc4988ce1c5474acec966c4d40712abfff806
이서인 <iseoin@iseoin-ui-MacBookAir.local>
2024-01-30 10:44:14 +0900

    Style: 이메일 유효성 검사 변수명 수정

--- signin/signin.js
+++ signin/signin.js
@@ -10,8 +10,8 @@ const passwordInput = document.querySelector('#password');
 //email 에러 메시지 호출
 emailInput.addEventListener('focusout', handleEmailCheck);
 function handleEmailCheck() {
-  const checkEmail = emailValidate(emailInput.value);
-  if (!checkEmail) {
+  const isEmailValid = emailValidate(emailInput.value);
+  if (!isEmailValid) {
     emailInput.classList.add('invalid');
     emailError.innerHTML = emailInput.value.trim() ? '올바른 이메일 주소가 아닙니다.' : '이메일을 입력해 주세요.';
     return;
