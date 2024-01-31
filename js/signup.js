e46446aea80c6a6254be47ef885d8285e270b127
armd482 <armd482@naver.com>
2024-01-31 15:40:10 +0900

    Adjust Feedback - changing input value check function in signup

--- js/signup.js
+++ js/signup.js
@@ -7,7 +7,12 @@ import {
   InputFocusEvent,
 } from '/js/login.js';
 
-import {TESTEMAIL, INPUTEMPTY, INPUTINVAILED} from '/js/variable.js';
+import {
+  TESTEMAIL,
+  INPUTEMPTY,
+  INPUTINVAILED,
+  INPUTCORRECT,
+} from '/js/variable.js';
 
 const emailInput = document.querySelector('#email-input');
 const emailWarningText = document.querySelector('#email-warning-text');
@@ -21,7 +26,6 @@ const signupForm = document.querySelector('#signup-form');
 const PASSWORD = 1;
 const PASSWORDCHECK = 2;
 
-let isVailed = [false, false, false];
 let dataInput = {
   email: null,
   password: null,
@@ -43,46 +47,42 @@ const checkPasswordType = value => {
 };
 
 const emailInputFocustOut = () => {
+  console.log('f-email');
   if (dataInput.email === TESTEMAIL) {
     viewWarningText(emailWarningText, '이미 사용 중인 이메일입니다.');
-    isVailed[0] = false;
     return;
   }
 
   const status = checkEmail(dataInput.email);
   if (status === INPUTEMPTY) {
     viewWarningText(emailWarningText, '이메일을 입력해주세요.');
-    isVailed[0] = false;
     return;
   }
 
   if (status === INPUTINVAILED) {
     viewWarningText(emailWarningText, '올바른 이메일 주소가 아닙니다.');
-    isVailed[0] = false;
     return;
   }
-  isVailed[0] = true;
 };
 
 const emailInputFocustIn = () => {
   hiddenWarningText(emailWarningText);
 };
 
+const checkPasswordEqual = () => {
+  return dataInput.password === dataInput.passwordCheck;
+};
+
 const passwordInputFocusOut = target => {
   const type = target.id === 'password-input' ? PASSWORD : PASSWORDCHECK;
   const textTarget =
     target.parentNode.parentNode.querySelector('.warning-text');
-  if (
-    type === PASSWORDCHECK &&
-    dataInput.password !== dataInput.passwordCheck
-  ) {
+  if (type === PASSWORDCHECK && checkPasswordEqual()) {
     viewWarningText(textTarget, '비밀번호가 일치하지 않아요');
-    isVailed[type] = false;
     return;
   }
   if (checkPassword(dataInput.password) === INPUTEMPTY) {
     viewWarningText(textTarget, '비밀번호를 입력해주세요.');
-    isVailed[type] = false;
     return;
   }
 
@@ -91,10 +91,8 @@ const passwordInputFocusOut = target => {
       textTarget,
       '비밀번호는 영문, 숫자 조합 8자 이상 입력해주세요.',
     );
-    isVailed[type] = false;
     return;
   }
-  isVailed[type] = true;
 };
 
 const passwordInputFocustIn = target => {
@@ -107,16 +105,36 @@ const eyeBtnOnclick = target => {
   visiblePassword(target);
 };
 
+const checkInputValidation = ([...args]) => {
+  for (let {target, callback} of args) {
+    for (let method of callback) {
+      if (method(target) !== INPUTCORRECT) {
+        if (method(target) !== true) return false;
+      }
+    }
+  }
+  return true;
+};
+
 const signupBtnOnclick = e => {
   e.preventDefault();
-  emailInputFocustOut(emailInput);
-  passwordInputFocusOut(passwordInput);
-  passwordInputFocusOut(passwordCheckInput);
-  for (let flag of isVailed) {
-    if (!flag) return;
+  let dataInspect = [
+    {target: dataInput.email, callback: [checkEmail]},
+    {target: dataInput.password, callback: [checkPassword]},
+    {
+      target: dataInput.passwordCheck,
+      callback: [checkPassword, checkPasswordEqual],
+    },
+  ];
+
+  const signupPass = checkInputValidation(dataInspect);
+  if (signupPass) {
+    window.location.href = '/page/folder';
+  } else {
+    emailInput.blur(emailInputFocustOut(emailInput));
+    password.blur(passwordInputFocusOut(passwordInput));
+    password.blur(passwordInputFocusOut(passwordCheckInput));
   }
-  console.log(isVailed);
-  window.location.href = '/page/folder';
 };
 
 InputFocusEvent(emailInput, emailInputFocustIn, e =>
