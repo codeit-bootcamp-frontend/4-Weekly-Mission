9d39ea136af87591ddd5ff40edf19503d29e726f
MinCheolS <ay5212@hanmail.net>
2024-01-31 21:04:23 +0900

    Refactor: signin.js 코드 수정

--- signin.js
+++ signin.js
@@ -1,13 +1,14 @@
 import { accountInfo } from "./javascript/account.js"
-import { emailRegex, passwordRegex } from "./javascript/regex.js"
-import { inputFunction } from "./javascript/signFunction.js"
 import { passwordToggle } from "./javascript/passwordToggle.js"
+import { emailFocus, emailBlur, passwordFocus, passwordBlur } from "./javascript/signFunction.js"
 
 // 
 const inputEmail = document.querySelector('.email-input')
 const inputPassword = document.querySelector('.password-input')
 const passwordEncryptButtonList = document.querySelector('.eye-btn')
 const signButton = document.querySelector('.sign-btn')
+const errorEmailMessage = document.querySelector('.error-email-message')
+const errorPasswordMessage = document.querySelector('.error-password-message')
 
 // 로그인 버튼, 경우에 따른 케이스 적용
 const onSubmit = (e) => {
@@ -15,28 +16,27 @@ const onSubmit = (e) => {
 
   const inputEmailValue = inputEmail.value
   const inputPasswordValue = inputPassword.value
-  const matchingAccountEmail = accountInfo.find((item) => item.email === inputEmailValue)
-  const matchingAccountPassword = accountInfo.find((item) => item.password === inputPasswordValue)
+  const matchedEmail = accountInfo.find((item) => item.email === inputEmailValue)?.email
+  const matchedPassword = accountInfo.find((item) => item.password === inputPasswordValue)?.password
   
 
   const inputErrorHandler = (inputType, errorMessage, type) => {
     if (type === 'email') {
       inputType.classList.add('input-error')
-      inputType.parentElement.lastElementChild.textContent = errorMessage
+      errorEmailMessage.textContent = errorMessage
+      return
     }
-    else {
-      inputType.classList.add('input-error')
-      inputType.parentElement.parentElement.lastElementChild.textContent = errorMessage
-    }
+    inputType.classList.add('input-error')
+    errorPasswordMessage.textContent = errorMessage
   }
 
-  if (inputEmailValue === matchingAccountEmail?.email && inputPasswordValue === matchingAccountPassword?.password) {
+  if (inputEmailValue === matchedEmail && inputPasswordValue === matchedPassword) {
     window.location.assign('folder.html')
   }
-  else if (inputEmailValue === matchingAccountEmail?.email && inputPasswordValue !== matchingAccountPassword?.password) {
+  else if (inputEmailValue === matchedEmail && inputPasswordValue !== matchedPassword) {
     inputErrorHandler(inputPassword, '비밀번호를 확인해 주세요', 'password')
   }
-  else if (inputEmailValue !== matchingAccountEmail?.email && inputPasswordValue === matchingAccountPassword?.password) {
+  else if (inputEmailValue !== matchedEmail && inputPasswordValue === matchedPassword) {
     inputErrorHandler(inputEmail, '이메일을 확인해 주세요', 'email')
   }
   else {
@@ -46,15 +46,15 @@ const onSubmit = (e) => {
 }
 
 // 이메일 입력란에 focus, blur 이벤트 추가
-inputEmail.addEventListener('focus', (e) => inputFunction.emailFocus(e))
-inputEmail.addEventListener('blur', (e) => inputFunction.emailBlur(e, emailRegex, accountInfo))
+inputEmail.addEventListener('focus', () => emailFocus(inputEmail, errorEmailMessage))
+inputEmail.addEventListener('blur', () => emailBlur(inputEmail, errorEmailMessage))
 
 // 비밀번호 입력란에 focus, blur 이벤트 추가
-inputPassword.addEventListener('focus', (e) => inputFunction.passwordFocus(e))
-inputPassword.addEventListener('blur', (e) => inputFunction.passwordBlur(e, passwordRegex))
+inputPassword.addEventListener('focus', () => passwordFocus(inputPassword, errorPasswordMessage))
+inputPassword.addEventListener('blur', () => passwordBlur(inputPassword, errorPasswordMessage))
 
 // 비밀번호 암호화 토글 click 이벤트 추가
-passwordEncryptButtonList.addEventListener('click', passwordToggle)
+passwordEncryptButtonList.addEventListener('click', () => passwordToggle(inputPassword))
 
 // 로그인 버튼에 click 이벤트 추가
 signButton.addEventListener('click', onSubmit)
