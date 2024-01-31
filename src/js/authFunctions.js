f06f401193e89f9a2b228d7a5dd44c814ec238f5
yohan11 <yipco77@naver.com>
2024-02-01 00:16:31 +0900

    refactor: 파라미터 타입 체크 추가

--- src/js/authFunctions.js
+++ src/js/authFunctions.js
@@ -2,34 +2,36 @@ import {REGEX_EMAIL, REGEX_PASSWORD, SRC_IMG_CLOSE_EYE, SRC_IMG_OPEN_EYE, TEST_U
 
 // 유효성 검증 관련 함수
 export const isFilledInput = (target) => {
-  return !(target.value === "");
+  return target.value && target.value !== "";
 };
 
 export const isValidEmailForm = (target) => {
-  return REGEX_EMAIL.test(target.value);
+  return target.value && REGEX_EMAIL.test(target.value);
 };
 
 export const isUniqueEmail = (target) => {
-  return target.value !== TEST_USER_EMAIL;
+  return target.value && target.value !== TEST_USER_EMAIL;
 };
 
 export const isValidPasswordForm = (target) => {
-  return REGEX_PASSWORD.test(target.value);
+  return target.value && REGEX_PASSWORD.test(target.value);
 };
 
 export const isMatchWithPassword = (target, inputPassword) => {
-  return target.value === inputPassword.value;
+  return target.value && target.value === inputPassword.value;
 };
 
 // 기타 함수
 export const toggleViewPassword = (target) => {
-  const targetInput = target.parentElement.previousElementSibling;
+  if (target instanceof EventTarget) {
+    const targetInput = target.parentElement.previousElementSibling;
 
-  if (targetInput.getAttribute("type") === "password") {
-    targetInput.setAttribute("type", "text");
-    target.setAttribute("src", SRC_IMG_OPEN_EYE);
-  } else {
-    targetInput.setAttribute("type", "password");
-    target.setAttribute("src", SRC_IMG_CLOSE_EYE);
+    if (targetInput.getAttribute("type") === "password") {
+      targetInput.setAttribute("type", "text");
+      target.setAttribute("src", SRC_IMG_OPEN_EYE);
+    } else {
+      targetInput.setAttribute("type", "password");
+      target.setAttribute("src", SRC_IMG_CLOSE_EYE);
+    }
   }
 };
