021ab561dfd38d1c59215194f905ea71f0d7cd56
ingssg <js2121good@gmail.com>
2024-02-01 17:24:37 +0900

    chore: upload for desktop environment

--- weekly-mission(week2)/src/constants.js
+++ weekly-mission(week2)/src/constants.js
@@ -5,7 +5,7 @@ const MASTER_ACCOUNT = Object.freeze({
 
 const REGEX = Object.freeze({
   EMAIL: /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-  PASSWORD: '',
+  PASSWORD: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/,
 });
 
 const ERROR_MSG = Object.freeze({
