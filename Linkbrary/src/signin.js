db3e95ce169e8378a5cfd22176af53f44c8b12ab
Hooni07 <gkxogns21@gmail.com>
2024-02-01 16:08:02 +0900

    feat: config week6 init files(using week5 template)

--- Linkbrary/src/signin.js
+++ Linkbrary/src/signin.js
@@ -6,6 +6,43 @@ import {
   TEST_USER,
 } from "./utils.js";
 
+const emailInput = document.querySelector("#email");
+const emailErrorMessage = document.querySelector("#email-error-message");
+emailInput.addEventListener("focusout", (event) => validateEmailInput(event.target.value));
+function validateEmailInput(email) {
+  if (email === "") {
+    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 입력해주세요.");
+    return;
+  }
+  if (!isEmailValid(email)) {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "올바른 이메일 주소가 아닙니다."
+    );
+    return;
+  }
+  removeInputError({ input: emailInput, errorMessage: emailErrorMessage });
+}
+
+const passwordInput = document.querySelector("#password");
+const passwordErrorMessage = document.querySelector("#password-error-message");
+passwordInput.addEventListener("focusout", (event) => validatePasswordInput(event.target.value));
+function validatePasswordInput(password) {
+  if (password === "") {
+    setInputError(
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호를 입력해주세요."
+    );
+    return;
+  }
+  removeInputError({ input: passwordInput, errorMessage: passwordErrorMessage });
+}
+
+const passwordToggleButton = document.querySelector("#password-toggle");
+passwordToggleButton.addEventListener("click", () =>
+  togglePassword(passwordInput, passwordToggleButton)
+);
+
 const signForm = document.querySelector("#form");
 signForm.addEventListener("submit", submitForm);
 function submitForm(event) {
--- Linkbrary/src/signup.js
+++ Linkbrary/src/signup.js
@@ -9,38 +9,78 @@ import {
 
 const emailInput = document.querySelector("#email");
 const emailErrorMessage = document.querySelector("#email-error-message");
-emailInput.addEventListener("focusout", (event) => validateEmailDuplicate(event.target.value));
-function validateEmailDuplicate(email) {
-  if (email === TEST_USER.email){
+emailInput.addEventListener("focusout", (event) => validateEmailInput(event.target.value));
+function validateEmailInput(email) {
+  if (email === "") {
+    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 입력해주세요.");
+    return false;
+  }
+  if (!isEmailValid(email)) {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "올바른 이메일 주소가 아닙니다."
+    );
+    return false;
+  }
+  if (email === TEST_USER.email) {
     setInputError(
       { input: emailInput, errorMessage: emailErrorMessage },
       "이미 사용 중인 이메일입니다."
     );
-    return;
+    return false;
   }
   removeInputError({ input: emailInput, errorMessage: emailErrorMessage });
+  return true;
 }
 
-const passwordCheckInput = document.querySelector("#password-check");
-const passwordCheckErrorMessage = document.querySelector("#password-check-error-message");
-let passwordCorrect = true;
-passwordCheckInput.addEventListener("focusout", (event) => validatePasswordCheckInput(event.target.value));
-function validatePasswordCheckInput(password) {
-  passwordCorrect = (password === passwordInput.value);
-  console.log(password, passwordInput.value, isCorrect);
-  if (!passwordCorrect) {
+const passwordInput = document.querySelector("#password");
+const passwordErrorMessage = document.querySelector("#password-error-message");
+passwordInput.addEventListener("focusout", (event) => validatePasswordInput(event.target.value));
+
+function validatePasswordInput(password) {
+  if (password === "" || !isPasswordValid(password)) {
     setInputError(
-      { input: passwordCheckInput, errorMessage: passwordCheckErrorMessage },
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    );
+    return false;
+  }
+  removeInputError({ input: passwordInput, errorMessage: passwordErrorMessage });
+  return true;
+}
+
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const confirmPasswordErrorMessage = document.querySelector("#confirm-password-error-message");
+confirmPasswordInput.addEventListener("focusout", (event) =>
+  validateConfirmPasswordInput(event.target.value)
+);
+function validateConfirmPasswordInput(confirmPassword) {
+  if (confirmPassword === "" || !isPasswordValid(confirmPassword)) {
+    setInputError(
+      { input: confirmPasswordInput, errorMessage: confirmPasswordErrorMessage },
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    );
+    return false;
+  }
+  if (passwordInput.value !== confirmPassword) {
+    setInputError(
+      { input: confirmPasswordInput, errorMessage: confirmPasswordErrorMessage },
       "비밀번호가 일치하지 않아요."
     );
-    return;
+    return false;
   }
-  removeInputError({ input: passwordCheckInput, errorMessage: passwordCheckErrorMessage });
+  removeInputError({ input: confirmPasswordInput, errorMessage: confirmPasswordErrorMessage });
+  return true;
 }
 
-const passwordCheckToggleButton = document.querySelector("#password-check-toggle");
-passwordCheckToggleButton.addEventListener("click", () =>
-  togglePassword(passwordCheckInput, passwordCheckToggleButton)
+const passwordToggleButton = document.querySelector("#password-toggle");
+passwordToggleButton.addEventListener("click", () =>
+  togglePassword(passwordInput, passwordToggleButton)
+);
+
+const confirmPasswordToggleButton = document.querySelector("#confirm-password-toggle");
+confirmPasswordToggleButton.addEventListener("click", () =>
+  togglePassword(confirmPasswordInput, confirmPasswordToggleButton)
 );
 
 const signForm = document.querySelector("#form");
@@ -48,19 +88,11 @@ signForm.addEventListener("submit", submitForm);
 function submitForm(event) {
   event.preventDefault();
 
-  const isTestUser 
-  = isEmailValid(emailInput.value) && isPasswordValid(passwordInput.value)
-    && passwordCorrect && (emailInput.value !== TEST_USER.email);
+  const isEmailInputValid = validateEmailInput(emailInput.value);
+  const isPasswordInputValid = validatePasswordInput(passwordInput.value);
+  const isConfirmPasswordInputValid = validateConfirmPasswordInput(confirmPasswordInput.value);
 
-  if (isTestUser) {
+  if (isEmailInputValid && isPasswordInputValid && isConfirmPasswordInputValid) {
     location.href = "/folder";
-    return;
   }
-  setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 확인해주세요.");
-  setInputError(
-    { input: passwordInput, errorMessage: passwordErrorMessage },
-    "비밀번호를 확인해주세요."
-  );
-  setInputError({ input: passwordCheckInput, errorMessage: passwordCheckErrorMessage },
-    "비밀번호가 일치한지 확인해주세요." );
 }
