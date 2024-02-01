fbc092a58896d38ce997d05262f8fdf4cc9c1c76
jinhok96 <jinhok96a@gmail.com>
2024-02-01 16:29:56 +0900

    test: 함수 분배 	modified:   scripts/ui-sign.js 	modified:   scripts/utils-sign.js

--- scripts/ui-sign.js
+++ scripts/ui-sign.js
@@ -18,50 +18,9 @@ const passwordCheckErrorMessageElement = document.querySelector("#error-password
 const eyeBtn = document.querySelector("#password-eye");
 const eyeBtnCheck = document.querySelector("#password-check-eye");
 
-//유효한 로그인 정보
-const VALID_USER = {
-  email: "test@codeit.com",
-  password: "codeit101",
-};
-
 /********************
  * UTILITY FUNCTION
  ********************/
-
-//이메일 양식 유효성 검사
-function isValidEmailFormat(emailInputValue) {
-  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  return regex.test(emailInputValue);
-}
-
-//회원가입 이메일 중복 검사
-function isEmailMatching(emailInputValue) {
-  return emailInputValue === VALID_USER.email;
-}
-
-//회원가입 비밀번호 양식 유효성 검사
-function isValidPasswordFormat(passwordInputValue) {
-  //8자 미만인 경우
-  if (passwordInputValue.length < 8) {
-    return false;
-  }
-
-  //숫자가 없는 경우
-  if (!/\d/.test(passwordInputValue)) {
-    return false;
-  }
-
-  //영문이 없는 경우
-  if (!/[a-zA-Z]/.test(passwordInputValue)) {
-    return false;
-  }
-  return true;
-}
-
-//회원가입 비밀번호 확인 유효성 검사
-function isPasswordMatching(passwordInputValue, passwordCheckInputValue) {
-  return passwordInputValue === passwordCheckInputValue;
-}
 
 //인풋 에러 켜기 -> 에러 테두리 스타일 추가, 에러 메시지 출력
 function showError(input, errorElement, message) {
@@ -75,49 +34,6 @@ function hideError(input, errorElement) {
   input.classList.remove("sign-input-error");
   errorElement.textContent = "";
   errorElement.classList.add("hidden");
-}
-
-//일치하는 유저 데이터 찾아서 반환
-function getValidUserInfo({ emailInputValue, passwordInputValue }) {
-  if (VALID_USER.email !== emailInputValue) {
-    return null;
-  }
-
-  if (VALID_USER.password !== passwordInputValue) {
-    return null;
-  }
-
-  return VALID_USER;
-}
-
-//로그인 유효성 검사
-function isValidUser(emailInputValue, passwordInputValue) {
-  return getValidUserInfo({ emailInputValue, passwordInputValue });
-}
-
-//회원가입 유효성 검사
-function isValidSignUp(emailInputValue, passwordInputValue, passwordCheckInputValue) {
-  if (!isValidEmailFormat(emailInputValue)) {
-    console.log(`case 1`);
-    return false;
-  }
-
-  if (isEmailMatching(emailInputValue)) {
-    console.log(`case 2`);
-    return false;
-  }
-
-  if (!isValidPasswordFormat(passwordInputValue)) {
-    console.log(`case 3`);
-    return false;
-  }
-
-  if (!isPasswordMatching(passwordInputValue, passwordCheckInputValue)) {
-    console.log(`case 4`);
-    return false;
-  }
-
-  return true;
 }
 
 //eyeBtn 비밀번호 보이게
@@ -147,3 +63,5 @@ function hidePasswordCheck() {
   passwordCheckInput.type = "password";
   imgElement.src = "../public/icon/eye-off.svg";
 }
+
+export {};
