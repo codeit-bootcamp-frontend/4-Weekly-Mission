5389bb2989c01e37bb3a7726545bdeef6c537ab5
dali1999 <dali1999@naver.com>
2024-02-02 04:00:06 +0900

    feat: 로그인 관련 POST요청 처리 기능 추가

--- script/signin.js
+++ script/signin.js
@@ -1,5 +1,4 @@
 import {
-  signForm,
   emailInput,
   passwordInput,
   emailErrorMessage,
@@ -31,21 +30,39 @@ passwordInput.addEventListener("focusout", () => {
 });
 
 //loginBtn click event
-loginBtn.addEventListener("click", (event) => {
-  if (
-    emailInput.value === "test@codeit.com" &&
-    passwordInput.value === "codeit101"
-  ) {
-    signForm.submit();
-  } else {
-    if (emailInput.value !== "test@codeit.com") {
-      emailErrorMessage.textContent = "이메일을 확인해 주세요";
-    }
-    if (passwordInput.value !== "codeit101") {
-      passwordErrorMessage.textContent = "비밀번호를 확인해 주세요";
-    }
-  }
+loginBtn.addEventListener("click", async (event) => {
   event.preventDefault();
+  try {
+    // if (emailInput.value !== "test@codeit.com") {
+    //   emailErrorMessage.textContent = "이메일을 확인해 주세요";
+    // } else if (passwordInput.value !== "codeit101") {
+    //   passwordErrorMessage.textContent = "비밀번호를 확인해 주세요";
+    // }
+
+    const signInUrl = "https://bootcamp-api.codeit.kr/api/sign-in";
+    const signInData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    const response = await fetch(signInUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signInData),
+    });
+    const signInDataJson = await response.json();
+    const accessToken = signInDataJson.data.accessToken;
+    if (response.ok) {
+      window.location.href = "/folder";
+      localStorage.setItem("access-token", accessToken);
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    console.log(`ERROR: ${error.message}`);
+  }
 });
 
 //eye-icon click event
