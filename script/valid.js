7e461b809a83c0bb10c80efbe9e1264e66275e0e
choiwonseokgit <cs0328@hanmail.net>
2024-02-02 22:52:47 +0900

    refactor: valid.js 피드백 내용대로 중복되는 코드 제거

--- script/valid.js
+++ script/valid.js
@@ -2,11 +2,7 @@
 function emailValid(email) {
   const EXIST_EMAIL = "test@codeit.com";
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 간단한 이메일 유효성 검사 정규식
-  if (email !== EXIST_EMAIL && email !== "" && emailRegex.test(email)) {
-    return true;
-  } else {
-    return false;
-  }
+  return email !== EXIST_EMAIL && email !== "" && emailRegex.test(email);
 }
 
 //비밀번호 유효성을 검사하는 함수입니다.
@@ -21,7 +17,7 @@ function passwordValid(password) {
 
 //비밀번호가 같은지 검사하는 함수입니다.
 function isPasswordSame(password1, password2) {
-  return password1 === password2 ? true : false;
+  return password1 === password2;
 }
 
 export { emailValid, passwordValid, isPasswordSame };
