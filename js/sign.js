b91bdb5cd04f3b134baba2c3259873b0e91b3b2f
LeeSeungHoon <ysh73900@gmail.com>
2024-02-02 16:47:54 +0900

    feat: signInApi Api호출함수, refactor: redirectToFolderPage함수

--- js/sign.js
+++ js/sign.js
@@ -10,6 +10,7 @@ import {
   handleEmailValidation,
   togglePasswordVisibility,
   enterKey,
+  redirectFolder,
 } from "./modules/auth-utils.js";
 
 const loginButton = document.querySelector(".login-button");
@@ -27,15 +28,42 @@ const handlePasswordValidation = () => {
   }
 };
 
+/* 특정 이메일과 비밀번호를 사용하여 로그인을 시도하는 API 호출 함수 */
+const signInApi = async (email, password) => {
+  try {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    console.error("URL 호출 실패:", error.message);
+  }
+};
+
 /* 특정 아이디 및 비밀번호로 로그인 시도 시 페이지 이동 */
-const redirectToFolderPage = () => {
-  if (
-    email.value === errorMessage.ALREADY_EMAIL &&
-    password.value === "codeit101"
-  ) {
-    const link = "./folder.html";
-    location.href = link;
-  } else {
+const redirectToFolderPage = async () => {
+  try {
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+    if (
+      emailValue === errorMessage.ALREADY_EMAIL &&
+      passwordValue === "sprint101"
+    ) {
+      const dataValue = await signInApi(emailValue, passwordValue);
+      if (dataValue) {
+        redirectFolder();
+      }
+    }
+  } catch (error) {
+    console.error("로그인 에러:", error.message);
     setInvalidStyle(email);
     setInvalidStyle(password);
     emailError.innerHTML = errorMessage.EMAIL_CHECK_MESSAGE;
