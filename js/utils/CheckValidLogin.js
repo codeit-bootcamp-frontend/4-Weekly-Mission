10c10f93e81f2651229415cbc909abd49e648673
codingaring <sksms2104@naver.com>
2024-02-02 20:05:24 +0900

    [Refactor] validEmail, validPassword 등 상수화 시켜서 분리

--- js/utils/CheckValidLogin.js
+++ js/utils/CheckValidLogin.js
@@ -1,6 +1,5 @@
 // 유효한 email, password 값
-const validEmail = "test@codeit.com";
-const validPassword = "codeit101";
+import { validEmail, validPassword } from "./constants.js";
 
 /** 로그인 정보 맞으면 0, 비밀번호만 틀리면 -1, 둘 다 틀리면 0 반환 */
 const checkValidLogin = function (email, password) {
@@ -13,4 +12,4 @@ const checkValidLogin = function (email, password) {
   }
 };
 
-export { validEmail, checkValidLogin };
+export { checkValidLogin };
---
+++ js/utils/constants.js
@@ -0,0 +1,5 @@
+// 유효한 email, password 값
+const validEmail = "test@codeit.com";
+const validPassword = "codeit101";
+
+export { validEmail, validPassword };
