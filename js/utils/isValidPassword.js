9531bcca5c399d438bccf32e41f0947a2b63d6f8
codingaring <sksms2104@naver.com>
2024-02-02 19:52:21 +0900

    [Refactor] 정규식 선언을 함수 안에서 하는 것으로 변경

--- js/utils/isValidPassword.js
+++ js/utils/isValidPassword.js
@@ -1,8 +1,7 @@
-// 비밀번호 정규식
-const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
 /** 입력받은 비밀번호가 정규식을 통과하는지 출력 */
 function isVaildPassword(password) {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
   return passwordRegex.test(password);
 }
 
