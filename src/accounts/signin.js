228eca68c81ae0ebad917b88682d4c877bbb8193
parkwoohyeok <dngur3111@naver.com>
2024-02-02 15:02:17 +0900

    add sign in/up request

--- src/accounts/signin.js
+++ src/accounts/signin.js
@@ -8,10 +8,15 @@ import {
 
 const emailInput = document.querySelector("#email");
 const emailErrorMessage = document.querySelector("#email-error-message");
-emailInput.addEventListener("focusout", (event) => validateEmailInput(event.target.value));
+emailInput.addEventListener("focusout", (event) =>
+  validateEmailInput(event.target.value)
+);
 function validateEmailInput(email) {
   if (email === "") {
-    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 입력해주세요.");
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 입력해주세요."
+    );
     return;
   }
   if (!isEmailValid(email)) {
@@ -26,7 +31,9 @@ function validateEmailInput(email) {
 
 const passwordInput = document.querySelector("#password");
 const passwordErrorMessage = document.querySelector("#password-error-message");
-passwordInput.addEventListener("focusout", (event) => validatePasswordInput(event.target.value));
+passwordInput.addEventListener("focusout", (event) =>
+  validatePasswordInput(event.target.value)
+);
 function validatePasswordInput(password) {
   if (password === "") {
     setInputError(
@@ -35,7 +42,10 @@ function validatePasswordInput(password) {
     );
     return;
   }
-  removeInputError({ input: passwordInput, errorMessage: passwordErrorMessage });
+  removeInputError({
+    input: passwordInput,
+    errorMessage: passwordErrorMessage,
+  });
 }
 
 const passwordToggleButton = document.querySelector("#password-toggle");
@@ -43,21 +53,37 @@ passwordToggleButton.addEventListener("click", () =>
   togglePassword(passwordInput, passwordToggleButton)
 );
 
+//6주차 추가 - 아이디 확인 리퀘스트
+async function signInRequest(id, pass) {
+  const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(TEST_USER),
+  });
+  if (
+    id === TEST_USER.email &&
+    pass === TEST_USER.password &&
+    response.status === 200
+  ) {
+    location.href = "/folder";
+  } else {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 확인해주세요."
+    );
+    setInputError(
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호를 확인해주세요."
+    );
+  }
+  return;
+}
+
 const signForm = document.querySelector("#form");
 signForm.addEventListener("submit", submitForm);
 function submitForm(event) {
   event.preventDefault();
-
-  const isTestUser =
-    emailInput.value === TEST_USER.email && passwordInput.value === TEST_USER.password;
-
-  if (isTestUser) {
-    location.href = "/folder";
-    return;
-  }
-  setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 확인해주세요.");
-  setInputError(
-    { input: passwordInput, errorMessage: passwordErrorMessage },
-    "비밀번호를 확인해주세요."
-  );
+  signInRequest(emailInput.value, passwordInput.value);
 }
