2ca27ef048c22db5e935de917c0c006cd95f0538
seolsis <seoljw1005@naver.com>
2024-02-04 02:31:43 +0900

    feat: 이메일 중복 확인 POST 요청, 유효한 회원가입 경우 /folder 페이지로 이동

--- js/signup.js
+++ js/signup.js
@@ -1,25 +1,25 @@
-const emailInput = document.querySelector('.LoginInputEmail');
-const pwInput = document.querySelector('.SignUpPwInput');
-const pwCorrectInput = document.querySelector('.SignUpPwCorrectInput');
-const eyeIcon = document.querySelector('.eye-button');
+const emailInput = document.querySelector(".LoginInputEmail");
+const pwInput = document.querySelector(".SignUpPwInput");
+const pwCorrectInput = document.querySelector(".SignUpPwCorrectInput");
+const eyeIcon = document.querySelector(".eye-button");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-const emailError = document.querySelector('.LoginEmailError');
-const pwError = document.querySelector('.LoginPwError');
-const pwCorrectError = document.querySelector('.LoginPwCorrectError');
+const emailError = document.querySelector(".LoginEmailError");
+const pwError = document.querySelector(".LoginPwError");
+const pwCorrectError = document.querySelector(".LoginPwCorrectError");
 
 // 비밀번호 토글 이벤트
 function toggleEyePw(event) {
   event.preventDefault();
-  console.log('toggleEyePw function called');
-  const type = pwInput.getAttribute('type');
-  if(type==='password') {
-    pwInput.setAttribute('type', 'password');
-    eyeIcon.src = './images/eye-on.svg';
+  console.log("toggleEyePw function called");
+  const type = pwInput.getAttribute("type");
+  if (type === "password") {
+    pwInput.setAttribute("type", "password");
+    eyeIcon.src = "./images/eye-on.svg";
   } else {
-    pwInput.setAttribute('type', 'text');
-    eyeIcon.src = './images/eye-off.svg';
+    pwInput.setAttribute("type", "text");
+    eyeIcon.src = "./images/eye-off.svg";
   }
 }
 
@@ -37,11 +37,11 @@ function isValidPassword(password) {
 function validateEmail() {
   let emailValue = emailInput.value.trim();
   if (!emailValue) {
-    showError(emailError, '이메일을 입력해 주세요.');
-  } else if(!isValidEmail(emailValue)) {
-    showError(emailError, '올바른 이메일 주소가 아닙니다.');
-  } else if (emailValue === 'test@codeit.com') {
-    showError(emailError, '이미 사용 중인 이메일입니다.');
+    showError(emailError, "이메일을 입력해 주세요.");
+  } else if (!isValidEmail(emailValue)) {
+    showError(emailError, "올바른 이메일 주소가 아닙니다.");
+  } else if (emailValue === "test@codeit.com") {
+    showError(emailError, "이미 사용 중인 이메일입니다.");
   } else {
     hideError(emailError);
   }
@@ -50,10 +50,10 @@ function validateEmail() {
 // 비밀번호 입력 검사
 function validatePassword() {
   let passwordValue = pwInput.value.trim();
-  if(!passwordValue) {
-    showError(pwError, '비밀번호를 입력해 주세요.'); 
+  if (!passwordValue) {
+    showError(pwError, "비밀번호를 입력해 주세요.");
   } else if (!isValidPassword(passwordValue)) {
-    showError(pwError, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'); 
+    showError(pwError, "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
   } else {
     hideError(pwError);
   }
@@ -64,7 +64,7 @@ function correctPassword() {
   let passwordValue1 = pwInput.value.trim();
   let passwordValue2 = pwCorrectInput.value.trim();
   if (passwordValue1 !== passwordValue2) {
-    showError(pwCorrectError, '비밀번호가 일치하지 않아요.')
+    showError(pwCorrectError, "비밀번호가 일치하지 않아요.");
   } else {
     hideError(pwCorrectError);
   }
@@ -73,57 +73,103 @@ function correctPassword() {
 // error 보여줌
 function showError(element, message) {
   element.textContent = message;
-  element.style.display = 'block';
-  if (element.classList.contains('LoginEmailError')) {
-    element.previousElementSibling.style.border = '1px solid red';
-  } else if (element.classList.contains('LoginPwError')) {
-    const passwordInput = element.previousElementSibling.querySelector('.SignUpPwInput');
+  element.style.display = "block";
+  if (element.classList.contains("LoginEmailError")) {
+    element.previousElementSibling.style.border = "1px solid red";
+  } else if (element.classList.contains("LoginPwError")) {
+    const passwordInput =
+      element.previousElementSibling.querySelector(".SignUpPwInput");
     if (passwordInput) {
-      passwordInput.style.border = '1px solid red';
+      passwordInput.style.border = "1px solid red";
     }
-  } else if (element.classList.contains('LoginPwCorrectError')) {
-    const correctInput = element.previousElementSibling.querySelector('.SignUpPwCorrectInput');
+  } else if (element.classList.contains("LoginPwCorrectError")) {
+    const correctInput = element.previousElementSibling.querySelector(
+      ".SignUpPwCorrectInput"
+    );
     if (correctInput) {
-      correctInput.style.border = '1px solid red';
+      correctInput.style.border = "1px solid red";
     }
   }
 }
 
 function hideError(element) {
   // 요소를 지우고 싶지만 공간은 두고싶은 경우
-  element.textContent = '';
+  element.textContent = "";
   // 요소를 지우고 공간도 지우고 싶은 경우
-  element.style.display = 'none';
-  if (element.classList.contains('LoginEmailError')) {
-    element.previousElementSibling.style.border = '';
-  } else if (element.classList.contains('LoginPwError')) {
-    const passwordInput = element.previousElementSibling.querySelector('.SignUpPwInput');
+  element.style.display = "none";
+  if (element.classList.contains("LoginEmailError")) {
+    element.previousElementSibling.style.border = "";
+  } else if (element.classList.contains("LoginPwError")) {
+    const passwordInput =
+      element.previousElementSibling.querySelector(".SignUpPwInput");
     if (passwordInput) {
-      passwordInput.style.border = '';
+      passwordInput.style.border = "";
     }
-  } else if (element.classList.contains('LoginPwCorrectError')) {
-    const correctInput = element.previousElementSibling.querySelector('.SignUpPwCorrectInput');
+  } else if (element.classList.contains("LoginPwCorrectError")) {
+    const correctInput = element.previousElementSibling.querySelector(
+      ".SignUpPwCorrectInput"
+    );
     if (correctInput) {
-      correctInput.style.border = '';
+      correctInput.style.border = "";
     }
   }
 }
 
 // Enter키로 회원가입
-document.addEventListener('keydown', function(event) {
-  if(event.key === 'Enter') {
-    document.querySelector('.LoginBtn').click();
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    document.querySelector(".LoginBtn").click();
   }
-})
+});
+
+// 이메일 중복 확인
+async function dupliEmail(email) {
+  const response = await fetch(
+    "https://bootcamp-api.codeit.kr/api/check-email",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+// 회원가입 요청
+async function signUp(email, password, passwordcheck) {
+  const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-up", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      paswordcheck: passwordcheck,
+    }),
+  });
+  return response.ok;
+}
 
 // focusout 되었을 때 emailInput, pwInput에서 validateEmail, validatePw검사
-emailInput.addEventListener('focusout', validateEmail);
-pwInput.addEventListener('focusout', validatePassword);
-pwCorrectInput.addEventListener('focusout', correctPassword);
+emailInput.addEventListener("focusout", validateEmail);
+pwInput.addEventListener("focusout", validatePassword);
+pwCorrectInput.addEventListener("focusout", correctPassword);
 
-const loginForm = document.querySelector('.LoginBtn');
+const loginForm = document.querySelector(".LoginBtn");
 
-loginForm.addEventListener('click', function(event) {
+// 오류 메시지 표시 함수
+function showSignUpError(message) {
+  showError(emailError, message);
+  showError(pwError, message);
+  showError(pwCorrectError, message);
+}
+loginForm.addEventListener("click", async function (event) {
   event.preventDefault();
 
   validateEmail();
@@ -134,11 +180,28 @@ loginForm.addEventListener('click', function(event) {
   let passwordValue = pwInput.value.trim();
   let passwordValue2 = pwCorrectInput.value.trim();
 
-  if(isValidEmail(emailValue) && isValidPassword(passwordValue) && passwordValue === passwordValue2) {
-    window.location.href = '/folder';
-  } else {
-    showError(emailError, '이메일을 확인해 주세요.');
-    showError(pwError, '비밀번호를 확인해 주세요.');
-    showError(pwCorrectError, '비밀번호를 확인해 주세요.');
+  try {
+    const emailCheck = await dupliEmail(emailValue);
+    if (emailCheck.isDuplicated) {
+      showError(emailError, "이미 사용 중인 이메일입니다.");
+      return;
+    }
+
+    const signUpResult = await signUp(
+      emailValue,
+      passwordValue,
+      passwordValue2
+    );
+    if (signUpResult) {
+      window.location.href = "/folder";
+      return;
+    } else {
+      // 올바른 형식 아닌 경우
+      showSignUpError("회원가입에 실패했습니다. 다시 시도해 주세요.");
+    }
+  } catch (error) {
+    // 요청 문제
+    console.log("Error during sign up:", error);
+    showSignUpError("요청 중 문제가 발생하였습니다.");
   }
-})
+});
