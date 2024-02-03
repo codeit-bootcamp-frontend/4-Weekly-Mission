c69d2631da5f263d05a5e772b1665402df237c8a
sihyonn <sihyeon982@naver.com>
2024-02-04 04:42:00 +0900

    docs: instance.js JSDoc 추가

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,3 +1,16 @@
+/**
+ * HTTP request용 모듈
+ *
+ * @async
+ * @function
+ * @param {string} url 요청 URL
+ * @param {string} method 요청할 HTTP 메서드
+ * @param {Object} [body] 요청 본문 데이터(optional)
+ * @param {Object} [headers] 요청에 추가적인 헤더(optional)
+ * @returns {Promise} 응답 데이터
+ * @throws {Error} 응답 상태가 'ok'가 아닐 경우 에러발생
+ */
+
 const baseUrl = 'https://bootcamp-api.codeit.kr/api';
 
 const request = async (url, method, body, headers = {}) => {
