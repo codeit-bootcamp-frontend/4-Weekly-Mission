e454d7aa502a510b6b7751f5c163fa3591130548
sihyonn <sihyeon982@naver.com>
2024-02-04 04:11:13 +0900

    feat: 로그인, 회원가입 시 성공 응답으로 받은 accessToken 저장 로직 추가

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -1,9 +1,11 @@
 import { post } from './instance.js';
 import API from '../constants/API.js';
+import { storeTokenInLocalStorage } from './tokenSetting.js';
 
 const postLogin = async ({ email, password }) => {
   try {
     const res = await post(API.AUTH.SIGN_IN, { email, password });
+    storeTokenInLocalStorage('login-token', res.data.accessToken);
     return res;
   } catch (error) {
     console.log('로그인 중 오류 발생');
@@ -24,6 +26,7 @@ const isEmailDuplicate = async ({ email }) => {
 const postSignUp = async ({ email, password }) => {
   try {
     const res = await post(API.AUTH.SIGN_UP, { email, password });
+    storeTokenInLocalStorage('signup-token', res.data.accessToken);
     return res;
   } catch (error) {
     console.log('회원가입 중 오류 발생');
