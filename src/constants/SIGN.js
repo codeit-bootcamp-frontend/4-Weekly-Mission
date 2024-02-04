c9165f6548c743cb54afde151e35dfe1607b4b90
sihyonn <sihyeon982@naver.com>
2024-02-04 13:42:20 +0900

    refactor: 비밀번호 정규식 요구사항에 맞춰 변경(특수문자 제외)

--- src/constants/SIGN.js
+++ src/constants/SIGN.js
@@ -5,14 +5,14 @@ const MESSAGE = Object.freeze({
   DUPLICATE_EMAIL: '이미 사용 중인 이메일입니다.',
 
   REQUIRED_PASSWORD: '비밀번호를 입력해 주세요.',
-  INVALID_PW_FORMAT: '8~20자 공백없이 영문/숫자/특수문자를 포함해 주세요.',
+  INVALID_PW_FORMAT: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
   CHECK_PASSWORD: '비밀번호를 확인해 주세요.',
   NOT_MATCH_PASSWORD: '비밀번호가 일치하지 않아요.',
 });
 
 const REGEX = Object.freeze({
   EMAIL_REGEX: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  PW_REGEX: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9])\S{8,20}$/,
+  PW_REGEX: /^(?=.*[a-zA-Z])(?=.*[0-9])\S{8,20}$/,
 });
 
 const ACTION = Object.freeze({
