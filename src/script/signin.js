a8f76cb85ca05465f56b732d3eccb1a759ad9db9
김동현 <dtc03003@naver.com>
2024-02-05 02:12:28 +0900

    feat: 6주차 완성

--- src/script/signin.js
+++ src/script/signin.js
@@ -9,14 +9,14 @@ import {
     eyeButton,
 } from "./sign.js";
 import { createError } from "./error/errorRelated.js";
-import { checkUserInfo } from "./verification.js";
+import { checkLogin } from "./verification.js";
 
 const loginButton = document.querySelector(".login_button");
 
 // 로그인
-function login(event) {
+async function login(event) {
     event.preventDefault();
-    const isLogin = checkUserInfo(email.value, password.value);
+    const isLogin = await checkLogin(email.value, password.value);
 
     if (isLogin) {
         window.location.href = "/folder.html";
@@ -25,6 +25,18 @@ function login(event) {
     createError(email, emailError, "이메일을 확인해 주세요.");
     createError(password, passwordError, "비밀번호를 확인해 주세요.");
 }
+
+// 페이지 진입 시 accessToken 확인
+function checkAccessToken() {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+        window.location.href = "/folder.html";
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    checkAccessToken();
+});
 
 // 이벤트 핸들러
 email.addEventListener("focusout", validateEmail);
--- src/script/signup.js
+++ src/script/signup.js
@@ -1,65 +1,36 @@
+import { email, password, toggleEye, eyeButton } from "./sign.js";
 import {
-    validateEmail,
-    validatePassword,
-    toggleEye,
-    email,
-    password,
-    emailError,
-    passwordError,
-    eyeButton,
-} from "./sign.js";
-import { createError, clearError } from "./error/errorRelated.js";
-import { checkEmail, isValidPassword } from "./verification.js";
+    duplicateEmail,
+    checkPassword,
+    checkConfirmPassword,
+    checkSignUp,
+    confirmPassword,
+} from "./verification.js";
 
-const confirmPassword = document.querySelector("#sign_confirm_password");
-const confirmPasswordError = document.querySelector("#error_confirm_password");
 const confirmEyeButton = document.querySelector(".confirm_eye_button");
 const signupButton = document.querySelector(".signup_button");
 
-// 사용중인 이메일 검사
-function duplicateEmail() {
-    const isDuplicate = checkEmail(email.value);
-
-    if (isDuplicate) {
-        createError(email, emailError, "이미 사용 중인 이메일입니다.");
-        return;
-    }
-    validateEmail();
-}
-
-// 비밀번호 조합 확인
-function checkPassword() {
-    if (!isValidPassword(password.value)) {
-        createError(password, passwordError, "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
-        return;
-    }
-    validatePassword();
-}
-
-// 비밀번호 확인 검사
-function checkConfirmPassword() {
-    const isConfirmPassword = password.value === confirmPassword.value;
-
-    if (!isConfirmPassword) {
-        createError(confirmPassword, confirmPasswordError, "비밀번호가 다릅니다.");
-        return;
-    }
-    clearError(confirmPassword, confirmPasswordError);
-}
-
-function signup(event) {
+async function signup(event) {
     event.preventDefault();
-    // duplicateEmail();
-    // checkPassword();
-    // checkConfirmPassword();
-    if (
-        emailError.innerText === "" &&
-        passwordError.innerText === "" &&
-        confirmPasswordError.innerText === ""
-    ) {
+    const isSignUp = await checkSignUp(email.value, password.value);
+
+    if (isSignUp) {
+        window.location.href = "/folder.html";
+        return;
+    }
+}
+
+// 페이지 진입 시 accessToken 확인
+function checkAccessToken() {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
         window.location.href = "/folder.html";
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    checkAccessToken();
+});
 
 // 이벤트 핸들러
 email.addEventListener("focusout", duplicateEmail);
