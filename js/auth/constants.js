1bca5872fca237f7f8f6e8d23934ea0ab51aa18c
Trophy198 <spsp8426@naver.com>
2024-02-04 15:30:02 +0900

    feat : 이메일 검증 메시지 추가 및 api 엔드포인트 상수 추가 - api 엔드포인트 및 이메일 유효성 검사 메시지 추가

--- js/auth/constants.js
+++ js/auth/constants.js
@@ -1,5 +1,11 @@
+export const BASE_URL = 'https://bootcamp-api.codeit.kr';
+export const SIGN_IN_ENDPOINT = '/api/sign-in';
+export const CHECK_EMAIL_ENDPOINT = '/api/check-email';
+export const SIGN_UP_ENDPOINT = '/api/sign-up';
+
 export const VALIDATION_MESSAGES = {
     email: {
+        incorrect: "이메일을 확인해 주세요.",
         alreadyInUse: "이미 사용 중인 이메일입니다.",
         invalid: "올바른 이메일 주소가 아닙니다.",
         empty: "이메일을 입력해 주세요."
