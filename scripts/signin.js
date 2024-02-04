4ad1109d6fa73c9a21c068e21dfb78cbf89969ce
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-04 11:32:34 +0900

    Refactor: api를 사용하여 유효한 값인지 확인

--- scripts/signin.js
+++ scripts/signin.js
@@ -6,3 +6,8 @@ $emailInput.addEventListener('focusout', checkValidEmail);
 $passwordInput.addEventListener('focusout', checkEmptyPassword);
 [...$btnEye].forEach(btn => btn.addEventListener('click', togglePassword));
 $form.addEventListener('submit', validateSignin);
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('accessToken')) {
+    location.href = '/folder.html'
+  }
+})
--- scripts/utils/validationUtils.js
+++ scripts/utils/validationUtils.js
@@ -11,8 +11,26 @@ const isValidPassword = () => {
   return passwordRegex.test($passwordInput.value);
 };
 const isMatchPassword = () => $passwordInput.value === $confirmPasswordInput.value;
-const isEmailExisting = () => $emailInput.value === EXIST_EMAIL;
 const isPasswordExisting = () => $passwordInput.value === EXIST_PASSWORD;
+const isEmailExisting = async () => {
+  try {
+    const response = await fetch('https://bootcamp-api.codeit.kr/api/check-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: $emailInput.value }),
+    });
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return false;
+  } catch (err) {
+    return true;
+  }
+};
 
 const checkValidEmail = () => {
   if (isValidEmail()) {
@@ -24,8 +42,11 @@ const checkValidEmail = () => {
   showError($emailInput, message);
 };
 
-const checkExistEmail = () => {
-  if (!isEmailExisting()) return;
+const checkExistEmail = async () => {
+  if (!(await isEmailExisting())) {
+    removeError($emailInput);
+    return;
+  }
   showError($emailInput, '이미 사용 중인 이메일입니다.');
 };
 
@@ -56,22 +77,79 @@ const checkMatchPassword = () => {
   showError($confirmPasswordInput, '비밀번호가 일치하지 않아요.');
 };
 
-const validateSignin = e => {
-  if (isEmailExisting() && isPasswordExisting()) return;
-
+const validateSignin = async e => {
   e.preventDefault();
-  showError($emailInput, '이메일을 확인해주세요.');
-  showError($passwordInput, '비밀번호를 확인해주세요.');
+
+  try {
+    const user = {
+      email: $emailInput.value,
+      password: $passwordInput.value,
+    };
+
+    const response = await fetch('https://bootcamp-api.codeit.kr/api/sign-in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const result = await response.json();
+    const accessToken = result.data.accessToken;
+
+    localStorage.setItem('accessToken', accessToken);
+    location.href = '/folder.html';
+  } catch (err) {
+    showError($emailInput, '이메일을 확인해주세요.');
+    showError($passwordInput, '비밀번호를 확인해주세요.');
+  }
 };
 
-const validateRegistration = e => {
-  if (isValidEmail() && isValidPassword() && isMatchPassword() && !isEmailExisting()) return;
-
+const validateRegistration = async e => {
   e.preventDefault();
-  checkValidEmail();
-  checkExistEmail();
-  checkValidPassword();
-  checkMatchPassword();
+
+  if (!isValidEmail()) {
+    const message = $emailInput.value ? '올바른 이메일 주소가 아닙니다.' : '이메일을 입력해주세요.';
+    showError($emailInput, message);
+    return;
+  }
+
+  if (!isValidPassword()) {
+    showError($passwordInput, '비밀번호는 영문, 숫자 조합 8자 이상 입력해주세요.');
+    return;
+  }
+
+  if (!isMatchPassword()) {
+    showError($confirmPasswordInput, '비밀번호가 일치하지 않아요.');
+    return;
+  }
+
+  try {
+    const user = {
+      email: $emailInput.value,
+      password: $passwordInput.value,
+    };
+
+    const response = await fetch('https://bootcamp-api.codeit.kr/api/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    location.href = '/folder.html';
+  } catch (err) {
+    showError($emailInput, '이미 사용 중인 이메일입니다.')
+  }
 };
 
 export {
