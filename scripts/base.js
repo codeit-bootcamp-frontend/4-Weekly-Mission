a435846c2f708742b81fb548866ce3ea04dbc070
Dodam0719 <blueoh719@gmail.com>
2024-02-04 20:24:52 +0900

    add(mentor): signin POST 요청 추가, accessToken 관련 로직 추가

--- scripts/base.js
+++ scripts/base.js
@@ -1,3 +1,12 @@
-export const ADMIN_EMAIL = "test@codeit.com";
-export const ADMIN_PASSWORD = "codeit101";
 export const INVALID_CLASS = "invalid";
+export const ADMIN_EMAIL = "test@codeit.com";
+/*
+export const ADMIN_PASSWORD = "codeit101";
+4주차 기본 요구사항에서의 비밀번호는 위와 같이 지시되어있었으나,
+6주차 기본 요구사항에서는 비밀번호가 아래와 같이 지시되어있음
+*/
+export const ADMIN_PASSWORD = "sprint101";
+export const FOLDER_URL = "/component/folder.html";
+export const API_SIGNIN = "https://bootcamp-api.codeit.kr/api/sign-in";
+export const HTTP_STATUS_OK = 200;
+export const HTTP_STATUS_BAD_REQUEST = 400;
--- scripts/signin.js
+++ scripts/signin.js
@@ -1,4 +1,12 @@
-import { INVALID_CLASS, ADMIN_EMAIL, ADMIN_PASSWORD } from "./base.js";
+import {
+  INVALID_CLASS,
+  ADMIN_EMAIL,
+  ADMIN_PASSWORD,
+  FOLDER_URL,
+  API_SIGNIN,
+  HTTP_STATUS_OK,
+  HTTP_STATUS_BAD_REQUEST,
+} from "./base.js";
 import { isValidEmail } from "./inputValidation.js";
 import { togglePasswordVisibility } from "./togglePasswordVisibility.js";
 
@@ -9,6 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const togglePasswordIcon = document.querySelector(
     "#togglePasswordVisibility"
   );
+  const accessToken = localStorage.getItem("accessToken");
+
+  // 로그인 페이지에 접근시 로컬 스토리지에 accessToken이 있는 경우 “/folder” 페이지로 이동
+  if (accessToken) {
+    window.location.href = FOLDER_URL;
+    return;
+  }
 
   // 이메일 입력 처리 함수 : 유효성 검사 및 에러 메시지 관리
   function handleEmailInput() {
@@ -54,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
   passwordInput.addEventListener("blur", handlePasswordInput);
   passwordInput.addEventListener("input", handlePasswordInput);
 
-  loginButton.addEventListener("click", function (event) {
+  loginButton.addEventListener("click", async function (event) {
     // 폼의 기본 제출 동작을 방지
     event.preventDefault();
 
@@ -65,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 정해진 이메일과 비밀번호가 입력된 경우
     if (emailValue === ADMIN_EMAIL && passwordValue === ADMIN_PASSWORD) {
-      window.location.href = "/component/folder.html";
+      window.location.href = FOLDER_URL;
     } else {
       // 정해진 이메일이 입력되지 않은 경우
       if (emailValue !== ADMIN_EMAIL) {
@@ -78,6 +93,45 @@ document.addEventListener("DOMContentLoaded", function () {
         passwordInput.classList.add(INVALID_CLASS);
       }
     }
+
+    async function signInAndRedirect(email, password) {
+      try {
+        // 'https://bootcamp-api.codeit.kr/api/sign-in'에 POST 요청 보내기
+        const response = await fetch(API_SIGNIN, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        });
+
+        // 응답코드 200 : 로그인 성공
+        if (response.status === HTTP_STATUS_OK) {
+          const data = await response.json();
+          localStorage.setItem("accessToken", data.accessToken);
+          window.location.href = FOLDER_URL;
+        }
+        // 응답코드 400 : 로그인 오류
+        else if (response.status === HTTP_STATUS_BAD_REQUEST) {
+          console.error("로그인 오류: 잘못된 요청");
+          alert("로그인 오류: 잘못된 요청");
+        }
+        // 그외 응답코드
+        else {
+          const errorData = await response.json();
+          console.error("로그인 실패: ", errorData.message);
+          alert("로그인 실패: ", errorData.message);
+        }
+      } catch (error) {
+        console.error("요청 실패: ", error.message);
+        alert("요청 실패: ", error.message);
+      }
+    }
+
+    signInAndRedirect(emailValue, passwordValue);
   });
 
   // 비밀번호 보이기/숨기기
