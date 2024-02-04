b1f4889839de866e24202b317850e4d7a64449f0
강경미 <gaem@gang-gyeongmiui-noteubug.local>
2024-02-04 23:27:57 +0900

    템플릿 코드로 6주차 구현

--- src/js/utils/constant.js
+++
@@ -1,9 +0,0 @@
-export { EMAIL_REGEX, USER_EMAIL, USER_PASSWORD, PASSWORD_REGEX };
-
-const EMAIL_REGEX =
-  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
-const USER_EMAIL = "test@codeit.com";
-const USER_PASSWORD = "codeit101";
-
-const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
