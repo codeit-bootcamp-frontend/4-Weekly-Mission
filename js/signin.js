494e4e6b07cd14ab5e91255e0165516db7d1d31f
devwqc <devwqc@gmail.com>
2024-02-04 12:12:25 +0900

    Add fetch request for login in signin.js

--- js/signin.js
+++ js/signin.js
@@ -6,10 +6,11 @@ import {
   addLoginError,
 } from './input.js';
 import { USER_TEST } from './test.js';
+import { API } from './api.js';
 
 const signinForm = document.querySelector('#signin-form');
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
 
   if (event.target.id !== 'signin-form') return;
@@ -24,16 +25,51 @@ function handleSubmit(event) {
   removeError(messageBoxEmail);
   removeError(messageBoxPassword);
 
-  if (USER_TEST.EMAIL !== inputEmail.value) {
+  // if (USER_TEST.EMAIL !== inputEmail.value) {
+  //   addLoginError(INPUT_TYPE.EMAIL, inputEmail, messageBoxEmail);
+  //   return;
+  // } else if (USER_TEST.PASSWORD !== inputPassword.value) {
+  //   addLoginError(INPUT_TYPE.PASSWORD, inputPassword, messageBoxPassword);
+  //   return;
+  // }
+
+  const isSignin = await signin({
+    email: inputEmail.value,
+    password: inputPassword.value,
+  });
+
+  if (!isSignin) {
     addLoginError(INPUT_TYPE.EMAIL, inputEmail, messageBoxEmail);
-    return;
-  } else if (USER_TEST.PASSWORD !== inputPassword.value) {
     addLoginError(INPUT_TYPE.PASSWORD, inputPassword, messageBoxPassword);
     return;
   }
 
-  // event.target.submit();
   window.location.href = '/folder';
+}
+
+async function signin(user) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+
+    const response = await fetch(API.getSignInUrl(), options);
+
+    if (!response.ok) {
+      throw Error(`TODO: ${response.status} response error handling`);
+    }
+
+    let json = await response.json();
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 signinForm.addEventListener('focusout', handleFocusoutInput);
