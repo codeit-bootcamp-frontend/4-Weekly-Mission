29959fa9dbd3fe2c1333303dca620d655f1bd7d4
minho <kasamin98@naver.com>
2024-02-05 03:10:12 +0900

    feat: 5주차 기본과제 signup.js 중 회원가입 실행 제외한 나머지 완료

--- sign/signup.js
+++ sign/signup.js
@@ -12,8 +12,6 @@ import {
 const email = document.querySelector('#email');  //input태그
 const emailError =  document.querySelector('#email-error-message'); //이메일p 태그
 
-console.log(email)
-
 function emailErrorMessageHandler(e){
   if(!email.value){
     emailError.textContent =  '이메일을 입력해 주세요.';
@@ -29,55 +27,69 @@ function emailErrorMessageHandler(e){
     errorColorRomove(email);
   }
 }
-
 email.addEventListener("focusout",emailErrorMessageHandler);
 
-// 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동합니다.
-const formElem = document.querySelector(".signin-form");
+
+
+
+//비밀번호 input에서 focus out 할 때, 값이 8자 미만으로 있거나 문자열만 있거나 숫자만 있는 경우, 
+//“비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 에러 메세지를 보입니다.
+// 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 에러 메세지를 보입니다.
+const password = document.querySelector('#password');  //input태그
+const PasswordError =  document.querySelector('#password-error-message'); //이메일p 태그
+let passwordReg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+console.log(passwordReg.test('asddf111'))
+
+function passwordErrorMessageHandler(e){
+  console.log(password.value)
+  if(!passwordReg.test(password.value)){
+    PasswordError.textContent ='비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.';
+    errorColorChange(password);
+  }else{
+    PasswordError.textContent = '';
+    errorColorRomove(password);
+  }
+}
+password.addEventListener("focusout",passwordErrorMessageHandler);
+
+//비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 
+//비밀번호 확인 input 아래에 “비밀번호가 일치하지 않아요.” 에러 메세지를 보입니다.
+const passwordConfirm = document.querySelector('#confirm-password');  //input태그
+const PasswordConfirmError =  document.querySelector('#confirm-password-error-message');
+function passwordErrorMessageCheckHandler(e){
+  if(!passwordConfirm.value){
+    PasswordConfirmError.textContent = '비밀번호를 입력해 주세요.';
+    errorColorChange(passwordConfirm);
+  }else if(password.value !== passwordConfirm.value){
+    PasswordConfirmError.textContent = '비밀번호가 일치하지 않아요.';
+    errorColorChange(passwordConfirm);
+  }else{
+    PasswordConfirmError.textContent = '';
+    errorColorRomove(passwordConfirm);
+  }
+}
+passwordConfirm.addEventListener("focusout",passwordErrorMessageCheckHandler);
+
+
+
+
+//회원가입을 실행할 경우, 문제가 있는 경우 문제가 있는 input에 에러 메세지로 알립니다.
+/*
+
+const formElem = document.querySelector(".signup-form");
 
 function handleSubmit(e) {
-  e.preventDefault();// 아이디/비밀번호가 다른 경우 submit의 기본기능을 무효화하는 코드가 들어가야 합니다.
-  if(email.value === "test@codeit.com" && password.value ==="codeit101"){
-    location.href = "/folder";
+
+  if(){
   }else{
-    //이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해 주세요.”
-    //, 비밀번호 input 아래에 “비밀번호를 확인해 주세요.” 에러 메세지를 보입니다.
-    emailError.textContent = '“이메일을 확인해 주세요.';
-    PasswordError.textContent =  '비밀번호를 확인해 주세요.';
-    errorColorChange(email);
-    errorColorChange(password);
+
   }
 }
 
 formElem.addEventListener("submit", handleSubmit);
+*/
 
-// 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 에러 메세지를 보입니다.
-const password = document.querySelector('#password');  //input태그
-const PasswordError =  document.querySelector('#password-error-message'); //이메일p 태그
 
-function passwordErrorMessageHandler(e){
-  if(!password.value){
-    PasswordError.textContent =  '비밀번호를 입력해 주세요.';
-    errorColorChange(password);
-  }else {
-    PasswordError.textContent = '';
-    errorColorRomove(password);
-  }
-}
-password.addEventListener("focusout",passwordErrorMessageHandler);
-
-// 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 에러 메세지를 보입니다.
-
-function passwordErrorMessageHandler(e){
-  if(!password.value){
-    PasswordError.textContent =  '비밀번호를 입력해 주세요.';
-    errorColorChange(password);
-  }else {
-    PasswordError.textContent = '';
-    errorColorRomove(password);
-  }
-}
-password.addEventListener("focusout",passwordErrorMessageHandler);
 
 
 
