8190879207291af4a2c2b22af06db57ae89d1a1b
Trophy198 <spsp8426@naver.com>
2024-02-04 19:07:52 +0900

    refactor : 중복 선언된 checkAcessTokenAndRedirect 제거

--- js/auth/storage.js
+++ js/auth/storage.js
@@ -13,14 +13,3 @@ export const saveAccessToken = (accessToken) => {
 export const getAccessToken = () => {
     return localStorage.getItem('accessToken');
   };
-
-  /**
-   * 로컬 스토리지에서 accessToken을 확인
-   * accessToken이 존재하면 '/folder.html' 페이지로 리다이렉트
-   */
-export const checkAccessTokenAndRedirect = () => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-        window.location.href = '/folder.html';
-    }
-  };
