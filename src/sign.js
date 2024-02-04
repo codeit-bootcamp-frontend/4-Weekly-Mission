789063f834fc0fde913f2da690e04e87424bb7d2
janyong <jyr5023@gmail.com>
2024-02-04 19:54:13 +0900

    Feat. 로그인 & 이메일 검증 API

--- src/sign.js
+++ src/sign.js
@@ -19,3 +19,20 @@ export function checkisValidEmail(email) {
   return emailRegex.test(email);
 }
 
+export function checkString(str) {
+  // 정규표현식을 사용하여 문자열에 숫자만 있는지 확인
+  const isNumberOnly = /^\d+$/.test(str);
+  // 정규표현식을 사용하여 문자열에 문자만 있는지 확인
+  const isLetterOnly = /^[a-zA-Z]+$/.test(str);
+  // 문자열의 길이가 8자 미만인지 확인
+  const isShort = str.length < 8;
+  if (isShort || isNumberOnly || isLetterOnly) {
+    return false;
+  }
+  return true;
+}
+
+export const userData = {
+  email: 'test@codeit.com',
+  password: 'codeit101'
+};
