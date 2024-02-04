27c2bf452546dc33e110f68784798827c65fc00c
Taeuk471 <asky990223@gmail.com>
2024-02-04 17:20:18 +0900

    :art: Make postRequest function for module

--- script/apiUtils.js
+++ script/apiUtils.js
@@ -1,14 +1,38 @@
 const apiUrl = 'https://bootcamp-api.codeit.kr/api';
 
+const postRequest = (endpoint, data) => {
+  return fetch (`${apiUrl}${endpoint}`, {
+    method : 'POST',
+    headers : {
+      'Content-Type' : 'application/json'
+    },
+    body : JSON.stringify(data),
+  });
+};
+
 export const postData = async (endpoint, data) => {
   try {
-    const response = await fetch(`${apiUrl}${endpoint}`, {
-      method : 'POST',
-      headers: {
-        'Content-Type' : 'application/json'
-      },
-      body : JSON.stringify(data),
-    });
+    const response = await postRequest(endpoint, data);
+    const responseData = await response.json();
+    if(responseData.success) {
+      window.location.href = '../folder.html'
+      return;
+    }
+    console.error("Login Failure")
+  }catch (error){
+    console.error('Error : Network Connection Failure', error);
+  }
+}
+
+/**
+ * 
+ * @param {*} api뒤에 들어갈 이메일 중복 확인을 위한 위치
+ * @param {*} data 
+ * @returns 
+ */
+export const checkEmailData = async (endpoint, data) => {
+  try {
+    const response = await postRequest(endpoint, data);
     const responseData = await response.json();
 
     if(responseData.success) {
