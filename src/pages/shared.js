b5e6b7d801245e801707dcdba16c1cce5061b19c
armd482 <armd482@naver.com>
2024-02-05 13:25:03 +0900

    Changing structure

---
+++ src/pages/shared.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/sharedPageComponents/header';
+import Contents from '../components/sharedPageComponents/contents';
+import Footer from '../components/sharedPageComponents/footer';
+
+export default function Main() {
+  return (
+    <>
+      <Header></Header>
+      <Contents></Contents>
+      <Footer></Footer>
+    </>
+  );
+}
