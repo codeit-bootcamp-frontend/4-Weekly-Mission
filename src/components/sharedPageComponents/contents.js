7c5d1b9b22de46a573207c9f90cf7124e7bd1494
armd482 <armd482@naver.com>
2024-02-05 22:13:12 +0900

    Change search icon

--- src/components/sharedPageComponents/contents.js
+++ src/components/sharedPageComponents/contents.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Card from './card';
 import search from '../../assets/Images/Search.png';
-/*eslint-disable*/
+
 export default function Contents({ links }) {
   return (
     <article>
-      <input
-        className="card-search"
-        placeholder="링크를 검색해 보세요"
-        type="search"
-      ></input>
-      <img src={search} alt="search" className="card-search-icon"></img>
+      <form>
+        <input
+          className="card-search"
+          placeholder="링크를 검색해 보세요"
+          type="search"
+        ></input>
+        <button type="submit" className="card-search-button">
+          <img src={search} alt="search" className="card-search-icon"></img>
+        </button>
+      </form>
       <div className="content-wrapper">
         {links && links.map((el) => <Card link={el} key={el.id}></Card>)}
       </div>
