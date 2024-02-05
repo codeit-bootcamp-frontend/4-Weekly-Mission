935615f46bd4fa7e01bb4cc8978eed4fccc93812
armd482 <armd482@naver.com>
2024-02-05 14:13:08 +0900

    Implement header profile using API

--- src/components/sharedPageComponents/header.js
+++ src/components/sharedPageComponents/header.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/Images/logo.svg';
 
 export default function Header() {
+  const [userProfileImage, setUserProfileImage] = useState(null);
+  const [userProfileEmail, setUserProfileEmail] = useState(null);
+  useEffect(() => {
+    userData();
+  }, []);
+
+  const userData = async () => {
+    const response = await fetch(
+      'https://bootcamp-api.codeit.kr/api/sample/user',
+    );
+    const result = await response.json();
+    console.log(result);
+    setUserProfileImage(result.profileImageSource);
+    setUserProfileEmail(result.email);
+  };
+
   return (
     <>
-      <div></div>
+      <nav>
+        <div className="menu">
+          <a href="">
+            <img src={logo} className="main-logo" />
+          </a>
+          <div className="user-profile">
+            <img
+              src={userProfileImage}
+              alt="userImage"
+              className="profile-image"
+            ></img>
+            <p>{userProfileEmail}</p>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
--- src/pages/shared.js
+++ src/pages/shared.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/sharedPageComponents/header';
 import Contents from '../components/sharedPageComponents/contents';
 import Footer from '../components/sharedPageComponents/footer';
+import '../styles/shared.css';
 
 export default function Main() {
   return (
