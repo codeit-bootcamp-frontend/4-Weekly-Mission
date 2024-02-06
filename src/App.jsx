6bda70563ea128c5ba929c700adf93f9083a25b9
Taejin Kim <rlaxowls1316@naver.com>
2024-02-06 16:18:56 +0900

    feat: add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
