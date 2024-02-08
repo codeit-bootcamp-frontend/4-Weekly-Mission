5ba7e1eba2f1e9d80040c81a32226f926b01e250
codingaring <sksms2104@naver.com>
2024-02-08 18:56:32 +0900

    [Feat] 리액트 작성 환경 생성

---
+++ my-app/src/header.js
@@ -0,0 +1,10 @@
+import Logo from ''
+
+function header() {
+  return (
+  <div>
+    <img>
+    <div></div>
+  </div>
+  )
+}
---
+++ my-app/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>안녕 리액트!</h1>,
+  document.getElementById('root')
+);
