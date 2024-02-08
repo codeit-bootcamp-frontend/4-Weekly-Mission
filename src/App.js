09e166131bd163727a7037e641633a796e61ee0e
MinCheolS <ay5212@hanmail.net>
2024-02-08 22:08:58 +0900

    Feat: Header.js 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/layout/Header';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
+import profileImg from '../../assets/profileImg.svg';
+import '../../style/Header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='HeaderContainer'>
+      <div className='HeaderContent'>
+        <a href='/src/App.js'>
+          <img className='Logo' src={logo} alt='logo img' />
+        </a>
+        <a href='/src/components/Signin.js'>
+          <div className='profileContent'>
+            <img className='profileImg' src={profileImg} alt='profile img' />
+            <span>Codeit@codeit.com</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
