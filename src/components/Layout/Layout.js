5820f9322246886e05ff99e663d3fdc09463db93
LeeSeungHoon <ysh73900@gmail.com>
2024-02-08 20:21:06 +0900

    feat: API에서 사용자 데이터를 불러오고 NavBar로 넘겨주기

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,29 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { getUser } from "../../api/getUser";
 import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 
 const Layout = ({ children }) => {
+  const [userProfile, setUserProfile] = useState({
+    email: null,
+    profileImageSource: null,
+  });
+
+  const getApiData = async () => {
+    try {
+      const userData = await getUser();
+      setUserProfile(userData);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
   return (
     <Fragment>
-      <NavBar />
+      <NavBar userProfile={userProfile} />
       <main>{children}</main>
       <Footer />
     </Fragment>
