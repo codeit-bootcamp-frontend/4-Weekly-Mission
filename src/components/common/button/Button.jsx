d69fbc9ea3ee279b6490f4ec0302c2340d23ccbd
sihyonn <sihyeon982@naver.com>
2024-02-08 17:59:19 +0900

    feat: Button컴포넌트(로그인버튼) 구현

---
+++ src/components/common/button/Button.jsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+
+const Styled = {
+  Button: styled.button`
+    width: 12.8rem;
+    padding: 1.6rem 2rem;
+    border-radius: 0.8rem;
+    background: linear-gradient(90.99deg, #6d6afe 0.12%, #6ae3fe 101.84%);
+    font-size: 1.8rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.white};
+  `,
+};
+
+// 현재: 로그인 버튼 -> 추후 : 공통 로그인버튼으로 변경
+function Button() {
+  return <Styled.Button>로그인</Styled.Button>;
+}
+
+export default Button;
