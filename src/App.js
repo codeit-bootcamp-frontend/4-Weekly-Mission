5ecf1f17b05d3857682be7d0c31458b3b438a7d2
jinhok96 <jinhok96a@gmail.com>
2024-02-09 18:10:30 +0900

    test: Gnb.js 생성 	modified:   src/App.js 	new file:   src/components/Gnb.css 	new file:   src/components/Gnb.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import { useEffect, useState } from "react-dom";
 import "./App.css";
+import Gnb from "./components/Gnb.js";
 import Shared from "./pages/Shared/Shared.js";
 
 function App() {
   return (
     <div>
       <div>안녕하세요 App.js입니다.</div>
+      <Gnb></Gnb>
       <Shared></Shared>
     </div>
   );
---
+++ src/components/Gnb.js
@@ -0,0 +1,8 @@
+import "./Gnb.css";
+
+//글로벌 네비게이션 바
+function Gnb() {
+  return <div>Gnb입니다.</div>;
+}
+
+export default Gnb;
