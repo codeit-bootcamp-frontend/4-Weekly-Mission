b06ec681074ff288d6a3a7548a61497bdb1ffff0
sihyonn <sihyeon982@naver.com>
2024-02-09 18:16:44 +0900

    feat: StarButton 컴포넌트 생성

---
+++ src/components/common/card/StarButton.jsx
@@ -0,0 +1,26 @@
+import { styled } from 'styled-components';
+
+import filledStar from 'assets/icon/filledStar.svg';
+import emptystar from 'assets/icon/emptyStar.svg';
+
+const Styled = {
+  Container: styled.button`
+    width: 3rem;
+    height: 3rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
+
+function StarButton({ isActive }) {
+  const src = isActive ? filledStar : emptystar;
+  return (
+    <Styled.Container>
+      <img src={src} alt="찜 버튼" />
+    </Styled.Container>
+  );
+}
+
+export default StarButton;
