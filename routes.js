b76f96e55467cefc17ef1dfa115e8140464452b5
mun-jihye <m84422583@gmail.com>
2024-02-09 14:13:43 +0900

    chore: 초기 설정

---
+++ routes.js
@@ -0,0 +1,7 @@
+const routes = {
+  home: "/",
+  signup: "/signup",
+  signin: "/signin",
+};
+
+export default routes;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import routes from "../routes";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path={routes.home} element={<MainPage />}></Route>
+
+      <Route path={routes.login} element={<SignInPage />}></Route>
+      <Route path={routes.signup} element={<SignUpPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
