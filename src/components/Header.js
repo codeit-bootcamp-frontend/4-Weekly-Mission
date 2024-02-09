7d0e4a985a02cffff8ea24e54584691f61e9d830
박지윤 <tkrhkfld123@naver.com>
2024-02-09 17:26:21 +0900

    💄 Style: Header 디자인 1차 구현 완료 (프로필 제외)

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../style/header.css';
+import Profile from './Profile';
+import logo from '../images/logo.svg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <a href="/">
+          <img src={logo} alt="로고" />
+        </a>
+      </div>
+      <div className="header-content">
+        <Profile />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
