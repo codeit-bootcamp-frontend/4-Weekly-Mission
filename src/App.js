1ffeab443d8a413077d68396c559f796f53724ca
jinhok96 <jinhok96a@gmail.com>
2024-02-09 17:57:33 +0900

    refactor: 리액트 초기 파일 구조 정리 및 테스트 	modified:   .gitignore 	new file:   README.old.md 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/pages/Shared/Shared.css 	new file:   src/pages/Shared/Shared.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect, useState } from "react-dom";
+import "./App.css";
+import Shared from "./pages/Shared/Shared.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>안녕하세요 App.js입니다.</div>
+      <Shared></Shared>
     </div>
   );
 }
---
+++ src/pages/Shared/Shared.js
@@ -0,0 +1,7 @@
+import "./Shared.css";
+
+function Shared() {
+  return <div>안녕하세요 Shared.js입니다.</div>;
+}
+
+export default Shared;
