2282229793767e50c95d4a604ff11f35f6c0fabc
78-artilleryman <ywy040150@gmail.com>
2024-02-10 01:38:07 +0900

    feat: Footer 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Footer />
     </div>
   );
 }
