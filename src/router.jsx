5f1c3d6a5e399274b4dbc698fa2596eb94c43419
sihyonn <sihyeon982@naver.com>
2024-02-10 21:04:44 +0900

    feat: 라우터 설정 추가

---
+++ src/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import routes from 'constants/routes';
+import Layout from 'components/template/Layout';
+import SharePage from 'pages/SharePage';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <Layout />,
+    children: [
+      {
+        //7주차 과제 요구사항 - 링크공유 페이지를 '/'에 보이도록, 추후 메인페이지로 변경 예정
+        path: routes.home,
+        element: <SharePage />,
+      },
+    ],
+  },
+]);
+
+export default router;
