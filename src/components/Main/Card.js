635f4bd0fbdb231b098f5fca866cfd12f44a852c
jinhok96 <jinhok96a@gmail.com>
2024-02-10 20:25:16 +0900

    chore: Card 컴포지션에 여러 props 적용 	modified:   src/components/Main/Card.js 	new file:   src/utils/formatDate.js

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,13 +1,18 @@
 import "./Card.css";
+import timeAgo from "../../utils/timeAgo";
+import formatDate from "../../utils/formatDate";
 
 function Card({ createdAt = "", url = "", title = "", description = "", imageSource = "" }) {
+  const createdTimeAgo = timeAgo(createdAt);
+  const createdDate = formatDate(createdAt);
+
   return (
     <div className="card">
       <img className="card-image width-full" src={imageSource} alt={title} />
       <div className="card-text-box">
-        <p className="time-ago text-color-text">10 minutes ago</p>
+        <p className="time-ago text-color-text">{createdTimeAgo}</p>
         <p className="card-name multiline-ellipsis">{description}</p>
-        <p className="create-date">2023. 3. 15</p>
+        <p className="create-date">{createdDate}</p>
       </div>
     </div>
   );
