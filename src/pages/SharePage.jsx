89cb8635832ba7b71935b38c75ffa263e13e37de
sihyonn <sihyeon982@naver.com>
2024-02-10 21:09:07 +0900

    feat: SharePage 생성

---
+++ src/pages/SharePage.jsx
@@ -0,0 +1,25 @@
+import { GridTemplate } from 'styles/commonStyle';
+
+import Card from 'components/common/card/Card';
+
+function SharePage() {
+  // 테스트 작업 중
+
+  return (
+    <>
+      <GridTemplate>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </GridTemplate>
+    </>
+  );
+}
+
+export default SharePage;
