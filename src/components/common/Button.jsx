79a5ff7c46d2d97a58de3f8e73de50cbda9e3730
mun-jihye <m84422583@gmail.com>
2024-02-10 22:13:00 +0900

    feat: 공통 컴포넌트 분리

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 /**
  * @param {object} props
  * @param {node} children
@@ -7,7 +9,7 @@
  */
 const Button = ({ children, onClick, disabled, className }) => {
   return (
-    <button
+    <StyledButton
       className={className}
       disabled={disabled}
       onClick={e => {
@@ -16,8 +18,38 @@ const Button = ({ children, onClick, disabled, className }) => {
       }}
     >
       {children}
-    </button>
+    </StyledButton>
   );
 };
 
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3.7rem;
+  cursor: pointer;
+  background-image: linear-gradient(
+    135deg,
+    ${props => props.theme.primary_color} 0%,
+    #6ae3fe 100%
+  );
+  border-radius: 0.8rem;
+  color: #f5f5f5;
+  font-size: 1.4rem;
+  font-weight: 600;
+
+  @media (min-width: 768px) {
+    height: 5.4rem;
+    border-radius: 0.8rem;
+    font-size: 1.8rem;
+  }
+
+  &.login {
+    width: 8rem;
+
+    @media (min-width: 768px) {
+      width: 12.8rem;
+    }
+  }
+`;
 export default Button;
