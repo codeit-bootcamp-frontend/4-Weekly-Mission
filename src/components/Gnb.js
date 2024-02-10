d3b3d2ea4f6a9e8fbb5a00b7f52eed18543e0837
whai2 <whai2125@gmail.com>
2024-02-10 16:17:47 +0900

    feat: 프로필 데이터가 없을 시, 로그인 버튼 보이게 하기 기능 구현

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -3,16 +3,22 @@ import './Gnb.css';
 import logoImg from '../assets/logo.svg';
 
 const Gnb = ({className, items}) => {
+  const isProfileAvailable = items && items.profileImageSource && items.email;
+
   return (
     <div className={className}>
       <div className='gnb-inner_ly'>
         <a href="/">
           <img className='logo' src={logoImg} alt="홈으로 연결된 Linkbrary 로고" />
         </a>
-        <div className='account'>
-          <img className="account-img" src={items.profileImageSource} alt="로그인 이미지" />
-          <span className='account-email'>{items.email}</span>
-        </div>
+        {isProfileAvailable ? (
+          <div className='account'>
+            <img className="account-img" src={items.profileImageSource} alt="프로필 이미지" />
+            <span className='account-email'>{items.email}</span>
+          </div>
+        ) : (
+          <button className='login-button'>로그인</button>
+        )}
       </div>
     </div> 
   )
