dd540663b7e1533a21a0e08729961575bbf68315
sihyonn <sihyeon982@naver.com>
2024-02-11 04:08:14 +0900

    feat: Layout에 Header, Footer 컴포넌트 추가

--- src/components/template/Layout.jsx
+++ src/components/template/Layout.jsx
@@ -3,38 +3,41 @@ import { Outlet } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import GNB from 'components/common/gnb/GNB';
+import Footer from 'components/common/footer/Footer';
+import Header from 'components/common/header/Header';
 
 const Styled = {
   Body: styled.div`
     width: 100vw;
     height: 100vh;
-    padding: 0 calc((100vw - 110rem) / 2 + 2rem);
-    @media (max-width: 1199px) {
-      padding: 0 3.2rem;
-    }
-
+    position: relative;
     background-color: ${({ theme }) => theme.color.white};
-    border-top: 1px solid #ccc; // 테스트용
   `,
 
   Container: styled.div`
     width: 100%;
+    padding: 0 calc((100vw - 110rem) / 2 + 2rem);
+    @media (max-width: 1199px) {
+      padding: 0 3.2rem;
+    }
   `,
 };
 
 function Layout({ children }) {
   const calculatePadding = () => {
-    if (!isMobile) return '9.3rem';
-    else return '6.3rem';
+    if (!isMobile) return '4rem';
+    else return '2rem';
   };
 
   return (
     <Styled.Body className="layout-body">
       <GNB />
+      <Header />
       <Styled.Container className="layout-container" style={{ paddingTop: calculatePadding() }}>
         <Outlet />
         {children}
       </Styled.Container>
+      <Footer />
     </Styled.Body>
   );
 }
