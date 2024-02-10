60eca70aa08d559216c2be83bb2dfe9c4dc1014d
mun-jihye <m84422583@gmail.com>
2024-02-10 22:13:27 +0900

    feat: 페이지 분리

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import GNB from '../components/common/layout/header/GNB';
+import styled from 'styled-components';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <Nav>
+      <GNB />
+    </Nav>
+  );
 };
 
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background-color: #edf7ff;
+`;
 export default MainPage;
