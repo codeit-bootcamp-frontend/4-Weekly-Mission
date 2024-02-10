df2767c6a9f98e3882cb2ce194058b56e7c76c45
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-10 22:25:51 +0900

    Refactor: 컴포넌트 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,40 @@
-import { useEffect, useState } from "react";
-import { getFolder } from "../api";
+import { useEffect, useState } from 'react';
+import { getFolder } from '../api';
 import '../styles/style.css';
 import Header from './Header';
-import Main from "./Main";
+import Main from './Main';
+import FolderSection from './FolderSection';
+import Footer from './Footer'
 
 function App() {
   const [folderName, setFolderName] = useState('');
   const [userName, setUserName] = useState('');
   const [profileImage, setProfileImage] = useState('');
-  
+  const [folderList, setFolderList] = useState([]);
+
   const handleLoad = async () => {
-    const {folder: {name, owner}} = await getFolder();
+    const {
+      folder: { name, owner, links },
+    } = await getFolder();
+
     setFolderName(name);
     setUserName(owner.name);
     setProfileImage(owner.profileImageSource);
-  }
+    setFolderList(links);
+  };
 
   useEffect(() => {
     handleLoad();
-  }, [])
+  }, []);
 
   return (
     <>
       <Header />
-      <Main folderName={folderName} userName={userName} imgSrc={profileImage}/>
+      <Main folderName={folderName} userName={userName} imgSrc={profileImage} />
+      <FolderSection folderList={folderList} />
+      <Footer />
     </>
-    );
+  );
 }
 
 export default App;
