79f0a0614bc0cb6cd3d67d4ca34ff852b9f2318e
aoooec <foottable@gmail.com>
2024-02-10 19:08:09 +0900

    feat: footer 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "./global.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
