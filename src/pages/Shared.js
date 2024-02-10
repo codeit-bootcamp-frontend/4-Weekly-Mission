839376286ebef31cfa6e64abddc7b45f893060ae
jae6269 <jae6269@gmail.com>
2024-02-11 01:53:33 +0900

    feat(Cards) : cards 컴포넌트 input 태그 / 디자인 추가

--- src/pages/Shared.js
+++ src/pages/Shared.js
@@ -1,12 +1,13 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Profile from '../components/Profile';
-
+import Cards from '../components/Cards';
 function Shared() {
   return (
     <>
       <Header></Header>
       <Profile></Profile>
+      <Cards></Cards>
       <Footer></Footer>
     </>
   );
