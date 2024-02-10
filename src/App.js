9367f2278f6e8184080cb602de9ad9ced1d204c0
HaeJungg <gpwnd0120@gmail.com>
2024-02-10 21:35:48 +0900

    ✨feat : search 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
+import Search from "./components/CardGrid/Search/Search";
 
 function App() {
   return (
     <div>
       <Header />
+      <Search />
       <Footer />
     </div>
   );
