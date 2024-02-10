049814e01e64a40e6f01a2481fcff57acd03a947
HaeJungg <gpwnd0120@gmail.com>
2024-02-10 19:46:41 +0900

    ✨feat : folder js 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import Profile from "./components/Folder/Folder";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Profile />
       <Footer />
     </div>
   );
---
+++ src/components/Folder/Folder.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getFolder } from "../../api";
+
+const Profile = () => {
+  const [folderData, setFolderData] = useState(null);
+  const [owner, setOwner] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const folderData = await getFolder();
+        setFolderData(folderData);
+        setOwner(folderData.folder.owner);
+      } catch (error) {
+        console.error("데이터 가져오기 실패:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="profile">
+      {owner && (
+        <div className="user-profile">
+          <img src={owner.profileImageSource} alt="프로필 이미지" />
+          <span>@{owner.name}</span>
+        </div>
+      )}
+      {folderData && (
+        <div className="folder-info">
+          <span>{folderData.folder.name}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
