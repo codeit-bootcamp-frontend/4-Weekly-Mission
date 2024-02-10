e44e73f6c61ae2d7dee309cab627b746c30b52ff
HaeJungg <gpwnd0120@gmail.com>
2024-02-10 23:02:54 +0900

    🎨style : 새 페이지로 이동, 이미지 없을 시 이미지 추가,  카드 css 추가

--- src/components/CardGrid/Cardgrid.js
+++ src/components/CardGrid/Cardgrid.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useCardData } from "./Card/useCardData";
 import Search from "./Search/Search";
+import "./Cardgrid.css";
+import defaultImage from "../../image/no_image.svg";
 
 const CardGrid = () => {
   const { formattedCards } = useCardData();
@@ -10,13 +12,20 @@ const CardGrid = () => {
       <Search />
       <div className="card-grid">
         {formattedCards.map((card) => (
-          <div key={card.id} className="card">
-            <img src={card.imageSource} alt={card.title} />
-            <div>
-              <span>{card.timeAgo}</span>
-              <p>{card.link}</p>
-              <span>{card.formattedCreatedAt}</span>
-            </div>
+          <div className="card">
+            <a
+              key={card.id}
+              href={card.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={card.imageSource || defaultImage} alt={card.title} />
+              <div>
+                <span>{card.timeAgo}</span>
+                <p>{card.description}</p>
+                <span>{card.formattedCreatedAt}</span>
+              </div>
+            </a>
           </div>
         ))}
       </div>
