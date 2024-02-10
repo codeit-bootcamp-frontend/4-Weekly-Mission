a1f2fb9fae47f6912240f6e8b1b0f8e8e6ff523f
jinhok96 <jinhok96a@gmail.com>
2024-02-10 19:54:41 +0900

    chore: 날짜 계산 함수 추가 	modified:   src/components/Main/Card.js 	new file:   src/utils/timeAgo.js

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,14 +1,12 @@
 import "./Card.css";
 
-function Card({ src = "", alt = "" }) {
+function Card({ createdAt = "", url = "", title = "", description = "", imageSource = "" }) {
   return (
     <div className="card">
-      <img className="card-image width-full" src={src} alt={alt} />
+      <img className="card-image width-full" src={imageSource} alt={title} />
       <div className="card-text-box">
         <p className="time-ago text-color-text">10 minutes ago</p>
-        <p className="card-name multiline-ellipsis">
-          Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc consequat. Tldkd
-        </p>
+        <p className="card-name multiline-ellipsis">{description}</p>
         <p className="create-date">2023. 3. 15</p>
       </div>
     </div>
