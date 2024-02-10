27adecae30bed783abcafb127c8bfe1e79b3610e
Jules <dali1999@naver.com>
2024-02-10 19:53:14 +0900

    feat(getUser): 사용자 정보 조회를 위한 API 요청 전송 기능 추가

--- src/api.jsx
+++ src/api.jsx
@@ -8,3 +8,12 @@ export async function getFolder() {
   const body = await response.json();
   return body;
 }
+
+export async function getUser() {
+  const response = await fetch(`${BASE_URL}/sample/user`);
+  if (!response.ok) {
+    throw new Error("사용자 정보를 불러오는데 실패했습니다.");
+  }
+  const body = await response.json();
+  return body;
+}
