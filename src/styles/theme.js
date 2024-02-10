cf20389408c89230e3885d2857bb14d8badb931c
mun-jihye <m84422583@gmail.com>
2024-02-10 22:13:58 +0900

    feat: 공통 스타일 분리

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  primary_color: '#6d6afe',
+  linkbrary_red: '#ff5b56',
+  linkbrary_black: '#111322',
+  linkbrary_white: '#ffffff',
+  linkbrary_gray100: '#373740',
+  linkbrary_gray60: '#9fa6b2',
+  linkbrary_gray20: '#ccd5e3',
+  linkbrary_gray10: '#e7effb',
+  linkbrary_bg: '#f0f6ff',
+};
