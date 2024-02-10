689a6f94a88b0aa7cdfe0994d5a0d7b8520b88c4
sihyonn <sihyeon982@naver.com>
2024-02-10 21:05:24 +0900

    feat: App 컴포넌트에 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { Route, Routes } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 import theme from 'styles/theme';
 import GlobalStyle from 'styles/globalStyle';
-
-import SharePage from 'pages/SharePage';
+import router from 'router.jsx';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes>
-        <Route path="/share" element={<SharePage />}></Route>
-      </Routes>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
