ccadb81ecfed3cf5c886470afb459ef57a3a1ef5
Jules <dali1999@naver.com>
2024-02-10 20:00:39 +0900

    feat(timeAgo): createAt과 현재 Date 의 차이를 보여주는 기능 추가

--- src/components/shared/CardList.jsx
+++ src/components/shared/CardList.jsx
@@ -7,6 +7,29 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}.`;
 }
 
+function timeAgo(value) {
+  const currentDate = new Date();
+  const createdDate = new Date(value);
+  const timeDiff = currentDate - createdDate;
+  const secondsDiff = Math.floor(timeDiff / 1000);
+  const minutesDiff = Math.floor(secondsDiff / 60);
+  const hoursDiff = Math.floor(minutesDiff / 60);
+  const daysDiff = Math.floor(hoursDiff / 24);
+  const monthsDiff = Math.floor(daysDiff / 30);
+  const yearsDiff = Math.floor(monthsDiff / 12);
+
+  if (minutesDiff < 2) return "1 minute ago";
+  if (minutesDiff < 60) return minutesDiff + " minutes ago";
+  if (hoursDiff === 1) return "1 hour ago";
+  if (hoursDiff < 24) return hoursDiff + " hours ago";
+  if (daysDiff === 1) return "1 day ago";
+  if (daysDiff < 30) return daysDiff + " days ago";
+  if (monthsDiff === 1) return "1 month ago";
+  if (monthsDiff < 12) return monthsDiff + " months ago";
+  if (yearsDiff === 1) return "1 year ago";
+  return Math.floor(monthsDiff / 12) + " years ago";
+}
+
 const Card = ({ item }) => {
   const { url, imageSource, description, createdAt } = item;
   return (
@@ -20,7 +43,7 @@ const Card = ({ item }) => {
           />
         </div>
         <div className="Card-content-wrapper">
-          <p>10 minutes ago</p>
+          <p>{timeAgo(createdAt)}</p>
           <p className="description">{description}</p>
           <p>{formatDate(createdAt)}</p>
         </div>
