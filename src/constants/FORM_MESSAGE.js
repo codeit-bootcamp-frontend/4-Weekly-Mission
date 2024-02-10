49901cb46164758cbcb21855982e06d0607c5696
sihyonn <sihyeon982@naver.com>
2024-02-11 05:06:13 +0900

    feat: SearchBar 컴포넌트 생성, SharePage에 추가

---
+++ src/constants/FORM_MESSAGE.js
@@ -0,0 +1,4 @@
+const PLACEHOLDER = Object.freeze({
+  SEARCH_LINK: '링크를 검색해 보세요.',
+});
+export default PLACEHOLDER;
--- src/pages/SharePage.jsx
+++ src/pages/SharePage.jsx
@@ -1,12 +1,15 @@
 import { GridTemplate } from 'styles/commonStyle';
 
+import SearchBar from 'components/SearchBar';
 import Card from 'components/common/card/Card';
+import PLACEHOLDER from 'constants/FORM_MESSAGE';
 
 function SharePage() {
   // 테스트 작업 중
 
   return (
     <>
+      <SearchBar placeholder={PLACEHOLDER.SEARCH_LINK} style={{ marginBottom: '4rem' }} />
       <GridTemplate>
         <Card />
         <Card />
