c6203f10d3055792650086a4fc1238b2e5958a80
jinhok96 <jinhok96a@gmail.com>
2024-02-10 21:08:30 +0900

    chore: Card 컴포넌트 이미지 적용 	modified:   src/components/Main/Card.css 	modified:   src/components/Main/Card.js

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -6,9 +6,16 @@ function Card({ createdAt = "", url = "", title = "", description = "", imageSou
   const createdTimeAgo = timeAgo(createdAt);
   const createdDate = formatDate(createdAt);
 
+  const backgroundImage = {
+    background: `url(${imageSource})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
     <div className="card">
-      <img className="card-image width-full" src={imageSource} alt={title} />
+      <div className="card-image width-full" style={backgroundImage} />
       <div className="card-text-box">
         <p className="time-ago text-color-text">{createdTimeAgo}</p>
         <p className="card-name multiline-ellipsis">{description}</p>
