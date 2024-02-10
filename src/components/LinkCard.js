2e20bb6759a2f83bcfdb186ae04a4b064e44034e
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-11 02:18:33 +0900

    Feat: 별 아이콘, 케밥 아이콘 추가

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,4 +1,6 @@
 import '../styles/LinkCard.css';
+import star from '../assets/star.svg';
+import kebab from '../assets/kebab.svg';
 
 /**
  * 1000미리초 -> 1초
@@ -72,9 +74,17 @@ function LinkCard({ url, createdAt, desc, imgUrl }) {
     <a href={url} className="link-card-area">
       <div className="img-group">
         {imgUrl ? <img src={imgUrl} alt="미리보기" className="img-card" /> : <div className="img-card no-img"></div>}
+        <button className="btn-star">
+          <img src={star} alt="별모양" />
+        </button>
       </div>
       <div className="card-info-group">
-        <p className="elapsed-time">{getElapsedTimeMessage(createdDays)}</p>
+        <div className="time-info-group">
+          <p className="elapsed-time">{getElapsedTimeMessage(createdDays)}</p>
+          <button className="more">
+            <img src={kebab} alt="더보기" />
+          </button>
+        </div>
         <p className="desc">{desc}</p>
         <p className="date">{`${createdDays.getFullYear()}. ${createdDays.getMonth() + 1}. ${createdDays.getDate()}`}</p>
       </div>
