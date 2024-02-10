945833a96c902a37d276309f7219191714cfcf1b
jae6269 <jae6269@gmail.com>
2024-02-11 00:39:23 +0900

    refactor : 폴더구조 조정 및 재작업 2024 02 11 위클리미션 내용 오해로 폴더구조 조정, 재시작 header,footer 컴포넌트화 및 프로필 데이터 받아오기 구현

---
+++ src/components/Cards.js
@@ -0,0 +1,3 @@
+function Cards() {}
+
+export default Cards;
---
+++ src/pages/Shared.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Profile from '../components/Profile';
+
+function Shared() {
+  return (
+    <>
+      <Header></Header>
+      <Profile></Profile>
+      <Footer></Footer>
+    </>
+  );
+}
+export default Shared;
