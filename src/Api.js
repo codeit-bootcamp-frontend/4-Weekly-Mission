4a9e0125cbf2587f01c6f3dfa024e7bcea42e6f2
Rhajiit <ksd9638@gmail.com>
2024-02-11 12:56:43 +0900

    Feat: HeadNav의 데이터를 서버에서 받아오도록 수정

--- src/Api.js
+++ src/Api.js
@@ -1,9 +1,12 @@
-const acceptUserDataFromApi = async function () {
-	const response = await fetch(
-		"https://bootcamp-api.codeit.kr/api/sample/user"
-	);
+const BASE_API_URL = "https://bootcamp-api.codeit.kr/api/sample/";
+
+const acceptDataFromApi = async function (parameter) {
+	const response = await fetch(BASE_API_URL + parameter);
+	if (!response.ok) {
+		throw new Error("정보를 받아오는데 실패햐였습니다.");
+	}
 	const convertedResponse = await response.json();
 	return convertedResponse;
 };
 
-export { acceptUserDataFromApi };
+export { acceptDataFromApi };
--- src/components/HeadNav.js
+++ src/components/HeadNav.js
@@ -1,7 +1,45 @@
 import "styles/reset.css";
 import "styles/defaultStyle.css";
 import "components/HeadNav.css";
-import ProfileData from "components/ProfileData.js";
+import { useEffect, useState } from "react";
+import { acceptDataFromApi } from "Api";
+
+const ProfileData = function () {
+	const [loginStatus, setLoginStatus] = useState(false);
+	const [accountEmail, setAccountEmail] = useState("");
+	const [profileImg, setProfileIMg] = useState("navProfile.png");
+
+	const user = "user";
+	const accountVerification = async (user) => {
+		const receivedData = await acceptDataFromApi(user);
+		if (!receivedData) return;
+
+		setAccountEmail(receivedData.email);
+		setProfileIMg(receivedData.profileImageSource);
+		setLoginStatus(true);
+	};
+
+	useEffect(() => {
+		accountVerification(user);
+	}, [loginStatus]);
+
+	return (
+		<>
+			{loginStatus && (
+				<div className="navProfile">
+					<img src={profileImg} alt="loggedInProfileImg" />
+					{accountEmail}
+				</div>
+			)}
+
+			{!loginStatus && (
+				<a href="html/signin.html" className="defaultBtn">
+					로그인
+				</a>
+			)}
+		</>
+	);
+};
 
 export default function HeadNav() {
 	return (
