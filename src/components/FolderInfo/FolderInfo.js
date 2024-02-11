ce9666fb8fdd3170628678a34651bbf873c9b51b
LeeSeungHoon <ysh73900@gmail.com>
2024-02-12 01:35:51 +0900

    refactor: null 또는 정의되지 않은 값의 속성 엑세스와 관련되 오류 방지

--- src/components/FolderInfo/FolderInfo.js
+++ src/components/FolderInfo/FolderInfo.js
@@ -2,15 +2,16 @@ import { Fragment } from "react";
 import "./FolderInfo.css";
 
 const FolderInfo = ({ profile, folderName }) => {
+  const { name, profileImageSource } = profile || {};
   return (
     <Fragment>
       <div className="folderInfo">
         <img
           className="folderInfo-profile"
-          src={profile.profileImageSource}
+          src={profileImageSource}
           alt="폴더 소유자의 프로필 이미지"
         />
-        <span className="folderInfo-owenrName">{profile.name}</span>
+        <span className="folderInfo-owenrName">{name}</span>
         <span className="folderInfo-folderName">{folderName}</span>
       </div>
     </Fragment>
