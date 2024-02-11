2a7b6e76e4c8a018154cdb3dbd5436e0e829c4a6
Trophy198 <spsp8426@naver.com>
2024-02-11 23:34:41 +0900

    feat : 버튼 컴포넌트 추가

---
+++ src/components/common/Button/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ className, onClick, children }) => {
+  return (
+    <button className={`${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
