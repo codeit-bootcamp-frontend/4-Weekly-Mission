e8c3daf442a6ba397c1e1a119490d5ca0debc51a
minbo <smb5174@naver.com>
2024-02-11 12:48:19 +0900

    feat: Footer 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Cardlist from "./components/Cardlist";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
       <Header />
       <Nav />
       <Cardlist />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import "../style/footer.css";
+import facebookIcon from "../images/facebook.svg";
+import twitterIcon from "../images/twitter.svg";
+import youtubeIcon from "../images/youtube.svg";
+import instagramIcon from "../images/instagram.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-text">@codeit - 2023</p>
+      <div className="footer-link">
+        <a href="/privacy">
+          <p className="footer-privacy">Privacy Policy</p>
+        </a>
+        <a href="/FAQ">
+          <p className="footer-faq">FAQ</p>
+        </a>
+      </div>
+      <div className="footer-icon">
+        <a
+          href="https://facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={facebookIcon} alt="페이스북" />
+        </a>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitterIcon} alt="트위터" />
+        </a>
+        <a
+          href="https://youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={youtubeIcon} alt="유튜브" />
+        </a>
+        <a
+          href="https://instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagramIcon} alt="인스타그램" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
