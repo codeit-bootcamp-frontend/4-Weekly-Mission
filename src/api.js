696ce0e10f2f51c978cf3b1b9f582bd42642ddaf
ggjiny <jnss599@gmail.com>
2024-02-11 19:53:31 +0900

    fix: api 주소 수정

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export async function getUserInfo() {
 }
 
 export async function getFolderData() {
-  const response = await fetch(`https://bootcamp-api.codeit.kr/api/sample/folde`);
+  const response = await fetch(`https://bootcamp-api.codeit.kr/api/sample/folder`);
   if (!response.ok) {
     throw new Error("데이터를 불러오는데 실패했습니다");
   }
