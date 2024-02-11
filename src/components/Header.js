0157971f04eab402344eb2d32f975c9560708cb4
yeeZinu <doglife222@gmail.com>
2024-02-12 01:41:23 +0900

    feat: Header컴포넌트에 필요한 정보 불러오기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,31 @@
-function Header(user) {
+import React from 'react';
+import Logo_Img from '../assets/logo.svg'
+
+function Header({ user }) {
+  // const userEmail = user.email;
+  // const userImg = user.profileImageSource;
+
   console.log(user);
+
+
   return (
     <div>
-      <h1>Header</h1>
+      <a href='/'>
+        <img src={Logo_Img} alt="Logo" />
+      </a>
+      <div>
+        {user ? (
+          <>
+            <img src={user.profileImageSource} alt="프로필 사진" className="user-profile" />
+            <span className="user-email">{user.email}</span>
+          </>
+        ) : (
+          <a href="/">
+            로그인
+          </a>
+        )}
+
+      </div>
     </div>
   )
 }
