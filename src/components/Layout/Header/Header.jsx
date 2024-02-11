462e1e94ef5bd8a3039a09a10b73781cd182ce24
devwqc <devwqc@gmail.com>
2024-02-12 01:07:45 +0900

    feat: 로그인 여부에 따른 Header 컴포넌트 Button 변경

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,8 +1,29 @@
 import './Header.css';
 import { Button } from '../../Button';
 import { IconLogo } from '../../Icon';
+import { useSetUser, useUser } from '../../../contexts/LoginContext';
+import { getUser } from '../../../api';
 
 function Header() {
+  const user = useUser();
+  const setUser = useSetUser();
+
+  const handleLoginClick = async () => {
+    try {
+      const user = await getUser();
+      if (!user) return;
+      setUser(user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const authButton = user ? (
+    <ButtonUser user={user} />
+  ) : (
+    <ButtonLogin onClick={handleLoginClick} />
+  );
+
   return (
     <header className="header">
       <div className="header__inner">
@@ -13,11 +34,29 @@ function Header() {
             </a>
           </div>
         </div>
-        <div className="header__auth">
-          <Button title="로그인" />
-        </div>
+        <div className="header__auth">{authButton}</div>
       </div>
     </header>
+  );
+}
+
+function ButtonLogin({ onClick }) {
+  return <Button title="로그인" onClick={onClick} />;
+}
+
+// 컴포넌트 분리 예정
+function ButtonUser({ user }) {
+  return (
+    <div className="button-user">
+      <div className="button-user__img__container">
+        <img
+          className="button-user__img"
+          src={user.profileImageSource}
+          alt={user.name}
+        />
+      </div>
+      <div className="button-user__email">{user.email}</div>
+    </div>
   );
 }
 
