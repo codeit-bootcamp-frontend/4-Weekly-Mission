57c683250fcf56d8a66f5972843cd52d4d0b2f6d
ingssg <js2121good@gmail.com>
2024-02-12 03:48:56 +0900

    fix: alt에 image 제거

--- src/pages/MainPage/UserMainProfile.js
+++ src/pages/MainPage/UserMainProfile.js
@@ -3,7 +3,7 @@ import "./UserMainProfile.css";
 export default function UserMainProfile({ user, folder }) {
   return (
     <div className="main-profile-container">
-      <img src="images/Avatar.svg" alt="profile-image"></img>
+      <img src="images/Avatar.svg" alt="profile"></img>
       <span className="profile-id">{user.name}</span>
       <span className="folder-name">{folder.name}</span>
     </div>
