11e8a862ee3d00f50520605f85d177424a4cb560
devwqc <devwqc@gmail.com>
2024-02-11 22:07:50 +0900

    feat: Shared 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 import { Layout } from './components/Layout';
+import Shared from './components/Shared/Shared';
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Shared />
+    </Layout>
+  );
 }
 
 export default App;
