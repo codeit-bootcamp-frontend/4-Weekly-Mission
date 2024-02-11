055d125d4ecbef0796fd5758de884c35097bb63c
choiwonseokgit <cs0328@hanmail.net>
2024-02-11 21:44:33 +0900

    feat: 카드 클릭시 링크로 이동 구현 및 description 도 prop에서 온 데이터로 변경 완료

--- src/components/Main.js
+++ src/components/Main.js
@@ -63,17 +63,18 @@ function Card({ card }) {
   const dateDiff = dateDiffCalc(dateOfCard);
   const formatDate = dateFormatter(dateOfCard);
 
+  const moveToUrl = () => {
+    window.open(card.url, "_blank");
+  };
+
   return (
-    <div className="card-container">
+    <div className="card-container" onClick={moveToUrl}>
       <img className="card-container-img" src={cardImage} alt="이미지"></img>
       <div className="card-container-texts">
         <div className="card-container-dateDiff">
           <div>{dateDiff}</div>
         </div>
-        <div className="card-container-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
-          mollitia.
-        </div>
+        <div className="card-container-description">{card.description}</div>
         <div className="card-conatiner-formatDate">{formatDate}</div>
       </div>
     </div>
