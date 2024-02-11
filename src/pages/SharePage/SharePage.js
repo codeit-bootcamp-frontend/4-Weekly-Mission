cf3e52bfc881a2e217b0a79a5a7d011d3aff3efd
LeeSeungHoon <ysh73900@gmail.com>
2024-02-12 00:13:23 +0900

    feat: cardList 렌더링

--- src/pages/SharePage/SharePage.js
+++ src/pages/SharePage/SharePage.js
@@ -1,12 +1,15 @@
 import { Fragment } from "react";
 import "./SharePage.css";
 
-const SharePage = ({ folderInfo, searchBar }) => {
+const SharePage = ({ folderInfo, searchBar, cardList }) => {
   return (
     <Fragment>
       <div className="sharePage">
         {folderInfo}
-        <div className="sharePage-content">{searchBar}</div>
+        <div className="sharePage-content">
+          {searchBar}
+          {cardList}
+        </div>
       </div>
     </Fragment>
   );
