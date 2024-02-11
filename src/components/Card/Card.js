810c6634f24f73df7a69684072bb165881121258
LeeSeungHoon <ysh73900@gmail.com>
2024-02-12 03:24:11 +0900

    refactor: 컴포넌트화 분리

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,16 @@
 import { Fragment } from "react";
-import { getElapsedTime } from "../../utils/getElapsedTime";
 import "./Card.css";
 
-function formatDate(value) {
-  const date = new Date(value);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
-}
-
-const Card = ({ item }) => {
-  const { title, createdAt, url, description, imageSource } = item;
+const Card = ({ children, onMouseOver, onMouseLeave }) => {
   return (
     <Fragment>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <img className="card-image" src={imageSource} alt="" />
-        <div className="card-text">
-          <p className="card-text-time-ago">{getElapsedTime(createdAt)}</p>
-          <p className="card-text-description">{description}</p>
-          <p className="card-text-date">{formatDate(createdAt)}</p>
-        </div>
-      </a>
+      <div
+        className="card"
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+      >
+        {children}
+      </div>
     </Fragment>
   );
 };
