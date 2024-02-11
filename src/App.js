6be0aac9ac485a9c32653801b68ad829708a5ac7
JA_X <envijs93@gmail.com>
2024-02-11 15:33:16 +0900

    Footer 컴포넌트 완성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import './styles/reset.css';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
+      <Footer />
     </div>
   );
 }
