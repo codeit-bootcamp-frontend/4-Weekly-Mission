0a0a2bfbeb3d9d5a2bc4398d74ba976e9efce40b
이진호 <tead7@naver.com>
2024-02-11 22:00:22 +0900

    Feat: 프로필 기능 완료

--- src/layout/Pagelayout/Layout.jsx
+++ src/layout/Pagelayout/Layout.jsx
@@ -1,10 +1,14 @@
 import { Footer } from "../Footer/Footer";
 import { NavigationBar } from "../Nav";
+import { useGetUser } from "../../data/useGetUser";
 
 export const Layout = ({ children }) => {
+  const { data } = useGetUser();
+  const { email, profileImageSource } = data || {};
+  const profile = data ? { email, profileImageSource } : null;
   return (
     <div>
-      <NavigationBar />
+      <NavigationBar profile={profile} />
       <main>{children}</main>
       <Footer />
     </div>
