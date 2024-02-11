3dc5d98af0d29350b4dab3c6082dbc8a942ea195
yeeZinu <doglife222@gmail.com>
2024-02-12 02:17:18 +0900

    feat: FolderList 컴포넌트에 필요한 데이터 불러오기

--- src/components/FolderList.js
+++ src/components/FolderList.js
@@ -1,10 +1,24 @@
+import React from "react";
 import Card from "./Card";
+import SearchIcon from '../assets/search.svg';
 
-function FolderList({folderList}) {
-  return(
+function FolderList({ folderList }) {
+  console.log('FolderList', folderList);
+  return (
     <section>
-      <h2>CardList</h2>
-      <Card />
+      <div>
+        <img src={SearchIcon} alt="검색아이콘"></img>
+        <input type="text" placeholder="링크를 검색해보세요" />
+      </div>
+      <ul>
+        {folderList.map(({id, url, imageSource, description, createdAt}) => {
+          return (
+            <li key={id}>
+              <Card url={url} imgUrl={imageSource} desc={description} createdAt={createdAt}/>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
