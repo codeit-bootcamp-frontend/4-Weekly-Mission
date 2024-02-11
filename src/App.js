26d62808b3df7cc7ed3cf124d32fed8a995df0ee
LeeSeungHoon <ysh73900@gmail.com>
2024-02-11 21:00:23 +0900

    feat: handleLoad 함수

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { getUser, getFolder } from "./api/api";
 import "./global.css";
 import Layout from "./components/Layout/Layout";
+import FolderInfo from "./components/FolderInfo/FolderInfo";
+import SharePage from "./pages/SharePage/SharePage";
 
 function App() {
+  const [userProfile, setUserProfile] = useState(null);
+  const [profile, setProfile] = useState(null);
+  const [folderName, setFolderName] = useState(null);
+
+  const handleLoad = async () => {
+    const userData = await getUser();
+    const { folder } = await getFolder();
+    setUserProfile(userData);
+    setProfile(folder.owner);
+    setFolderName(folder.name);
+  };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
   return (
     <Fragment>
-      <Layout></Layout>
+      <Layout userProfile={userProfile}>
+        <SharePage
+          folderInfo={<FolderInfo profile={profile} folderName={folderName} />}
+        />
+      </Layout>
     </Fragment>
   );
 }
