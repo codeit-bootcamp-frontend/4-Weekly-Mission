e1afdea986d1cf6f8d9485c4387a89fbe4fa8c42
devwqc <devwqc@gmail.com>
2024-02-11 22:52:28 +0900

    fix: svg 컴포넌트의 kebab-case 속성을 camelCase로 변경

--- src/components/Icon/IconSearch.jsx
+++ src/components/Icon/IconSearch.jsx
@@ -13,17 +13,17 @@ function IconSearch() {
             id="Ellipse_739"
             d="M7.84393 13.8363C11.1534 13.8363 13.8363 11.1534 13.8363 7.84393C13.8363 4.53444 11.1534 1.85156 7.84393 1.85156C4.53444 1.85156 1.85156 4.53444 1.85156 7.84393C1.85156 11.1534 4.53444 13.8363 7.84393 13.8363Z"
             stroke="#666666"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             id="Line_181"
             d="M12.0117 12.3232L14.3611 14.6665"
             stroke="#666666"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </g>
       </g>
