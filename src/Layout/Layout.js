5699c236c4a88951350a3aff615a893e238f6624
skoo1100 <skoo062125@gmail.com>
2024-02-11 18:52:45 +0900

    Footer 생성

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getData } from "../Data/getData";
 import { NavigationBar } from "../Component/Nav/NavigationBar"
+import { Footer } from "../Component/Footer/Footer";
 
 export const Layout = () => {
   const [profile, setProfile] = useState(null);
@@ -12,6 +13,7 @@ export const Layout = () => {
   return (
     <div>
       <NavigationBar profile={profile} />
+      <Footer />
     </div>
   );
 };
