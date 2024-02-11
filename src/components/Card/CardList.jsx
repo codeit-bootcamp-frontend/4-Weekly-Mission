ea8179806fd9e41fff2f0d27d040de9d9c8ac857
devwqc <devwqc@gmail.com>
2024-02-11 16:08:46 +0900

    feat: CardList 컴포넌트 구현

---
+++ src/components/Card/CardList.jsx
@@ -0,0 +1,13 @@
+import Card from './Card';
+
+function CardList({ items = [] }) {
+  return (
+    <div className="card-list">
+      {items.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default CardList;
