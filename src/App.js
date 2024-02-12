2b6a586d303df98f8b786b8b9b657ffcac7b7cf2
yeona813 <aya813@naver.com>
2024-02-12 20:57:47 +0900

    feat : NavigationBar, Profile 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import "./colors.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <Footer />
     </div>
   );
