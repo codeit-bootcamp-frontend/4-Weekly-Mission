6f44191583782b7d4dccbb7a117beade2cc10519
yeona813 <aya813@naver.com>
2024-02-13 01:02:03 +0900

    feat : Header 생성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Footer from "./components/Footer";
 import "./colors.css";
 import NavigationBar from "./components/NavigationBar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Header />
       <Footer />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+
+function Header() {
+  const [folderData, setFolderData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://bootcamp-api.codeit.kr/api/sample/folder"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch folder data");
+      }
+      const data = await response.json();
+      setFolderData(data.folder);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="Header">
+      {folderData && (
+        <>
+          <img
+            className="Header-profile"
+            src={folderData.owner.profileImageSource}
+            alt="폴더 소유자 프로필 이미지"
+          />
+          <span className="Header-owner">{folderData.owner.name}</span>
+          <h2 className="Header-folder">{folderData.name}</h2>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Header;
