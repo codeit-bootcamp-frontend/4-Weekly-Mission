5d399bbdb22fda26975af4af8661b2dc313a0c0f
Harry <yjkang1223@naver.com>
2024-02-12 19:45:20 +0900

    feature: Show user profile when user data exist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import Footer from "./footer/Footer.js";
 import NavBar from "./nav-bar/NavBar.js";
+import getUserData from "./userData.js";
 import "./global.css";
 
-const profile = "";
+const App = () => {
+  const [profile, setProfile] = useState(null);
 
-function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await getUserData();
+        const { email, profileImageSource } = data;
+        setProfile({ email, profileImageSource });
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <NavBar profile={profile} />
@@ -12,6 +26,6 @@ function App() {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,16 @@
+import "./Profile.css";
+
 const Profile = function ({ profile }) {
-  return;
-  <span>{profile}</span>;
+  return (
+    <div className="Profile">
+      <img
+        className="Profile-img"
+        src={profile.profileImageSource}
+        alt="프로필"
+      />
+      <span className="Profile-email">{profile.email}</span>
+    </div>
+  );
 };
 
 export default Profile;
