79562813f2997a193176ffcfc5b765600556d81e
BIBI <80617716+JeongSabibi@users.noreply.github.com>
2024-02-12 20:50:07 +0900

    [정사비비] Week7 (#568)
    * Change setting
    
    * feat: Shared 페이지 구현
    
    * feat: 기능 추가
    
    * feat: 로그인 버튼 구현

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+import './styles/header.css';
+
+const Header = ({ user, folder }) => {
+  return (
+    <header>
+      <nav>
+        <a href="/">
+          <img className="logo" src={logo} alt="logo" />
+        </a>
+        {user ? (
+          <div className="profile-container">
+            <img src={user.profileImageSource} alt="profileImage" className="profile-image"></img>
+            <p className="profile-email">{user.email}</p>
+          </div>
+        ) : (
+          <a className="login" href="/">
+            로그인
+          </a>
+        )}
+      </nav>
+      <div className="folder">
+        <img src={folder.profileImageSource} alt="userImage" className="folder-user-image" />
+        <p className="folder-user-name">@{folder.ownerName}</p>
+        <p className="folder-name">{folder.folderName}</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
