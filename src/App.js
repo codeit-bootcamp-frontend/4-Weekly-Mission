29d23fb8271bef59c98f68348c35e1079d4eca0b
yeona813 <aya813@naver.com>
2024-02-12 11:41:08 +0900

    feat : Footer 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import "./colors.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
