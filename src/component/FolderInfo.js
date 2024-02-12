00ebb419c2098b2d40ac62ee5f6c1eb075c77eea
kimgaeun <kimgaeun21@naver.com>
2024-02-13 00:16:24 +0900

    header에 folder 정보 추가

---
+++ src/component/FolderInfo.js
@@ -0,0 +1,49 @@
+﻿import React, { useState, useEffect } from "react";
+import "../style/header.css";
+
+function FolderInfo() {
+  const [folderInfo, setFolderInfo] = useState({
+    ownerName: "",
+    folderName: "",
+    profileImage: "",
+  });
+
+  useEffect(() => {
+    const getFolderInfo = async () => {
+      try {
+        const response = await fetch(
+          "https://bootcamp-api.codeit.kr/api/sample/folder"
+        );
+        if (!response.ok) {
+          throw new Error("error");
+        }
+        const answer = await response.json();
+        setFolderInfo({
+          ownerName: answer.folder.owner.name,
+          folderName: answer.folder.name,
+          profileImage: answer.folder.owner.profileImageSource,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getFolderInfo();
+  }, []);
+
+  return (
+    <div className="folderInfo">
+      <div className="ownerInfo">
+        <img
+          className="folderImage"
+          src={folderInfo.profileImage}
+          alt={folderInfo.ownerName}
+        />
+        <p className="ownerName">@{folderInfo.ownerName}</p>
+      </div>
+
+      <p className="folderName">{folderInfo.folderName}</p>
+    </div>
+  );
+}
+
+export default FolderInfo;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,23 @@
 ﻿import "../style/header.css";
 import logo from "../image/logo.svg";
 import UserInfo from "./UserInfo";
+import FolderInfo from "./FolderInfo";
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <a href="/">
-          <img src={logo} alt="로고" />
-        </a>
-      </div>
-      <div className="userinfo">
-        <UserInfo />
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="로고" />
+          </a>
+        </div>
+        <div className="userinfo">
+          <UserInfo />
+        </div>
+      </header>
+      <FolderInfo />
+    </>
   );
 }
 
