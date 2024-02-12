f9b3c8b134b1a84e7ee4a614c84af25bd9263879
이서인 <seoin2269@naver.com>
2024-02-13 02:26:38 +0900

    style: 폴더 구조 변경

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import twitter from './images/akar-icons_twitter-fill.svg';
 import youtube from './images/akar-icons_youtube-fill.svg';
 import instagram from './images/ant-design_instagram-filled.svg';
 import search from './images/shared/search.svg';
+import card1 from './images/shared/card/1.png';
+import card2 from './images/shared/card/2.png';
+import card3 from './images/shared/card/3.png';
+import card4 from './images/shared/card/4.png';
+import card5 from './images/shared/card/5.png';
+import card6 from './images/shared/card/6.png';
+import card7 from './images/shared/card/7.png';
+import card8 from './images/shared/card/8.png';
+import card9 from './images/shared/card/9.png';
 
 import './App.css';
+import { useState } from 'react';
 import './style/shared.css';
 
 function App() {
@@ -19,7 +29,23 @@ function App() {
           </a>
         </div>
         <div className="headerBtn">
-          <button onClick={() => (window.location.href = './signin')}>로그인</button>
+          <button>로그인</button>
+          {async function login(body) {
+            try {
+              const result = await fetch('https://bootcamp-api.codeit.kr/api/sample/user', {
+                method: 'POST',
+                headers: { 'content-Type': 'application/json' },
+                body: JSON.stringify({ body }),
+              });
+              if (!result.ok) {
+                <button>로그인</button>;
+              } else {
+                console.log(result.status);
+              }
+            } catch (error) {
+              console.error(error);
+            }
+          }}
         </div>
       </header>
       <section>
@@ -36,9 +62,10 @@ function App() {
           <img src={search} alt="돋보기 아이콘" />
           <p>링크를 검색해 보세요</p>
         </div>
+
         <div className="mainContent">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(num => (
-            <img key={num} src={`./images/shared/card/${num}.png`} alt={`${num}번 카드`} />
+            <img key={num} src={`card${num}`} alt={`${num}번 카드`} />
           ))}
         </div>
       </main>
