91161ebed884574501b2f91c1e8d47c0f0ee0611
황준용 <wnsdyd3333@gmail.com>
2024-02-12 20:26:18 +0900

    feat: shared페이지 search-bar

--- src/pages/shared/shared.jsx
+++ src/pages/shared/shared.jsx
@@ -1,9 +1,13 @@
 import Header from "./header/Header.jsx";
+import SearchBar from "./search-bar/SearchBar.jsx";
 
 function Shared() {
   return (
     <>
       <Header />
+      <article>
+        <SearchBar />
+      </article>
     </>
   );
 }
