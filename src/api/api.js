f453cd8e27fcc0893a8822d898303d6de22d9a82
JA_X <envijs93@gmail.com>
2024-02-12 18:37:46 +0900

    Refactor: 베이스 url 상수화

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
-const getSampleUserData = async () => {
-  const response = await fetch('https://bootcamp-api.codeit.kr/api/sample/user');
-  const data = await response.json();
+const BASE_URL = 'https://bootcamp-api.codeit.kr/api/sample/';
 
+const getSampleUserData = async () => {
+  const response = await fetch(`${BASE_URL}user`);
+  const data = await response.json();
   return data;
 };
 
 const getSampleFolderData = async () => {
-  const response = await fetch('https://bootcamp-api.codeit.kr/api/sample/folder');
+  const response = await fetch(`${BASE_URL}folder`);
   const { folder } = await response.json();
   return folder;
 };
