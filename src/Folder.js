5567b582b43af85878902c36430647b03e9e1c00
whai2 <whai2125@gmail.com>
2024-02-12 22:55:49 +0900

    refactor(mentor): 폴터 컴포넌트 조건부 렌더링 수정

--- src/Folder.js
+++ src/Folder.js
@@ -3,29 +3,27 @@ import Card from './components/Card';
 import './Folder.css';
 
 const Folder = ({items}) => {
-  const isShowFolderLinks = items && items.links;
+  if(!items) {
+    return (
+      <div>파트 1에서 만들었던 랜딩 페이지 적용</div>
+    )
+  }
 
   return (
-    <>
-      {isShowFolderLinks ? (
-        <div className='container-folder'>
-          <SearchBar className='search-bar_ly'/>
-          <ul className='card-frame_ly'> 
-            {items.links.map(({ createdAt, imageSource, title, description, url, id }) => (
-              <Card key={id} 
-                imgSrc={imageSource}
-                title={title}
-                description={description}
-                time={createdAt}
-                url={url}
-              />
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>파트 1에서 만들었던 랜딩 페이지 적용</div>
-      )}
-    </>
+    <div className='container-folder'>
+      <SearchBar className='search-bar_ly'/>
+      <ul className='card-frame_ly'> 
+        {items.links.map(({ createdAt, imageSource, title, description, url, id }) => (
+          <Card key={id} 
+            imgSrc={imageSource}
+            title={title}
+            description={description}
+            time={createdAt}
+            url={url}
+          />
+        ))}
+      </ul>
+    </div>
   )
 }
 
