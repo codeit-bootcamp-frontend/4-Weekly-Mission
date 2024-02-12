28fe7143cd3408a0d216c0ee34288ea6c44695c8
JA_X <envijs93@gmail.com>
2024-02-12 18:35:28 +0900

    Feat: Card 링크 이동 및 CardList 반응형 완성

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,13 @@ import { getFormattedDate, getTimeAgo } from '../utils/date';
 export default function Card({ data }) {
   const defaultImageSource = 'https://placehold.co/600x400';
   return (
-    <div className="cardBox">
+    <a href={data.url} target="_blank" className="cardBox">
       <img src={data.imageSource || defaultImageSource} alt="카드 이미지" />
       <div className="cardDataArea">
         <div className="cardTimeAgo">{getTimeAgo(data.createdAt)}</div>
         <div className="cardDesc">{data.description}</div>
         <div className="cardCreatedAt">{getFormattedDate(data.createdAt)}</div>
       </div>
-    </div>
+    </a>
   );
 }
