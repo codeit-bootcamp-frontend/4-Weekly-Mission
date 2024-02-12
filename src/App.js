ff8d298db2d27cdd01b1e306f10370b1f11d2a47
min3eo <kane7674@naver.comm>
2024-02-12 17:20:49 +0900

    feat: Add search bar, some functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Nav } from './components/Nav';
 // import CardList from '/components/CardList/index';
+import { FolderInfo } from './components/Info';
+import { SearchBar } from './components/Search';
 import { Footer } from './components/Footer';
-import "./global.css";
 
+import "./global.css";
+import { useGetFolder } from './components/data/useGet'
 
 function App() {
-  return (
+  const { data } = useGetFolder();
+  const { profileImage, ownerName, folderName, links } = data || {};
+
+  return ( // 할 일: 컨테이너 하나를 더 만들어서 감싸기
     <div>
       <Nav />
-      
+      <FolderInfo profileImage={profileImage} ownerName={ownerName} folderName={folderName} />
+      <div className="search-bar-container"> 
+        <SearchBar />
+      </div>
       <Footer />
     </div> 
   );
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,11 +7,20 @@ export const ROUTE = {
 };
 
 //시간 상수 
-// const second = 1000;
-// const minute = second * 60;
-// const hour = minute * 60;
-// const day = hour * 24;
-// const month = day * 31;
-// const year = month * 12;
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const month = day * 31;
+const year = month * 12;
+
+export const TIME_IN_MILLISECONDS = {
+    second,
+    minute,
+    hour,
+    day,
+    month,
+    year,
+};
   
 
