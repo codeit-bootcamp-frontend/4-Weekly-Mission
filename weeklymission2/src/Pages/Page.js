76df62f1019bcd68afa7848561efe6e9f4a0bd06
eugene9851 <eugene9851@naver.com>
2024-02-12 19:37:16 +0900

    map error 디버깅

--- weeklymission2/src/Pages/Page.js
+++ weeklymission2/src/Pages/Page.js
@@ -2,28 +2,24 @@ import { useState, useEffect } from 'react';
 import { Header } from '../Components/Header';
 import { Footer } from '../Components/Footer';
 import { Main } from '../Components/Main';
-import { getUserData, getFolderData } from '../APIs/PageApi';
+import { getUserData } from '../APIs/PageApi';
 import '../styles/Page.css';
+import { useFolder } from './useFolder';
 
 
 
 export function Page() {
   const [profile, setProfile] = useState({email:null, image:null})
-  const [folder, setFolder] = useState({profileImage:null, profileName: null, folderName: null})
-  const [cardLinks, setCardLinks] = useState({cardLinks: null})
-
 
   useEffect(() => {
     getData()
   }, [])
 
+  const {cardLinks, folder} = useFolder()
 
   const getData = async() => {
     const userData = await getUserData()
-    const { profileImage, profileName, folderName, cardLinks } = await getFolderData()
     setProfile(userData)
-    setFolder({ profileImage, profileName, folderName })
-    setCardLinks(cardLinks)
   }
 
   return (
