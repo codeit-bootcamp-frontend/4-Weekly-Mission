1430279816d0b1b1e227165363a391df5e24db6a
kimgaeun <kimgaeun21@naver.com>
2024-02-13 02:23:33 +0900

    footer구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./component/Header";
 import Main from "./component/Main";
+import Footer from "./component/Footer";
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,35 @@
+﻿import "../style/footer.css";
+import facebookIcon from "../image/facebook.svg";
+import twitterIcon from "../image/twitter.svg";
+import youtubeIcon from "../image/youtube.svg";
+import instagramIcon from "../image/instagram.svg";
+
+function Footer() {
+  return (
+    <footer>
+      <div class="footer_frame">
+        <div class="maker">©codeit - 2023</div>
+        <div class="sub">
+          <a href="./privacy.html">Privacy Policy</a>
+          <a href="./faq.html">FAQ</a>
+        </div>
+        <div class="icons">
+          <a href="https://www.facebook.com/">
+            <img class="icon" src={facebookIcon} alt="facebook" />
+          </a>
+          <a href="https://twitter.com/home?lang=ko">
+            <img class="icon" src={twitterIcon} alt="twitter" />
+          </a>
+          <a href="https://www.youtube.com/">
+            <img class="icon" src={youtubeIcon} alt="youtube" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img class="icon" src={instagramIcon} alt="instagram" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
