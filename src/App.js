ce99b823e917036cd3f1e51acf42bc516f267ce8
강경미 <gaem@gang-gyeongmiui-noteubug.local>
2024-02-13 23:52:27 +0900

    7주차 미완성”

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./stylesheets/main.css";
+import "./App.css";
+import Contents from "./Components/Contents.js";
+import Footer from "./Components/Footer.js";
+import Hero from "./Components/Hero.js";
+import Navigation from "./Components/Navigation.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <Navigation />
       </header>
-    </div>
+      <main>
+        <Hero />
+        <Contents />
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
