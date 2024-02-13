fb00921faa391a7a4c4e0fd9ac9cc2c4023cfd5e
suzinxix <suzinxix@gmail.com>
2024-02-13 19:31:38 +0900

    style: delete div tag in Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,12 +4,11 @@ import { noImage } from "assets";
 import { formatDate, getTimeDifference } from "utils/helpers/dateUtils";
 
 function Card({ item, onClick }) {
-  const { createdAt, description, imageSource, title, url } = item;
+  const { createdAt, description, imageSource } = item;
   return (
     <div className={styles.container} onClick={onClick}>
       <div className={styles.imgWrapper}>
         <img src={imageSource ?? noImage} className={styles.image} />
-        <div></div>
       </div>
       <div className={styles.info}>
         <div className={styles.difference}>{getTimeDifference(createdAt)}</div>
