869aacb5fd946686e8a785c4353504611897a748
byunjiin <jibyun5808@naver.com>
2024-02-13 16:10:35 +0900

    이미지 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import Privacy from './Privacy';
-import Faq from './Faq';
+// import Privacy from './Privacy';
+// import Faq from './Faq';
 
 import '../css/Footer.css';
 
@@ -10,14 +10,14 @@ function Footer(){
     <div className="footerBox">
       <div className="copyright">©codeit - 2023</div>
       <div className="footerLink">
-        <Privacy className="privacy">Privacy Policy</Privacy>
-        <Faq className="faq">FAQ</Faq>
+        <Link to="/Privacy" className="privacy">Privacy Policy</Link>
+        <Link to="Faq" className="faq">FAQ</Link>
       </div>
       <div className="icons">
-        <a href="https://www.facebook.com/?locale=ko_KR"><img src='../../public/img/icons_facebook-fill.svg' alt="페이스북"/></a>
-        <a href="https://twitter.com/?lang=ko"><img src='../../public/img/icons_twitter-fill.svg' alt="트위터"/></a>
-        <a href="https://www.youtube.com/"><img src='../../public/img/icons_youtube-fill.svg' alt="유튜브"/></a>
-        <a href="https://www.instagram.com/"><img src='../../public/img/icons_instagram-filled.svg' alt="인스타그램"/></a>
+        <a href="https://www.facebook.com/?locale=ko_KR"><img src='/img/icons_facebook-fill.svg' alt="페이스북"/></a>
+        <a href="https://twitter.com/?lang=ko"><img src='/img/icons_twitter-fill.svg' alt="트위터"/></a>
+        <a href="https://www.youtube.com/"><img src='/img/icons_youtube-fill.svg' alt="유튜브"/></a>
+        <a href="https://www.instagram.com/"><img src='/img/icons_instagram-filled.svg' alt="인스타그램"/></a>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom';
-
-import App from '../App';
-import Signin from './Signin';
-
 import '../css/Header.css';
 
 function Header(){
   return(
     <div className="headerBox">
-      <App><img src='../../public/img/logo.svg' className="logo" alt="로고"/></App>
-      <Signin className="login">로그인</Signin>
+      <Link to="/"><img src='/img/logo.svg' className="logo" alt="로고"/></Link>
+      <Link to="/Signin" className="login">로그인</Link>
     </div>
   );
 }
