4830bddd724496f4dbd34a0c00f3a2b31ed94c8f
mun-jihye <m84422583@gmail.com>
2024-02-14 00:28:16 +0900

    refactor: fetch 요청 함수 에러 전파 코드 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,10 @@
 export const fetchRequest = async (url, method, data) => {
-  try {
-    const res = await fetch(`${url}`, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: data ? JSON.stringify(data) : null,
-    });
-
-    return res;
-  } catch (e) {
-    throw Error(e);
-  }
+  const res = await fetch(`${url}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: data ? JSON.stringify(data) : null,
+  });
+  return res;
 };
