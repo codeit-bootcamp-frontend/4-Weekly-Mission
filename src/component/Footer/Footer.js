58a2c5d7805b3981e90f7add8e825b1dc7e7802f
JaeBeen95 <nin2348@gmail.com>
2024-02-13 12:16:04 +0900

    sns상수화 수정 및 중복사항 map메서드로 정리

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -2,12 +2,30 @@ import "./Footer.css";
 import facebookIcon from "../../images/facebook.svg";
 import twitterIcon from "../../images/instagram.svg";
 import youtubeIcon from "../../images/youtube.svg";
-import instagramICon from "../../images/instagram.svg";
+import instagramIcon from "../../images/instagram.svg";
 
-const FACEBOOK = "https://www.facebook.com/";
-const TWITTER = "https://www.twitter.com/";
-const YOUTUBE = "https://www.youtube.com/";
-const INSTAGRAM = "https://www.instagram.com/";
+const SNS_LIST = [
+  {
+    name: "facebook",
+    logo: facebookIcon,
+    href: "https://www.facebook.com/",
+  },
+  {
+    name: "twitter",
+    logo: twitterIcon,
+    href: "https://www.twitter.com/",
+  },
+  {
+    name: "youtube",
+    logo: youtubeIcon,
+    href: "https://www.youtube.com/",
+  },
+  {
+    name: "instagram",
+    logo: instagramIcon,
+    href: "https://www.instagram.com/",
+  },
+];
 
 const Footer = () => {
   return (
@@ -18,31 +36,20 @@ const Footer = () => {
           <a href="/">Privacy Policy</a>
           <a href="/">FAQ</a>
         </div>
-        <div className="sns">
-          <a href={FACEBOOK} target="_blank" rel="noopener noreferrer">
-            <img
-              src={facebookIcon}
-              alt="facebook 홈페이지로 연결된 facebook 로고"
-            />
-          </a>
-          <a href={TWITTER} target="_blank" rel="noopener noreferrer">
-            <img
-              src={twitterIcon}
-              alt="twitter 홈페이지로 연결된 twitter 로고"
-            />
-          </a>
-          <a href={YOUTUBE} target="_blank" rel="noopener noreferrer">
-            <img
-              src={youtubeIcon}
-              alt="youtube 홈페이지로 연결된 youtube 로고"
-            />
-          </a>
-          <a href={INSTAGRAM} target="_blank" rel="noopener noreferrer">
-            <img
-              src={instagramICon}
-              alt="instagram 홈페이지로 연결된 instagram 로고"
-            />
-          </a>
+        <div className="Sns">
+          {SNS_LIST.map((data) => (
+            <a
+              key={data.name}
+              href={data.href}
+              target="_blank"
+              rel="no opener noreferrer"
+            >
+              <img
+                src={data.logo}
+                alt={`${data.name} 홈페이지로 연결된 ${data.name} 로고`}
+              />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
