171614413b2e9802556b360e4231a7f020b98d1f
Junyoung Park <9851248@gmail.com>
2024-02-13 22:53:10 +0900

    refactor: Modify Destructure assign id

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,10 +5,10 @@ const Cards = ({ links }) => {
   return (
     <S.CardsSection>
       {links.map(link => {
-        const { createdAt, url, title, description, imageSource } = link;
+        const { id, createdAt, url, title, description, imageSource } = link;
         return (
           <Card
-            key={link.id}
+            key={id}
             createdAt={createdAt}
             url={url}
             title={title}
