a25b34f039d1c46288a99de07cfa0c2bd94cd5ce
Taejin Kim <rlaxowls1316@naver.com>
2024-02-14 23:29:05 +0900

    feat: add strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
