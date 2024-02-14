287b1a101b498d70be869c02318f9a185f1078e1
thgus5335 <thgus5335@naver.com>
2024-02-15 08:16:22 +0900

    :sparkles: Add: Footer and fix config
    Add Footer and fix config

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import iconFacebook from '../assets/icons/facebook.svg';
+import iconTwitter from '../assets/icons/twitter.svg';
+import iconYoutube from '../assets/icons/youtube.svg';
+import iconInstagram from '../assets/icons/instagram.svg';
+import '../styles/Footer.css';
+
+function Footer() {
+  const facebook = 'https://www.facebook.com/?locale=ko_KR';
+  const twitter = 'https://twitter.com/?lang=ko';
+  const youtube = 'https://www.youtube.com/';
+  const instagram = 'https://www.instagram.com/';
+
+  return (
+    <footer>
+      <div className="footer-container">
+        <p className="footer-company">©codeit - 2023</p>
+        <div className="footer-privacy-faq">
+          <a className="privacy" href="/pages/privacy.html">
+            Privacy Policy
+          </a>
+          <a className="faq" href="/pages/faq.html">
+            FAQ
+          </a>
+        </div>
+        <div className="footer-icons">
+          <a href={facebook}>
+            <img className="footer-icon" src={iconFacebook} alt="페이스북으로 이동하기." />
+          </a>
+          <a href={twitter}>
+            <img className="footer-icon" src={iconTwitter} alt="트위터로 이동하기." />
+          </a>
+          <a href={youtube}>
+            <img className="footer-icon" src={iconYoutube} alt="유튜브로 이동하기." />
+          </a>
+          <a href={instagram}>
+            <img className="footer-icon" src={iconInstagram} alt="인스타그램으로 이동하기." />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -1,9 +1,11 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Folder() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
