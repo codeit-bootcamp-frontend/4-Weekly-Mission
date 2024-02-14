9dfb6abc27f1326b9784f7f690d2505b22cd1e4f
Junyoung Park <9851248@gmail.com>
2024-02-14 10:48:53 +0900

    feat: Add react-router-dom

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import * as S from './App.styles';
+import { getFolder } from '../../utils/api';
+import Navbar from '../../components/Navbar/Navbar';
+import UserProfile from '../../components/UserProfile/UserProfile';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import Cards from '../../components/Cards/Cards';
+import Footer from '../../components/Footer/Footer';
+
+const Main = () => {
+  const [folder, setFolder] = useState({});
+  const { owner, links, name } = folder;
+
+  const fetchFolder = async () => {
+    try {
+      const { folder } = await getFolder();
+      setFolder(folder);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <S.AppLayout>
+      <Navbar loginButtonClick={fetchFolder} />
+      <S.HomeSection>
+        {owner && (
+          <UserProfile
+            title={`@${owner?.name}`}
+            image={{ URL: owner?.profileImageSource, size: '9rem' }}
+            direction='column'
+          />
+        )}
+        {name && <S.HomeFolderName>{name}</S.HomeFolderName>}
+      </S.HomeSection>
+      <S.MainLayout>
+        <S.MainBox>
+          <SearchBar />
+          {links && <Cards links={links} />}
+        </S.MainBox>
+      </S.MainLayout>
+      <Footer />
+    </S.AppLayout>
+  );
+};
+
+export default Main;
---
+++ src/pages/Main/Main.styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const AppLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 1920px;
+`;
+
+export const HomeSection = styled.section`
+  padding-top: 5rem;
+`;
+
+export const HomeFolderName = styled.h2`
+  padding: 1rem 0;
+`;
+
+export const MainLayout = styled.main`
+  width: 100vw;
+  margin - top: 2rem;
+  background: #ffffff;
+`;
+
+export const MainBox = styled.div`
+  padding: 2rem 5rem;
+`;
