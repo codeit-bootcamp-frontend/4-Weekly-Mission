fcb2f7764a90bb0f70784bfd84ce1f9325d7c751
jinhok96 <jinhok96a@gmail.com>
2024-02-14 18:34:37 +0900

    refactor: 헤더에 css module, classnames 적용 	modified:   src/components/Header/Header.js 	renamed:    src/components/Header/Header.css -> src/components/Header/Header.module.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import propTypes from 'prop-types';
-import './Header.css';
+import classNames from 'classnames';
+import styles from './Header.module.css';
 import Gnb from './Gnb';
 
 function Header({ children }) {
+  const headerClasses = classNames(styles.header, 'background-bg', 'width-full');
+
   return (
-    <header className="header background-bg width-full">
+    <header className={headerClasses}>
       <Gnb />
       {children}
     </header>
