54936e210f70dd9b873b88078396d6c4bada07cd
Yeojin Yoon <roseiris1001@gmail.com>
2024-02-14 18:28:00 +0900

    2주차 생성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import '../src/App.css';
-import { useEffect, useState } from 'react';
-import { HeaderArea } from './components/HeaderArea';
-import { TopBodyArea } from './components/TopBodyArea';
-import { ContentsArea } from './components/ContentsArea';
-import { FooterArea } from './components/FooterArea';
-import { getLoginUserInfo, getFolder } from './utils/apis';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [userInfo, setUserInfo] = useState([]);
-  const [links, setlinks] = useState([]);
-  const [folder, setFolder] = useState([]);
-
-  const handleLoad = async () => {
-    const userInfo = await getLoginUserInfo();
-    setUserInfo(userInfo);
-
-    const { folder } = await getFolder();
-    setFolder(folder);
-    setlinks(folder.links);
-  };
-
-  useEffect(() => {
-    handleLoad();
-  }, []);
-
   return (
-    <div>
-      <header>
-        <HeaderArea email={userInfo.email}></HeaderArea>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <body>
-        <TopBodyArea
-          name={userInfo.name}
-          folderName={folder.name}
-        ></TopBodyArea>
-        <ContentsArea links={links}></ContentsArea>
-      </body>
-      <footer>
-        <FooterArea></FooterArea>
-      </footer>
     </div>
   );
 }
