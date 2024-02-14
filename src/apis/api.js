b0b086b68107e25c4b4bac6900cc47211ed139f3
XDS\u1014827 <rj2303@naver.com>
2024-02-14 22:24:39 +0900

    프로필 데이터 에러 핸들링 추가

--- src/apis/api.js
+++ src/apis/api.js
@@ -4,7 +4,7 @@ export const getProfileData = async () => {
   );
 
   if (!response.ok) {
-    throw new Error("프로필 데이터를 불러오는데 실패했습니다.");
+    throw new Error("로그인이 실패했습니다.");
   }
 
   const data = await response.json();
@@ -14,7 +14,7 @@ export const getProfileData = async () => {
 
 export const getFolderData = async () => {
   const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folderd"
+    "https://bootcamp-api.codeit.kr/api/sample/folder"
   );
 
   if (!response.ok) {
--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -3,7 +3,7 @@ import { getProfileData } from "../../apis/api";
 import "./Profile.css";
 
 const Profile = () => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(undefined);
 
   const getData = async () => {
     try {
@@ -19,10 +19,20 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="Profile">
-      <img src={user.profileImageSource} alt="사용자 프로필 이미지" />
-      <span>{user.email}</span>
-    </div>
+    <>
+      {user ? (
+        <div className="Profile">
+          <img src={user.profileImageSource} alt="사용자 프로필 이미지" />
+          <span>{user.email}</span>
+        </div>
+      ) : (
+        <div>
+          <a className="loginBtn" href="#">
+            로그인
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
