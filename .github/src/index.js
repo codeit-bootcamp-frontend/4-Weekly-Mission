86c9a50a8dd7822fc99cc3591504c82ebae1b7b8
김보경 <rlaqhrud6263@naver.com>
2024-02-14 18:40:33 +0900

    Rebuild React App & week8 Mission Start

---
+++ .github/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { Fragment } from "react";
+
+const Linkbrary = ReactDOM.createRoot(document.getElementById("Linkbrary"));
+Linkbrary.render(
+  <Fragment>
+    <App />
+  </Fragment>
+);
