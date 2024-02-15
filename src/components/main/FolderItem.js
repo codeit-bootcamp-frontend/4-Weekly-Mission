65fcca05c7542d010ac515355598e694b0cc4452
hongjaehoun <ehswkfn123@naver.com>
2024-02-15 22:22:46 +0900

    formatTimeAgo 함수 파일 분리, folderItem 컴포넌트 수정

--- src/components/main/FolderItem.js
+++ src/components/main/FolderItem.js
@@ -1,25 +1,6 @@
-import { useEffect, useState } from "react";
 import styles from "./FolderItem.module.css";
+import * as functions from "../../functions/formatTimeAgo.js";
 function FolderItem({ folderUrl, folderTitle, folderDate, folderImageSource }) {
-  const now = new Date().getTime();
-  const creadtedDate = new Date(`${folderDate}`).getTime();
-  const minutes = (now - creadtedDate) / (1000 * 60);
-  const hours = (now - creadtedDate) / (1000 * 60 * 60);
-  const days = (now - creadtedDate) / (1000 * 60 * 60 * 24);
-  const months = (now - creadtedDate) / (1000 * 60 * 60 * 24 * 30);
-  const years = (now - creadtedDate) / (1000 * 60 * 60 * 24 * 30 * 12);
-  const [creadtedAt, setCreadtedAt] = useState("");
-  useEffect(() => {
-    if (Math.floor(months) == 1) {
-      setCreadtedAt("1 month ago");
-    } else if (Math.floor(months) > 1 && Math.floor(months) <= 11) {
-      setCreadtedAt(`${Math.floor(months)} months ago`);
-    } else if (Math.floor(months) == 12) {
-      setCreadtedAt("1 year ago");
-    } else {
-      setCreadtedAt(`${Math.floor(Math.floor(months) / 12)} years ago`);
-    }
-  }, []);
   return (
     <a href={folderUrl} className={styles.folder_link}>
       <div className={styles.folder_item_box}>
@@ -27,7 +8,9 @@ function FolderItem({ folderUrl, folderTitle, folderDate, folderImageSource }) {
           <img src={folderImageSource} className={styles.folder_image} />
         </div>
         <div className={styles.folder_detail_box}>
-          <div className={styles.folder_createdAt}>{creadtedAt}</div>
+          <div className={styles.folder_createdAt}>
+            {functions.formatTimeAgo(folderDate)}
+          </div>
           <div className={styles.folder_title}>{folderTitle}</div>
           <div className={styles.folder_date}>{folderDate.slice(0, 10)}</div>
         </div>
