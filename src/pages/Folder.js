06f93eb5f5fa49c91662ce0dc89b781ccef9db1b
thgus5335 <thgus5335@naver.com>
2024-02-15 09:11:48 +0900

    :sparkles: Add: folder information
    Add folder information(profile)

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -1,10 +1,19 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import FolderInfo from '../components/FolderInfo';
 
 function Folder() {
   return (
     <div>
       <Header />
+
+      <FolderInfo />
+
+      {/* <FolderContent>
+        <SearchBar />
+        <CardList />
+      </FolderContent> */}
+
       <Footer />
     </div>
   );
