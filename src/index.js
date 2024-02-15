7da4b44d0ba8d1deecc9c224b8426374a0aabcd0
Rhajiit <ksd9638@gmail.com>
2024-02-15 10:20:47 +0900

    Rename: Folder.js를 컴포넌트 폴더로 이동

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Folder from "Folder";
+import Folder from "components/Folder";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
