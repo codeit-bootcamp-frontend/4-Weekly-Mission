681239441540cd4528e8a037f40ad9b384a9a7c4
JA_X <envijs93@gmail.com>
2024-02-16 01:59:22 +0900

    Refator: Card 컴포넌트 rel 속성 추가 및 alt 값 수정

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -4,8 +4,8 @@ import { getFormattedDate, getTimeAgo } from '../../utils/date.js';
 export default function Card({ data }) {
   const defaultImageSource = 'https://placehold.co/600x400';
   return (
-    <a href={data.url} target="_blank" className="cardBox">
-      <img src={data.imageSource || defaultImageSource} alt="카드 이미지" />
+    <a href={data.url} target="_blank" rel="noopener noreferrer" className="cardBox">
+      <img src={data.imageSource || defaultImageSource} alt="카드썸네일" />
       <div className="cardDataArea">
         <div className="cardTimeAgo">{getTimeAgo(data.createdAt)}</div>
         <div className="cardDesc">{data.description}</div>
