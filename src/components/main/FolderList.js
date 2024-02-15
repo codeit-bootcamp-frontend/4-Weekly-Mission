f6ffcbadba966eddb3a33b4e2fa81dada5ea6014
hongjaehoun <ehswkfn123@naver.com>
2024-02-15 21:57:24 +0900

    FolderList 컴포넌트에 useFetch 훅 적용

--- src/components/main/FolderList.js
+++ src/components/main/FolderList.js
@@ -1,26 +1,18 @@
-import { useEffect, useState } from "react";
 import FolderItem from "./FolderItem";
 import styles from "./FolderList.module.css";
-function FolderList() {
-  const [folderList, setFolderList] = useState({});
+import { useFetch } from "../../hooks/useFetch";
 
-  useEffect(() => {
-    const fetchFolderData = async () => {
-      const res = await fetch(
-        "https://bootcamp-api.codeit.kr/api/sample/folder"
-      );
-      const result = res.json();
-      return result;
-    };
-    fetchFolderData().then((res) => setFolderList(res));
-  }, []);
+const folderUrl = "https://bootcamp-api.codeit.kr/api/sample/folder";
+
+function FolderList() {
+  const { data: folderData } = useFetch(folderUrl);
+
   return (
     <div className={styles.folder_list_wrapper}>
-      {folderList.folder?.links.map((i) => {
+      {folderData.folder?.links.map((i) => {
         return (
           <a href={i.url} className={styles.folder_link} key={i.id}>
             <FolderItem
-              key={i.id}
               folderTitle={i.title}
               folderImageSource={i.imageSource}
               folderDate={i.createdAt}
