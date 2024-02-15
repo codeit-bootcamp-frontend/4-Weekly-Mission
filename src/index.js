04620719cd01d777c87a1def7b5ad338992b0301
Taeuk471 <asky990223@gmail.com>
2024-02-15 22:20:47 +0900

    :tada: Week7 start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Page from './page';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Page />
+);
---
+++ src/page.js
@@ -0,0 +1,13 @@
+import './page.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
