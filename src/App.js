35741b9efaf6474336499741647eef557b19ba6c
yeona <aya813@naver.com>
2024-02-16 01:01:53 +0900

    refactor : SharedPage.js 내부에서 직접 useGetFolder.js 호출 구현 및 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import "./colors.css";
 import "./global.css";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import Header from "./components/Header/Header";
 import SharedPage from "./components/SharedPage/SharedPage";
-import SearchBar from "./components/SearchBar/SearchBar";
-import CardList from "./components/CardList/CardList";
-import ReadCard from "./components/ReadCard";
-import useGetFolder from "./hooks/useGetFolder";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const { data } = useGetFolder();
-  const { profileImage, ownerName, folderName, links } = data || {};
-
   return (
     <div className="App">
       <NavigationBar />
-      <SharedPage
-        folderInfo={
-          <Header
-            profileImage={profileImage}
-            ownerName={ownerName}
-            folderInfo={folderName}
-          />
-        }
-        searchBar={<SearchBar />}
-        cardList={
-          <CardList>
-            {links?.map((link) => (
-              <ReadCard key={link?.id} {...link} />
-            ))}
-          </CardList>
-        }
-      />
+      <SharedPage />
       <Footer />
     </div>
   );
--- src/components/SharedPage/SharedPage.js
+++ src/components/SharedPage/SharedPage.js
@@ -1,12 +1,28 @@
+import useGetFolder from "../../hooks/useGetFolder";
+import CardList from "../CardList/CardList";
+import Header from "../Header/Header";
+import ReadCard from "../ReadCard";
+import SearchBar from "../SearchBar/SearchBar";
 import "./SharedPage.css";
 
-function SharedPage({ folderInfo, searchBar, cardList }) {
+function SharedPage() {
+  const { data } = useGetFolder();
+  const { profileImage, ownerName, folderName, links } = data || {};
+
   return (
     <div className="SharedPage">
-      {folderInfo}
+      <Header
+        profileImage={profileImage}
+        ownerName={ownerName}
+        folderInfo={folderName}
+      />
       <div className="SharedPage-items">
-        {searchBar}
-        {cardList}
+        <SearchBar />
+        <CardList>
+          {links?.map((link) => (
+            <ReadCard key={link?.id} {...link} />
+          ))}
+        </CardList>
       </div>
     </div>
   );
