010fb046e5f7b82cec0c149b838f5b216dd2c003
LeeSeungHoon <ysh73900@gmail.com>
2024-02-15 20:36:31 +0900

    rename: Card로 폴더명 변경

---
+++ src/components/Card/OnlyCard/OnlyCard.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { CardContent } from "../CardContent/CardContent";
+import CardImage from "../CardImage/CardImage";
+import Card from "../CardWrapper/CardWrapper";
+
+const OnlyCard = ({ items }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseOver = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+  return (
+    <a href={items} target="_blank" rel="noopener noreferrer">
+      <Card onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+        <CardImage items={items} isZoomedIn={isHovered} />
+        <CardContent items={items} isZoomedIn={isHovered} />
+      </Card>
+    </a>
+  );
+};
+
+export default OnlyCard;
