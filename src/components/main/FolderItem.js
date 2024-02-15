3aeccd1f167d9e52c36755c1356f26bfc861d401
hongjaehoun <ehswkfn123@naver.com>
2024-02-16 01:46:57 +0900

    FolderItem 컴포넌트 이미지 hover, no_image 상태 설정 , 세부 스타일 수정

--- src/components/main/FolderItem.js
+++ src/components/main/FolderItem.js
@@ -4,9 +4,19 @@ function FolderItem({ folderUrl, folderTitle, folderDate, folderImageSource }) {
   return (
     <a href={folderUrl} className={styles.folder_link}>
       <div className={styles.folder_item_box}>
-        <div className={styles.folder_image_box}>
-          <img src={folderImageSource} className={styles.folder_image} />
-        </div>
+        {folderImageSource ? (
+          <div className={styles.folder_image_box}>
+            <img src={folderImageSource} className={styles.folder_image} />
+          </div>
+        ) : (
+          <div className={styles.folder_image_box}>
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/folder_no_image.svg`}
+              className={styles.folder_no_image}
+            />
+          </div>
+        )}
+
         <div className={styles.folder_detail_box}>
           <div className={styles.folder_createdAt}>
             {functions.formatTimeAgo(folderDate)}
