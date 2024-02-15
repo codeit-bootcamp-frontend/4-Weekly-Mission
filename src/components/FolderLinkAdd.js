9277ffc2a5f5e6bd9e2a3c7185dddb2017054d63
Rhajiit <ksd9638@gmail.com>
2024-02-15 15:21:04 +0900

    Feat: /Folder 페이지 생성

---
+++ src/components/FolderLinkAdd.js
@@ -0,0 +1,13 @@
+import FolderLinkAddBar from "components/FolderLinkAddBar";
+import FolderCardCollection from "components/FolderShareCollection";
+
+function FolderShare() {
+	return (
+		<>
+			<FolderLinkAddBar />
+			<FolderCardCollection />
+		</>
+	);
+}
+
+export default FolderShare;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import FolderShare from "components/FolderShare";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "components/App";
+import FolderLinkAdd from "components/FolderLinkAdd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<App />}>
+				<Route path="folder" element={<FolderLinkAdd />} />
 				<Route path="share" element={<FolderShare />} />
 			</Route>
 		</Routes>
