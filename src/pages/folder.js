48d0149859ea48406a7718f23195b1df61ec7944
박지윤 <tkrhkfld123@naver.com>
2024-02-15 21:02:45 +0900

    💄 Style: 페이지 경로 이동

---
+++ src/pages/folder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const folder = () => {
+  return (
+    <div>
+      <Header />
+      <p>나는 폴더얌</p>
+    </div>
+  );
+};
+
+export default folder;
