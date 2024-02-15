90e9e28edc0711e191ceab05874c45e10ffa6fe0
JA_X <envijs93@gmail.com>
2024-02-16 02:29:08 +0900

    Refactor: snsList 상수화 및 map 리스트업

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
-import instagram from '../images/instagram.svg';
-import facebook from '../images/facebook.svg';
-import twitter from '../images/twitter.svg';
-import youtube from '../images/youtube.svg';
+import SNS_DATA from '../constant/sns';
 import './Footer.css';
 
 export default function Footer() {
+  const snsDataList = Object.entries(SNS_DATA);
+
   return (
     <div className="footer">
       <div className="footerFrame">
@@ -15,18 +14,11 @@ export default function Footer() {
             <a href="/">FAQ</a>
           </div>
           <div className="sns">
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-              <img src={facebook} alt="facebook 홈페이지로 연결된 facebook 로고" />
-            </a>
-            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-              <img src={twitter} alt="twitter 홈페이지로 연결된 twitter 로고" />
-            </a>
-            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-              <img src={youtube} alt="youtube 홈페이지로 연결된 youtube 로고" />
-            </a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-              <img src={instagram} alt="instagram 홈페이지로 연결된 instagram 로고" />
-            </a>
+            {snsDataList.map(([title, data]) => (
+              <a key={data.link} href={data.link} target="_blank" rel="noopener noreferrer">
+                <img src={data.imgSource} alt={title} />
+              </a>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/constant/sns.js
@@ -0,0 +1,25 @@
+import instagramImg from '../images/instagram.svg';
+import facebookImg from '../images/facebook.svg';
+import twitter from '../images/twitter.svg';
+import youtube from '../images/youtube.svg';
+
+const SNS_DATA = {
+  facebook: {
+    link: 'https://www.facebook.com/',
+    imgSource: facebookImg,
+  },
+  twitter: {
+    link: 'https://twitter.com/',
+    imgSource: twitter,
+  },
+  youtube: {
+    link: 'https://www.youtube.com/',
+    imgSource: youtube,
+  },
+  instagram: {
+    link: 'https://www.instagram.com/',
+    imgSource: instagramImg,
+  },
+};
+
+export default SNS_DATA;
