132e3349a7ac4d8ec2bf01f85caea1924714b3bf
hongjaehoun <ehswkfn123@naver.com>
2024-02-17 03:26:53 +0900

    folder 데이터 fetch 및 필터 버튼 디자인 완성

--- src/components/main/FolderFilterBox.js
+++ src/components/main/FolderFilterBox.js
@@ -1,22 +1,18 @@
+import { useEffect, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import styles from "./FolderFilterBox.module.css";
 import FolderFilterButton from "./FolderFilterButton";
-function FolderFilterBox() {
 
-  const folderData = useFetch()
-  const filterButtonDatas = {
-    filters: [
-      { filterName: "전체", id: 0 },
-      { filterName: "⭐️ 즐겨찾기", id: 1 },
-      { filterName: "코딩 팁", id: 2 },
-      { filterName: "채용 사이트", id: 3 },
-      { filterName: "나만의 장소", id: 4 },
-    ],
-  };
+const folderUrl = "https://bootcamp-api.codeit.kr/api/users/3/folders";
+
+function FolderFilterBox() {
+  const { data: folderData } = useFetch(folderUrl);
+
   return (
     <div className={styles.link_filter_box}>
-      {filterButtonDatas.filters.map(({ filterName, id }) => {
-        return <div>1</div>;
+      <FolderFilterButton name="전체" />
+      {folderData?.data.map(({ name, id }) => {
+        return <FolderFilterButton name={name} key={id} />;
       })}
     </div>
   );
--- src/components/main/FolderFilterButton.js
+++ src/components/main/FolderFilterButton.js
@@ -1,7 +1,7 @@
 import styles from "./FolderFilterButton.module.css";
 
-function FolderFilterButton({ filterName }) {
-  return <button className={styles.link_filter_button}>{filterName}</button>;
+function FolderFilterButton({ name, id }) {
+  return <button className={styles.link_filter_button}>{name}</button>;
 }
 
 export default FolderFilterButton;
