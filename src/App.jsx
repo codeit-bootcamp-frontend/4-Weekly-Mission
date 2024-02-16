51395edf8191e8be91d370cd6212a243018f2a74
sihyonn <sihyeon982@naver.com>
2024-02-16 14:32:31 +0900

    feat: react-query 설치 및 초기세팅

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,32 @@ import { RouterProvider } from 'react-router-dom';
 import theme from 'styles/theme';
 import GlobalStyle from 'styles/globalStyle';
 import router from 'router.jsx';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1 * 60 * 1000,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      console.log(error, query);
+    },
+  }),
+});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
