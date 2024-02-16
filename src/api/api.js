8bc94c8bdd2c4d8ca0b4568f5939348197b06948
김동현 <dtc03003@naver.com>
2024-02-16 16:26:09 +0900

    feat: 환경변수

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+export async function getUserInfo() {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/user`);
+
+  if (!response.ok) {
+    throw new Error("유저정보를 불러오는데 실패했습니다.");
+  }
+
+  const body = await response.json();
+
+  return body;
+}
+
+export async function getCards() {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/folder`);
+
+  if (!response.ok) {
+    throw new Error("카드를 불러오는데 실패했습니다.");
+  }
+
+  const body = await response.json();
+
+  return body;
+}
