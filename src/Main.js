8df9aa1075f9a8693e9b02227fe4911b8badd809
yeona <aya813@naver.com>
2024-02-16 21:11:07 +0900

    feat : FolderPage.js 생성 및 React Router 페이지 설정

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SharedPage from "./pages/SharedPage";
+import FolderPage from "./pages/FolderPage";
 
 function Main() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/shared" element={<SharedPage />} />
+        <Route path="/folder" element={<FolderPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/FolderPage.js
@@ -0,0 +1,15 @@
+import "../colors.css";
+import "../global.css";
+import NavigationBar from "../components/NavigationBar/NavigationBar";
+import Footer from "../components/Footer/Footer";
+
+function FolderPage() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default FolderPage;
