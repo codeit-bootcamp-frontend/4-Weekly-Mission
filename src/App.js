538e132434e8122ead016eda95a8c272801cdbff
JA_X <envijs93@gmail.com>
2024-02-16 14:39:51 +0900

    Feat: 폴더 간 페이지 이동 라우터로 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
 import Shared from './pages/Shared.js';
+import Landing from './pages/Landing.js';
+import Folder from './pages/Folder.js';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Shared />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/shared" element={<Shared />} />
+        <Route path="/folder" element={<Folder />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Folder.js
@@ -0,0 +1,3 @@
+export default function Folder() {
+  return <div>폴더 페이지</div>;
+}
