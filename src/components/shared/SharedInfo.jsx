042f3ace1a4ed806335459f288ddfc6b46913db4
YseonMo <132892991+YseonMo@users.noreply.github.com>
2024-02-17 02:07:55 +0900

    feat: prop으로 폴더 정보를 전달받아 소유자 프로필, 이름, 폴더명 렌더링하는 기능 추가

--- src/components/shared/SharedInfo.jsx
+++ src/components/shared/SharedInfo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./SharedInfo.css";
 
-const SharedInfo = () => {
+const SharedInfo = ({ folderInfo, folderName }) => {
+  const { profileImageSource, name } = folderInfo;
+
   return (
     <div className="SharedInfo">
-      <img className="SharedInfo-owner-img" />
-      <p className="SharedInfo-owner-name"></p>
-      <h1 className="SharedInfo-folder-name"></h1>
+      <img className="SharedInfo-owner-img" src={profileImageSource} />
+      <p className="SharedInfo-owner-name">{name}</p>
+      <h1 className="SharedInfo-folder-name">{folderName}</h1>
     </div>
   );
 };
-
 export default SharedInfo;
