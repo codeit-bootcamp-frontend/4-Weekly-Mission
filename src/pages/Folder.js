680023bc9ad674f600842f82e2f25a8c514a7579
hongjaehoun <ehswkfn123@naver.com>
2024-02-16 19:20:25 +0900

    Head 컴포넌트 삭제후 Nav, Profile 컴포넌트로 분리

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -1,9 +1,5 @@
 function Folder() {
-  return (
-    <div>
-      <h1>폴더 페이지입니다</h1>
-    </div>
-  );
+  return <></>;
 }
 
 export default Folder;
--- src/pages/Shared.js
+++ src/pages/Shared.js
@@ -1,4 +1,5 @@
-import Header from "../components/header/Header";
+import Nav from "../components/header/Nav";
+import Profile from "../components/header/Profile";
 import LinkSearchInput from "../components/main/LinkSearchInput";
 import FolderList from "../components/main/FolderList";
 import Footer from "../components/footer/Footer";
@@ -6,7 +7,8 @@ import styles from "./Shared.module.css";
 function Shared() {
   return (
     <>
-      <Header />
+      <Nav />
+      <Profile />
       <div className={styles.main_wrapper}>
         <LinkSearchInput />
         <FolderList />
