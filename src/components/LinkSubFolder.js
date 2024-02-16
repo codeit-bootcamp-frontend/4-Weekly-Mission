88ff939b43c75db4d341e4031276596582ae5bee
Rhajiit <ksd9638@gmail.com>
2024-02-16 19:01:37 +0900

    Design: /folder 버튼 디자인 구현

--- src/components/LinkSubFolder.js
+++ src/components/LinkSubFolder.js
@@ -1,58 +1,94 @@
+import styled from "styled-components";
 import folders from "folders_mockData.json";
+import "components/LinkSubFolder.css";
+
+const SubFolderBtnList = styled.div`
+	display: flex;
+	gap: 8px;
+`;
+
+const SubFolderUtilList = styled.div`
+	display: flex;
+	gap: 12px;
+`;
+
+const SubFolderUtil = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 24px;
+`;
+
+const SubFolderTitle = styled.p`
+	font-family: Pretendard;
+	font-size: 24px;
+	font-style: normal;
+	font-weight: 600;
+	line-height: normal;
+	letter-spacing: -0.2px;
+`;
+
+const Button = styled.button`
+	display: flex;
+	background-color: var(--LBrary-white);
+	align-items: center;
+	text-align: center;
+	gap: 4px;
+`;
 
 function AddSubFolder() {
 	return (
-		<button>
-			폴더 추가 <img alt="AddFolderButton" />
-		</button>
+		<Button className="add-sub-folder">
+			폴더 추가 <img src={"add.svg"} alt="AddFolderButton" />
+		</Button>
 	);
 }
 
 function SubFolders({ subFolderData }) {
 	const { data } = subFolderData;
 	return (
-		<div>
+		<SubFolderBtnList>
 			{data.map((item) => (
-				<button>{item.name}</button>
+				<Button className="link-sub-folder-list">{item.name}</Button>
 			))}
-		</div>
+		</SubFolderBtnList>
 	);
 }
 
 function CurrentSubFolder(currentFolder) {
-	return <>{currentFolder.currentFolder}</>;
+	return <SubFolderTitle>{currentFolder.currentFolder}</SubFolderTitle>;
 }
 
 function HandleCurrentSubFolder() {
 	return (
-		<div>
-			<button>
-				<img alt="ShareButtonImg" />
+		<SubFolderUtilList>
+			<Button className="sub-folder-utility">
+				<img src={"share.svg"} alt="ShareButtonImg" />
 				공유
-			</button>
-			<button>
-				<img alt="RenameButtonImg" />
+			</Button>
+			<Button className="sub-folder-utility">
+				<img src={"pen.svg"} alt="RenameButtonImg" />
 				이름 변경
-			</button>
-			<button>
-				<img alt="DeleteButtonImg" />
+			</Button>
+			<Button className="sub-folder-utility">
+				<img src={"Group 36.svg"} alt="DeleteButtonImg" />
 				삭제
-			</button>
-		</div>
+			</Button>
+		</SubFolderUtilList>
 	);
 }
 
 export default function LinkSubFolder() {
 	return (
 		<div>
-			<div>
+			<SubFolderUtil>
 				<SubFolders subFolderData={folders} />
 				<AddSubFolder />
-			</div>
-			<div>
+			</SubFolderUtil>
+			<SubFolderUtil>
 				<CurrentSubFolder currentFolder={"Testing"} />
 				<HandleCurrentSubFolder />
-			</div>
+			</SubFolderUtil>
 		</div>
 	);
 }
