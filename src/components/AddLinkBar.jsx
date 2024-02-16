6f79daa0119d38cf93bf13bd92009225b8390749
choiwonseokgit <cs0328@hanmail.net>
2024-02-16 22:51:50 +0900

    feat: AddLinkBar 컴포넌트 생성

---
+++ src/components/AddLinkBar.jsx
@@ -0,0 +1,18 @@
+import styles from "./AddLinkBar.module.css";
+import link from "../images/link.svg";
+
+function AddLinkBar() {
+  return (
+    <div className={styles.area}>
+      <div className={styles.container}>
+        <div className={styles.inputContainer}>
+          <img src={link} alt="링크" />
+          <input type="text" placeholder="링크를 추가해 보세요" />
+        </div>
+        <button>추가하기</button>
+      </div>
+    </div>
+  );
+}
+
+export default AddLinkBar;
