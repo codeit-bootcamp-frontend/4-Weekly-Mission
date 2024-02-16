c3a55693f57601def86a851da1bc9dbda9903a09
seoyeon <ysynop7@namver.com>
2024-02-17 00:02:15 +0900

    fix: Catch error in api.js and Footer.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://bootcamp-api.codeit.kr/api/sample/'
 export async function getUserInfo(path) {
   const response = await fetch(BASE_URL + path);
 
-  if (!response.ok) {
+  if (!response?.ok) {
     throw new Error("유저 정보를 불러오는데 실패했습니다.")
   }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ import '../css/Footer.css';
 function Footer() {
   return (
     <footer>
-      <div class="footer-inner">
-        <div class="company-info">codeit-2023</div>
-        <div class="QnA-links">
+      <div className="footer-inner">
+        <div className="company-info">codeit-2023</div>
+        <div className="QnA-links">
           <a href="/privacy.html">Privacy Policy</a>
           <a href="/faq.html">FAQ</a>
         </div>
-        <div class="sns-links">
+        <div className="sns-links">
           <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'><img alt="facebook" src={facebook} /></a>
           <a href="https://twitter.com/" target='_blank' rel='noreferrer'><img alt="twitter" src={twitter} /></a>
           <a href="https://www.youtube.com/" target='_blank' rel='noreferrer'><img alt="youtube" src={youtube} /></a>
