c307d376470c2ba85a4b83bbe6c3db5d4bb17ae6
codingaring <sksms2104@naver.com>
2024-02-16 13:49:13 +0900

    [Feat] 8주차 위클리 미션을 위한 템플릿 코드 불러오기

--- week8/src/index.js
+++ week8/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SharedPage from "./page-layout/SharedPage/sharedPage";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SharedPage />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
