f7a7544a6b4e4f0549c53c56d9429bd031e39339
mun-jihye <m84422583@gmail.com>
2024-02-16 23:46:36 +0900

    feat: sharedpage와 folderpage, homepage 라우터 적용

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,15 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import routes from './utils/constants/routes';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
-import MainPage from './pages/MainPage';
 import MainLayout from 'layout/MainLayout';
 import FolderPage from 'pages/FolderPage';
+import HomePage from 'pages/HomePage';
+import SharedPage from 'pages/SharedPage';
 
 function AppRouter() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path={routes.home} element={<MainPage />}></Route>
+        <Route path={routes.home} element={<HomePage />}></Route>
+        <Route path={routes.shared} element={<SharedPage />}></Route>
         <Route path={routes.folder} element={<FolderPage />}></Route>
       </Route>
       <Route path={routes.signin} element={<SignInPage />}></Route>
