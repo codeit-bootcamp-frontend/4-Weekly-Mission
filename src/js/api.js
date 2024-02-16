91eb2ad34323d3b7104e72bc95d8fe8921ddd4f9
이서인 <seoin2269@naver.com>
2024-02-16 14:22:43 +0900

    style: 전역 css 수정

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,13 @@
-export async function login() {
-  const response = await fetch('https://bootcamp-api.codeit.kr/api/sample/user');
-  const body = await response.json();
-  return body;
+export async function fetchLogin() {
+  try {
+    const response = await fetch('https://bootcamp-api.codeit.kr/api/sample/user');
+    if (!response.ok) {
+      throw new Error('');
+    } else {
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    throw new Error('');
+  }
 }
