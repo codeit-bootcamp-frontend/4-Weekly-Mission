9b16a85768a11e938f5f48ffddf782556933d3a0
KiHwanChong <joelkh784@gmail.com>
2024-02-16 15:59:04 +0900

    Feat: create AddLinkBar to FolderPage

--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -1,11 +1,15 @@
 import FolderList from "../components/FolderList";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AddLinkBar from "../components/AddLinkBar";
+import SearchBar from "../components/SearchBar";
 
 const FolderPage = () => {
   return (
     <div>
       <Header />
+      <AddLinkBar />
+      <SearchBar />
       <FolderList />
       <Footer />
     </div>
