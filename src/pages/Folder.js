21a5df2c60da1b55d31b75652d4457dcbe798644
hongjaehoun <ehswkfn123@naver.com>
2024-02-16 20:56:18 +0900

    Folder 페이지 Footer 컴포넌트 추가

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -1,11 +1,13 @@
 import FolderNav from "../components/header/FolderNav";
 import AddLink from "../components/header/AddLink";
+import Footer from "../components/footer/Footer";
 function Folder() {
   const userDataUrl = "https://bootcamp-api.codeit.kr/api/users/1";
   return (
     <>
       <FolderNav userDataUrl={userDataUrl} />
       <AddLink />
+      <Footer />
     </>
   );
 }
