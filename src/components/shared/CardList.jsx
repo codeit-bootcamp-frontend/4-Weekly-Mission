08b1e77969806b14c5dcd244b1c6429244d5fc8d
YseonMo <132892991+YseonMo@users.noreply.github.com>
2024-02-17 02:04:05 +0900

    feat: prop으로 폴더 링크 정보들 전달받아 Card 렌더링하는 기능 추가

--- src/components/shared/CardList.jsx
+++ src/components/shared/CardList.jsx
@@ -1,29 +1,45 @@
 import React from "react";
+import defaultCardImg from "../../assets/image/default_card_img.jpg";
 import "./CardList.css";
 
-const Card = () => {
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}.`;
+}
+
+const Card = ({ item }) => {
+  const { url, imageSource, description, createdAt } = item;
   return (
     <div className="Card">
-      <a target="_blank">
+      <a href={url} target="_blank">
         <div className="Card-img-warpper">
-          <img className="Card-img" alt="카드 이미지" />
+          <img
+            className="Card-img"
+            src={imageSource ? imageSource : defaultCardImg}
+            alt="카드 이미지"
+          />
         </div>
         <div className="Card-content-wrapper">
-          <p></p>
-          <p className="description"></p>
-          <p></p>
+          <p>10 minutes ago</p>
+          <p className="description">{description}</p>
+          <p>{formatDate(createdAt)}</p>
         </div>
       </a>
     </div>
   );
 };
 
-const CardList = () => {
+const CardList = ({ items }) => {
   return (
     <ul className="CardList">
-      <Card />
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <Card item={item} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
-
 export default CardList;
