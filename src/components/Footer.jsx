38152ff5b336f1c509025a2a66863332f85c68cb
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-17 00:16:25 +0900

    Refactor: Footer 링크 아이템 컴포넌트로 분리

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,35 @@
 import '../styles/Footer.css';
-import facebookIcon from '../assets/facebook.svg';
-import instagramIcon from '../assets/instagram.svg';
-import twitterIcon from '../assets/twitter.svg';
-import youtubeIcon from '../assets/youtube.svg';
+import SnsItem from './SnsItem';
+
+const SNS_ITEMS = [
+  {
+    name: 'facebook',
+    address: 'https://www.facebook.com/',
+  },
+  {
+    name: 'twitter',
+    address: 'https://twitter.com/',
+  },
+  {
+    name: 'youtube',
+    address: 'https://www.youtube.com/',
+  },
+  {
+    name: 'instagram',
+    address: 'https://www.instagram.com/',
+  },
+];
+
+const LINK_ITEMS = [
+  {
+    name: 'Privacy Policy',
+    address: '/privacy',
+  },
+  {
+    name: 'FAQ',
+    address: '/faq',
+  },
+];
 
 function Footer() {
   return (
@@ -10,38 +37,14 @@ function Footer() {
       <div className="footer-group">
         <span className="copyright">©codeit - 2023</span>
         <ul className="link-list">
-          <li>
-            <a href="/privacy" className="link-item">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="/faq" className="link-item">
-              FAQ
-            </a>
-          </li>
+          {LINK_ITEMS.map(item => (
+            <SnsItem key={item.name} name={item.name} address={item.address} />
+          ))}
         </ul>
         <ul className="sns-list">
-          <li>
-            <a href="https://www.facebook.com/" target="_blank" className="sns-item" rel="noreferrer">
-              <img src={facebookIcon} alt="페이스북으로 이동" />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/" target="_blank" className="sns-item" rel="noreferrer">
-              <img src={twitterIcon} alt="트위터로 이동" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/" target="_blank" className="sns-item" rel="noreferrer">
-              <img src={youtubeIcon} alt="유튜브로 이동" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/" target="_blank" className="sns-item" rel="noreferrer">
-              <img src={instagramIcon} alt="인스타그램으로 이동" />
-            </a>
-          </li>
+          {SNS_ITEMS.map(item => (
+            <SnsItem key={item.name} name={item.name} address={item.address} imgSrc={item.imgSrc} />
+          ))}
         </ul>
       </div>
     </footer>
