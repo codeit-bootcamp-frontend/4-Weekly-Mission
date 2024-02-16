03c0add72380413c2686f26b446a492a49ea6660
eugene9851 <eugene9851@naver.com>
2024-02-16 11:21:55 +0900

    프로필영역데이터 getUserData수정

--- weeklymission2/src/APIs/PageApi.js
+++ weeklymission2/src/APIs/PageApi.js
@@ -1,31 +1,36 @@
-const BASE_API = 'https://bootcamp-api.codeit.kr/api';
+const BASE_API = "https://bootcamp-api.codeit.kr/api";
 
-export const getUserData = async() => {
-  const userData = {email:null, image:null}
-  try{
-    const response = await fetch(`${BASE_API}/sample/user`)
-    const result = await response.json()
-    userData.email = result.email
-    userData.image = result.profileImageSource
-    return userData
-  } catch(error) {
-    console.log(error)
-    return userData = null
+export const getUserData = async () => {
+  const userData = { email: null, image: null };
+  try {
+    const response = await fetch(`${BASE_API}/users/1`);
+    const result = await response.json();
+    userData.email = result.data[0].email;
+    userData.image = result.data[0].image_source;
+    return userData;
+  } catch (error) {
+    console.log(error);
+    return (userData = null);
   }
-}
+};
 
-export const getFolderData = async() => {
-  const folderData = {profileImage:null, profileName: null, folderName: null, cardLinks: null}
-  try{
-    const response = await fetch(`${BASE_API}/sample/folder`)
-    const result = await response.json()
-    folderData.profileImage = result.folder.owner.profileImageSource
-    folderData.profileName = result.folder.owner.name
-    folderData.folderName = result.folder.name
-    folderData.cardLinks = result.folder.links
-    return folderData
-  } catch(error) {
-    console.log(error)
-    return folderData = null
+export const getFolderData = async () => {
+  const folderData = {
+    profileImage: null,
+    profileName: null,
+    folderName: null,
+    cardLinks: null,
+  };
+  try {
+    const response = await fetch(`${BASE_API}/sample/folder`);
+    const result = await response.json();
+    folderData.profileImage = result.folder.owner.profileImageSource;
+    folderData.profileName = result.folder.owner.name;
+    folderData.folderName = result.folder.name;
+    folderData.cardLinks = result.folder.links;
+    return folderData;
+  } catch (error) {
+    console.log(error);
+    return (folderData = null);
   }
-}
+};
--- weeklymission2/src/Pages/Page.js
+++ weeklymission2/src/Pages/Page.js
@@ -1,32 +1,30 @@
-import { useState, useEffect } from 'react';
-import { Header } from '../Components/Header';
-import { Footer } from '../Components/Footer';
-import { Main } from '../Components/Main';
-import { getUserData } from '../APIs/PageApi';
-import '../styles/Page.css';
-import { useFolder } from './useFolder';
-
-
+import { useState, useEffect } from "react";
+import { Header } from "../Components/Header";
+import { Footer } from "../Components/Footer";
+import { Main } from "../Components/Main";
+import { getUserData } from "../APIs/PageApi";
+import "../styles/Page.css";
+import { useFolder } from "../useHooks/useFolder";
 
 export function Page() {
-  const [profile, setProfile] = useState({email:null, image:null})
+  const [profile, setProfile] = useState({ email: null, image: null });
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
-  const getData = async() => {
-    const userData = await getUserData()
-    setProfile(userData)
-  }
+  const getData = async () => {
+    const userData = await getUserData();
+    setProfile(userData);
+  };
 
-  const { folder, cardLinks } = useFolder()
+  const { folder, cardLinks } = useFolder();
 
   return (
     <>
       <Header profile={profile} folder={folder} />
-      <Main cards={cardLinks}/>
+      <Main cards={cardLinks} />
       <Footer />
     </>
-  )
+  );
 }
