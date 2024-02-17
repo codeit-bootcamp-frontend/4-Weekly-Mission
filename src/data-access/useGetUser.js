ef413ca93faf8fa9c3720d465e2adaacecc985a8
Harry <yjkang1223@naver.com>
2024-02-17 15:44:02 +0900

    feature: Get user information from users/1

--- src/data-access/useGetUser.js
+++ src/data-access/useGetUser.js
@@ -1,8 +1,18 @@
 import { useAsync } from "../util/useAsync";
 import { axiosInstance } from "util/axiosInstance";
+import { DEFAULT_PROFILE } from "util/constant";
 
 export const useGetUser = () => {
-  const getUser = () => axiosInstance.get("sample/user");
+  const getUser = () => axiosInstance.get("users/1");
   const { loading, error, data } = useAsync(getUser);
-  return { loading, error, data };
+  const userData = data ? data?.data[0] : null;
+  const formattedData = userData
+    ? {
+        id: userData.id,
+        name: userData.name,
+        email: userData.email,
+        profileImageSource: userData.image_source || DEFAULT_PROFILE,
+      }
+    : null;
+  return { loading, error, data: formattedData };
 };
--- src/util/constant.js
+++ src/util/constant.js
@@ -20,3 +20,5 @@ export const TIME_IN_MILLISECONDS = {
   month,
   year,
 };
+
+export const DEFAULT_PROFILE = "/images/profile-default.svg";
