01bce9118904a463d30819aecb7cb975566fe87c
seolsis <seoljw1005@naver.com>
2024-02-17 14:36:28 +0900

    FIX: Footer.js 파일 분리

--- src/js/Favorites.js
+++ src/js/Favorites.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "../css/Favorites.css";
 import Nav from "./Nav";
+import Footer from "./Footer";
 
 function Favorites() {
-  const [folder, setFolder] = useState(null);
+  const [folder, setFolder] = useState();
 
   useEffect(() => {
     const fetchFolderData = async () => {
@@ -106,61 +107,7 @@ function Favorites() {
           ))}
         </div>
       </div>
-      <footer>
-        <div class="footer-box">
-          <span class="copyright">©codeit - 2023</span>
-          <div class="footer-links">
-            <a class="footer-link" href="privacy.html">
-              Privacy Policy
-            </a>
-            <a class="footer-link" href="faq.html">
-              FAQ
-            </a>
-          </div>
-          <div class="sns">
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="./images/facebook.svg"
-                alt="facebook 홈페이지로 연결된 facebook 로고"
-              />
-            </a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="./images/twitter.svg"
-                alt="twitter 홈페이지로 연결된 twitter 로고"
-              />
-            </a>
-            <a
-              href="https://www.youtube.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="./images/youtube.svg"
-                alt="youtube 홈페이지로 연결된 youtube 로고"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="./images/instagram.svg"
-                alt="instagram 홈페이지로 연결된 instagram 로고"
-              />
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
