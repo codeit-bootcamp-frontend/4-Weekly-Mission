effe656611635df58b6caef2f7c4bcaac60b1528
Harry <yjkang1223@naver.com>
2024-02-18 00:11:04 +0900

    style: Add starred icon & kebab menu button

--- src/page-layout/FolderPage/FolderPage.jsx
+++ src/page-layout/FolderPage/FolderPage.jsx
@@ -1,15 +1,27 @@
+import { AddLink } from "ui/AddLink";
 import { SearchBar } from "ui/SearchBar";
 import { CardList } from "ui/CardList";
-import { AddLink } from "ui/AddLink";
+import { EditableCard } from "ui/EditableCard";
+import { useGetFolder } from "data-access/useGetFolder";
+import { NoLink } from "ui/NoLink/NoLink";
 import "./FolderPage.css";
 
 export const FolderPage = () => {
+  const { data } = useGetFolder();
+  const { links } = data || {};
+
   return (
     <div className="FolderPage">
       <AddLink />
       <div className="FolderPage-items">
         <SearchBar />
-        <CardList></CardList>
+        <CardList>
+          {data ? (
+            links?.map((link) => <EditableCard key={link?.id} {...link} />)
+          ) : (
+            <NoLink>저장된 링크가 없습니다</NoLink>
+          )}
+        </CardList>
       </div>
     </div>
   );
