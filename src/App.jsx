00eef7cc60c8801d932321c2b2280165b411eb56
Jin-Chanyong <cksdyd324@gmail.com>
2024-02-17 16:41:37 +0900

    refactor: 컴포넌트 폴더에 index.js 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import { UserProvider } from "./context/UserContext";
 
 function App() {
