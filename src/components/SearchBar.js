533a342cd6483c2900a10b6923a246daf92eb8af
박지윤 <tkrhkfld123@naver.com>
2024-02-17 13:10:48 +0900

    💄 Style: 폴더 페이지 header 구현

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import searchIcon from '../images/Search.svg';
+import '../style/searchbar.css';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="search-bar-box">
+      <form className="search-bar" onSubmit={handleSearch}>
+        <img src={searchIcon} alt="Search" />
+        <input
+          type="text"
+          placeholder="링크를 검색해 보세요."
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
