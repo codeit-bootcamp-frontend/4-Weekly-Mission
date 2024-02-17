9884ee13da75b6bc4e6a710d2b58983166e36bee
yohan11 <yipco77@naver.com>
2024-02-17 20:55:59 +0900

    feat: Card 컴포넌트 내 별, 케밥 아이콘 추가

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,43 @@ import styles from "./css/Card.module.css";
 const SAMPLE_IMAGE_URL = "images/sample_image.svg";
 
 const Card = ({ cardImage = SAMPLE_IMAGE_URL, cardTime, cardDescription }) => {
+  const handleClickStar = (e) => {
+    e.preventDefault();
+    alert("clicked star");
+  };
+  const handleClickKebab = (e) => {
+    e.preventDefault();
+    alert("clicked Kebab");
+  };
+
   return (
     <div className={styles.Card}>
       <div className={styles.card_image_container}>
         <img src={cardImage} alt="card profile" className={styles.card_image} />
+        <img
+          src="/icons/star_icon.svg"
+          alt="add this link to favorite"
+          className={styles.star_icon}
+          onClick={handleClickStar}
+        />
       </div>
       <div className={styles.card_txt}>
-        <div className="font-thin font-13px" style={{ color: "#666666" }}>
-          {cardTime["timeDifference"]}
+        <div className={styles.card_option_container}>
+          <span className="font-thin font-13px" style={{ color: "#666666" }}>
+            {cardTime["timeDifference"]}
+          </span>
+          <img
+            src="/icons/kebab_icon.svg"
+            alt="view more options"
+            onClick={handleClickKebab}
+          />
         </div>
+
         <div className={`${styles.card_contents} font-thin font-16px`}>
           {cardDescription}
         </div>
-        <div className=" font-thin font-14px" style={{ color: "#333333" }}>
-          {cardTime["createdDate"]}
+        <div className="font-thin font-14px" style={{ color: "#333333" }}>
+          {cardTime["createdDateString"]}
         </div>
       </div>
     </div>
