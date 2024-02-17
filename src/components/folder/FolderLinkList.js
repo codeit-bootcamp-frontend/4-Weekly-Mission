e13a2b92c4f1ae817618bef13bb37945fc4a7ba4
Songeun <hannahkeeien@gmail.com>
2024-02-17 18:38:11 +0900

    feature: 폴더버튼 클릭시 색상 변경 기능 추가

--- src/components/folder/FolderLinkList.js
+++ src/components/folder/FolderLinkList.js
@@ -9,6 +9,8 @@ function FolderLinkList() {
   const [currentFolderName, setCurrentFolderName] = useState("전체");
   const [folderListData, setFolderListData] = useState([]);
   const [linksData, setLinksData] = useState([]);
+  //prevId값과 같은 Id를 가진 폴더버튼은 배경색이 변경된다
+  const [prevId, setPrevId] = useState(1);
 
   useEffect(() => {
     const GetMyFolderList = async () => {
@@ -28,10 +30,17 @@ function FolderLinkList() {
       console.log(err);
     }
   }, []);
-  const NamedButton = ({ children, onClick, folderData }) => {
+  const NamedButton = ({ children, onClick, folderData, prevId }) => {
     const DataNameClick = () => onClick(folderData);
+
     return (
-      <button className="folder-button" onClick={DataNameClick}>
+      <button
+        className={
+          //prevId값과 폴더 버튼의 Id값이 일치하면(해당 폴더버튼이 이전 폴더 버튼이 되었다면) 폴더버튼의 배경색을 바꾼다
+          prevId == folderData.id ? "folder-button color" : "folder-button"
+        }
+        onClick={DataNameClick}
+      >
         {children}
       </button>
     );
@@ -42,12 +51,20 @@ function FolderLinkList() {
     const { data } = result;
     setLinksData(data);
     setCurrentFolderName(folderData.name);
+    //만약 이전 버튼 상태일 때 현재 버튼이 클릭됐다면
+    if (prevId != folderData.id) {
+      //현재 누른 버튼이 이전 버튼이 되고 버튼 배경색이 바뀐다
+      setPrevId(folderData.id);
+    }
   };
   const handleAllButtonClick = async () => {
     const result = await GetAllLinks();
     const { data } = result;
     setLinksData(data);
     setCurrentFolderName("전체");
+    if (prevId != 1) {
+      setPrevId(1);
+    }
   };
   return (
     <>
@@ -56,7 +73,10 @@ function FolderLinkList() {
         <div>
           <div className="folder-buttons">
             <div>
-              <button className="all-button" onClick={handleAllButtonClick}>
+              <button
+                className={prevId == 1 ? "all-button color" : "all-button"}
+                onClick={handleAllButtonClick}
+              >
                 전체
               </button>
               {/* 컴포넌트로 따로 빼고 싶지만 setLinksData와 연결되어 있어서 못뺌 */}
@@ -66,6 +86,7 @@ function FolderLinkList() {
                     key={data.id}
                     onClick={handleDataNameClick}
                     folderData={data}
+                    prevId={prevId}
                   >
                     {data.name}
                   </NamedButton>
