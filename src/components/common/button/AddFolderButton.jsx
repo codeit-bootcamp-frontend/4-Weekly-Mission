49a67537e2ebf65b35dcf6e0cbf768b440d0a96b
sihyonn <sihyeon982@naver.com>
2024-02-17 15:36:08 +0900

    feat: AddFolderButton 컴포넌트 UI 생성

---
+++ src/components/common/button/AddFolderButton.jsx
@@ -0,0 +1,33 @@
+import addIcon from 'assets/icon/add-icon.svg';
+import { styled } from 'styled-components';
+
+const Styled = {
+  Button: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+  `,
+
+  Label: styled.div`
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.color.primary};
+  `,
+
+  Icon: styled.img`
+    width: 1.6rem;
+    height: 1.6rem;
+  `,
+};
+
+function AddFolderButton() {
+  return (
+    <Styled.Button>
+      <Styled.Label>폴더추가</Styled.Label>
+      <Styled.Icon src={addIcon} alt="폴더추가 버튼" />
+    </Styled.Button>
+  );
+}
+
+export default AddFolderButton;
