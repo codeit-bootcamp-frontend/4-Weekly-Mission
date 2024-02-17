1baf6f063ddf066f34bc6fa2194b86f3c3eeac5d
yubin <yb3143@gmail.com>
2024-02-17 17:52:26 +0900

    .

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import App from "./pages/App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Folder from "./components/folder";
+// import Folder from "./components/folder";
 import Home from "./components/Home";
 
 function Main() {
