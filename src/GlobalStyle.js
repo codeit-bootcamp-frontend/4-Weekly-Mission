5c6a85e5fe66fe0f7d0d62e209e54b2b70a652c2
Trophy198 <spsp8426@naver.com>
2024-02-17 20:04:33 +0900

    refactor : 전역 css -> 글로벌 styled-components 스타일로 변경

---
+++ src/GlobalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --primary-color: #6d6afe;
+    --red-color: #ff5b56;
+    --black-color: #111322;
+    --white-color: #ffffff;
+    --gray100-color: #3e3e43;
+    --gray60-color: #9fa6b2;
+    --Linkbrary-gray20: #ccd5e3;
+    --gray10-color: #e7effb;
+    --Linkbrary-bg: #f0f6ff;
+    --gra-purpleblue-to-skyblue: linear-gradient(91deg, #6d6afe 0.12%, #6ae3fe 101.84%);
+    --Fandom-K-gra-blue-to-pink: linear-gradient(271deg, #fe578f -9.84%, #68e8f9 107.18%);
+  }
+
+  body {
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
