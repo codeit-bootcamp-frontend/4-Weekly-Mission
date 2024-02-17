5d211b9e1351210cf33b06b47f2490438fd41843
박지윤 <tkrhkfld123@naver.com>
2024-02-17 13:11:22 +0900

    💄 Style: folder 페이지 버튼 구현

---
+++ src/components/FolderAddButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import folderAddIcon from '../images/folderAdd.svg';
+
+const FolderAddButton = () => {
+  return (
+    <div>
+      <button className="folder-add-button">
+        <p className="folder-add-text">폴더 추가</p>
+        <img className="folder-add" src={folderAddIcon} />
+      </button>
+    </div>
+  );
+};
+
+export default FolderAddButton;
