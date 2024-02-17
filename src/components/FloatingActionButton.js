8bc20d50dc5c7cd2a19cfec83e415792fecf3d71
박지윤 <tkrhkfld123@naver.com>
2024-02-17 13:11:22 +0900

    💄 Style: folder 페이지 버튼 구현

---
+++ src/components/FloatingActionButton.js
@@ -0,0 +1,13 @@
+import '../style/floatingActionButton.css';
+import addIcon from '../images/folderAdd.svg';
+
+function FloatingActionButton() {
+  return (
+    <button className="floating-action-button">
+      <p className="floating-action-button-text">폴더추가</p>
+      <img src={addIcon} />
+    </button>
+  );
+}
+
+export default FloatingActionButton;
---
+++ src/components/FolderCategoryButtons.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../style/folderContent.css';
+import FolderAddButton from './FolderAddButton';
+import FloatingActionButton from './FloatingActionButton';
+
+function FolderCategoryButton({ folder, onClick }) {
+  return (
+    <button className="folder-button" onClick={() => onClick(folder.id)}>
+      {folder.name}
+    </button>
+  );
+}
+
+function FolderCategoryButtons({ folders, handleFolderClick }) {
+  return (
+    <div>
+      <div className="folder-box">
+        <div className="folder-button-box">
+          <button
+            className="folder-button"
+            onClick={() => handleFolderClick('all')}
+          >
+            전체
+          </button>
+          {folders.map((folder) => (
+            <FolderCategoryButton
+              key={folder.id}
+              folder={folder}
+              onClick={handleFolderClick}
+            />
+          ))}
+        </div>
+        {window.innerWidth <= 768 ? (
+          <FloatingActionButton />
+        ) : (
+          <FolderAddButton />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default FolderCategoryButtons;
