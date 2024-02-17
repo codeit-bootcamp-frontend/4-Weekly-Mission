bd44315f17dd3db0f001e0151eb66aac06b079a8
seolsis <seoljw1005@naver.com>
2024-02-17 15:29:34 +0900

    FEAT: Folder.js 기본 틀 생성

--- src/js/Folder.js
+++ src/js/Folder.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import "../css/Folder.css";
 
 function Folder() {
-  <></>;
+  return (
+    <>
+      <Nav />
+      <div className="FolderInputBox">
+        <input
+          className="FolderAddInput"
+          placeholder="🔗 링크를 추가해 보세요"
+        />
+        <button className="FolderAddButton">추가하기</button>
+      </div>
+      <div className="FolderContentBox">
+        <input
+          className="FolderSearchInput"
+          placeholder="🔎 링크를 검색해 보세요."
+        />
+        <div className="FolderContents"></div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Folder;
