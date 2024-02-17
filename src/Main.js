9a8eed86f1ffa2e67db4c293aa233863d37da82d
Yeojin Yoon <roseiris1001@gmail.com>
2024-02-17 10:38:47 +0900

    테스트

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,12 @@ import HomePage from './pages/HomePage';
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="shared" element={<SharedPage />} />
-        </Routes>
-      </App>
+      {/* <App> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shared" element={<SharedPage />} />
+      </Routes>
+      {/* </App> */}
     </BrowserRouter>
   );
 }
