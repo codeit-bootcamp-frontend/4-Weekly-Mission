3cc008f45869b129a4195f7a54c7349aef55f95a
devwqc <devwqc@gmail.com>
2024-02-17 22:22:32 +0900

    refactor(mentor): api 함수에 try ~ catch 적용

--- src/api.js
+++ src/api.js
@@ -1,20 +1,30 @@
 const BASE_URL = 'https://bootcamp-api.codeit.kr/api';
 
 export async function getFolder({ limit = 9 }) {
-  const query = `limit=${Number(limit) || 1}`;
-  const response = await fetch(`${BASE_URL}/sample/folder?${query}`);
-  if (!response.ok) {
-    throw new Error('폴더를 불러오는데 실패했습니다.');
+  try {
+    const query = `limit=${Number(limit) || 1}`;
+    const response = await fetch(`${BASE_URL}/sample/folder?${query}`);
+    if (!response.ok) {
+      throw new Error('폴더를 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-  const data = await response.json();
-  return data;
 }
 
 export async function getUser() {
-  const response = await fetch(`${BASE_URL}/sample/user`);
-  if (!response.ok) {
-    throw new Error('유저를 불러오는데 실패했습니다.');
+  try {
+    const response = await fetch(`${BASE_URL}/sample/user`);
+    if (!response.ok) {
+      throw new Error('유저를 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-  const data = await response.json();
-  return data;
 }
