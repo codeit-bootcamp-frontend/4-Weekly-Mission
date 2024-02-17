40a9f3cbdde6863086b4c074a02e146d7cd62caf
JA_X <envijs93@gmail.com>
2024-02-17 14:29:59 +0900

    Feat: FloatingActionButton 구현 및 기타 페이지 반응형 구현을 위한  css 수정 완료

--- src/components/FolderDetails.js
+++ src/components/FolderDetails.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useFetchData from '../hooks/useFetchData';
+import FloatingActionButton from './common/FloatingActionButton';
 import CardList from './CardList';
 import SearchBar from './common/SearchBar';
 import add from '../images/add.svg';
@@ -9,14 +10,25 @@ import remove from '../images/remove.svg';
 import '../components/FolderDetails.css';
 
 export default function FolderDetails({ folderListData }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [selectedFolder, setSelectedFolder] = useState('all');
   const selectedFolderData = useFetchData('targetUserFolderLinkList', 1, selectedFolder) || [];
-  console.log(selectedFolder);
-  console.log(selectedFolderData);
 
   const handleFolderClick = folderId => {
     setSelectedFolder(folderId);
   };
+
+  const handleWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowWidth);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowWidth);
+    };
+  }, []);
 
   return (
     <div className="folderDetailsContainer">
@@ -37,10 +49,14 @@ export default function FolderDetails({ folderListData }) {
             </div>
           ))}
         </div>
-        <div className="addButton">
-          <p>폴더 추가</p>
-          <img src={add} alt="더하기" />
-        </div>
+        {windowWidth >= 767 ? (
+          <div className="addButton">
+            <p>폴더 추가</p>
+            <img src={add} alt="더하기" />
+          </div>
+        ) : (
+          <FloatingActionButton />
+        )}
       </div>
       <div className="actionButtonContainer">
         <p>{selectedFolder === 'all' ? '전체' : '유용한 글'}</p>
