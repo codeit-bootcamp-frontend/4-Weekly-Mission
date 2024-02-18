175f5ad5568e3f0ab763c86f4e61daef2831c4df
codingaring <sksms2104@naver.com>
2024-02-18 21:38:56 +0900

    [feat] folder 카드목록 상단바 컴포넌트 레이아웃 추가

--- week8/src/ui/Classification/Classification.jsx
+++ week8/src/ui/Classification/Classification.jsx
@@ -2,6 +2,7 @@ import { getCategory } from "data-access/getCategory";
 import "./Classification.css";
 import { Button } from "StyledComponents/Buttons";
 import { useEffect, useState } from "react";
+import { CategoryNav } from "ui/CategoryNav/CategoryNav";
 
 export function Classification() {
   const [data, setData] = useState();
@@ -15,15 +16,30 @@ export function Classification() {
     handleLoadCategory();
   }, []);
 
+  const handleCategoryActive = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
-    <div className="classification">
-      <div className="classification-buttons">
-        <Button key="all">전체</Button>
-        {data?.map((category) => (
-          <Button key={category?.id}>{category.name}</Button>
-        ))}
+    <>
+      <div className="classification">
+        <div className="classification-buttons">
+          <Button key="all" value="all">
+            전체
+          </Button>
+          {data?.map((category) => (
+            <Button
+              onClick={handleCategoryActive}
+              key={category?.id}
+              value={category.name}
+            >
+              {category.name}
+            </Button>
+          ))}
+        </div>
+        <button className="add-folder-button">폴더 추가 +</button>
       </div>
-      <button className="add-folder-button">폴더 추가 +</button>
-    </div>
+      <CategoryNav />
+    </>
   );
 }
