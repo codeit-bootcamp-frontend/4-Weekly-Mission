f33afe55864fc8b5966202a052e4e4741ac69ff5
whai2 <whai2125@gmail.com>
2024-02-19 00:56:44 +0900

    refactor(mentor): 편집 아이콘 상수명 수정

--- src/components/FolderEdit.jsx
+++ src/components/FolderEdit.jsx
@@ -2,7 +2,7 @@ import shareImg from '../assets/folder/share.svg';
 import renameImg from '../assets/folder/pen.svg';
 import deleteImg from '../assets/folder/delete.svg';
 
-const FOLDER_EDITS = Object.freeze([
+const FOLDER_EDIT_ICONS = Object.freeze([
   { src: shareImg, name: "공유" },
   { src: renameImg, name: "이름변경" },
   { src: deleteImg, name: "삭제" },
@@ -17,7 +17,7 @@ const FolderEdit = ({folderName}) => {
 
   return (
     <div className='folder-edits'>
-      {FOLDER_EDITS.map(({ src, name }) => (
+      {FOLDER_EDIT_ICONS.map(({ src, name }) => (
         <div className='folder-edit' key={name}>
           <img src={src} alt={name} />
           <span className='edit-text'>{name}</span>
--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,4 +1,4 @@
-const TOTAL_LIST = '전체';
+const TOTAL_LIST_NAME = '전체';
 
 const FolderList = ({folderList, totalHandler, listHandler}) => {
   if (!folderList.length) {
@@ -8,7 +8,7 @@ const FolderList = ({folderList, totalHandler, listHandler}) => {
   return (
     <div className='container-folder-list'>
       <div className='folder-list'>
-        <button className='folder-list_btn' onClick={totalHandler}>{TOTAL_LIST}</button>
+        <button className='folder-list_btn' onClick={totalHandler}>{TOTAL_LIST_NAME}</button>
         {folderList.map(({ id, name }) => (
           <button key={id} className='folder-list_btn' onClick={() => listHandler(name, id)}>
             {name}
