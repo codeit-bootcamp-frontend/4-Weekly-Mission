b6cc35190929281d71a8ee8fd95bee7cc330bbd6
LeeSeungHoon <ysh73900@gmail.com>
2024-02-19 00:30:09 +0900

    fix: User 데이터 관리 삭제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Fragment, useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { getUser, getFolder } from "./api/api";
+import { getFolder } from "./api/api";
 import "./global.css";
-import Layout from "./components/Layout/Layout";
 import FolderInfo from "./components/FolderInfo/FolderInfo";
 import SharePage from "./pages/SharePage/SharePage";
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -12,61 +11,58 @@ import FolderPage from "./pages/FolderPage/FolderPage";
 import FolderHeader from "./components/FolderHeader/FolderHeader";
 
 function App() {
-  const [userProfile, setUserProfile] = useState();
   const [folderData, setFolderData] = useState();
   const [items, setitems] = useState([]);
 
   const handleLoad = async () => {
-    const userData = await getUser();
     const { folder } = await getFolder();
-    setUserProfile(userData);
     setFolderData(folder);
     setitems(folder.links);
   };
+
   useEffect(() => {
     handleLoad();
   }, []);
+
   return (
     <Fragment>
-      <Layout userProfile={userProfile}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" />
-            <Route
-              path="/shared"
-              element={
-                <SharePage
-                  folderInfo={<FolderInfo profile={folderData} />}
-                  searchBar={<SearchBar />}
-                  cardList={
-                    <CardList>
-                      {items?.map((item) => (
-                        <OnlyCard key={item.id} items={item} {...item} />
-                      ))}
-                    </CardList>
-                  }
-                />
-              }
-            />
-            <Route
-              path="/folder"
-              element={
-                <FolderPage
-                  folderHeader={<FolderHeader />}
-                  searchBar={<SearchBar />}
-                  cardList={
-                    <CardList>
-                      {items?.map((item) => (
-                        <OnlyCard key={item.id} items={item} {...item} />
-                      ))}
-                    </CardList>
-                  }
-                />
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route
+            path="/shared"
+            element={
+              <SharePage
+                folderInfo={<FolderInfo profile={folderData} />}
+                searchBar={<SearchBar />}
+                cardList={
+                  <CardList>
+                    {items?.map((item) => (
+                      <OnlyCard key={item.id} items={item} {...item} />
+                    ))}
+                  </CardList>
+                }
+              />
+            }
+          />
+          <Route
+            path="/folder"
+            element={
+              <FolderPage
+                folderHeader={<FolderHeader />}
+                searchBar={<SearchBar />}
+                cardList={
+                  <CardList>
+                    {items?.map((item) => (
+                      <OnlyCard key={item.id} items={item} {...item} />
+                    ))}
+                  </CardList>
+                }
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </Fragment>
   );
 }
