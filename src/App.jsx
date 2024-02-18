dfb9273fff437c29cd3800df528ebf89442efe62
whai2 <whai2125@gmail.com>
2024-02-19 00:15:55 +0900

    refactor: 앱 css 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from'react-router-dom';
 
 import SharedPage from './pages/SharedPage';
 import FolderPage from './pages/FolderPage';
-import './App.css';
 
 const App = () => {
 
