4099cc052c553136143850180d648d4d26d46b4d
이서인 <seoin2269@naver.com>
2024-02-18 18:11:21 +0900

    feat: 프로필 데이터 불러오기

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './images/logo.png';
 
 import '../../App.css';
@@ -11,9 +13,37 @@ export default function Header() {
           <img className="logo" src={logo} alt="Linkbrary로고" />
         </a>
       </div>
-      <div className="headerButton">
-        <button>로그인</button>
-      </div>
+      <Profile />
     </header>
+  );
+}
+
+function Profile({ owner }) {
+  const [profileData, setProfileData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://bootcamp-api.codeit.kr/api/sample/user');
+        setProfileData(response.data.user.owner);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      {profileData === null ? (
+        <div className="headerButton">
+          <button>로그인</button>
+        </div>
+      ) : (
+        <div id="profileArea">
+          <img id="profileImage" src={owner.profileImageSource} alt="프로필 이미지" />
+          <p id="profileName">{owner.email}</p>
+        </div>
+      )}
+    </>
   );
 }
