1a46eb70c3c2c51429c91014c184be0fee9e0c23
devwqc <devwqc@gmail.com>
2024-02-18 15:19:51 +0900

    feat: link svg 파일 IconLink로 컴포넌트화

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,6 +1,7 @@
 export { default as IconFacebook } from './IconFacebook';
 export { default as IconInstagram } from './IconInstagram';
 export { default as IconKebabMenu } from './IconKebabMenu';
+export { default as IconLink } from './IconLink';
 export { default as IconLogo } from './IconLogo';
 export { default as IconSearch } from './IconSearch';
 export { default as IconStar } from './IconStar';
