ba5898f9ae059cc4f6b8402f8f019ae2282bba17
LeeSeungHoon <ysh73900@gmail.com>
2024-02-19 06:21:51 +0900

    feat: 폴더 목록 선택 기능 추가

--- src/components/Folder/FolderList/FolderList.js
+++ src/components/Folder/FolderList/FolderList.js
@@ -1,11 +1,27 @@
-const FolderList = ({ folderList }) => {
-  console.log(folderList);
+import "./FolderList.css";
+
+const FolderList = ({ folderList, onSelectFolderList }) => {
+  const handleClickFolderList = (e) => {
+    onSelectFolderList({ id: e.target.id });
+  };
   return (
     <div className="folderList">
+      <span
+        className="folderList-button"
+        onClick={handleClickFolderList}
+        id={null}
+      >
+        전체
+      </span>
       {folderList?.map((item) => (
-        <button className="folderList-button" key={item.id}>
+        <span
+          className="folderList-button"
+          key={item.id}
+          id={item.id}
+          onClick={handleClickFolderList}
+        >
           {item.name}
-        </button>
+        </span>
       ))}
     </div>
   );
