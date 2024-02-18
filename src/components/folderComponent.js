448661a904b6f05f601186c1ab0ad42241b30a9b
gun7741 <gun7741@naver.com>
2024-02-18 22:02:07 +0900

    fix : folderComponent 오류

--- src/components/folderComponent.js
+++ src/components/folderComponent.js
@@ -1,32 +1,17 @@
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
-import "../style/foldercomponent.css";
-import fetchData from "../api/FetchData";
-function Folder() {
-=======
 import "../style/folderComponent.css";
+import fetchData from "../api/FetchData";
 function FolderComponent() {
->>>>>>> d1912a80bace889db0573732226821ff3e7f4a22
   const [folderData, setFolderData] = useState({
     ownerName: null,
     folderName: null,
     profileImage: null,
   });
   useEffect(() => {
-<<<<<<< HEAD
     const fetchFolderData = async () => {
       try {
         const data = await fetchData("sample/folder");
         if (data) {
-=======
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://bootcamp-api.codeit.kr/api/sample/folder",
-        );
-        const data = await response.json();
-        if (response.ok) {
->>>>>>> d1912a80bace889db0573732226821ff3e7f4a22
           setFolderData({
             ownerName: data.folder.owner.name,
             folderName: data.folder.name,
@@ -35,17 +20,10 @@ function FolderComponent() {
         }
       } catch (e) {
         console.error(e);
-<<<<<<< HEAD
         alert("error", e);
       }
     };
-
     fetchFolderData();
-=======
-      }
-    };
-    fetchData();
->>>>>>> d1912a80bace889db0573732226821ff3e7f4a22
   }, []);
 
   return (
@@ -60,8 +38,4 @@ function FolderComponent() {
     </div>
   );
 }
-<<<<<<< HEAD
-export default Folder;
-=======
 export default FolderComponent;
->>>>>>> d1912a80bace889db0573732226821ff3e7f4a22
