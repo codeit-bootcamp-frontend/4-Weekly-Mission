ca19f6ef49690d62dc07da89f35cfd4fec6e341f
LeeSeungHoon <ysh73900@gmail.com>
2024-02-19 01:16:26 +0900

    feat: folder페이지일때 navBar 스타일변경 및 handleLoadUser함수

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,34 @@
-import Logo from "../../assets/images/logo.png";
-
-import "./NavBar.css";
+import { useEffect, useState } from "react";
 import { Profile } from "../Profile/Profile";
+import { getUser } from "../../api/api";
+import { useNavStyleChange } from "../../hooks/hooks";
+import Logo from "../../assets/images/logo.png";
+import "./NavBar.css";
+import { useLocation } from "react-router-dom";
 
-const NavBar = ({ userProfile }) => {
+const NavBar = () => {
+  const [userData, setUserData] = useState();
+  const location = useLocation();
+
+  const isFolderPage = location.pathname === "/folder";
+  const className = isFolderPage ? "folderPageNav" : "nav";
+
+  const handleLoadUser = async () => {
+    const data = await getUser();
+    setUserData(data);
+  };
+
+  useEffect(() => {
+    handleLoadUser();
+  }, []);
   return (
-    <nav>
-      <div className="nav">
+    <nav className={className}>
+      <div className="nav-content">
         <a href="/">
           <img src={Logo} alt="Linkbrary 로고" />
         </a>
-        {userProfile ? (
-          <Profile userProfile={userProfile} />
+        {userData ? (
+          <Profile userData={userData} />
         ) : (
           <a className="login-btn" href="./html/signIn.html">
             로그인
