2f23f633ac58221dd4f3ff033c56170e0c3e4ad5
LeeSeungHoon <ysh73900@gmail.com>
2024-02-19 04:50:34 +0900

    feat: handleLoadFolderList 함수

--- src/pages/FolderPage/FolderPage.js
+++ src/pages/FolderPage/FolderPage.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import FolderList from "../../components/Folder/FolderList/FolderList";
 import Layout from "../../components/Layout/Layout";
+import { getFolderList } from "../../api/api";
 import "./FolderPage.css";
 
 const FolderPage = ({ folderHeader, searchBar }) => {
+  const [folderList, setFolderList] = useState([]);
+  const handleLoadFolderList = async () => {
+    const folderListdata = await getFolderList({ userId: 1 });
+    setFolderList(folderListdata.data);
+  };
+
+  useEffect(() => {
+    handleLoadFolderList();
+  }, []);
+
   return (
     <Layout>
       <div className="folderPage">
@@ -9,6 +22,9 @@ const FolderPage = ({ folderHeader, searchBar }) => {
         <div className="folderPage-content">
           {searchBar}
           <div className="folderPage-noLink">저장된 링크가 없습니다</div>
+          <div className="folderPage-Link">
+            <FolderList folderList={folderList} />
+          </div>
         </div>
       </div>
     </Layout>
