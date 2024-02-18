37c0907302c2e295d73a783d84775fc5323a138c
min3eo <kane7674@naver.comm>
2024-02-18 12:54:02 +0900

    feat: 라우터 초기 설정

--- src/App.js
+++ src/App.js
@@ -6,27 +6,42 @@ import { SearchBar } from "./ui/SearchBar";
 import { CardList } from "./ui/CardList";
 import { useGetFolder } from "data-access/useGetFolder";
 import { ReadOnlyCard } from "ui/ReadOnlyCard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const { data } = useGetFolder();
   const { profileImage, ownerName, folderName, links } = data || {};
 
   return (
-    <Layout>
-      <SharedPage
-        folderInfo={
-          <FolderInfo profileImage={profileImage} ownerName={ownerName} folderName={folderName} />
-        }
-        searchBar={<SearchBar />}
-        cardList={
-          <CardList>
-            {links?.map((link) => (
-              <ReadOnlyCard key={link?.id} {...link} />
-            ))}
-          </CardList>
-        }
-      />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route
+            path="/shared"
+            element={
+              <SharedPage
+                folderInfo={
+                  <FolderInfo
+                    profileImage={profileImage}
+                    ownerName={ownerName}
+                    folderName={folderName}
+                  />
+                }
+                searchBar={<SearchBar />}
+                cardList={
+                  <CardList>
+                    {links?.map((link) => (
+                      <ReadOnlyCard key={link?.id} {...link} />
+                    ))}
+                  </CardList>
+                }
+              />
+            }
+          />
+          <Route path="/folder" element={<SharedPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
