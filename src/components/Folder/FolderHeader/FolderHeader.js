cb1947ffdfe282b78b6840f97ed49d7489da5062
LeeSeungHoon <ysh73900@gmail.com>
2024-02-19 04:49:09 +0900

    remove: 폴더 위치 변경

---
+++ src/components/Folder/FolderHeader/FolderHeader.js
@@ -0,0 +1,19 @@
+import "./FolderHeader.css";
+
+const FolderHeader = () => {
+  return (
+    <div className="folderHeader">
+      <div className="folderHeader-link">
+        <div className="folderHeader-link-icon"></div>
+        <input
+          type="link"
+          className="folderHeader-link-input"
+          placeholder="링크를 추가해보세요."
+        />
+        <button className="folderHeader-link-button">추가하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default FolderHeader;
