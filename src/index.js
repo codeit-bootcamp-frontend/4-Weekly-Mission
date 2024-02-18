8458ce9251f8e7dccde00f0848443cb401e2a6e0
Taeuk471 <asky990223@gmail.com>
2024-02-19 03:25:27 +0900

    :art: Change name of mainpage to SharedPage.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import Page from './pages/mainLoginPage';
+import Page from './pages/SharedPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
