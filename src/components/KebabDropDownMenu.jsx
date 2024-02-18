365eb61a1a8ab63ff3c063b87cb99069bfdae095
Trophy198 <spsp8426@naver.com>
2024-02-18 20:47:52 +0900

    feat : 케밥 드롭다운 컴포넌트 추가 - 아이콘 바깥 클릭시 ref로 DOM 접근하여 닫히는 기능 추가

---
+++ src/components/KebabDropDownMenu.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import KebabIcon from './icons/KebabIcon';
+import DropdownMenuItem from './DropdownMenuItem';
+
+const DropdownMenu = styled.div`
+  position: absolute;
+  background-color: #ffffff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 5px 0;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+`;
+
+const KebabDropdownMenu = ({
+  isActive,
+  onClick,
+  linkId,
+  onDelete,
+  onAddToFolder,
+}) => {
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        onClick(linkId);
+      }
+    }
+
+    if (isActive) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isActive, onClick, linkId]);
+  return (
+    <div ref={dropdownRef}>
+      <KebabIcon
+        onClick={event => {
+          event.preventDefault();
+          event.stopPropagation();
+          onClick(linkId);
+        }}
+      />
+      {isActive && (
+        <DropdownMenu>
+          <DropdownMenuItem
+            onClick={event => {
+              event.preventDefault();
+              event.stopPropagation();
+              // onDelete(linkId) 삭제 기능 추가 예정
+            }}
+          >
+            삭제하기
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={event => {
+              event.preventDefault();
+              event.stopPropagation();
+              // onAddToFolder(linkId) 폴더 추가 기능 추가 예정
+            }}
+          >
+            폴더에 추가
+          </DropdownMenuItem>
+        </DropdownMenu>
+      )}
+    </div>
+  );
+};
+
+export default KebabDropdownMenu;
