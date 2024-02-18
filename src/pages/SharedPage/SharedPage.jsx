2ae83e1080789db071a02918ce1f4f3e2cfd097f
Trophy198 <spsp8426@naver.com>
2024-02-18 16:27:30 +0900

    refactor : 기존 HomePage 페이지에서 SharedPage 로 변경 및 스타일링 추가 - HomePage의 기능을 -> SharedPage의 기능으로 변경 - styled-components 추가

---
+++ src/pages/SharedPage/SharedPage.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { getFolderData } from '../../apis/folder';
+import Folder from '../../components/Folder/Folder';
+import CardGrid from '../../components/Card/Card';
+import CardSearchbar from '../../components/common/SearchBar/CardSearchbar';
+import FolderContainer from './sharedPageStyle';
+
+const SharedPage = () => {
+  const [folders, setFolders] = useState(null);
+  const [cards, setCards] = useState(null);
+
+  useEffect(() => {
+    getFolderData()
+      .then(data => {
+        setFolders(data.folder);
+        setCards(data.folder.links);
+      })
+      .catch(error => {
+        alert('데이터를 불러오는데 실패했습니다.');
+      });
+  }, []);
+
+  return (
+    <>
+      {folders && <Folder folder={folders} />}
+      <FolderContainer>
+        <CardSearchbar />
+        {cards && <CardGrid links={cards} />}
+      </FolderContainer>
+    </>
+  );
+};
+
+export default SharedPage;
