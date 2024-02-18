4b7d85d2f65ad77a483220364e590ac88b4a194a
김경주 <popo505@naver.com>
2024-02-19 02:06:50 +0900

    feat: shared 페이지

--- src/pages/shared.js
+++ src/pages/shared.js
@@ -1,5 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import CardList from "../components/CardList";
+import Footer from "../components/Footer";
+
 function Shared() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <CardList />
+      <Footer />
+    </div>
+  );
 }
 
 export default Shared;
