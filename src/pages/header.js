8590d3f4268671692f45f3329dc5d1943731f93c
Taeuk471 <asky990223@gmail.com>
2024-02-18 23:38:31 +0900

    :art: Connect Header.js and API

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,14 +1,14 @@
 import '../styles/header.css';
 import logo from '../assets/logo/logo.svg';
 
-const Header = () => {
+const Header = ({ userInfo }) => {
   return (
     <header>
       <nav>
         <img className='main-logo' src={logo} alt='Linkbrary logo.' />
         <div className>
-          <img className='profile-image' src={logo} alt='Profile.' />
-          <p className='profile-email'>This is email address</p>
+          <img className='profile-image' src={userInfo?.profileImageSource} alt='Profile.' />
+          <p className='profile-email'>{userInfo?.email}</p>
         </div>
       </nav>
     </header>
--- src/pages/mainLoginPage.js
+++ src/pages/mainLoginPage.js
@@ -1,12 +1,30 @@
+import { useState, useEffect } from 'react';
+import { getUser } from '../api/BaseUrl';
+
 import Header from './header';
 import Footer from './footer';
 import Info from './info';
 import Folder from './folder';
 
 const Page = () => {
+  const [user, setUser] = useState(null);
+
+  const handleLoad = async (getState, setState) => {
+    try {
+      const state = await getState();
+      setState(state);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleLoad(getUser, setUser);
+  }, []);
+
   return (
     <div>
-      <Header />
+      <Header userInfo={user} />
       <Info />
       <Folder />
       <Footer />
