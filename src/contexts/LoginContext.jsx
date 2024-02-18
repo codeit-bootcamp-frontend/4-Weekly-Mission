20d926037a499a269466998801389e24afb55bf2
devwqc <devwqc@gmail.com>
2024-02-18 15:30:44 +0900

    comment: LoginContext 존재이유 주석 추가

--- src/contexts/LoginContext.jsx
+++ src/contexts/LoginContext.jsx
@@ -2,6 +2,12 @@ import { createContext, useContext, useState } from 'react';
 
 const LoginContext = createContext();
 
+// 아직 Provider의 역할을 하지 못한다.
+// 해당 Context를 통해 하고 싶은 행위는 이렇다.
+// localStorage에서 token이 있을 때 api로 token을 검증한다.
+// user 검증이 됐다면 자동 로그인과 로그인 상태 유지하기.
+
+// 모든 컴포넌트에서 로그인 상태를 유지하고 싶은 목적으로 생성.
 export function LoginProvider({ children }) {
   const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState(null);
