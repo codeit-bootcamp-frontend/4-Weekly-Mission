94c3810839fe26ff5a0cf2bed9088817e750aa52
devwqc <devwqc@gmail.com>
2024-02-18 23:41:13 +0900

    feat: 추가 svg 파일 IconAdd 컴포넌트화

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,5 +1,6 @@
 export { default as IconFacebook } from './svg/sns/IconFacebook';
 export { default as IconInstagram } from './svg/sns/IconInstagram';
+export { default as IconAdd } from './svg/IconAdd';
 export { default as IconKebabMenu } from './svg/IconKebabMenu';
 export { default as IconLink } from './svg/IconLink';
 export { default as IconLogo } from './svg/IconLogo';
