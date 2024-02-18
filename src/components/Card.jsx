baab8c5f313659e95cf9a988058e42d0c746acdc
whai2 <whai2125@gmail.com>
2024-02-18 14:23:56 +0900

    feat: 카드 컴포넌트 케밥, 별모양 버튼 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import { calculateDateDifference, formatDate } from '../utils/dateCalculate';
 import defaultImg from '../assets/logo.svg';
+import starIcon from '../assets/card/star.svg';
+import kebabIcon from '../assets/card/kebab.svg';
 
 import './Card.css'
 
@@ -7,15 +9,19 @@ const Card = ({imgSrc, createdAt, title, description, url}) => {
   return (
     <li className='card'>
       <a href={url} target="_blank" rel="noreferrer noopener">
-        <img src={imgSrc ?? defaultImg} alt='이미지' className='card-img'/>
+        <img src={imgSrc ?? defaultImg} alt='이미지' className='card-img' />
+      </a>
         <div className='card-info'>
-          <span className='time'>{calculateDateDifference(createdAt)}</span>
+          <div className='container-kebab'>
+            <span className='time'>{calculateDateDifference(createdAt)}</span>
+            <img src={kebabIcon} alt='목록' className='card-info_kebab' />
+          </div>
           <span className='description'>{title}<br/>{description}</span>
           <span className='date'>{formatDate(createdAt)}</span>
         </div>
-      </a>
+      <img src={starIcon} alt='중요 표시' className='card-img_star-icon' />
     </li>
-  ) // ReactRouter는 아직 적용하지 않았습니다. 이유: 아직 진도가 나가지 않음.
+  ) 
 }
 
 export default Card;
