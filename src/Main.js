fa00d92288c52ef9db44ddeb29d133b69d0b2725
LimDongHyeon <ilhai3@naver.com>
2024-02-19 02:02:25 +0900

    링크이동 버튼 추가

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import { RecoilRoot } from 'recoil';
 
 function Main() {
     return (
-        <BrowserRouter>
-            <RecoilRoot>
-                <App />
-            </RecoilRoot>
-        </BrowserRouter>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
     );
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,27 @@ import '../assets/common/common.css';
 import Footer from './common/Footer';
 import Shared from '../pages/shared/Shared';
 import Folder from '../pages/folder/Folder';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      <div id='wrap'>
-        <Routes>
-          <Route path="/" element={<div>main</div>}/>
-          <Route path="/folder" element={<Folder/>}/>
-          <Route path="/shared" element={<Shared/>} />
-        </Routes>
-      </div>
-      <Footer/>
+      <BrowserRouter>
+        <div id='wrap'>
+          <Routes>
+            <Route path="/" element={
+            <div className='linkText'>              
+              <Link to="/folder">Folder 이동</Link>
+              <Link to="/shared">Shared 이동</Link>
+            </div>}
+            />
+            <Route path="folder" element={<Folder/>}/>
+            <Route path="shared" element={<Shared/>} />
+          </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
     </>
   );
 }
