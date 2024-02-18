75fcd8a4511959c113e13744f71b81fa62cb42ea
codingaring <sksms2104@naver.com>
2024-02-18 13:05:21 +0900

    [refactor] folder의 카테고리 button 스타일 컴포넌트 적용

--- week8/src/ui/Classification/Classification.jsx
+++ week8/src/ui/Classification/Classification.jsx
@@ -1,15 +1,16 @@
 import "./Classification.css";
+import { Button } from "ui/StyledComponents/Buttons";
 
 export function Classification() {
   return (
     <div className="classification">
       <div className="classification-buttons">
-        <button>전체</button>
-        <button>⭐즐겨찾기</button>
-        <button>코딩 팁</button>
-        <button>채용 사이트</button>
-        <button>유용한 글</button>
-        <button>나만의 장소</button>
+        <Button>전체</Button>
+        <Button>⭐즐겨찾기</Button>
+        <Button>코딩 팁</Button>
+        <Button>채용 사이트</Button>
+        <Button>유용한 글</Button>
+        <Button>나만의 장소</Button>
       </div>
       <button className="add-folder-button">폴더 추가 +</button>
     </div>
