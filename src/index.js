1f472b583fd03f52556adc695107ab389f58270f
hong <hsw5971@naver.com>
2024-02-18 17:35:53 +0900

    codeit 제공 코드 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/App.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
