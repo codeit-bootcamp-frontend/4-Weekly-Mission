92339e62df797ed66baf617e80e4617a57a4b27f
JinahKim <kimjinah.dev@gmail.com>
2024-02-19 06:56:59 +0900

    ✨ Feat: Header, Main, Footer 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./stylesheets/main.css";
+import "./App.css";
+import { Header } from "./Header";
+import { Main } from "./Main";
+import { Footer } from "./Footer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
