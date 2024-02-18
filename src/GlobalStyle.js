2c3a5a82e821ed3f424e134dce88bf77d0ad6f65
Trophy198 <spsp8426@naver.com>
2024-02-18 16:09:48 +0900

    fix : styled-componets 에서 내부에 import를 사용하고 있는 경고 수정

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
-
   * {
     margin: 0;
     padding: 0;
