250b851cbb47338424290dfbbbbc75c6f2115033
Jules <dali1999@naver.com>
2024-02-18 12:49:16 +0900

    feat: Router를 사용해 페이지 경로 지정

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../Main";
+import Folder from "../pages/Folder";
+import HomaPage from "../pages/HomaPage";
 import Shared from "../pages/Shared";
-import "./App.css";
 
 function App() {
-  return <Shared />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<HomaPage />} />
+          <Route path="shared" element={<Shared />} />
+          <Route path="folder" element={<Folder />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
