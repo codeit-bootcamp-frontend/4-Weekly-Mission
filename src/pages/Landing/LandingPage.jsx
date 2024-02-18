b675629451c2762624ada70a8ffcda46d10afdcc
devwqc <devwqc@gmail.com>
2024-02-18 11:13:18 +0900

    design: LandingPage 임시 style 적용

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,15 +1,16 @@
+import './LandingPage.css';
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
-    <>
+    <div className="landing">
       <h1>Landing page</h1>
       <div>
         <div>
           <Link to="shared">shared 페이지</Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
