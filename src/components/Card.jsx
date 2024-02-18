223d151a2d327d0fc4b6722dcc998f499c5895e5
choiwonseokgit <cs0328@hanmail.net>
2024-02-18 23:15:34 +0900

    feat: useToggle 훅 추가 및 카드 컴포넌트 적용

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,20 @@
 import { dateDiffCalc, dateFormatter } from "../util/datecalc";
 import emptyLogo from "../images/emptylogo.svg";
+import kebab from "../images/kebab.svg";
+import useToggle from "../hooks/useToggle";
+import star from "../images/star.svg";
+
+function SelectMenu() {
+  return (
+    <div className="selectMenu-container">
+      <div className="selectMenu">삭제하기</div>
+      <div className="selectMenu clicked">폴더에 추가</div>
+    </div>
+  );
+}
 
 function Card({ link }) {
-  // console.log(link);
+  const [kebabValue, KebabTogglefunc] = useToggle(false);
   const cardImage =
     link.imageSource || link["image_source"]
       ? link.imageSource || link["image_source"]
@@ -16,16 +28,28 @@ function Card({ link }) {
   };
 
   return (
-    <div className="card-container" onClick={moveToUrl}>
-      <img className="card-container-img" src={cardImage} alt="이미지"></img>
-      <div className="card-container-texts">
-        <div className="card-container-dateDiff">
-          <div>{dateDiff}</div>
+    <>
+      <div className="card-container" onClick={moveToUrl}>
+        <img className="card-container-img" src={cardImage} alt="이미지"></img>
+        <img className="star" src={star} alt="star" />
+        <div className="card-container-texts">
+          <div className="card-container-dateDiff">
+            <div>{dateDiff}</div>
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                KebabTogglefunc();
+              }}
+            >
+              <img src={kebab} alt="kebab" />
+              {kebabValue ? <SelectMenu /> : null}
+            </div>
+          </div>
+          <div className="card-container-description">{link.description}</div>
+          <div className="card-conatiner-formatDate">{formatDate}</div>
         </div>
-        <div className="card-container-description">{link.description}</div>
-        <div className="card-conatiner-formatDate">{formatDate}</div>
       </div>
-    </div>
+    </>
   );
 }
 
