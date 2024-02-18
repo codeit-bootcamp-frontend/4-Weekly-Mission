27b7b1d5f1dda67f7b15123734215cd515e86cd8
Kim Min Jeong <minjeong9919@naver.com>
2024-02-18 17:34:38 +0900

    Feat: 카드 컴포넌의 옵션 리스트 구현 및 모바일 폴더 추가 버튼 구현

--- src/components/common/FolderItem.js
+++ src/components/common/FolderItem.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useState } from "react";
 import { CalcTime } from "./../../calculator";
 import { ReactComponent as Star } from "../../assets/icons/card_star.svg";
@@ -8,6 +9,7 @@ function FolderItem({ item }) {
   const [isHovering, setIsHovering] = useState(false);
   const { imageSource, createdAt, description, url, id } = item;
   const { created_at, favorite, image_source } = item;
+  const [optionMenu, setOptionMenu] = useState(false);
 
   let time = 0;
   let img_src = "";
@@ -52,7 +54,17 @@ function FolderItem({ item }) {
         <div className="folder-textBox">
           <div className="time-kebab">
             <p id="time">{time}</p>
-            <Kebab />
+            <Kebab
+              onClick={(e) => {
+                e.preventDefault();
+                setOptionMenu(!optionMenu);
+                console.log(optionMenu);
+              }}
+            ></Kebab>
+            <MenuOptions optionMenu={optionMenu}>
+              <Option>삭제하기</Option>
+              <Option>폴더에 추가</Option>
+            </MenuOptions>
           </div>
           <p id="info">{description}</p>
           <p id="date">2023. 3. 15</p>
@@ -61,5 +73,27 @@ function FolderItem({ item }) {
     </a>
   );
 }
+
+const MenuOptions = styled.div`
+  width: 100px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: 20px;
+  border: 1px;
+  display: ${({ optionMenu }) => (optionMenu ? "block" : "none")};
+`;
+
+const Option = styled.p`
+  padding: 7px 12px;
+  background-color: #fff;
+  color: #333236;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-align: center;
+`;
 
 export default FolderItem;
--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -30,7 +30,7 @@ const Folder = () => {
   }, [listId]);
 
   return (
-    <>
+    <Container>
       <GlobalStyle />
       <HeaderElement $positionval="static" />
       <FolderInput />
@@ -42,10 +42,15 @@ const Folder = () => {
       ) : (
         <NoLinkMsg>저장된 링크가 없습니다.</NoLinkMsg>
       )}
+      <AddFolderBtn>폴더 추가 +</AddFolderBtn>
       <FooterElement />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin: 0px;
+`;
 
 const NoLinkMsg = styled.p`
   color: #000;
@@ -56,6 +61,31 @@ const NoLinkMsg = styled.p`
   font-weight: 400;
   line-height: 24px; /* 150% */
   margin-top: 40px;
+`;
+
+const AddFolderBtn = styled.button`
+  border: none;
+  border-radius: 20px;
+  border: 1px solid var(--Linkbrary-white, #fff);
+  background: var(--Linkbrary-primary-color, #6d6afe);
+  position: sticky;
+  left: 40%;
+  bottom: 101px;
+  padding: 8px 24px;
+  display: none;
+
+  color: var(--Linkbrary-gray10, #e7effb);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.3px;
+
+  @media (max-width: 774px) {
+    display: block;
+  }
 `;
 
 export default Folder;
