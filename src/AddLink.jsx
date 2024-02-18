0c29d71faef8a7832d3c091643adaa8df844b539
whai2 <whai2125@gmail.com>
2024-02-18 23:52:20 +0900

    feat(mentor): 링크 추가 입력 창을 입력 태그로 수정

--- src/AddLink.jsx
+++ src/AddLink.jsx
@@ -5,13 +5,13 @@ import './AddLink.css';
 const AddLink = () => {
   return (
     <div className='container-add-link'>
-      <div className='add-link'>
+      <form className='add-link'>
         <div className='add-link_icon-box'>
           <img src={linkIcon} alt='링크' />
-          <span className='text-add-link'>링크를 추가해 보세요</span>
+          <input className='text-add-link' placeholder='링크를 추가해 보세요' />
         </div>
         <button className='add-link_button'>추가하기</button>
-      </div>
+      </form>
     </div>
   )
 }
