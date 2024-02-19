80fce88639d7a3105a96cc26258389a5834697ba
Trophy198 <spsp8426@naver.com>
2024-02-20 00:34:36 +0900

    refactor : app 에서 사용하지 않는 contextprovider 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import GlobalStyle from './GlobalStyle';
 import Router from './Router';
-import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
   return (
-    <AuthProvider>
+    <>
       <GlobalStyle />
       <Router />
-    </AuthProvider>
+    </>
   );
 };
 
