90809771ea67f2367c4bb0e2fb3e8a11183b7ad4
이진호 <tead7@naver.com>
2024-02-20 00:13:33 +0900

    Feat: Floating Action Button 추가

--- src/layout/PageBody/Contain/Card/CardTagList/CardTagList.js
+++ src/layout/PageBody/Contain/Card/CardTagList/CardTagList.js
@@ -1,11 +1,38 @@
 import "./CardTagList.css";
+import { useState, useEffect } from "react";
 
 export const CardTagList = ({ children }) => {
+  const [isFloating, setIsFloating] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        // 화면 너비가 768px보다 작으면
+        setIsFloating(true); // Floating Action Button으로 변경
+      } else {
+        setIsFloating(false); // 그렇지 않으면 일반 버튼 유지
+      }
+    };
+
+    // 페이지 로드시와 화면 크기 변경시 handleResize 함수 호출
+    window.addEventListener("resize", handleResize);
+    handleResize(); // 초기 로드시 한 번 호출
+
+    // Clean-up 함수 등록
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); //
+
   return (
     <div className="Contain">
       <div className="CardTagList">
         {children}
-        <div className="CardTagListPlus">폴더추가+</div>
+        <div
+          className={
+            isFloating ? "CardTagListPlus floating" : "CardTagListPlus"
+          }
+        >
+          폴더 추가 +
+        </div>
       </div>
     </div>
   );
