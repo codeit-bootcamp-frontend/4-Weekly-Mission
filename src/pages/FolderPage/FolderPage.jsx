0597081e504a261b5ce01ba6db57c50c32f956a5
Trophy198 <spsp8426@naver.com>
2024-02-20 01:32:57 +0900

    refactor : FolderPage 컴포넌트에서 기존 useEffect에 api 요청 및 렌더방식 수정 및 에러처리 모달 및 hooks 연결 - 에러처리에 대한 모달 및 hooks 기능 연결 - Folder id에 대한 LinkData 불러오는 방식 변경

--- src/pages/FolderPage/FolderPage.jsx
+++ src/pages/FolderPage/FolderPage.jsx
@@ -5,36 +5,55 @@ import { getLinkData, getLinkByIdData } from '../../apis/link';
 import Card from '../../components/Card/Card';
 import { getFolderData } from '../../apis/folder';
 import FolderControls from '../../components/FolderPage/SortButtonGroup/FolderControls';
+import useErrorMessageModal from '../../hooks/useErrorMessageModal';
+import ErrorMessageModal from '../../components/common/Modal/ErrorMessageModal';
 
 const FolderPage = () => {
   const [folders, setFolders] = useState(null);
   const [cards, setCards] = useState(null);
   const [selectedFolderId, setSelectedFolderId] = useState(null);
+  const { isModalOpen, errorMessage, showErrorModal, hideErrorModal } =
+    useErrorMessageModal();
 
   useEffect(() => {
-    getFolderData().then(res => {
-      setFolders(res.data);
-    });
+    const fetchFolders = async () => {
+      try {
+        const response = await getFolderData();
+        setFolders(response.data);
+      } catch (error) {
+        showErrorModal(error.message);
+      }
+    };
 
-    getLinkData().then(res => {
-      setCards(res.data);
-    });
-  }, []);
+    fetchFolders();
+  }, [showErrorModal]);
 
-  const handleFolderClick = async id => {
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try {
+        const response =
+          selectedFolderId === null
+            ? await getLinkData()
+            : await getLinkByIdData(selectedFolderId);
+        setCards(response.data);
+      } catch (error) {
+        showErrorModal(error.message);
+      }
+    };
+
+    fetchLinks();
+  }, [selectedFolderId, showErrorModal]);
+
+  const handleFolderClick = id => {
     setSelectedFolderId(id);
-
-    if (id === null) {
-      const res = await getLinkData();
-      setCards(res.data);
-      return;
-    }
-
-    const res = await getLinkByIdData(id);
-    setCards(res.data);
   };
+
   return (
     <>
+      {isModalOpen && (
+        <ErrorMessageModal message={errorMessage} onClose={hideErrorModal} />
+      )}
+
       <LinkInput />
       <CardSearchbar />
       {folders && (
