42c3263ae4c468000f4819a394452310779d5cbe
Trophy198 <spsp8426@naver.com>
2024-02-20 00:02:33 +0900

    refactor : Navbar styled-components 디자인 라이브러리로 변경 및 유저 데이터 처리 수정 - Location을 사용해 특정 페이지에서만 다른 api요청으로 수정

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,40 +1,73 @@
-import './navbar.css';
-import { useAuth } from '../../../contexts/AuthContext';
-import MainLogo from '../../../assets/icons/logo.svg';
-import Button from '../../common/Button/Button';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import mainLogo from '../../../assets/icons/logo.svg';
+import { getSampleUser, getUserById } from '../../../apis/auth';
+import {
+  NavbarContainer,
+  NavbarList,
+  NavbarItem,
+  LogoLink,
+  LoginButton,
+  UserProfileImage,
+  UserProfileContainer,
+  UserProfileEmail,
+} from './navbarStyle';
 
 const Navbar = () => {
-  const { user } = useAuth();
-  const handleLoginButton = () => {
-    /** 후에 dom 연결 */
+  const [user, setUser] = useState(null);
+  const location = useLocation();
+  const isFolderPage = !location.pathname.startsWith('/folder');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        let userData;
+        if (location.pathname.startsWith('/folder')) {
+          const data = await getUserById();
+          userData = {
+            email: data.data[0].email,
+            profileImageSource: data.data[0].image_source,
+          };
+        } else {
+          userData = await getSampleUser();
+        }
+        setUser(userData);
+      } catch (error) {
+        alert('유저 정보 데이터 로딩에 실패하였습니다.');
+        setUser(null);
+      }
+    };
+
+    fetchUserData();
+  }, [location.pathname]);
+
+  const handleLoginButtonClick = () => {
+    // 로그인 로직 구현
   };
 
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <a href="/" className="navbar-logo-link">
-            <img src={MainLogo} alt="Linkbrary Logo" />
-          </a>
-        </li>
-        <li className="navbar-item">
+    <NavbarContainer $isFolderPage={isFolderPage}>
+      <NavbarList>
+        <NavbarItem>
+          <LogoLink href="/">
+            <img src={mainLogo} alt="Linkbrary Logo" />
+          </LogoLink>
+        </NavbarItem>
+        <NavbarItem>
           {user ? (
-            <div className="navbar-profile">
-              <img
-                className="navbar-userprofile-img"
+            <UserProfileContainer>
+              <UserProfileImage
                 src={user.profileImageSource}
-                alt="User Profile"
+                alt="사용자 프로필"
               />
-              <span className="navbar-userprofile-email">{user.email}</span>
-            </div>
+              <UserProfileEmail>{user.email}</UserProfileEmail>
+            </UserProfileContainer>
           ) : (
-            <Button className="navbar-login-button" onClick={handleLoginButton}>
-              로그인
-            </Button>
+            <LoginButton onClick={handleLoginButtonClick}>로그인</LoginButton>
           )}
-        </li>
-      </ul>
-    </nav>
+        </NavbarItem>
+      </NavbarList>
+    </NavbarContainer>
   );
 };
 
---
+++ src/components/layout/Navbar/navbarStyle.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import Button from '../../common/Button/Button';
+
+export const NavbarContainer = styled.nav`
+  width: 100%;
+  background-color: var(--Linkbrary-bg);
+  position: ${({ $isFolderPage }) => ($isFolderPage ? 'sticky' : 'static')};
+  top: 0;
+  left: 0;
+  z-index: 999;
+  padding: 1.25rem 32px;
+
+  @media (max-width: 767px) {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+`;
+
+export const NavbarList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  margin: 0;
+`;
+
+export const NavbarItem = styled.li`
+  /* 스타일을 추가 예정 */
+`;
+
+export const LogoLink = styled.a`
+  color: white;
+  text-decoration: none;
+  display: block;
+  text-align: center;
+`;
+
+export const LoginButton = styled(Button)`
+  width: 8rem;
+  padding: 1rem 1.25rem;
+  border: none;
+  font-size: 1.125rem;
+  border-radius: 8px;
+  color: #fff;
+  background: var(--gra-purpleblue-to-skyblue);
+  cursor: pointer;
+`;
+
+export const UserProfileImage = styled.img`
+  width: 28px;
+  height: 28px;
+  margin-right: 6px;
+  border-radius: 50%;
+`;
+
+export const UserProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserProfileEmail = styled.span`
+  margin-left: 6px;
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
