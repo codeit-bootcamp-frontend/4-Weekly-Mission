e71b22521de2993d2f46a201f36b319062285f2c
Trophy198 <spsp8426@naver.com>
2024-02-20 01:58:48 +0900

    refactor : Navbar 컴포넌트 데이터 요청 방식 개선 및 에러 처리 추가

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import mainLogo from '../../../assets/icons/logo.svg';
 import { getSampleUser, getUserById } from '../../../apis/auth';
+import useErrorMessageModal from '../../../hooks/useErrorMessageModal';
+import ErrorMessageModal from '../../common/Modal/ErrorMessageModal';
 import {
   NavbarContainer,
   NavbarList,
@@ -16,58 +18,62 @@ import {
 const Navbar = () => {
   const [user, setUser] = useState(null);
   const location = useLocation();
-  const isFolderPage = !location.pathname.startsWith('/folder');
+  const { isModalOpen, errorMessage, showErrorModal, hideErrorModal } =
+    useErrorMessageModal();
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        let userData;
-        if (location.pathname.startsWith('/folder')) {
-          const data = await getUserById();
-          userData = {
-            email: data.data[0].email,
-            profileImageSource: data.data[0].image_source,
-          };
-        } else {
-          userData = await getSampleUser();
-        }
-        setUser(userData);
+        const isFolderPage = location.pathname.startsWith('/folder');
+        const userData = isFolderPage
+          ? await getUserById()
+          : await getSampleUser();
+
+        setUser({
+          email: userData.email || userData.data[0].email,
+          profileImageSource:
+            userData.profileImageSource || userData.data[0].image_source,
+        });
       } catch (error) {
-        alert('유저 정보 데이터 로딩에 실패하였습니다.');
-        setUser(null);
+        showErrorModal(error.message);
       }
     };
 
     fetchUserData();
-  }, [location.pathname]);
+  }, [location.pathname, showErrorModal]);
 
   const handleLoginButtonClick = () => {
     // 로그인 로직 구현
   };
 
   return (
-    <NavbarContainer $isFolderPage={isFolderPage}>
-      <NavbarList>
-        <NavbarItem>
-          <LogoLink href="/">
-            <img src={mainLogo} alt="Linkbrary Logo" />
-          </LogoLink>
-        </NavbarItem>
-        <NavbarItem>
-          {user ? (
-            <UserProfileContainer>
-              <UserProfileImage
-                src={user.profileImageSource}
-                alt="사용자 프로필"
-              />
-              <UserProfileEmail>{user.email}</UserProfileEmail>
-            </UserProfileContainer>
-          ) : (
-            <LoginButton onClick={handleLoginButtonClick}>로그인</LoginButton>
-          )}
-        </NavbarItem>
-      </NavbarList>
-    </NavbarContainer>
+    <>
+      {isModalOpen && (
+        <ErrorMessageModal message={errorMessage} onClose={hideErrorModal} />
+      )}
+      <NavbarContainer>
+        <NavbarList>
+          <NavbarItem>
+            <LogoLink href="/">
+              <img src={mainLogo} alt="Linkbrary Logo" />
+            </LogoLink>
+          </NavbarItem>
+          <NavbarItem>
+            {user ? (
+              <UserProfileContainer>
+                <UserProfileImage
+                  src={user.profileImageSource}
+                  alt="사용자 프로필"
+                />
+                <UserProfileEmail>{user.email}</UserProfileEmail>
+              </UserProfileContainer>
+            ) : (
+              <LoginButton onClick={handleLoginButtonClick}>로그인</LoginButton>
+            )}
+          </NavbarItem>
+        </NavbarList>
+      </NavbarContainer>
+    </>
   );
 };
 
