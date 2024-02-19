ed702e63495d4fca778788891adcfd1f7d5a838b
LeeSeungHoon <ysh73900@gmail.com>
2024-02-20 01:42:22 +0900

    refactor: 구조 분해 할당에서 불필요한 이름 변경 제거

--- src/components/Card/CardImage/CardImage.js
+++ src/components/Card/CardImage/CardImage.js
@@ -3,7 +3,7 @@ import defaultImage from "../../../assets/images/card-default.png";
 import star from "../../../assets/images/folder/star.svg";
 
 const CardImage = ({ items, isZoomedIn }) => {
-  const { imageSource: imageSource, image_source: image_sourse } = items;
+  const { imageSource, image_source: image_sourse } = items;
   const imageSourceValue = imageSource || image_sourse;
   const className = isZoomedIn ? "card-image card-image-zoom-in" : "card-image";
 
