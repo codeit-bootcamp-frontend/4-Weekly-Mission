0c31c5676ead57883dc18dc761727857ad8b81ea
Trophy198 <spsp8426@naver.com>
2024-02-20 01:34:27 +0900

    refactor : Footer 컴포넌트 styled-components 디자인 적용 및 중복된 아이템에 대한 객체타입 배열 추가

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,44 +1,45 @@
-import './footer.css';
 import YoutubeIcon from '../../../assets/icons/akar-icons-youtube-fill.svg';
 import TwitterIcon from '../../../assets/icons/akar-icons-twitter-fill.svg';
 import FacebookIcon from '../../../assets/icons/akar-icons-facebook-fill.svg';
 import InstagramIcon from '../../../assets/icons/ant-design-instagram-filled.svg';
+import {
+  FooterCenter,
+  FooterContainer,
+  FooterIcon,
+  FooterLeft,
+  FooterLink,
+  FooterRight,
+  FooterSocialLink,
+} from './footerStyle';
+
+const socialLinks = [
+  { name: 'Facebook', icon: FacebookIcon, url: 'https://facebook.com' },
+  { name: 'Twitter', icon: TwitterIcon, url: 'https://twitter.com' },
+  { name: 'YouTube', icon: YoutubeIcon, url: 'https://youtube.com' },
+  { name: 'Instagram', icon: InstagramIcon, url: 'https://instagram.com' },
+];
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-left">©codeit - 2023</div>
-      <ul className="footer-center">
-        <li>
-          <a href="/privacy.html">Privacy Policy</a>
-        </li>
-        <li>
-          <a href="/faq.html">FAQ</a>
-        </li>
-      </ul>
-      <ul className="footer-right">
-        <li>
-          <a href="https://facebook.com" target="_blank" rel="noreferrer">
-            <img src={FacebookIcon} alt="Facebook icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com" target="_blank" rel="noreferrer">
-            <img src={TwitterIcon} alt="Twitter icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://youtube.com" target="_blank" rel="noreferrer">
-            <img src={YoutubeIcon} alt="YouTube icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">
-            <img src={InstagramIcon} alt="Instagram icon" />
-          </a>
-        </li>
-      </ul>
-    </footer>
+    <FooterContainer>
+      <FooterLeft>©codeit - 2023</FooterLeft>
+      <FooterCenter>
+        <FooterLink href="/privacy.html">Privacy Policy</FooterLink>
+        <FooterLink href="/faq.html">FAQ</FooterLink>
+      </FooterCenter>
+      <FooterRight>
+        {socialLinks.map(link => (
+          <FooterSocialLink
+            key={link}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FooterIcon src={link.icon} alt={`${link.name} 아이콘`} />
+          </FooterSocialLink>
+        ))}
+      </FooterRight>
+    </FooterContainer>
   );
 };
 
