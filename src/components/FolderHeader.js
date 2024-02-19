25e40c04f0b13372224cf0e9706a58aba8ded848
김경주 <popo505@naver.com>
2024-02-19 12:42:51 +0900

    feat: folder 헤더 영역

---
+++ src/components/FolderHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FolderProfile from "./FolderProfile";
+import "../styles/header.css";
+
+function FolderHeader() {
+  return (
+    <header className="header">
+      <a href="/">
+        <img className="header-logo" src="img/logo.svg" alt="홈 연결 로고" />
+      </a>
+      <div className="header-profile">
+        <FolderProfile />
+      </div>
+    </header>
+  );
+}
+
+export default FolderHeader;
--- src/pages/folder.js
+++ src/pages/folder.js
@@ -1,5 +1,17 @@
+import React from "react";
+import FolderHeader from "../components/FolderHeader";
+import FolderMain from "../components/FolderMain";
+import CardList from "../components/CardList";
+import Footer from "../components/Footer";
+
 function Folder() {
-  return <div></div>;
+  return (
+    <div>
+      <FolderHeader />
+      <FolderMain />
+      <Footer />
+    </div>
+  );
 }
 
 export default Folder;
