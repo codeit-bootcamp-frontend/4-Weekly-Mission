02ffe2dff574fd0c53d6d7f137f2a7eec132a93d
whai2 <whai2125@gmail.com>
2024-02-19 19:17:50 +0900

    refactor(mentor): gnb users state를 user로 단수화

--- src/components/Gnb.jsx
+++ src/components/Gnb.jsx
@@ -6,13 +6,13 @@ import logoImg from '../assets/logo.svg';
 import './Gnb.css';
 
 const Gnb = ({className}) => {
-  const [users, setUsers] = useState({});
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     const fetchUser = async () => {
       const { data } = await getUser();
       
-      setUsers(data[0]); 
+      setUser(data[0]); 
       // 하드 코딩 이유: 아마, 회원 인증에서 처리해 users의 넘버를 넘겨 유저들 배열 중에서 특정 유저에 대한 정보로 접근할 듯합니다.
       // 따라서, 현재로서는 동적으로 접근하는 방법이 떠오르지 않아, 하드 코딩을 진행했습니다.
     };
@@ -20,7 +20,7 @@ const Gnb = ({className}) => {
     fetchUser();
   }, []);
 
-  const isShowProfile = users && users.image_source && users.email;
+  const isShowProfile = user && user.image_source && user.email;
 
   return (
     <nav className={className}>
@@ -30,8 +30,8 @@ const Gnb = ({className}) => {
         </a>
         {isShowProfile ? (
           <div className='account'>
-            <img className="account-img" src={users.image_source} alt={`${users.email} 프로필`} />
-            <span className='account-email'>{users.email}</span>
+            <img className="account-img" src={user.image_source} alt={`${user.email} 프로필`} />
+            <span className='account-email'>{user.email}</span>
           </div>
         ) : (
           <button className='login-button'>로그인</button>
