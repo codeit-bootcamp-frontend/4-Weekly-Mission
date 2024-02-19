0dae8ec8db70b2a0af2d072bd65eaa008f34587a
Sumin <csmxx99@naver.com>
2024-02-20 00:57:37 +0900

    FolderProfile 컴포넌트 완료

--- src/components/FolderProfile.jsx
+++ src/components/FolderProfile.jsx
@@ -1,14 +1,92 @@
+import styled from "styled-components";
+
+const FolderProfileDiv = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 10px auto 40px;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  background: var(--Linkbrary-bg, #f0f6ff);
+
+  @media ${(props) => props.theme.tabletS} {
+    padding: 20px auto 60px;
+  }
+`;
+
+const OwnerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+  p {
+    color: #000;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: -0.2px;
+    margin: 0px;
+  }
+
+  @media ${(props) => props.theme.tabletS} {
+    gap: 20px;
+
+    p {
+      font-size: 40px;
+    }
+  }
+`;
+
+const ProfileDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  p {
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+  @media ${(props) => props.theme.tabletS} {
+    gap: 12px;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+    p {
+      font-size: 16px;
+      line-height: 24px;
+    }
+  }
+`;
+
 export default function FolderProfile({ ownerProfile }) {
   return (
-    <div>
-      <div>
-        <img
-          src={ownerProfile?.folder?.owner?.profileImageSource}
-          alt="폴더 소유자의 프로필 사진"
-        />
-        <p>{ownerProfile?.folder?.owner?.name}</p>
-      </div>
-      <p>{ownerProfile?.folder?.name}</p>
-    </div>
+    <FolderProfileDiv>
+      <OwnerDiv>
+        <ProfileDiv>
+          <img
+            src={ownerProfile?.folder?.owner?.profileImageSource}
+            alt="폴더 소유자의 프로필 사진"
+          />
+          <p>{ownerProfile?.folder?.owner?.name}</p>
+        </ProfileDiv>
+        <p>{ownerProfile?.folder?.name}</p>
+      </OwnerDiv>
+    </FolderProfileDiv>
   );
 }
