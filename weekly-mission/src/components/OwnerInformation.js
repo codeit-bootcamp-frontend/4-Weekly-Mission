8c6417b8c218e9b60f0ff833bd1a67f89afa094a
parkhyeonwoo <pq120wal39@gmail.com>
2024-02-19 20:47:04 +0900

    refactor : OwerInformation component refactoring

--- weekly-mission/src/components/OwnerInformation.js
+++ weekly-mission/src/components/OwnerInformation.js
@@ -7,58 +7,53 @@ const OwnerCard = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 2rem;
-    
-    .owner-information {
-        text-align: center;
-    }
-
-    .owner-img {
-        width: 6rem;
-        height: 6rem;
-        margin: auto;
-
-    }
-
-    .owner-img>img {
-        width: 100%;
-        border-radius: 50%;
-    }
-
-    .owner-name {
-        font-size: 1.2rem;
-        line-height: 150%;
-        margin-top: 1.2rem;
-        display: inline-flex;
-    }
-
-    .folder-name {
-        font-size: 3rem;
-        color: #000;
-        text-align: center;
-        line-height: 1;
-    }
 `;
 
 
+const CardInner = styled.div`
+    text-align: center;
+`;
+
+const ImgBox = styled.div`
+    width: 6rem;
+    height: 6rem;
+    margin: auto;
+    & > img{
+        width: 100%;
+        border-radius: 50%;
+    }
+`;
+
+const CardName = styled.span`
+    font-size: 1.2rem;
+    line-height: 150%;
+    margin-top: 1.2rem;
+    display: inline-flex;
+`;
+
+const FolderName = styled.strong`
+    font-size: 3rem;
+    color: #000;
+    text-align: center;
+    line-height: 1;
+`;
 
 function OwnerInformation({ information }) {
-    const {name, owner} = information;
+    const { name, owner } = information;
 
     return (
         <OwnerCard>
-            <div className='owner-information'>
-                <div className='owner-img'>
+            <CardInner>
+                <ImgBox>
                     <img src={owner.profileImageSource} alt='폴더소유자 프로필 이미지' />
-                </div>
-                <span className='owner-name'>
+                </ImgBox>
+                <CardName>
                     @{owner.name}
-                </span>
-            </div>
-            <div className='folder'>
-                <strong className='folder-name'>
-                    {name}
-                </strong>
-            </div>
+                </CardName>
+            </CardInner>
+            <FolderName>
+                {name}       
+            </FolderName>
         </OwnerCard>
     );
 };
