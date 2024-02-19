73ef7cea2697f0d7c86df6351c29e61aa67aa6fc
Trophy198 <spsp8426@naver.com>
2024-02-20 00:32:58 +0900

    refactor : 초기 렌더방식 useEffect -> useGetApi hooks 방식으로 변경 및 에러 처리 모달 연결

--- src/pages/SharedPage/SharedPage.jsx
+++ src/pages/SharedPage/SharedPage.jsx
@@ -1,27 +1,40 @@
-import { useState, useEffect } from 'react';
-import { getFolderData } from '../../apis/folder';
+import { useEffect } from 'react';
+import useGetApi from '../../hooks/useGetApi';
+import { getSampleFolderData } from '../../apis/folder';
 import Profile from '../../components/SharedPage/Profile/Profile';
 import Card from '../../components/Card/Card';
 import CardSearchbar from '../../components/common/SearchBar/CardSearchbar';
 import FolderContainer from './sharedPageStyle';
+import ErrorMessageModal from '../../components/common/Modal/ErrorMessageModal';
+import useErrorMessageModal from '../../hooks/useErrorMessageModal';
 
 const SharedPage = () => {
-  const [profiles, setProfiles] = useState(null);
-  const [cards, setCards] = useState(null);
+  const { data, error, resetError } = useGetApi(getSampleFolderData, {
+    folder: null,
+  });
+
+  const { isModalOpen, errorMessage, showErrorModal, hideErrorModal } =
+    useErrorMessageModal();
+
+  const profiles = data?.folder;
+  const cards = data?.folder?.links;
 
   useEffect(() => {
-    getFolderData()
-      .then(data => {
-        setProfiles(data.folder);
-        setCards(data.folder.links);
-      })
-      .catch(error => {
-        alert('데이터를 불러오는데 실패했습니다.');
-      });
-  }, []);
+    if (error) {
+      showErrorModal(error.message);
+    }
+  }, [error, showErrorModal]);
+
+  const handleCloseModal = () => {
+    hideErrorModal();
+    resetError();
+  };
 
   return (
     <>
+      {isModalOpen && (
+        <ErrorMessageModal message={errorMessage} onClose={handleCloseModal} />
+      )}
       {profiles && <Profile {...profiles} />}
       <FolderContainer>
         <CardSearchbar />
