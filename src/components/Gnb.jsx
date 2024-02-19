8ce02597af567d72134aa2b3c1129ad59f342ca9
whai2 <whai2125@gmail.com>
2024-02-19 19:20:52 +0900

    refactor(mentor): 유저 정보 호출 인덱스로 접근하지 않고, 배열로 접근

--- src/components/Gnb.jsx
+++ src/components/Gnb.jsx
@@ -11,8 +11,9 @@ const Gnb = ({className}) => {
   useEffect(() => {
     const fetchUser = async () => {
       const { data } = await getUser();
-      
-      setUser(data[0]); 
+      const [user] = data;
+
+      setUser(user); 
       // 하드 코딩 이유: 아마, 회원 인증에서 처리해 users의 넘버를 넘겨 유저들 배열 중에서 특정 유저에 대한 정보로 접근할 듯합니다.
       // 따라서, 현재로서는 동적으로 접근하는 방법이 떠오르지 않아, 하드 코딩을 진행했습니다.
     };
