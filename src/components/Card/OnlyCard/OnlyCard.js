f1b7ce9cb9abf37a3f84d8e510bab0d80efaa33c
LeeSeungHoon <ysh73900@gmail.com>
2024-02-20 04:24:52 +0900

    feat: 기본 동작을 방지하고 이벤트 전파를 중지하는 함수

--- src/components/Card/OnlyCard/OnlyCard.js
+++ src/components/Card/OnlyCard/OnlyCard.js
@@ -7,11 +7,16 @@ const OnlyCard = ({ items }) => {
   const [isHovered, setIsHovered] = useState(false);
   const handleMouseOver = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
+
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
   return (
     <a href={items.url} target="_blank" rel="noopener noreferrer">
       <Card onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
         <CardImage items={items} isZoomedIn={isHovered} />
-        <CardContent items={items} isZoomedIn={isHovered} />
+        <CardContent items={items} handleLinkClick={handleLinkClick} />
       </Card>
     </a>
   );
