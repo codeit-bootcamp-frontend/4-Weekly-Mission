d428cabfda51a89c58a1597eb0e42d9126523b19
김경주 <popo505@naver.com>
2024-02-19 21:37:02 +0900

    feat: 링크 추가 영역

---
+++ src/components/FolderMain.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/foldermain.css";
+
+function FolderMain() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="link-container">
+      <form className="link" onSubmit={handleSubmit}>
+        <img src="img/link.svg" alt="link" />
+        <input className="link-input" type="text" placeholder="링크를 추가해 보세요." />
+        <button className="button-linkadd">추가하기</button>
+      </form>
+    </div>
+  );
+}
+
+export default FolderMain;
