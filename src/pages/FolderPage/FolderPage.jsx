a05ff582059803ff01f887afc66efc80ceebf69b
Trophy198 <spsp8426@naver.com>
2024-02-20 00:39:28 +0900

    feat : 폴더 페이지에 대한 컴포넌트 추가

--- src/pages/FolderPage/FolderPage.jsx
+++ src/pages/FolderPage/FolderPage.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LinkInput from '../../components/FolderPage/LinkInput/LinkInput';
+import CardSearchbar from '../../components/common/SearchBar/CardSearchbar';
+import { getLinkData, getLinkByIdData } from '../../apis/link';
+import Card from '../../components/Card/Card';
+import { getFolderData } from '../../apis/folder';
+import FolderControls from '../../components/FolderPage/SortButtonGroup/FolderControls';
 
 const FolderPage = () => {
-  return <div>FolderPage</div>;
+  const [folders, setFolders] = useState(null);
+  const [cards, setCards] = useState(null);
+  const [selectedFolderId, setSelectedFolderId] = useState(null);
+
+  useEffect(() => {
+    getFolderData().then(res => {
+      setFolders(res.data);
+    });
+
+    getLinkData().then(res => {
+      setCards(res.data);
+    });
+  }, []);
+
+  const handleFolderClick = async id => {
+    setSelectedFolderId(id);
+
+    if (id === null) {
+      const res = await getLinkData();
+      setCards(res.data);
+      return;
+    }
+
+    const res = await getLinkByIdData(id);
+    setCards(res.data);
+  };
+  return (
+    <>
+      <LinkInput />
+      <CardSearchbar />
+      {folders && (
+        <FolderControls
+          folders={folders}
+          selectedFolderId={selectedFolderId}
+          onClick={handleFolderClick}
+        />
+      )}
+      {cards && <Card links={cards} />}
+    </>
+  );
 };
 
 export default FolderPage;
