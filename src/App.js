a4312a47a251ed0b67a09ea10eb94db1283e7500
SeungAAA <SeungAAA@naver.com>
2024-02-19 09:21:47 +0900

    [Add]: fix sharedPage url '/' to /shared

--- src/App.js
+++ src/App.js
@@ -6,41 +6,28 @@ import { SearchBar } from './ui/SearchBar';
 import { CardList } from './ui/CardList';
 import { useGetFolder } from 'data-access/useGetFolder';
 import { ReadOnlyCard } from 'ui/ReadOnlyCard';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const { data } = useGetFolder();
   const { profileImage, ownerName, folderName, links } = data || {};
 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route
-            path='/shared'
-            element={
-              <SharedPage
-                folderInfo={
-                  <FolderInfo
-                    profileImage={profileImage}
-                    ownerName={ownerName}
-                    folderName={folderName}
-                  />
-                }
-                searchBar={<SearchBar />}
-                cardList={
-                  <CardList>
-                    {links?.map((link) => (
-                      <ReadOnlyCard key={link?.id} {...link} />
-                    ))}
-                  </CardList>
-                }
-              />
-            }
-          />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <SharedPage
+        folderInfo={
+          <FolderInfo profileImage={profileImage} ownerName={ownerName} folderName={folderName} />
+        }
+        searchBar={<SearchBar />}
+        cardList={
+          <CardList>
+            {links?.map((link) => (
+              <ReadOnlyCard key={link?.id} {...link} />
+            ))}
+          </CardList>
+        }
+      />
+    </Layout>
   );
 }
 
