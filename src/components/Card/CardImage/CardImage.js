999b448b8e5bd6b4fb884191887fb3eb84e06a60
LeeSeungHoon <ysh73900@gmail.com>
2024-02-20 01:34:14 +0900

    feat: star img 추가

--- src/components/Card/CardImage/CardImage.js
+++ src/components/Card/CardImage/CardImage.js
@@ -1,5 +1,6 @@
 import "./CardImage.css";
 import defaultImage from "../../../assets/images/card-default.png";
+import star from "../../../assets/images/folder/star.svg";
 
 const CardImage = ({ items, isZoomedIn }) => {
   const { imageSource: imageSource, image_source: image_sourse } = items;
@@ -7,11 +8,14 @@ const CardImage = ({ items, isZoomedIn }) => {
   const className = isZoomedIn ? "card-image card-image-zoom-in" : "card-image";
 
   return (
-    <div
-      style={{ backgroundImage: `url(${imageSourceValue ?? defaultImage})` }}
-      className={className}
-      alt=""
-    />
+    <div className="cardImage">
+      <div
+        style={{ backgroundImage: `url(${imageSourceValue ?? defaultImage})` }}
+        className={className}
+        alt=""
+      />
+      <img className="starImg" src={star} alt="likeButton" />
+    </div>
   );
 };
 
