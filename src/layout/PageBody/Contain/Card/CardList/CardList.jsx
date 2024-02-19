45764fd4b10491cbcefded9eba73947c694e5b2f
이진호 <tead7@naver.com>
2024-02-20 00:21:58 +0900

    Fix:오류 수정

--- src/layout/PageBody/Contain/Card/CardList/CardList.jsx
+++ src/layout/PageBody/Contain/Card/CardList/CardList.jsx
@@ -1,8 +1,35 @@
 import { NullCardList } from "../NullCardList/NullCardList";
 import "./CardList.css";
 import { CardOption } from "./Option";
+import { useLocation } from "react-router-dom";
 
 export const CardList = ({ folderId, children }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  if (currentPath === "/shared") {
+    return (
+      <>
+        <div className="CardList_header_contain"></div>
+        <div className="CardList">{children}</div>
+      </>
+    );
+  } else if (currentPath === "/folder") {
+    return (
+      <>
+        <div className="CardList_header_contain">
+          <div className="CardList_header_word">유용한 글</div>
+          <div>{folderId === null && <CardOption />}</div>
+        </div>
+        {folderId === null ? (
+          <div className="CardList">{children}</div>
+        ) : (
+          <NullCardList />
+        )}
+      </>
+    );
+  }
+
   return (
     <>
       <div className="CardList_header_contain">
--- src/layout/PageBody/Contain/Card/CardTagList/CardTagList.js
+++ src/layout/PageBody/Contain/Card/CardTagList/CardTagList.js
@@ -7,18 +7,15 @@ export const CardTagList = ({ children }) => {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 768) {
-        // 화면 너비가 768px보다 작으면
-        setIsFloating(true); // Floating Action Button으로 변경
+        setIsFloating(true);
       } else {
-        setIsFloating(false); // 그렇지 않으면 일반 버튼 유지
+        setIsFloating(false);
       }
     };
 
-    // 페이지 로드시와 화면 크기 변경시 handleResize 함수 호출
     window.addEventListener("resize", handleResize);
-    handleResize(); // 초기 로드시 한 번 호출
+    handleResize();
 
-    // Clean-up 함수 등록
     return () => window.removeEventListener("resize", handleResize);
   }, []); //
 
