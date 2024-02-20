3b74e7c2bda79df5a3efb2ac928794a54868ade9
JA_X <envijs93@gmail.com>
2024-02-20 20:09:45 +0900

    Feat: 팝오버에서 삭제하기 버튼 클릭 시 링크 삭제 구현 완료

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import { getFormattedDate, getTimeAgo } from '../../utils/date.js';
+import RemoveModal from './modal/RemoveModal.js';
 import noImage from '../../images/noImage.svg';
 import kebab from '../../images/kebab.svg';
 import emptyStar from '../../images/emptyStar.svg';
 import './Card.css';
 
 export default function Card({ cardData, selectedCardId, setSelectedCardId }) {
+  const [modals, setModals] = useState({
+    removeModal: false,
+  });
+
+  const toggleModal = modalName => {
+    setModals(prevModals => ({
+      ...prevModals,
+      [modalName]: !prevModals[modalName],
+    }));
+  };
+
+  const handleModalClose = (e, modalName) => {
+    e.preventDefault();
+    toggleModal(modalName);
+  };
+
   const handlePopOver = e => {
     e.preventDefault();
     setSelectedCardId(cardData.id);
@@ -27,7 +45,7 @@ export default function Card({ cardData, selectedCardId, setSelectedCardId }) {
           </button>
           {selectedCardId === cardData.id && (
             <div className="popOver">
-              <div>삭제하기</div>
+              <div onClick={e => handleModalClose(e, 'removeModal')}>삭제하기</div>
               <div>폴더에 추가</div>
             </div>
           )}
@@ -35,6 +53,13 @@ export default function Card({ cardData, selectedCardId, setSelectedCardId }) {
         <div className="cardDesc">{cardData.description || 'No Description'}</div>
         <div className="cardCreatedAt">{getFormattedDate(cardData.createdAt || cardData.created_at)}</div>
       </div>
+      {modals.removeModal && (
+        <RemoveModal
+          modalTitle="링크 삭제"
+          titleContent={cardData.url}
+          onClose={e => handleModalClose(e, 'removeModal')}
+        />
+      )}
     </a>
   );
 }
