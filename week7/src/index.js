40a22fbedca6b5b80e531dbaf2192210e45ed5fb
codingaring <sksms2104@naver.com>
2024-02-20 11:46:53 +0900

    [delete] 중복되고 불필요한 파일 삭제

--- week7/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SharedPage from "./page-layout/SharedPage/sharedPage";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SharedPage />);
--- week7/src/page-layout/SharedPage/sharedPage.js
+++
@@ -1,7 +0,0 @@
-function SharedPage(){
-  return (
-    <></>
-  );
-}
-
-export default SharedPage;
