4df8fd186c99b2bd17e335b81103f9820a63e7af
jinhok96 <jinhok96a@gmail.com>
2024-02-20 19:43:47 +0900

    chore: AddLInkButton 추가 및 AddLinkBar 크기 조정 	modified:   src/components/Common/AddLInkButton.module.css 	modified:   src/components/Header/AddLinkBar.js 	modified:   src/components/Header/AddLinkBar.module.css

--- src/components/Header/AddLinkBar.js
+++ src/components/Header/AddLinkBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 import styles from './AddLinkBar.module.css';
 import addLinkIcon from '../../assets/images/link.svg';
-// import AddLinkButton from '../Common/AddLInkButton';
+import AddLinkButton from '../Common/AddLInkButton';
 
 function AddLinkBar() {
   const [value, setValue] = useState('');
@@ -11,13 +11,14 @@ function AddLinkBar() {
     setValue(e.target.value);
   };
 
-  const containerClasses = classNames(styles.container, 'position-relative', 'width-full', 'margin-auto');
+  const addLinkClasses = classNames(styles['add-link-bar'], 'position-relative', 'width-full', 'margin-auto');
   const inputClasses = classNames(styles['add-link-bar-input'], 'background-white', 'text-color-gray60', 'width-full');
-  const inputImgClasses = classNames(styles['add-link-bar-icon'], 'position-absolute');
-  // const addLinkButtonClasses = classNames('position-absolute', styles['add-link-button']);
+  const inputImgClasses = classNames(styles['add-link-bar-icon']);
+  const addLinkButtonClasses = classNames('position-absolute', styles['add-link-bar-button']);
+  const containerClasses = classNames(styles['add-link-bar-container'], 'flex-row', 'position-absolute', 'width-full');
 
   return (
-    <div className={containerClasses}>
+    <div className={addLinkClasses}>
       <input
         className={inputClasses}
         type="text"
@@ -25,8 +26,10 @@ function AddLinkBar() {
         onChange={handleInputChange}
         placeholder="링크를 추가해 보세요"
       />
-      <img className={inputImgClasses} src={addLinkIcon} alt="addLinkIcon" />
-      {/* <AddLinkButton className={addLinkButtonClasses} /> */}
+      <div className={containerClasses}>
+        <img className={inputImgClasses} src={addLinkIcon} alt="addLinkIcon" />
+        <AddLinkButton className={addLinkButtonClasses} />
+      </div>
     </div>
   );
 }
