048e030341ce41db07e0f3e1cd0dcabfe6eeee4a
LeeSeungHoon <ysh73900@gmail.com>
2024-02-21 01:52:16 +0900

    fix: span -> button

--- src/components/Folder/FolderOptionMenu/FolderOptionMenu.js
+++ src/components/Folder/FolderOptionMenu/FolderOptionMenu.js
@@ -5,15 +5,15 @@ const FolderOptionMenu = () => {
     <div className="folderOption-options">
       <div className="folderOption-option">
         <div className="shareIcon"></div>
-        <span>공유</span>
+        <button>공유</button>
       </div>
       <div className="folderOption-option">
         <div className="nameChangeIcon"></div>
-        <span>이름 변경</span>
+        <button>이름 변경</button>
       </div>
       <div className="folderOption-option">
         <div className="deleteIcon"></div>
-        <span>삭제</span>
+        <button>삭제</button>
       </div>
     </div>
   );
