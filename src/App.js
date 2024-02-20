25dee97cf4d301cc1c6454410ab39b180b4af411
jinhok96 <jinhok96a@gmail.com>
2024-02-20 16:31:42 +0900

    chore: 페이지 주소 변경 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 import SharedPage from './pages/SharedPage/SharedPage';
+import FolderPage from './pages/FolderPage/FolderPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SharedPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="shared" element={<SharedPage />} />
+        <Route path="folder" element={<FolderPage />} />
       </Routes>
     </Router>
   );
