94f490aace6e6f6b332873ec1eb2a934634933aa
jinhok96 <jinhok96a@gmail.com>
2024-02-20 22:42:06 +0900

    chore: FAB에 className prop 추가 	modified:   src/components/Common/FlaotingActionButton.js

--- src/components/Common/FlaotingActionButton.js
+++ src/components/Common/FlaotingActionButton.js
@@ -2,8 +2,8 @@ import classNames from 'classnames';
 import styles from './FloatingActionButton.module.css';
 import Button from './Button';
 
-function FloatingActionButton({ text, onClick }) {
-  const buttonClasses = classNames(styles['floating-action-button'], 'text-color-gray10');
+function FloatingActionButton({ className, text, onClick }) {
+  const buttonClasses = classNames(styles['floating-action-button'], 'text-color-gray10', className);
   const button = <Button className={buttonClasses} text={text} onClick={onClick} />;
 
   return button;
