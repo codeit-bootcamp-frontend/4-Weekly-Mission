28eab80f9159787b699b55e4c3bf6289eef89d08
SeungAAA <SeungAAA@naver.com>
2024-02-20 12:02:11 +0900

    test only

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+//야
