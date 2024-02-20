3f305ab36085c845cbe2336f2bbb3adc10d29108
Taejin Kim <rlaxowls1316@naver.com>
2024-02-20 15:49:21 +0900

    feat: implement useHandleModalClick

---
+++ src/hooks/useHandleModalClick.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export default function useHandleModalClick(modalRef, handleModalClose) {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        handleModalClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [modalRef, handleModalClose]);
+}
