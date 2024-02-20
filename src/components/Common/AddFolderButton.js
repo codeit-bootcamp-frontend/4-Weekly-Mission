960b2b27463e184309a11977fe0d3531e9245c16
jinhok96 <jinhok96a@gmail.com>
2024-02-20 22:53:07 +0900

    chore: AddFolderButton 스타일 추가 	modified:   src/assets/styles/colors.css 	modified:   src/components/Common/AddFolderButton.js 	modified:   src/components/Common/AddFolderButton.module.css

--- src/components/Common/AddFolderButton.js
+++ src/components/Common/AddFolderButton.js
@@ -1,12 +1,15 @@
 import classNames from 'classnames';
 import styles from './AddFolderButton.module.css';
 import Button from './Button';
+import AddIcon from '../../assets/images/add.svg';
 
-function AddFolderButton({ className, text, onClick }) {
-  const buttonClasses = classNames(styles['add-folder-button'], className);
+function AddFolderButton({ className, onClick }) {
+  const buttonClasses = classNames(styles['add-folder-button'], 'text-color-primary', className);
+  const iconClasses = classNames(styles['add-folder-icon']);
   const button = (
     <Button className={buttonClasses} onClick={onClick}>
-      {text}
+      폴더 추가
+      <img className={iconClasses} src={AddIcon} alt="AddIcon" />
     </Button>
   );
 
