db8577f2ada98d7a806ee64c36e50d7ea0e5af09
JA_X <envijs93@gmail.com>
2024-02-21 00:32:04 +0900

    Feat: addLinkModal 구현 완료

--- src/components/AddLink.js
+++ src/components/AddLink.js
@@ -1,21 +1,27 @@
 import { useState } from 'react';
+import AddLinkModal from './common/modal/AddLinkModal';
 import Button from './common/Button';
 import link from '../images/link.svg';
 import './AddLink.css';
 
-export default function AddLink() {
+export default function AddLink({ folderListData }) {
   const [showModal, setShowModal] = useState(false);
+  const [newLinkUrl, setNewLinkUrl] = useState('');
   const toggleShowModal = () => setShowModal(!showModal);
+  const folderNameAndLinkList = folderListData?.map(({ name, link, id }) => [name, link.count, id]);
 
   return (
     <div className="addLinkContainer">
       <div className="addLink">
         <div className="linkInput">
           <img src={link} alt="링크" />
-          <input placeholder="링크를 추가해 보세요" />
+          <input value={newLinkUrl} placeholder="링크를 추가해 보세요" onChange={e => setNewLinkUrl(e.target.value)} />
         </div>
         <Button onClick={toggleShowModal}>추가하기</Button>
       </div>
+      {showModal && (
+        <AddLinkModal folderNameAndLinkList={folderNameAndLinkList} onClose={toggleShowModal} linkUrl={newLinkUrl} />
+      )}
     </div>
   );
 }
