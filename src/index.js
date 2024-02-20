e39a2a7423f1cc4a8ea91a44b51db3fcd61490c6
parkwoohyeok <dngur3111@naver.com>
2024-02-20 10:55:37 +0900

    Bug fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
