2aaef664276b1861de447854aec72df5787dcc11
김동현 <dtc03003@naver.com>
2024-02-20 23:20:25 +0900

    style: App 파일 위치 변경

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Banner /> */}
+      <Card />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/App.js";
+import App from "./App.jsx";
 import "./reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
