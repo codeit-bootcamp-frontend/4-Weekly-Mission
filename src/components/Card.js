8284e389759ca0d36888bcaa6de6428f6fc1ce62
김동현 <dtc03003@naver.com>
2024-02-20 23:18:06 +0900

    style: 커스텀훅 분리

---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import "./Card.css";
+import searchImg from "../assets/svg/search.svg";
+import CardList from "./CardList.js";
+import { getFolders } from "../api/api.js";
+import { useEffect, useState } from "react";
+
+function Card() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const handleLoad = async () => {
+      try {
+        const result = await getFolders();
+        const { folder } = result;
+        setItems(folder.links);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    handleLoad();
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card_inner">
+        <form className="card_search_bar">
+          <div className="search">
+            <img src={searchImg} alt="" />
+            <p>링크를 검색해 보세요.</p>
+          </div>
+        </form>
+        <CardList items={items} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
