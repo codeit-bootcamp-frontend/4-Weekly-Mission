7c78eb0b7ea1c5ab10143666cd6fe83fe5181cda
beom <gb3187@naver.com>
2024-02-21 15:52:11 +0900

    week 8 - SeachBar 컴포넌트 화

--- src/components/FolderSection.js
+++ src/components/FolderSection.js
@@ -1,20 +1,12 @@
 import "./FolderSection.css";
-import searchIcon from "../assets/Search.svg";
+import SearchBar from "./SearchBar";
 
 export default function FolderSection() {
   return (
     <div className="FolderSection">
       <div className="FolderSection-Frame">
-        <div className="searchBar">
-          <div className="searchBarElement">
-            <img id="searchIcon" src={searchIcon} alt="검색 아이콘" />
-            <input
-              type="text"
-              id="inputText"
-              placeholder="링크를 검색해보세요."
-            />
-          </div>
-        </div>
+        <SearchBar />
+        <div className="FolderBtnList"></div>
       </div>
     </div>
   );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import "./SearchBar.css";
+import searchIcon from "../assets/Search.svg";
+
+export default function SearchBar() {
+  return (
+    <div className="searchBar">
+      <div className="searchBarElement">
+        <img id="searchIcon" src={searchIcon} alt="검색 아이콘" />
+        <input type="text" id="inputText" placeholder="링크를 검색해보세요." />
+      </div>
+    </div>
+  );
+}
