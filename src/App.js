8d79cecba5916ea59a799c3b12879ed0c492ad42
codingaring <sksms2104@naver.com>
2024-02-21 17:01:15 +0900

    ✨[feat] 루트 페이지에서 다른 경로로 이동하는 버튼 생성

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { SearchBar } from "./ui/SearchBar";
 import { CardList } from "./ui/CardList";
 import { useGetFolder } from "data-access/useGetFolder";
 import { ReadOnlyCard } from "ui/ReadOnlyCard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { FolderPage } from "page-layout/FolderPage/FolderPage";
 import { AddLinkBar } from "ui/AddLinkBar/AddLinkBar";
-import { SharedPossibleCard } from "ui/SharedPossibleCard";
 import { getFolders } from "data-access/getFolders";
 import { useEffect, useState } from "react";
 import { FolderContent } from "ui/FolderContent/FolderContent";
+import { Button } from "./StyledComponents/Buttons";
 
 function App() {
   const [folders, setFolders] = useState();
@@ -33,6 +33,21 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Link to="/shared">
+                  {" "}
+                  <Button>shared</Button>
+                </Link>
+                <Link to="/folder">
+                  {" "}
+                  <Button>folder</Button>
+                </Link>
+              </div>
+            }
+          />
           <Route
             path="/shared"
             element={
