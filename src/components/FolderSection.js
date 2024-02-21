3fa505de4fd3e34554878980d6edbedebb4e2692
beom <gb3187@naver.com>
2024-02-21 16:37:36 +0900

    week 8 - FolderBtnList 삭제및 재 구성

--- src/components/FolderSection.js
+++ src/components/FolderSection.js
@@ -1,13 +1,32 @@
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
-import FolderBtnList from "./FolderBtnList";
+import addImg from "../assets/add.svg";
 import "./FolderSection.css";
 
 export default function FolderSection() {
+  const [folderName, setFolderName] = useState("");
+  const folderNameClick = (e) => {
+    setFolderName(e.target.textContent);
+  };
   return (
     <div className="FolderSection">
       <div className="FolderSection-Frame">
         <SearchBar />
-        <FolderBtnList />
+        <div className="FolderBtnList">
+          <div className="FolderBtn">
+            <button onClick={folderNameClick}>전체</button>
+            <button onClick={folderNameClick}>⭐️ 즐겨찾기</button>
+            <button onClick={folderNameClick}>코딩 팁</button>
+            <button onClick={folderNameClick}>채용 사이트</button>
+            <button onClick={folderNameClick}>유용한 글</button>
+            <button onClick={folderNameClick}>나만의 장소</button>
+          </div>
+          <div className="AddFolder">
+            <span>폴더 추가</span>
+            <img src={addImg} alt="폴더 추가 버튼" />
+          </div>
+        </div>
+        <div className=""></div>
       </div>
     </div>
   );
