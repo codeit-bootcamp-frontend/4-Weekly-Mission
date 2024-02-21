6df9c102e08f6eb2f12372c6b8c284260d0dab9a
JA_X <envijs93@gmail.com>
2024-02-21 13:03:24 +0900

    Refactor: Card 컴포넌트에서 a 태그 내부에 modal 컴포넌트 위치 시 이벤트 전파 처리 문제가 발생하여 모달 컴포넌트 뎁스를 a 태그와 같은 선상에 놓는걸로 수정하여 해결

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -30,31 +30,33 @@ export default function Card({ cardData, selectedCardId, setSelectedCardId, fold
     setSelectedCardId(cardData.id);
   };
   return (
-    <a className="cardBox" href={cardData.url} target="_blank" rel="noopener noreferrer">
-      <div className="cardThumbnailContainer">
-        <img
-          className="cardThumbnail"
-          src={cardData.imageSource || cardData.image_source || noImage}
-          alt="카드썸네일"
-        />
-      </div>
-      <img className="cardStar" src={emptyStar} alt="빈 별" />
-      <div className="cardDataArea">
-        <div className="cardTimeAgo">
-          <p>{getTimeAgo(cardData.createdAt || cardData.created_at)}</p>
-          <button onClick={handlePopOver}>
-            <img src={kebab} alt="케밥" />
-          </button>
-          {selectedCardId === cardData.id && (
-            <div className="popOver">
-              <div onClick={e => handleModalClose(e, 'removeModal')}>삭제하기</div>
-              <div onClick={e => handleModalClose(e, 'addLinkModal')}>폴더에 추가</div>
-            </div>
-          )}
+    <>
+      <a className="cardBox" href={cardData.url} target="_blank" rel="noopener noreferrer">
+        <div className="cardThumbnailContainer">
+          <img
+            className="cardThumbnail"
+            src={cardData.imageSource || cardData.image_source || noImage}
+            alt="카드썸네일"
+          />
         </div>
-        <div className="cardDesc">{cardData.description || 'No Description'}</div>
-        <div className="cardCreatedAt">{getFormattedDate(cardData.createdAt || cardData.created_at)}</div>
-      </div>
+        <img className="cardStar" src={emptyStar} alt="빈 별" />
+        <div className="cardDataArea">
+          <div className="cardTimeAgo">
+            <p>{getTimeAgo(cardData.createdAt || cardData.created_at)}</p>
+            <button onClick={handlePopOver}>
+              <img src={kebab} alt="케밥" />
+            </button>
+            {selectedCardId === cardData.id && (
+              <div className="popOver">
+                <div onClick={e => handleModalClose(e, 'removeModal')}>삭제하기</div>
+                <div onClick={e => handleModalClose(e, 'addLinkModal')}>폴더에 추가</div>
+              </div>
+            )}
+          </div>
+          <div className="cardDesc">{cardData.description || 'No Description'}</div>
+          <div className="cardCreatedAt">{getFormattedDate(cardData.createdAt || cardData.created_at)}</div>
+        </div>
+      </a>
       {modals.removeModal && (
         <RemoveModal
           modalTitle="링크 삭제"
@@ -69,6 +71,6 @@ export default function Card({ cardData, selectedCardId, setSelectedCardId, fold
           onClose={e => handleModalClose(e, 'addLinkModal')}
         />
       )}
-    </a>
+    </>
   );
 }
