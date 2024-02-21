729e821e1e2dc063ae8573fb3db320307d7e9b94
beom <gb3187@naver.com>
2024-02-21 16:53:57 +0900

    week 8 - 공유 이름 변경 삭제 아이콘바 생성

--- src/components/FolderSection.js
+++ src/components/FolderSection.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import addImg from "../assets/add.svg";
+import shareIcon from "../assets/share.svg";
+import penIcon from "../assets/pen.svg";
+import deleteIcon from "../assets/delete.svg";
 import "./FolderSection.css";
 
 export default function FolderSection() {
@@ -26,7 +29,17 @@ export default function FolderSection() {
             <img src={addImg} alt="폴더 추가 버튼" />
           </div>
         </div>
-        <div className=""></div>
+        <div className="FolderNameBar">
+          <span>{folderName}</span>
+          <div className="OptionIcon">
+            <img src={shareIcon} alt="공유 아이콘" />
+            <span>공유</span>
+            <img src={penIcon} alt="이름 변경 아이콘" />
+            <span>이름 변경</span>
+            <img src={deleteIcon} alt="삭제 아이콘" />
+            <span>삭제</span>
+          </div>
+        </div>
       </div>
     </div>
   );
