cbe37735132bae53d2f14461b0b50fb1139a8f94
whai2 <whai2125@gmail.com>
2024-02-21 16:47:58 +0900

    style: 프리티어, eslint js 파일 적용

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+        jest: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
