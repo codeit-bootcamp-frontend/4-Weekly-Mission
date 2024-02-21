ddbb9f12135e0a59f0677fbf56cf47f15090b299
LeeSeungHoon <ysh73900@gmail.com>
2024-02-21 20:19:01 +0900

    feat: 파일을 내보내기 위한 index.js 추가

---
+++ src/components/Folder/index.js
@@ -0,0 +1,4 @@
+export { FolderAdd } from "./FolderAdd";
+export { FolderHeader } from "./FolderHeader";
+export { FolderList } from "./FolderList";
+export { FolderOption } from "./FolderOption/FolderOption";
