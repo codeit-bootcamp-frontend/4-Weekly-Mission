74b4bc299e4bfb2533b1e162ad705d7ee807ac6c
jinhok96 <jinhok96a@gmail.com>
2024-02-23 04:32:47 +0900

    chore: api 호출 관련 error, loading 로직 개선 	modified:   src/hooks/useFetch.js 	modified:   src/services/api.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,43 @@
-async function useFetch(apiUrl) {
+import { useEffect, useState } from 'react';
+
+function useFetch(apiUrl) {
   const API_URL = process.env.REACT_APP_API_URL;
 
-  const url = new URL(apiUrl, API_URL);
+  // data, error 초기값은 비어있음, loading 초기값은 로딩 중인 상태
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const response = await fetch(url);
-  const result = await response.json();
-  const responseError = result?.error ?? 'Fetch response failed';
+  // apiUrl 또는 API_URL이 변경될 경우 실행
+  useEffect(() => {
+    // apiUrl, API_URL이 변경될 경우 이를 정확히 업데이트하기 위해 fetchData 함수를 useEffect 내부에서 선언
+    const fetchData = async () => {
+      try {
+        const url = new URL(apiUrl, API_URL);
+        const response = await fetch(url);
+        const result = await response.json();
 
-  if (!response.ok) {
-    throw new Error(`User response error: ${responseError}`);
-  }
+        const responseError = result?.error ?? 'Fetch response failed';
 
-  console.log(result);
+        if (!response.ok) {
+          throw new Error(`User response error: ${responseError}`);
+        }
 
-  return result;
+        setData(result);
+      } catch (errorData) {
+        const errorString = errorData.toString();
+        setError(errorString);
+      } finally {
+        setLoading(false); // 로딩 끝
+      }
+    };
+
+    // 함수 호출
+    fetchData();
+  }, [apiUrl, API_URL]);
+
+  // 객체 반환
+  return { data, loading, error };
 }
 
 export default useFetch;
--- src/services/api.js
+++ src/services/api.js
@@ -2,17 +2,23 @@ import useFetch from 'hooks/useFetch';
 
 export async function getUserInfo() {
   const url = 'users/1';
-  return useFetch(url);
+
+  const { data, loading, error } = useFetch(url);
+  return { data, loading, error };
 }
 
 // SharedPage에서 사용
 export async function getFoldersInfo() {
   const url = 'sample/folder';
-  return useFetch(url);
+
+  const { data, loading, error } = useFetch(url);
+  return { data, loading, error };
 }
 
 // 파라미터(id)가 있을 경우 users/1/links?folderId={해당 폴더 ID}로 확장되도록 변경할 것
 export async function getLinksInfo() {
   const url = `users/1/links`;
-  return useFetch(url);
+
+  const { data, loading, error } = useFetch(url);
+  return { data, loading, error };
 }
