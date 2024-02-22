e1b4b26a07a736d40885ded6cf9d652954ef2439
hongjaehoun <ehswkfn123@naver.com>
2024-02-22 13:47:31 +0900

    refactor : footer 부분 소셜 아이코 데이터 컴포넌트 안에서 관리

--- src/components/footer/FooterSocialLinkBox.js
+++ src/components/footer/FooterSocialLinkBox.js
@@ -1,21 +1,45 @@
 import styles from "./FooterSocialLinkBox.module.css";
 import FooterSocialLinkItem from "./FooterSocialLinkItem";
-import data from "./SocialNetworkServicesData.json";
 function FooterSocialLinkBox() {
-  const SocialNetworkServiceName = data.socialNetworkServices;
+  const socialNetworkServiceName = {
+    socialNetworkServices: [
+      {
+        socialServiceName: "facebook",
+        socialUrl: "https://www.facebook.com/",
+        id: 0,
+      },
+      {
+        socialServiceName: "twitter",
+        socialUrl: "https://www.twitter.com/",
+        id: 1,
+      },
+      {
+        socialServiceName: "youtube",
+        socialUrl: "https://www.youtube.com/",
+        id: 2,
+      },
+      {
+        socialServiceName: "instagram",
+        socialUrl: "https://www.instagram.com/",
+        id: 3,
+      },
+    ],
+  };
 
   return (
     <ul className={styles.social_icon_wrapper}>
-      {SocialNetworkServiceName.map(({ socialServiceName, socialUrl, id }) => {
-        return (
-          <FooterSocialLinkItem
-            socialNetworkServiceName={socialServiceName}
-            socialUrl={socialUrl}
-            key={id}
-            altImageName={socialServiceName}
-          />
-        );
-      })}
+      {socialNetworkServiceName.socialNetworkServices.map(
+        ({ socialServiceName, socialUrl, id }) => {
+          return (
+            <FooterSocialLinkItem
+              socialNetworkServiceName={socialServiceName}
+              socialUrl={socialUrl}
+              key={id}
+              altImageName={socialServiceName}
+            />
+          );
+        }
+      )}
     </ul>
   );
 }
