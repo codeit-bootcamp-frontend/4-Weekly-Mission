298e918d1a961d5d9631966318879037d43ca1fb
Kim Min Jeong <minjeong9919@naver.com>
2024-02-23 02:10:23 +0900

    Design: 카드 컴포넌트 styled-components 적용화 및 기본이미지 적용

--- src/components/common/FolderItem.js
+++ src/components/common/FolderItem.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { CalcTime } from "./../../calculator";
+import logo from "../../assets/icons/logo.png";
 import { ReactComponent as Star } from "../../assets/icons/card_star.svg";
 import { ReactComponent as Kebab } from "../../assets/icons/kebab.svg";
 import "../../style/shared.css";
@@ -32,28 +33,24 @@ function FolderItem({ item }) {
 
   return (
     <a href={url} target="_blank" rel="noreferrer">
-      <div
-        className="folder"
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        <div className="imgBox">
+      <Folder onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <ImageContainer>
           {img_src ? (
-            <div className="folderImageBox">
-              <img
-                src={img_src}
-                alt={id}
-                className={`folderImage ${isHovering ? "grow" : "folder-img"}`}
-              ></img>
-              <Star className="star" fill={favorite ? "purple" : "black"} />
-            </div>
+            <img
+              src={img_src}
+              alt={id}
+              className={`folderImage ${isHovering ? "grow" : "folder-img"}`}
+            ></img>
           ) : (
-            <p>no image</p>
+            <DefaultImage>
+              <img src={logo} alt="logo" />
+            </DefaultImage>
           )}
-        </div>
-        <div className="folder-textBox">
-          <div className="time-kebab">
-            <p id="time">{time}</p>
+          <Star className="star" fill={favorite ? "purple" : "black"} />
+        </ImageContainer>
+        <TextBox>
+          <TimeContainer>
+            <TimeText>{time}</TimeText>
             <Kebab
               onClick={(e) => {
                 e.preventDefault();
@@ -65,14 +62,97 @@ function FolderItem({ item }) {
               <Option>삭제하기</Option>
               <Option>폴더에 추가</Option>
             </MenuOptions>
-          </div>
-          <p id="info">{description}</p>
-          <p id="date">2023. 3. 15</p>
-        </div>
-      </div>
+          </TimeContainer>
+          <Description>{description}</Description>
+          <DateText>2023. 3. 15</DateText>
+        </TextBox>
+      </Folder>
     </a>
   );
 }
+
+const Folder = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 15px;
+  box-shadow: 0px 5px 25px 0px rgba(0, 0, 0, 0.08);
+  position: relative;
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 230px;
+  overflow: hidden;
+  margin: 0px;
+  border-radius: 15px 15px 0px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DefaultImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #dddfff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextBox = styled.div`
+  width: 100%;
+  height: 135px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 15px 20px;
+  border-radius: 0px 0px 15px 15px;
+`;
+
+const Description = styled.div`
+  width: 100%;
+  height: 49px;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  margin: 0px;
+  color: #000;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+`;
+
+const TimeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  position: relative;
+`;
+
+const TimeText = styled.text`
+  font-size: 13px;
+  font-weight: 400;
+  color: #666;
+  margin: 0px;
+`;
+
+const DateText = styled.text`
+  overflow: hidden;
+  color: #333;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin: 0px;
+`;
 
 const MenuOptions = styled.div`
   width: 100px;
