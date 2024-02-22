7fc0283f55ed99d4796a6a12e999411573164724
minbo <smb5174@naver.com>
2024-02-22 16:45:12 +0900

    feat: 케밥 버튼을 누르면 팝오버 기능 추가

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -4,36 +4,51 @@ import kebab from "../images/kebab.svg";
 import emptyStar from "../images/emptyStar.svg";
 import "./Card.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Card({ data }) {
+  const [showPopover, setShowPopover] = useState(false);
+
+  const togglePopover = () => {
+    setShowPopover(!showPopover);
+  };
+
   function handleImgError(e) {
     e.target.src = noImage;
   }
 
   return (
-    <Link
-      to={data.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="cardBox"
-    >
-      <div className="cardImgBox">
+    <div className="cardBox">
+      <Link
+        to={data.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="cardImgBox"
+      >
         <img
           className="cardImg"
           src={data.image_source || noImage}
           alt="카드썸네일"
           onError={handleImgError}
         />
-      </div>
+      </Link>
       <img className="cardStar" src={emptyStar} alt="빈 별" />
       <div className="cardData">
         <div className="cardTimeAgo">
           <p>{getTimeAgo(data.created_at)}</p>
-          <img src={kebab} alt="케밥" />
+          <div className="kebab-popover">
+            <img onClick={togglePopover} src={kebab} alt="케밥" />
+            {showPopover && (
+              <div className="popover">
+                <div className="popover-delete">삭제하기</div>
+                <div className="popover-folder-add">폴더에 추가</div>
+              </div>
+            )}
+          </div>
         </div>
         <div className="cardInfo">{data.description || "No Description"}</div>
         <div className="cardCreatedAt">{getFormattedDate(data.created_at)}</div>
       </div>
-    </Link>
+    </div>
   );
 }
