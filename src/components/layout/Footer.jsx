d1ac1dca3f9847a821ee114fa91a86785ba6a5e6
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-23 02:31:43 +0900

    Refactor: footer 아이콘 영역 수정
    아이콘 컴포넌트 이름을 범용적으로 변경
    아이콘 이미지를 props로 넘겨준다

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,22 +1,30 @@
 import '../../styles/Footer.css';
-import SnsItem from './footer/SnsItem';
+import IconWithLink from './footer/IconWithLink';
+import facebookIcon from '../../assets/facebook.svg';
+import twitterIcon from '../../assets/twitter.svg';
+import youtubeIcon from '../../assets/youtube.svg';
+import instagramIcon from '../../assets/instagram.svg';
 
 const SNS_ITEMS = [
   {
     name: 'facebook',
     address: 'https://www.facebook.com/',
+    imgUrl: facebookIcon,
   },
   {
     name: 'twitter',
     address: 'https://twitter.com/',
+    imgUrl: twitterIcon,
   },
   {
     name: 'youtube',
     address: 'https://www.youtube.com/',
+    imgUrl: youtubeIcon,
   },
   {
     name: 'instagram',
     address: 'https://www.instagram.com/',
+    imgUrl: instagramIcon,
   },
 ];
 
@@ -37,13 +45,13 @@ function Footer() {
       <div className="footer-group">
         <span className="copyright">©codeit - 2023</span>
         <ul className="link-list">
-          {LINK_ITEMS.map(item => (
-            <SnsItem key={item.name} name={item.name} address={item.address} />
+          {LINK_ITEMS.map(({ name, address }) => (
+            <IconWithLink key={name} name={name} address={address} />
           ))}
         </ul>
         <ul className="sns-list">
-          {SNS_ITEMS.map(item => (
-            <SnsItem key={item.name} name={item.name} address={item.address} imgSrc={item.imgSrc} />
+          {SNS_ITEMS.map(({ name, address, imgUrl }) => (
+            <IconWithLink key={name} name={name} address={address} imgUrl={imgUrl} />
           ))}
         </ul>
       </div>
---
+++ src/components/layout/footer/IconWithLink.jsx
@@ -0,0 +1,14 @@
+const IconWithLink = ({ name, address, imgUrl }) => {
+  const target = /^http/.test(address) ? '_blank' : '_self';
+  const rel = target === '_blank' ? 'noopener noreferrer' : '';
+
+  return (
+    <li>
+      <a href={address} target={target} rel={rel} className="item">
+        {imgUrl ? <img src={imgUrl} alt={name} /> : `${name}`}
+      </a>
+    </li>
+  );
+};
+
+export default IconWithLink;
