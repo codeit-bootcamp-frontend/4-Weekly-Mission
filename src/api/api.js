bc77654c78d05115fae8946329902faa3b887132
minbo <smb5174@naver.com>
2024-02-22 11:21:25 +0900

    refactor: api 호출시 에러 처리 부분 구현

--- src/api/api.js
+++ src/api/api.js
@@ -1,26 +1,38 @@
 const BASE_URL = "https://bootcamp-api.codeit.kr/api/";
 
 export const profileDataFetch = async () => {
-  const response = await fetch(`${BASE_URL}users/1`);
-  const { data } = await response.json();
-  return data[0];
+  try {
+    const response = await fetch(`${BASE_URL}users/1`);
+    const { data } = await response.json();
+    return data[0];
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const folderListDataFetch = async () => {
-  const response = await fetch(`${BASE_URL}users/1/folders`);
-  const { data } = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${BASE_URL}users/1/folders`);
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const selectedFolderDataFetch = async (folderData) => {
-  let response;
-  if (folderData.id) {
-    response = await fetch(
-      `${BASE_URL}users/1/links?folderId=${folderData.id}`
-    );
-  } else {
-    response = await fetch(`${BASE_URL}users/1/links`);
+  try {
+    let response;
+    if (folderData.id) {
+      response = await fetch(
+        `${BASE_URL}users/1/links?folderId=${folderData.id}`
+      );
+    } else {
+      response = await fetch(`${BASE_URL}users/1/links`);
+    }
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
   }
-  const { data } = await response.json();
-  return data;
 };
