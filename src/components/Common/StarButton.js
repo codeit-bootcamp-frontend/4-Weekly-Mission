e8706b5fa4a70c1dc3ea2e026d07c6daecf6ebf8
jinhok96 <jinhok96a@gmail.com>
2024-02-22 22:45:49 +0900

    chore: StarButton 컴포넌트 생성 	modified:   src/components/Common/KebabButton.js 	new file:   src/components/Common/StarButton.js 	new file:   src/components/Common/StarButton.module.css

--- src/components/Common/StarButton.js
+++ src/components/Common/StarButton.js
@@ -1,30 +1,30 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-import KebabImg from 'assets/images/kebab.svg';
+import StarImg from 'assets/images/star.svg';
 
 import Button from 'components/Common/Button';
 import styles from 'components/Common/KebabButton.module.css';
 
-function KebabButton({ className, onClick }) {
-  const buttonClasses = classNames(styles['kebab-button'], 'background-none', 'border-none', className);
+function StarButton({ className, onClick }) {
+  const buttonClasses = classNames(styles['star-button'], 'background-none', 'border-none', className);
   const buttonImageClasses = classNames('width-full');
 
   return (
     <Button className={buttonClasses} onClick={onClick}>
-      <img className={buttonImageClasses} src={KebabImg} alt="KebabIcon" />
+      <img className={buttonImageClasses} src={StarImg} alt="StarIcon" />
     </Button>
   );
 }
 
-KebabButton.propTypes = {
+StarButton.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func,
 };
 
-KebabButton.defaultProps = {
+StarButton.defaultProps = {
   className: '',
   onClick: null,
 };
 
-export default KebabButton;
+export default StarButton;
