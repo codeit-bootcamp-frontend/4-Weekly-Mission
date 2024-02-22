47af108c55dd6826414d1f8d7cd129c08844bd2e
jinhok96 <jinhok96a@gmail.com>
2024-02-22 19:07:14 +0900

    chore: API_URL을 함수 내부로 이동 	modified:   src/hooks/useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL;
-
 async function useFetch(apiUrl) {
+  const API_URL = process.env.REACT_APP_API_URL;
+
   const url = new URL(apiUrl, API_URL);
 
   const response = await fetch(url);
