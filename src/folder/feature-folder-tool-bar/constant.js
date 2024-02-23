75bb8f71af65a257670ec3ec2ca7d73330784e69
yubin <yb3143@gmail.com>
2024-02-23 15:04:04 +0900

    Add constant modalText

--- src/folder/feature-folder-tool-bar/constant.js
+++ src/folder/feature-folder-tool-bar/constant.js
@@ -2,14 +2,17 @@ export const BUTTONS = [
   {
     iconSource: "images/share.svg",
     text: "공유",
+    modalText: "공유하기",
   },
   {
     iconSource: "images/pen.svg",
     text: "이름 변경",
+    modalText: "이름 변경하기",
   },
   {
     iconSource: "images/trash.svg",
     text: "삭제",
+    modalText: "삭제하기",
   },
 ];
 
