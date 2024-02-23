e06e8f6f3b63f5968705629b222d8975e8edad8c
Rhajiit <ksd9638@gmail.com>
2024-02-23 20:22:08 +0900

    Refactor: LinkAddBar에 form을 적용하여 엔터로도 submit이 되도록 수정

--- src/components/FolderLinkAddBar.js
+++ src/components/FolderLinkAddBar.js
@@ -3,11 +3,11 @@ import "components/FolderLinkAddBar.css";
 export default function FolderLinkAddBar() {
 	return (
 		<div className="background-decoration-color">
-			<div className="input-position-origin link-add">
+			<form className="input-position-origin link-add">
 				<label htmlFor="add-link" />
 				<input id="add-link" placeholder="링크를 추가해 보세요." />
 				<button className="link-add-btn default-btn">추가하기</button>
-			</div>
+			</form>
 		</div>
 	);
 }
