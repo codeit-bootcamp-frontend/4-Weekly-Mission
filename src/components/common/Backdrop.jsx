78f1a7c2a7ff20b138aa87d4b49d55babb72f8f3
Jules <dali1999@naver.com>
2024-02-23 12:21:04 +0900

    Co-authored-by: D5ng <D5ng@users.noreply.github.com> Co-authored-by: hanjinhe <hanjinhe@users.noreply.github.com>

---
+++ src/components/common/Backdrop.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+function Backdrop({isClose }) {
+  return <Container onClick={isClose}>backdrop</Container>;
+}
+
+export default Backdrop;
--- src/components/folder/FoderOptionMenu.jsx
+++ src/components/folder/FoderOptionMenu.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import shareIcon from "../../assets/svg/share.svg";
 import editIcon from "../../assets/svg/edit.svg";
 import deleteIcon from "../../assets/svg/trash.svg";
+import Modal, { ModalWithInput } from "../common/Modal";
+import { createPortal } from "react-dom";
+import Backdrop from "../common/Backdrop";
 
 const Container = styled.ul`
   display: flex;
   gap: 15px;
   align-items: center;
 
-  a {
+  button {
     display: flex;
     gap: 3px;
     text-decoration: none;
     color: var(--gray2);
+    background-color: white;
+    border: none;
+    cursor: pointer;
 
     img {
       width: 17px;
@@ -30,30 +36,57 @@ const MENU_LIST = [
     title: "공유",
     url: "/",
     icon: shareIcon,
+    modal: "share",
   },
   {
     title: "이름 변경",
     url: "/",
     icon: editIcon,
+    modal: "edit",
   },
   {
     title: "삭제",
     url: "/",
     icon: deleteIcon,
+    modal: "delete",
   },
 ];
 
 function FoderOptionMenu() {
+  const [active, setActive] = useState(false);
+
+  const openModal = () => setActive(true);
+
+  const closeModal = () => setActive(false);
+
+  const ModalHOC = ModalWithInput(Modal);
+
+  const modal = createPortal(
+    <ModalHOC isClose={closeModal} />,
+    // <Modal isClose={closeModal} />,
+    document.getElementById("modal")
+  );
+
+  const backdrop = createPortal(
+    <Backdrop isClose={closeModal} />,
+    document.getElementById("backdrop")
+  );
+
+  // this
+  // Function.prototype.call
+  // Function.prototype.apply
+  // Function.prototype.bind
+
   return (
     <Container>
       {MENU_LIST.map((list) => (
-        <li key={list.title}>
-          <a href={list.url}>
-            <img src={list.icon} alt={list.title} />
-            <span>{list.title}</span>
-          </a>
-        </li>
+        <button key={list.title} onClick={openModal.bind(this, list)}>
+          <img src={list.icon} alt={list.title} />
+          <span>{list.title}</span>
+        </button>
       ))}
+      {active && modal}
+      {active && backdrop}
     </Container>
   );
 }
