fe20f6e86ec6b19f67f962c1f0927b316c497064
codingaring <sksms2104@naver.com>
2024-02-23 11:54:25 +0900

    ♻️  refactor : ElapsedTime에 moment.js 사용

--- src/util/constant.js
+++ src/util/constant.js
@@ -4,19 +4,3 @@ export const ROUTE = {
   개인정보처리방침: "/privacy",
   FAQ: "/faq",
 };
-
-const SECOND = 1000;
-const MINUTE = SECOND * 60;
-const HOUR = MINUTE * 60;
-const DAY = HOUR * 24;
-const MONT = DAY * 31;
-const YEAR = MONT * 12;
-
-export const TIME_IN_MILLISECONDS = {
-  SECOND,
-  MINUTE,
-  HOUR,
-  DAY,
-  MONT,
-  YEAR,
-};
