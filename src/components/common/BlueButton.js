334fe559df55fedd79a5139751b03b6251f71e2c
Kim Min Jeong <minjeong9919@naver.com>
2024-02-23 14:54:15 +0900

    Refactor: 버튼 props 추가 수정

--- src/components/common/BlueButton.js
+++ src/components/common/BlueButton.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { styled } from "styled-components";
+import { COLORS } from "../../constants/colors";
 
-export const BlueButton = ({ width, margin, padding, text }) => {
+export const BlueButton = ({
+  width,
+  heigth,
+  margin,
+  padding,
+  text,
+  fontSize,
+  radius,
+}) => {
   return (
-    <Button width={width} margin={margin} padding={padding}>
+    <Button
+      width={width}
+      margin={margin}
+      padding={padding}
+      color={COLORS.White}
+      fontSize={fontSize}
+      radius={radius}
+    >
       {text}
     </Button>
   );
@@ -12,16 +28,16 @@ export const BlueButton = ({ width, margin, padding, text }) => {
 const Button = styled.button`
   display: block;
   width: ${({ width }) => (width ? width : "auto")}px;
-  height: auto;
+  height: ${({ height }) => (height ? height : "auto")}px;
   margin: ${({ margin }) => (margin ? margin : "auto")};
   padding: ${({ padding }) => (padding ? padding : "auto")};
   border: 0px;
-  border-radius: 8px;
+  border-radius: ${({ radius }) => radius || radius}px;
   background: linear-gradient(91deg, #6d6afe 0.12%, #6ae3fe 101.84%);
   cursor: pointer;
 
-  font-size: 14px;
+  font-size: ${({ fontSize }) => fontSize || 14}px;
   font-weight: 600;
   line-height: 21.6px;
-  color: #f5f5f5;
+  color: ${({ color }) => color};
 `;
