17afc505d73f7a7fcfc10d3cf18feb54f4089ece
Kim Min Jeong <minjeong9919@naver.com>
2024-02-23 15:21:50 +0900

    Refactor: 절대 경로로 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
-import HeaderElement from "./../components/common/HeaderElement";
+import HeaderElement from "components/common/HeaderElement";
 import "../styles/home.css";
-import { Headline } from "./../components/home/Headline";
+import { Headline } from "components/home/Headline";
 
 export const Home = () => {
   return (
     <>
       <HeaderElement></HeaderElement>
       <Headline />
+      <div className="cards-img">cards_img</div>
     </>
   );
 };
