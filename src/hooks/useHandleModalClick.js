d6cd2e2141680cd985f27c03668c72ec543f0e6b
LeeSeungHoon <ysh73900@gmail.com>
2024-02-23 17:43:42 +0900

    feat: 모달클릭 custom hook

---
+++ src/hooks/useHandleModalClick.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+
+/**
+ *  @hooks
+ *  Modal 외부클릭시 handleModalClose 실행
+ */
+
+export const useHandleModalClick = (modalRef, handleModalClose) => {
+  useEffect(() => {
+    // OutSide Click => Modal Close
+    const handleClickOutside = (e) => {
+      console.log("modalRef.current:", modalRef.current);
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        handleModalClose();
+      }
+    };
+
+    // ESC keydown => Modal Close
+    const handleKeyDownEsc = (e) => {
+      if (e.key === "Escape") {
+        handleModalClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDownEsc);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDownEsc);
+    };
+  }, [modalRef, handleModalClose]);
+};
