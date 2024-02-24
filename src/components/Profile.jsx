b504dfdc73cd0ff763ac8afbde92e66b8b543d81
Dodam0719 <blueoh719@gmail.com>
2024-02-25 01:02:02 +0900

    🎨 refactor: Profile에 useAsync, api 연결

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,24 @@
+import { useGetData } from "./Hooks/useAsync";
+
 // SharedHeader의 Profile 부분
 function Profile() {
+  const { data, error, loading } = useGetData("/sample/folder");
+
+  if (loading) return <div>Profile 로딩중...</div>;
+  if (error) return <div>Profile에 에러가 발생했습니다.</div>;
+
+  console.log(data);
+
   return (
     <div>
       <h2>SharedHeader의 Profile입니다.</h2>
+      <div>
+        <div>
+          <img src={data.folder.owner.profileImageSource} alt="profile" />
+          <span>{data.folder.owner.name}</span>
+        </div>
+        <span>{data.folder.name}</span>
+      </div>
     </div>
   );
 }
