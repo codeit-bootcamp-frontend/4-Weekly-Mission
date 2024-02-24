152263b056b432206bb03b880852e93b304d5c74
yeona <aya813@naver.com>
2024-02-24 20:20:06 +0900

    feat : ModalAdd 컴포넌트 생성 및 기능 구현

--- src/components/Folder/LinkBar/LinkBar.js
+++ src/components/Folder/LinkBar/LinkBar.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { useGetFolderById } from "../../../hooks/useGetFolder";
 import "./LinkBar.css";
+import ModalAdd from "../Modal/ModalAdd/ModalAdd";
 
 function LinkBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+  const { data } = useGetFolderById();
+  const dataArray = data?.data;
+
   return (
     <div className="LinkBar">
       <div className="LinkBar-item">
@@ -14,7 +25,12 @@ function LinkBar() {
           type="text"
           placeholder="링크를 추가해 보세요"
         />
-        <button className="LinkBar-button">추가하기</button>
+        <button className="LinkBar-button" onClick={handleOpenModal}>
+          추가하기
+        </button>
+        {isModalOpen && (
+          <ModalAdd setIsModalOpen={setIsModalOpen} dataArray={dataArray} />
+        )}
       </div>
     </div>
   );
