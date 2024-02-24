cbca022dcb9ade3179a2c13aad796066ebab8002
Dodam0719 <blueoh719@gmail.com>
2024-02-25 02:17:32 +0900

    ♻️ 🎨 refactor: SharedContent에 CommonContent 연결

--- src/containers/SharedContent.jsx
+++ src/containers/SharedContent.jsx
@@ -1,4 +1,4 @@
-// import CommonContent from "../components/common/CommonContent";
+import CommonContent from "../components/common/CommonContent";
 
 // SharedContent FolderContent 겹치는 부분은 공통 컴포넌트인 CommonContent로 관리
 // SharedContent에서는 CommonContent 그대로 사용
@@ -6,7 +6,7 @@ function SharedContent() {
   return (
     <div>
       <h2>SharedContent입니다.</h2>
-      {/* <CommonContent /> */}
+      <CommonContent />
     </div>
   );
 }
