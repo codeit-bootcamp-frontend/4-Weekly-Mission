e12a7063207a4378e4134d023bb79d2a888095fe
Dodam0719 <blueoh719@gmail.com>
2024-02-25 03:17:45 +0900

    ♻️ 💄 refactor: CommonContentSearch에 css 연결

--- src/components/common/CommonContentSearch.jsx
+++ src/components/common/CommonContentSearch.jsx
@@ -1,8 +1,10 @@
+import style from "../../styles/CommonContentSearch.module.css";
+
 // CommonContentSearch -> 링크 검색 부분(기능은 추후 구현)
 function CommonContentSearch() {
   return (
-    <div>
-      <h2>CommonContentSearch입니다.</h2>
+    <div className={style.content_search}>
+      <input type="text" placeholder="링크를 검색해 보세요." />
     </div>
   );
 }
