a97091dac5683490eb3c1c101db8f87261bbdcad
yeona <aya813@naver.com>
2024-02-24 19:22:54 +0900

    refactor : CardContent css 수정

--- src/components/common/CardContent/CardContent.js
+++ src/components/common/CardContent/CardContent.js
@@ -3,12 +3,14 @@ import "./CardContent.css";
 function CardContent({ elapsedTime, description, createdAt }) {
   return (
     <div className="CardContent">
-      <span className="cardContent-elapsed-time">{elapsedTime}</span>
-      <img
-        className="kebab"
-        src="images/kebab.svg"
-        alt="더보기를 나타내는 점 3개"
-      />
+      <span className="CardContent-elapsed-time">{elapsedTime}</span>
+      <button className="CardContent-kebab-button">
+        <img
+          className="kebab"
+          src="images/kebab.svg"
+          alt="더보기를 나타내는 점 3개"
+        />
+      </button>
       <p className="CardContent-description">{description}</p>
       <span className="CardContent-created-at">{createdAt}</span>
     </div>
