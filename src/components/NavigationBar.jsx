4162c857ae33cb1ca36ee1e809f600b6bab4a012
Dodam0719 <blueoh719@gmail.com>
2024-02-25 01:44:22 +0900

    ♻️ 💄 refactor: NavigationBar.css 수정

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -31,25 +31,26 @@ function NavigationBar() {
   const { data, error, loading } = useGetData("/sample/user");
   console.log(data);
 
-  if (loading) return <div>로딩중...</div>;
-  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!data) return <div>NavigationBar data가 null입니다.</div>;
+  if (loading) return <div>NavigationBar 로딩중...</div>;
+  if (error) return <div>NavigationBar에 에러가 발생했습니다.</div>;
 
   return (
-    <div className={style.NavigationBar}>
-      <div className={style.LogoAccountWrapper}>
-        <img className={style.Logo} src={logo} alt="LinkbraryLogo" />
+    <div className={style.navigation_bar}>
+      <div className={style.logo_account_container}>
+        <img className={style.logo} src={logo} alt="LinkbraryLogo" />
         {data ? (
-          <div className={style.Account}>
+          <div className={style.account}>
             <img
-              className={style.ProfileImg}
+              className={style.profile_img}
               // src="{profile.profileImageSource || profile.image_source}"
               src={data.profileImageSource}
               alt="Profile"
             />
-            <span className={style.ProfileEmail}>{data.email}</span>
+            <span className={style.profile_email}>{data.email}</span>
           </div>
         ) : (
-          <span className={style.SignInButton}>로그인</span>
+          <span className={style.sign_in_button}>로그인</span>
         )}
       </div>
     </div>
