d76028747ecb60102091afe5c7b3a89499eedbc8
janyong <wldud5023@naver.com>
2024-02-24 23:37:12 +0900

    Feat: 모달기능 기본구상

--- src/components/folder/addLink.js
+++ src/components/folder/addLink.js
@@ -2,7 +2,12 @@ import { useFolder } from "../../hooks/useFolder";
 import LINK_LOGO from "../../assets/image/link.svg";
 import "./folderInfo.css";
 import "./addLink.css";
+import CustomModal from '../modal/Modal'
+import { useState } from "react";
+import { FolderAddStyle } from "../modal/folderaddstyle";
+
 export const AddLink = () => {
+  const [open , setOpen] = useState(false)
   return (
     <div className="infoContainer">
       <div className="linkFlexBox">
@@ -11,8 +16,11 @@ export const AddLink = () => {
             <img src={LINK_LOGO} />
             <input placeholder="링크를 추가해보세요" />
           </div>
-          <button className="addButton">추가하기</button>
+          <button className="addButton" onClick={()=> setOpen(!open)}>추가하기</button>
         </div>
+        <CustomModal isOpen={open} onClose={()=>setOpen(false)}>
+          <FolderAddStyle/>
+        </CustomModal>
       </div>
     </div>
   );
---
+++ src/components/modal/folderaddstyle.js
@@ -0,0 +1,14 @@
+
+import './folderaddstyle.css';
+
+export const FolderAddStyle = () =>{
+    
+    
+    return(
+        <div className="modal_container">
+            <h2>폴더 이름 변경</h2>
+            <input></input>
+            <button>변경하기</button>
+        </div>
+    )
+}
