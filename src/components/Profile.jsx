2e27b7301c6a2d1f5afe122481ff3755147cf0cd
Dodam0719 <blueoh719@gmail.com>
2024-02-25 01:31:38 +0900

    🎨 refactor: Profile에 css 연결

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,28 @@
 import { useGetData } from "./Hooks/useAsync";
+import styles from "../styles/Profile.module.css";
 
 // SharedHeader의 Profile 부분
 function Profile() {
   const { data, error, loading } = useGetData("/sample/folder");
 
+  if (!data) return <div>Profile data가 null입니다.</div>;
   if (loading) return <div>Profile 로딩중...</div>;
   if (error) return <div>Profile에 에러가 발생했습니다.</div>;
 
   console.log(data);
 
   return (
-    <div>
-      <h2>SharedHeader의 Profile입니다.</h2>
-      <div>
-        <div>
-          <img src={data.folder.owner.profileImageSource} alt="profile" />
-          <span>{data.folder.owner.name}</span>
+    <div className={styles.profile}>
+      <div className={styles.profile_container}>
+        <div className={styles.avatar_and_username}>
+          <img
+            className={styles.avatar}
+            src={data.folder?.owner.profileImageSource}
+            alt="profile"
+          />
+          <span className={styles.username}>{data.folder?.owner.name}</span>
         </div>
-        <span>{data.folder.name}</span>
+        <span className={styles.foldername}>{data.folder?.name}</span>
       </div>
     </div>
   );
