51ff26313e715b303efa1098339b9a06b22796a3
Dodam0719 <blueoh719@gmail.com>
2024-02-25 02:15:24 +0900

    ♻️ 🎨 refactor: SharedPage에 SharedContent 연결

--- src/containers/SharedContent.jsx
+++ src/containers/SharedContent.jsx
@@ -1,14 +1,14 @@
 // import CommonContent from "../components/common/CommonContent";
 
-// // SharedContent FolderContent 겹치는 부분은 공통 컴포넌트인 CommonContent로 관리
-// // SharedContent에서는 CommonContent 그대로 사용
-// function SharedContent() {
-//   return (
-//     <div>
-//       <h2>SharedContent입니다.</h2>
-//       <CommonContent />
-//     </div>
-//   );
-// }
+// SharedContent FolderContent 겹치는 부분은 공통 컴포넌트인 CommonContent로 관리
+// SharedContent에서는 CommonContent 그대로 사용
+function SharedContent() {
+  return (
+    <div>
+      <h2>SharedContent입니다.</h2>
+      {/* <CommonContent /> */}
+    </div>
+  );
+}
 
-// export default SharedContent;
+export default SharedContent;
--- src/pages/SharedPage.jsx
+++ src/pages/SharedPage.jsx
@@ -2,6 +2,7 @@
 // import SharedContent from "../containers/SharedContent";
 // import CommonFooter from "../components/common/CommonFooter";
 
+import SharedContent from "../containers/SharedContent";
 import SharedHeader from "../containers/SharedHeader";
 
 // // SharedPage와 FolderPage의 footer는 완전히 동일해서 공통 컴포넌트인 CommonFooter로 관리
@@ -22,6 +23,7 @@ function SharedPage() {
     <div>
       <h2>SharedPage 컴포넌트입니다.</h2>
       <SharedHeader />
+      <SharedContent />
     </div>
   );
 }
