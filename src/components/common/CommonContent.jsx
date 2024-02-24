08dd6c754b740fe68c71dfc5919dd4d30c4dec14
Dodam0719 <blueoh719@gmail.com>
2024-02-25 03:24:32 +0900

    ♻️ 💄 refactor: CommonContent에 css 연결

--- src/components/common/CommonContent.jsx
+++ src/components/common/CommonContent.jsx
@@ -1,13 +1,13 @@
 import CommonContentSearch from "./CommonContentSearch";
 // import CardList from "./CardList";
+import style from "../../styles/CommonContent.module.css";
 
 // SharedContent에는 additionalContent 없음X, FolderHeader에서는 additionalContent 있음O
 function CommonContent(additionalContent) {
   // CommonContentSearch -> 링크 검색 부분(기능은 추후 구현)
   // CardList -> 카드 목록 부분(SharedContent와 FolderHeader의 API 주소 다름 주의!)
   return (
-    <div>
-      <h2>CommonContent입니다.</h2>
+    <div className={style.content_container}>
       <CommonContentSearch />
       {/* {additionalContent && <div>{additionalContent}</div>} */}
       {/* <CardList /> */}
