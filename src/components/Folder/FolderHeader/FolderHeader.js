94f7507f7683af7ad70904e99fea287c26306e38
LeeSeungHoon <ysh73900@gmail.com>
2024-02-26 00:07:43 +0900

    feat: 클릭시 AddLinkModal 띄우기

--- src/components/Folder/FolderHeader/FolderHeader.js
+++ src/components/Folder/FolderHeader/FolderHeader.js
@@ -1,6 +1,18 @@
+import { useModal } from "../../../hooks/useModal";
+import { AddLinkModal } from "../../Modal";
 import "./FolderHeader.css";
 
-export const FolderHeader = () => {
+export const FolderHeader = ({
+  linkUrl,
+  handleLinkAddInputChange,
+  folderList,
+}) => {
+  const {
+    openModal: AddOpenModal,
+    modalRef: AddModalRef,
+    handleModalClose: AddHandleModalClose,
+    handleModalOpen: AddHandleModalOpen,
+  } = useModal();
   return (
     <div className="folderHeader">
       <div className="folderHeader-link">
@@ -8,9 +20,25 @@ export const FolderHeader = () => {
         <input
           type="link"
           className="folderHeader-link-input"
+          value={linkUrl}
           placeholder="링크를 추가해보세요."
+          onChange={handleLinkAddInputChange}
         />
-        <button className="folderHeader-link-button">추가하기</button>
+        <button
+          className="folderHeader-link-button"
+          onClick={AddHandleModalOpen}
+        >
+          추가하기
+        </button>
+        {AddOpenModal && (
+          <AddLinkModal
+            linkUrl={linkUrl}
+            folderList={folderList}
+            openModal={AddOpenModal}
+            modalRef={AddModalRef}
+            handleModalClose={AddHandleModalClose}
+          />
+        )}
       </div>
     </div>
   );
