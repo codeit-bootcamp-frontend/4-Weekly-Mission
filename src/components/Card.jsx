b0aa4fe061decfd1a70b96d6e48aa3f7020e3855
Dodam0719 <blueoh719@gmail.com>
2024-02-25 20:22:03 +0900

    ♻️ 💄 refactor: Card에 css 연결

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import CardHeader from "./CardHeader";
 import CardContent from "./CardContent";
 import CardFooter from "./CardFooter";
+import style from "../styles/Card.module.css";
 
 // 카드 컴포넌트를 클릭하면 해당하는 링크로 새로운 창을 띄워서 이동하게 해주세요.
 // 이미지 -> (card.imageSource 활용)
@@ -13,7 +14,7 @@ function Card({ id, createdAt, url, title, description, imageSource }) {
   };
 
   return (
-    <div onClick={handleClick}>
+    <div className={style.card} onClick={handleClick}>
       <img src={imageSource} alt={title} />
       <CardHeader createdAt={createdAt} />
       <CardContent description={description} />
