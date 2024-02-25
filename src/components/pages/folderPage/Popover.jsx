1ab11feffa02d435515ce5bf60147a87e54f4290
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-25 13:58:27 +0900

    Refactor: handle함수 하나로 합치기

--- src/components/pages/folderPage/Popover.jsx
+++ src/components/pages/folderPage/Popover.jsx
@@ -2,22 +2,17 @@ import { modalTypes } from '../../../util/constants';
 import styles from './Popover.module.css';
 
 const Popover = ({ handlePopoverClick, handleModalBtnClick, handleSelectedLink }) => {
-  const onDeleteBtnClick = e => {
-    handleModalBtnClick(e);
-    handleSelectedLink();
-  };
-
-  const handleAddBtnClick = e => {
+  const onBtnClick = e => {
     handleModalBtnClick(e);
     handleSelectedLink();
   };
 
   return (
     <div className={styles.container} onClick={handlePopoverClick}>
-      <button className={`popover-item ${styles.btn}`} data-modal={modalTypes.deleteLink} onClick={onDeleteBtnClick}>
+      <button className={`popover-item ${styles.btn}`} data-modal={modalTypes.deleteLink} onClick={onBtnClick}>
         삭제하기
       </button>
-      <button className={`popover-item ${styles.btn}`} data-modal={modalTypes.add} onClick={handleAddBtnClick}>
+      <button className={`popover-item ${styles.btn}`} data-modal={modalTypes.add} onClick={onBtnClick}>
         폴더에 추가
       </button>
     </div>
