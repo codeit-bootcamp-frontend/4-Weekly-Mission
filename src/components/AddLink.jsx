b8d84d006a5b2f9f9c727dfea7c9ee1607415c85
JI-SOOLEE <elenfl0122@gmail.com>
2024-02-26 00:26:45 +0900

    Complete AddLink component

--- src/components/AddLink.jsx
+++ src/components/AddLink.jsx
@@ -1,5 +1,16 @@
+import link from '../assets/link.svg';
+import Styles from '../styles/Folder.module.css';
+
 const AddLink = () => {
-  return <></>;
+  return (
+    <div className={Styles.AddLink}>
+      <div className={Styles.AddLinkGroup}>
+        <img className={Styles.LinkIcon} src={link} alt='링크 아이콘' />
+        <p className={Styles.AddLinkText}>링크를 추가해 보세요</p>
+      </div>
+      <button className={Styles.AddLinkBtn}>추가하기</button>
+    </div>
+  );
 };
 
 export default AddLink;
