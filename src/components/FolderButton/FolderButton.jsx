1043c72a98e2c51249a33e0293a8e41c5ba8b1a2
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-02-25 21:24:51 +0900

    updated

--- src/components/FolderButton/FolderButton.jsx
+++ src/components/FolderButton/FolderButton.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-function getFolderStyle({ isActive }) {
-  return {
-    background: isActive ? "var(--Linkbrary-primary-color, #6d6afe)" : "white",
-    color: isActive ? "white" : "var(--Linkbrary-primary-color, #6d6afe)",
-  };
-}
-
 const FolderButton = (props) => {
   const { folderName, onClick, folderId } = props;
   return (
-    <NavLink to={`/folder/${folderId}`} style={getFolderStyle}>
+    <S.NavLink to={`/folder/${folderId}`} style={getFolderStyle}>
       <S.ButtonStyle onClick={() => onClick(folderId)}>
         {" "}
         {folderName}
       </S.ButtonStyle>
-    </NavLink>
+    </S.NavLink>
   );
 };
 
 export default FolderButton;
+
+const navTheme = {
+  foreground: "var(--Linkbrary-primary-color, #6d6afe)",
+  background: "white",
+};
 
 const S = {
   ButtonStyle: styled.button`
@@ -32,5 +30,17 @@ const S = {
     border-radius: 5px;
     border: 1px solid var(--Linkbrary-primary-color, #6d6afe);
     background: #fff;
+  `,
+  NavLink: styled(NavLink)`
+    ${({ isActive }) =>
+      isActive
+        ? css`
+            color: ${navTheme.background};
+            background-color: ${navTheme.foreground};
+          `
+        : css`
+            color: ${navTheme.foreground};
+            background-color: ${navTheme.background};
+          `}
   `,
 };
