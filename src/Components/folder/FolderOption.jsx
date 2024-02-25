4f1019982e55d809044cf03013dddfe643a2cc47
78-artilleryman <ywy040150@gmail.com>
2024-02-25 19:24:08 +0900

    feat: 폴더 삭제 모달 생성

--- src/Components/folder/FolderOption.jsx
+++ src/Components/folder/FolderOption.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { useFolder } from "../../context/FolderContext";
+import { useModal } from "../../hooks/useModal";
+import ModalContainer from "../common/Modal/Modal";
+import * as Modal from "../common/Modal/Modal";
 
 const OptionList = styled.div`
   display: flex;
@@ -17,28 +20,39 @@ const Option = styled.div`
   color: var(--Linkbrary-gray60, #9fa6b2);
   font-size: 14px;
   font-weight: 600;
+  cursor: pointer;
 `;
 
 function FolderOption() {
   const { folderFilter } = useFolder();
+  const { openModal, handleModalOpen, handleModalClose } = useModal();
 
   return folderFilter.name === "전체" ? (
     ""
   ) : (
-    <OptionList>
-      <Option>
-        <img src="/Icons/share.svg" alt="공유" />
-        <p>공유</p>
-      </Option>
-      <Option>
-        <img src="/Icons/pen.svg" alt="이름 변경" />
-        <p>이름 변경</p>
-      </Option>
-      <Option>
-        <img src="/Icons/delete.svg" alt="삭제" />
-        <p>삭제</p>
-      </Option>
-    </OptionList>
+    <>
+      {openModal && (
+        <ModalContainer onClick={handleModalClose}>
+          <Modal.Title>폴더추가</Modal.Title>
+          <Modal.SubTitle>{folderFilter.name}</Modal.SubTitle>
+          <Modal.RedButton>추가하기</Modal.RedButton>
+        </ModalContainer>
+      )}
+      <OptionList>
+        <Option>
+          <img src="/Icons/share.svg" alt="공유" />
+          <p>공유</p>
+        </Option>
+        <Option>
+          <img src="/Icons/pen.svg" alt="이름 변경" />
+          <p>이름 변경</p>
+        </Option>
+        <Option onClick={handleModalOpen}>
+          <img src="/Icons/delete.svg" alt="삭제" />
+          <p>삭제</p>
+        </Option>
+      </OptionList>
+    </>
   );
 }
 
