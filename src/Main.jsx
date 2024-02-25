d9e954580a72670a370f83ebaa80c631f782c2a8
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-25 11:56:33 +0900

    Feat: 루트에 접근하면 folderPage로 리다이렉트

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SharedPage from './components/pages/SharedPage';
 import Layout from './components/layout/Layout';
 import FolderPage from './components/pages/FolderPage';
+import LandingPage from './components/pages/LandingPage';
 
 const Main = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<FolderPage />} />
+        <Route index element={<LandingPage />} />
         <Route path="shared" element={<SharedPage />} />
         <Route path="folder" element={<FolderPage />} />
       </Route>
