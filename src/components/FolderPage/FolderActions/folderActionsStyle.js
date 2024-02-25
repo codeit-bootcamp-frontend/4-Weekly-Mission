787433d18b287cf1b6f003a08b7b5bf1e2d7a140
Trophy198 <spsp8426@naver.com>
2024-02-25 23:52:03 +0900

    refactor : 불필요한 props 삭제

--- src/components/FolderPage/FolderActions/folderActionsStyle.js
+++ src/components/FolderPage/FolderActions/folderActionsStyle.js
@@ -6,9 +6,6 @@ export const IconContainer = styled.div`
   align-items: center;
   justify-content: flex-start;
   gap: 16px;
-  color: #9fa6b2;
-  font-size: 0.875rem;
-  font-weight: 600;
 
   @media (max-width: 768px) {
     margin: 1.5rem 0;
