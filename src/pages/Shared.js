9e7313cb0d1c83ad75b9d1a18883499c37638502
Kim Min Jeong <minjeong9919@naver.com>
2024-02-25 17:10:25 +0900

    Fix: shared 페이지도 folder 페이지와 동일한 모달 추가

--- src/pages/Shared.js
+++ src/pages/Shared.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getFolderInfo } from "./../api/api";
 import HeaderElement from "./../components/common/HeaderElement";
 import FooterElement from "./../components/common/FooterElement";
@@ -6,17 +7,31 @@ import Input from "../components/common/Input";
 import FolderList from "../components/common/FolderList";
 import useGetJson from "./../hooks/uesGetJson";
 import "../styles/shared.css";
+import { SharedModal } from "components/common/modals/SharedModal";
+import { EditNameModal } from "components/common/modals/EditNameModal";
+import { DeleteModal } from "components/common/modals/DeleteModal";
+import { AddFolderModal } from "components/common/modals/AddFolderModal";
 
 function Shared() {
   const foldersData = useGetJson(getFolderInfo);
   const folders = foldersData?.folder?.links || [];
 
+  const [isModal, setIsModal] = useState(null);
+
   return (
     <>
+      <SharedModal $isVisible={isModal} setIsVisible={setIsModal} />
+      <EditNameModal $isVisible={isModal} setIsVisible={setIsModal} />
+      <DeleteModal $isVisible={isModal} setIsVisible={setIsModal} />
+      <AddFolderModal $isVisible={isModal} setIsVisible={setIsModal} />
       <HeaderElement></HeaderElement>
       <SharedSection></SharedSection>
       <Input />
-      <FolderList items={folders}></FolderList>
+      <FolderList
+        items={folders}
+        $isVisible={isModal}
+        setIsVisible={setIsModal}
+      ></FolderList>
       <FooterElement></FooterElement>
     </>
   );
