40084fe51004fb4a64caaba62fe0e2face5e33f7
Trophy198 <spsp8426@naver.com>
2024-02-25 23:51:25 +0900

    feat : 각 아이콘 컴포넌트에 대한 onClick 이벤트 추가

--- src/components/icons/DeleteIcon.jsx
+++ src/components/icons/DeleteIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as Delete } from '../../assets/icons/delete.svg';
 
-const DeleteIcon = ({ text }) => {
+const DeleteIcon = ({ text, onClick }) => {
   return (
-    <IconContainer>
+    <IconContainer onClick={onClick}>
       <Delete />
       <IconText>{text}</IconText>
     </IconContainer>
@@ -13,9 +13,15 @@ const DeleteIcon = ({ text }) => {
 
 export default DeleteIcon;
 
-const IconContainer = styled.div`
+const IconContainer = styled.button`
   display: flex;
   align-items: center;
+  border: none;
+  background-color: #fff;
+  color: #9fa6b2;
+  font-size: 0.875rem;
+  font-weight: 600;
+  cursor: pointer;
 `;
 
 const IconText = styled.span`
--- src/components/icons/EditIcon.jsx
+++ src/components/icons/EditIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as Edit } from '../../assets/icons/pen.svg';
 
-const EditIcon = ({ text }) => {
+const EditIcon = ({ text, onClick }) => {
   return (
-    <IconContainer>
+    <IconContainer onClick={onClick}>
       <Edit />
       <IconText>{text}</IconText>
     </IconContainer>
@@ -13,9 +13,15 @@ const EditIcon = ({ text }) => {
 
 export default EditIcon;
 
-const IconContainer = styled.div`
+const IconContainer = styled.button`
   display: flex;
   align-items: center;
+  border: none;
+  background-color: #fff;
+  color: #9fa6b2;
+  font-size: 0.875rem;
+  font-weight: 600;
+  cursor: pointer;
 `;
 
 const IconText = styled.span`
