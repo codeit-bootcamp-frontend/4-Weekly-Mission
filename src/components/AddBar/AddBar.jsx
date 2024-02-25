20d1622b19d6e8a022d63615b6eae617f70297a3
Junyoung Park <9851248@gmail.com>
2024-02-26 02:24:50 +0900

    refactor: Modify CSS

--- src/components/AddBar/AddBar.jsx
+++ src/components/AddBar/AddBar.jsx
@@ -6,11 +6,11 @@ import styles from './AddBar.module.scss';
 const AddBar = () => {
   return (
     <section className={styles.layout}>
-      <colorBox className={styles.colorBox}>
+      <div className={styles.colorBox}>
         <HiOutlineLink className={styles.linkLogo} />
         <input className={styles.input} placeholder='링크를 추가 해보세요' />
         <GradientButton className={styles.gradientButton}>추가하기</GradientButton>
-      </colorBox>
+      </div>
     </section>
   );
 };
