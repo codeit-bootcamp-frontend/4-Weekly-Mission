a05d6441f2ed0642e04381d97f01d4956d307dba
Dodam0719 <blueoh719@gmail.com>
2024-02-25 22:05:50 +0900

    ♻️ 💄 refactor: CardContent에 css 연결

--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -1,6 +1,8 @@
+import style from "../styles/CardContent.module.css";
+
 // CardContent -> 설명 표시되는 부분(card.description 활용)
 function CardContent({ description }) {
-  return <span>{description}</span>;
+  return <span className={style.card_content}>{description}</span>;
 }
 
 export default CardContent;
