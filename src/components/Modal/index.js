9fa5327f8304b19b3af06c1ab37a1b018d14d74f
LeeSeungHoon <ysh73900@gmail.com>
2024-02-26 00:03:26 +0900

    feat: Modal들 한꺼번에 내보내기

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,1 +1,6 @@
-export * from "./Modal";
+export { DeleteFolderModal } from "./DeleteFolderModal";
+export { EditFolderNameModal } from "./EditFolderNameModal";
+export { ShareFolderModal } from "./ShareFolderModal";
+export { DeleteLinkModal } from "./DeleteLinkModal";
+export { AddFolderModal } from "./AddFolderModal";
+export { AddLinkModal } from "./AddLinkModal";
