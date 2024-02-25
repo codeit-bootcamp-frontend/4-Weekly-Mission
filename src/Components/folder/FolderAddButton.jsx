7dab19628084491ef7764363b672f049a851071e
78-artilleryman <ywy040150@gmail.com>
2024-02-25 19:16:51 +0900

    feat: 인풋 모달 박스 생성

--- src/Components/folder/FolderAddButton.jsx
+++ src/Components/folder/FolderAddButton.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { IoMdAdd } from "react-icons/io";
+import { useModal } from "../../hooks/useModal";
+import ModalContainer from "../common/Modal/Modal";
+import * as Modal from "../common/Modal/Modal";
 
 const AddButton = styled.div`
   display: flex;
   gap: 4px;
   width: 20%;
   justify-content: flex-end;
+  cursor: pointer;
 `;
 
 const Name = styled.p`
@@ -22,11 +26,21 @@ const iconStyle = {
 };
 
 function FolderAddButton() {
+  const { openModal, handleModalOpen, handleModalClose } = useModal();
   return (
-    <AddButton>
-      <Name>폴더추가</Name>
-      <IoMdAdd style={iconStyle} />
-    </AddButton>
+    <>
+      {openModal && (
+        <ModalContainer onClick={handleModalClose}>
+          <Modal.Title>폴더추가</Modal.Title>
+          <Modal.Input placeholder="내용 입력"></Modal.Input>
+          <Modal.BlueButton>추가하기</Modal.BlueButton>
+        </ModalContainer>
+      )}
+      <AddButton onClick={handleModalOpen}>
+        <Name>폴더추가</Name>
+        <IoMdAdd style={iconStyle} />
+      </AddButton>
+    </>
   );
 }
 
