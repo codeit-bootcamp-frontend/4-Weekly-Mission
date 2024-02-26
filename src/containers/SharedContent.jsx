ecf09660660246716f19421959de9315534d0e50
Dodam0719 <blueoh719@gmail.com>
2024-02-26 20:04:31 +0900

    ♻️ 💄 refactor: tablet 반응형 css 수정

--- src/containers/SharedContent.jsx
+++ src/containers/SharedContent.jsx
@@ -1,10 +1,11 @@
 import CommonContent from "../components/common/CommonContent";
+import style from "../styles/SharedContent.module.css";
 
 // SharedContent FolderContent 겹치는 부분은 공통 컴포넌트인 CommonContent로 관리
 // SharedContent에서는 CommonContent 그대로 사용
 function SharedContent() {
   return (
-    <div>
+    <div className={style.shared_content}>
       {/* <h2>SharedContent입니다.</h2> */}
       <CommonContent />
     </div>
