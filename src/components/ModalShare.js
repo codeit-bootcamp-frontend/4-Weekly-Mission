fe3ceb7a25ad2ae3f2709fb3930b054eff1410c2
thgus5335 <thgus5335@naver.com>
2024-02-27 04:38:57 +0900

    :sparkles: Add: copy link function
    Add copy link function

--- src/components/ModalShare.js
+++ src/components/ModalShare.js
@@ -2,8 +2,20 @@ import shareKaKao from '../assets/icons/share-kakao.svg';
 import shareFacebook from '../assets/icons/share-facebook.svg';
 import shareLink from '../assets/icons/share-link.svg';
 import '../styles/ModalShare.css';
+import { useLocation } from "react-router-dom";
 
 const ModalShare = ({ subTitle }) => {
+  const location = useLocation();
+
+  const handleCopyClipBoard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("링크가 복사되었습니다.");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
   return (
     <div className='modal-share-content'>
@@ -18,7 +30,7 @@ const ModalShare = ({ subTitle }) => {
           <p>페이스북</p>
         </div>
         <div className='sns-icon'>
-          <a><img src={shareLink} /></a>
+          <a onClick={() => handleCopyClipBoard(`https://linklibrary313.netlify.app${location.pathname}`)}><img src={shareLink} /></a>
           <p>링크 복사</p>
         </div>
       </div>
