5af040fd0288c259cbb1061b6df70ce45dd35361
jinhok96 <jinhok96a@gmail.com>
2024-02-28 17:44:34 +0900

    chore: 주석 추가 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import 'assets/styles/utility.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// ModalProvider로 App을 감싸면 App의 모든 요소에서 ModalProvider에 접근 가능
 root.render(
   <React.StrictMode>
     <ModalProvider>
