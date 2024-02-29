cb3f78fc8e4f0a802111f2aed42f3906bb226d88
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-29 22:22:59 +0900

    refactor: Outlet 적용

--- src/components/common/modal/frame/Frame.jsx
+++ src/components/common/modal/frame/Frame.jsx
@@ -1,10 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import { exitBtnId, modalBackground } from '../../../../util/constants';
 import styles from './Frame.module.css';
 
-const Frame = ({ children, handleCloseModal }) => (
+const Frame = ({ handleCloseModal }) => (
   <div className={styles.background} onClick={handleCloseModal} id={modalBackground}>
     <div className={styles.modal}>
-      {children}
+      <Outlet />
       <button className={styles.exit} id={exitBtnId} />
     </div>
   </div>
