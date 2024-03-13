ac7a1789c338cd164dc3c9ba78551aa7da5f521f
serinny <155131802+serinny@users.noreply.github.com>
2024-03-13 13:00:42 +0900

    Revert "[최민준] week7"

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import { useEffect, useState } from 'react';
-import { getUserData, getFolderData } from './api/api';
-import Header from './components/common/header/Header';
-import Main from './components/main/Main';
-import Footer from './components/common/footer/Footer';
-import './common.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [user, setUser] = useState([]);
-  const [folderOwner, setFolderOwner] = useState([]);
-  const [folderName, setFolderName] = useState('');
-  const [cardData, setCardData] = useState([]);
-
-  // get user sample data from api
-  const getUserInformation = async () => {
-    const userInformation = await getUserData();
-    setUser(userInformation);
-  };
-  useEffect(() => {
-    getUserInformation();
-  }, []);
-
-  // get folder sample data from api
-  const getFolderInformation = async () => {
-    const folderInformation = await getFolderData();
-    setFolderOwner(folderInformation.folder.owner);
-    setFolderName(folderInformation.folder.name);
-    setCardData(folderInformation.folder.links);
-  };
-  useEffect(() => {
-    getFolderInformation();
-  }, []);
   return (
-    <>
-      <Header user={user} />
-      <Main
-        user={user}
-        folderOwner={folderOwner}
-        folderName={folderName}
-        cardData={cardData}
-      />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
