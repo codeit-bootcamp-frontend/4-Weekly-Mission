44c75128c08872d1b521a57f82d8ea4ad287b480
JeongSabibi <jsbb528@naver.com>
2024-03-14 03:54:01 +0900

    🐛 Fix: 폴더 탭 클릭 시 focus 되도록 수정 및 코드 포맷 변경
    fix: 폴더 탭 클릭 시 focus 되도록 수정 및 코드 포맷 변경

--- src/components/folder/FolderTabItem.js
+++ src/components/folder/FolderTabItem.js
@@ -1,6 +1,6 @@
 import Tab from './Tab';
 
-const FolderTabItem = ({ folder, setItem, isSelected }) => {
+const FolderTabItem = ({ folder, setItem }) => {
   const { name } = folder;
   return <Tab onClick={() => setItem({ id: folder.id, name: folder.name })}>{name}</Tab>;
 };
