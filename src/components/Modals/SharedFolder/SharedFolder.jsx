4654ed8785d053536f1d1473800e94f5fb231bb6
codingaring <sksms2104@naver.com>
2024-03-14 21:03:20 +0900

    ✨ feat : 카카오톡 공유하기 기능 추가

--- src/components/Modals/SharedFolder/SharedFolder.jsx
+++ src/components/Modals/SharedFolder/SharedFolder.jsx
@@ -2,9 +2,43 @@ import * as S from "./SharedFolderStyled";
 import "./SharedFolder";
 import { Modal } from "../Modal/Modal";
 import { handleCopyClipBoard } from "util/copyClipBoard";
+import { useEffect } from "react";
+
+const { Kakao } = window;
 
 export function SharedFolder({ isOpenModal, handleModalClose, selectFolder }) {
-  return (
+  useEffect(() => {
+    Kakao.cleanup();
+    console.log("key:", process.env.REACT_APP_KAKAO_KEY);
+    Kakao.init(process.env.REACT_APP_KAKAO_KEY);
+    console.log("check!!:", Kakao.isInitialized());
+  }, []);
+
+  const shareKakao = () => {
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "Linkbrary",
+        description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요",
+        imageUrl: "https://codingaring-week11-linkbrary.netlify.app",
+        link: {
+          mobileWebUrl:
+            "https://codingaring-week11-linkbrary.netlify.app/folder",
+        },
+      },
+      buttons: [
+        {
+          title: "Linkbrary",
+          link: {
+            mobileWebUrl:
+              "https://codingaring-week11-linkbrary.netlify.app/folder",
+          },
+        },
+      ],
+    });
+  };
+
+  https: return (
     <Modal
       title="폴더 공유"
       isOpenModal={isOpenModal}
@@ -12,7 +46,7 @@ export function SharedFolder({ isOpenModal, handleModalClose, selectFolder }) {
     >
       <S.FolderName>{selectFolder}</S.FolderName>
       <S.ButtonContainer>
-        <S.SharedButton>
+        <S.SharedButton onClick={shareKakao}>
           <S.KakaoTalkIcon />
           <S.IconText>카카오톡</S.IconText>
         </S.SharedButton>
