ccf16535e8f00b040a9baa1a7e5e5a5dffb397aa
Jules <dali1999@naver.com>
2024-03-16 22:58:56 +0900

    feat: sns공유 및 링크 공유 기능 추가

--- src/components/Modal/ShareSnsList.jsx
+++ src/components/Modal/ShareSnsList.jsx
@@ -15,6 +15,7 @@ const ListContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 7px;
+  cursor: pointer;
   img {
     width: 42px;
   }
@@ -28,22 +29,40 @@ const SHARE_LIST = [
   {
     name: "카카오톡",
     img: kakaoImg,
+    url: "https://www.kakaocorp.com/service/KakaoTalk",
   },
   {
     name: "페이스북",
     img: facebookImg,
+    url: "https://www.facebook.com/",
   },
   {
     name: "링크복사",
     img: linkImg,
+    onClick: () => {
+      const currentURL = window.location.href;
+      navigator.clipboard
+        .writeText(currentURL)
+        .then(() => alert("링크가 복사되었습니다."))
+        .catch((error) =>
+          console.error("링크 복사 중 오류가 발생했습니다.", error)
+        );
+    },
   },
 ];
 
 function ShareLists() {
+  const handleShare = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <Container>
       {SHARE_LIST.map((list) => (
-        <ListContainer key={list.name}>
+        <ListContainer
+          key={list.name}
+          onClick={list.onClick ? list.onClick : () => handleShare(list.url)}
+        >
           <img src={list.img} alt="" />
           <span>{list.name}</span>
         </ListContainer>
