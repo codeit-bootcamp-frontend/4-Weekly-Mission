121667a2d5b9764336669854378eb00b0d208299
lim sangmin <sangmin0205@gmail.com>
2024-03-16 17:56:49 +0900

    feat: Gnb Cta Addlink 컴포넌트 개발(완) input delete 컴포넌트 개발중

---
+++ src/components/delete/delete.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "classnames/bind";
+
+function Delete({}) {
+  return;
+}
+
+export default Delete;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./App";
+import "./styles/global.scss";
+import {createRoot} from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
