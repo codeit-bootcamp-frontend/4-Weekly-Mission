cb83be1e0e8585313167e585e50b07e2da55b2cd
Rhajiit <ksd9638@gmail.com>
2024-03-18 04:51:51 +0900

    💄 Style: KebabMenu 스타일 파일 분리

--- src/components/Utils/KebabMenu.tsx
+++ src/components/Utils/KebabMenu.tsx
@@ -1,50 +1,5 @@
 import { useState } from "react";
-import styled from "styled-components";
-
-const KebabDiv = styled.div`
-  position: absolute;
-  flex-direction: column;
-  align-items: flex-start;
-  top: 16px;
-  gap: 4px;
-  box-shadow: 0px 2px 8px 0px rgba(51, 50, 54, 0.1);
-
-  z-index: 1;
-`;
-
-const KebabButton = styled.button`
-  color: var(--LBrary-Gray100);
-  font-family: Pretendard;
-  background-color: var(--LBrary-White);
-  border: none;
-  width: 100px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  font-size: 1.4rem;
-  background-color: ;
-  padding: 7px 12px;
-  text-align: center;
-
-  &:hover {
-    color: var(--LBrary-Primary-color);
-    background-color: var(--LBrary-Gray10);
-  }
-`;
-
-const KebabCancel = styled.button`
-  position: fixed;
-  top: 0;
-  left: 0;
-  border: none;
-  width: 100vw;
-  height: 100vh;
-  background-color: transparent;
-
-  &:hover {
-    cursor: default;
-  }
-`;
+import * as S from "./KebabMenu.style";
 
 export default function KebabMenu({ items, data = false }: any) {
   const [kebabMenuPop, setKebabMenuPop] = useState(false);
@@ -54,27 +9,27 @@ export default function KebabMenu({ items, data = false }: any) {
   };
 
   return (
-    <div className="kebab-menu-position-origin">
-      <button type="button" onClick={handleKebabToggle} className="kebab">
+    <S.KebabMenuOriginPosition>
+      <S.KebabToggleButton type="button" onClick={handleKebabToggle}>
         <img src="kebab.svg" alt="kebabButton" />
-      </button>
+      </S.KebabToggleButton>
       {kebabMenuPop && (
         <>
-          <KebabDiv>
+          <S.KebabDiv>
             {items.map((item: any) => (
-              <KebabButton
+              <S.KebabMenuButton
                 key={item.btnName}
                 onClick={() =>
                   item.kebabHandle(item.type, [data, ...item.data])
                 }
               >
                 {item.btnName}
-              </KebabButton>
+              </S.KebabMenuButton>
             ))}
-          </KebabDiv>
-          <KebabCancel onClick={handleKebabToggle} />
+          </S.KebabDiv>
+          <S.KebabCancel onClick={handleKebabToggle} />
         </>
       )}
-    </div>
+    </S.KebabMenuOriginPosition>
   );
 }
