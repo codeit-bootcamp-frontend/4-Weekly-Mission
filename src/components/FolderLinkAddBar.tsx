ed3eb4ebf5d3f30d72d7b7721ceeb55a113d66c1
Rhajiit <ksd9638@gmail.com>
2024-03-17 15:05:39 +0900

    🧪 Comment: 카카오 공유 기능 임시 비활성화.

--- src/components/FolderLinkAddBar.tsx
+++ src/components/FolderLinkAddBar.tsx
@@ -1,8 +1,9 @@
 import "./FolderLinkAddBar.css";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 export default function FolderLinkAddBar({ handleSubmit, subFolderList }: any) {
   const [inputValue, setInputValue] = useState("");
+  const addLInkInputRef = useRef("");
 
   const handleLinkAdd: any = (e: ChangeEvent) => {
     e.preventDefault();
@@ -13,13 +14,7 @@ export default function FolderLinkAddBar({ handleSubmit, subFolderList }: any) {
     <div className="background-decoration-color">
       <form onSubmit={handleLinkAdd} className="input-position-origin link-add">
         <label htmlFor="add-link" />
-        <input
-          // onInput={(e) => {
-          //   setInputValue(e.target.value);
-          // }}
-          id="add-link"
-          placeholder="링크를 추가해 보세요."
-        />
+        <input id="add-link" placeholder="링크를 추가해 보세요." />
         <button className="link-add-btn default-btn">추가하기</button>
       </form>
     </div>
--- src/components/Utils/KakaoTalkShare.tsx
+++ src/components/Utils/KakaoTalkShare.tsx
@@ -1,13 +1,13 @@
-const { Kakao } = window as any;
+const { Kakao }: any = window as any;
 
 // 배포한 자신의 사이트
 
 // init 해주기 전에 clean up 을 해준다.
-Kakao.cleanup();
+// Kakao.cleanup();
 // 자신의 js 키를 넣어준다.
-Kakao.init("da964a62e055d3e70fdf6f70db10c052");
+// Kakao.init("da964a62e055d3e70fdf6f70db10c052");
 // 잘 적용되면 true 를 뱉는다.
-console.log(Kakao.isInitialized());
+// console.log(Kakao.isInitialized());
 
 const shareKakao = (url: any, query = "") => {
   Kakao.Share.sendDefault({
