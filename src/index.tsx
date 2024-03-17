6ac1b4a50f2058642033776cbcfe85d3f0f4ad5f
Rhajiit <ksd9638@gmail.com>
2024-03-17 15:12:14 +0900

    💄 Style: defaultStyle.css를 index.css로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import Share from "src/pages/Share";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "src/components/App";
 import Folder from "src/pages/Folder";
+import "./index.css";
+import "./styles/reset.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
