c1a3d7db18017167282a1f8dffda4d220ea952dc
Rhajiit <ksd9638@gmail.com>
2024-03-18 17:36:15 +0900

    ♻️ Refactor: ModalAddLinkToSubFolder에서 즐겨찾기 폴더를 filter를 이용해 걸러내도록 함

--- src/components/Modal/ModalAddLinkToSubFolder/ModalAddLinkToSubFolder.tsx
+++ src/components/Modal/ModalAddLinkToSubFolder/ModalAddLinkToSubFolder.tsx
@@ -3,7 +3,7 @@ import * as S from "../ModalLoader.style";
 import * as SS from "./ModalAddLinkToSubFolder.style";
 
 export default function ModalAddLinkToFolder({ modalData }: any) {
-  const [targetLink, [favor, ...folderList]] = [...modalData];
+  const [targetLink, [...folderList]] = [...modalData];
   const [SelectedFolder, setSelectedFolder] = useState("");
 
   const handleSelectFolder = (key: any) => {
@@ -17,21 +17,23 @@ export default function ModalAddLinkToFolder({ modalData }: any) {
         <S.ModalCaption>{targetLink}</S.ModalCaption>
       </S.ModalTitle>
       <S.ShareButtonLayout>
-        {folderList.map((item: any) => (
-          <SS.SubFolderDesireToAddLinkButton
-            onClick={() => handleSelectFolder(item.name)}
-            $state={item.name === SelectedFolder}
-            key={item.name}
-          >
-            <h2>
-              {item.name}
-              <span>{`${item.length || 0}개 링크`}</span>
-            </h2>
-            {item.name === SelectedFolder && (
-              <img src="check.svg" alt="CheckedFolder" />
-            )}
-          </SS.SubFolderDesireToAddLinkButton>
-        ))}
+        {folderList
+          .filter((item: any) => item.favorite !== true)
+          .map((item: any) => (
+            <SS.SubFolderDesireToAddLinkButton
+              onClick={() => handleSelectFolder(item.name)}
+              $state={item.name === SelectedFolder}
+              key={item.name}
+            >
+              <h2>
+                {item.name}
+                <span>{`${item.length || 0}개 링크`}</span>
+              </h2>
+              {item.name === SelectedFolder && (
+                <img src="check.svg" alt="CheckedFolder" />
+              )}
+            </SS.SubFolderDesireToAddLinkButton>
+          ))}
       </S.ShareButtonLayout>
       <S.ModalButton>추가하기</S.ModalButton>
     </>
