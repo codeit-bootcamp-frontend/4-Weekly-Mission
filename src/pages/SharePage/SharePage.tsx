246936f18f9a908db1d3fb0e7e6d57cb2b5eb386
Rhajiit <ksd9638@gmail.com>
2024-03-18 18:42:57 +0900

    ♻️ Refactor: SharePage Type 지정 확인 및 import 순서 정리

--- src/pages/SharePage/SharePage.tsx
+++ src/pages/SharePage/SharePage.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
+import { acceptDataFromApi } from "src/Api";
+import * as S from "./SharePage.style";
+
+// Components
+import HeadNav from "src/components/BasicLayout/HeadNav";
 import ShareFolderProfile from "src/pages/SharePage/Components/SharePageProfile";
 import LinkSearchBar from "src/components/LinkCard/LinkSearchBar";
 import LinkCardCollection from "src/components/LinkCard/LinkCardCollection";
-import * as S from "./SharePage.style";
-import { useEffect, useState } from "react";
-import { acceptDataFromApi } from "src/Api";
-import HeadNav from "src/components/BasicLayout/HeadNav";
+
+// Types
 import UserLinkDataType from "src/@types/UserLinkDataType";
 
+/**
+ *
+ * @description /share 페이지를 구현하는 컴포넌트.
+ * @returns
+ */
 export default function SharePage() {
   const [items, setItems] = useState<UserLinkDataType[]>([]);
 
