c207e6571349d7e285b0281964ff26414d34b574
zoonyoung <9851248@gmail.com>
2024-03-19 22:02:53 +0900

    refactor: Modify createAxiosInstance to export default

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
-export const createAxiosInstance = (baseURL = '', headers = {}) => {
+export const createAxiosInstance = () => {
+  const baseURL = process.env.REACT_APP_AXIOS_BASE_URL;
+  const headers = {
+    'Content-Type': process.env.REACT_APP_AXIOS_CONTENT_TYPE
+  };
+
   return axios.create({
     baseURL: baseURL,
     headers: headers
   });
 };
+
+export default createAxiosInstance;
