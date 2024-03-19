1a85821c90c538c76cc1e2c51041c4264c3b6f36
zoonyoung <9851248@gmail.com>
2024-03-20 08:38:21 +0900

    refactor: Modify size, direction props(inline style to className('lg', 'sm', 'column')

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,15 +1,11 @@
 import { DEFAULT_USER_IMAGE } from '../../constant/constant';
+import { cn } from '../../utils/classNames';
 import styles from './UserProfile.module.scss';
 
-const UserProfile = ({ title, image, direction = 'row' }) => {
+const UserProfile = ({ title, image, direction = 'row', size }) => {
   return (
-    <section className={`${styles[direction + 'Section']}`}>
-      <img
-        className={styles.userImage}
-        style={{ width: image.size || '5rem' }}
-        src={image.URL || DEFAULT_USER_IMAGE}
-        alt='user'
-      />
+    <section className={cn(styles.layout, styles[direction])}>
+      <img className={cn(styles.userImage, styles[size])} src={image || DEFAULT_USER_IMAGE} alt='user' />
       <p>{title}</p>
     </section>
   );
