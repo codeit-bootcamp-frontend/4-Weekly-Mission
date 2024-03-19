d30a4bb6b218310057253145acc6fc821f083419
whai2 <whai2125@gmail.com>
2024-03-19 16:13:44 +0900

    feat: 테일윈드 설치

---
+++ README.md
@@ -0,0 +1,2 @@
+# css 적용
+- [ ] css 적용 방식을 선택한다. (css in js vs tailwind)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
