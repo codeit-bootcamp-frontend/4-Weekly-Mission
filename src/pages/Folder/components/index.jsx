f6ce0868478549508a22d870f55478ffed072cae
Taejin Kim <rlaxowls1316@naver.com>
2024-03-19 19:16:21 +0900

    모든 파일 제거

--- src/pages/Folder/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import { FolderAddLinkArea } from "./FolderAddLinkArea";
-import { FolderCategory } from "./FolderCategory";
-import { FolderControl } from "./FolderControl";
-import { FolderHeader } from "./FolderHeader";
-
-export { FolderAddLinkArea, FolderCategory, FolderControl, FolderHeader };
--- src/pages/Lading/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function Landing() {
-  return <div>랜딩 페이지 입니다.</div>;
-}
