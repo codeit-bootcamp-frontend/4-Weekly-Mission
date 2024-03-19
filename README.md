33429b08059daf8626def49162cc037e4cb63fe3
whai2 <whai2125@gmail.com>
2024-03-19 17:07:50 +0900

    fix : import 오류 수정

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # css 적용
-- [ ] css 적용 방식을 선택한다. (css in js vs tailwind)
+- [x] css 적용 방식을 선택한다. (css in js vs tailwind)
+
+# app Router 적용
+- [x] 앱 라우터를 적용한다.
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import HomePage from "./home-page"
 
 const inter = Inter({ subsets: ['latin'] })
 
