1d446bc48e9d1a08ab0ee9deda0fc58569b9edaa
whai2 <whai2125@gmail.com>
2024-03-19 20:05:44 +0900

    feat: gnb 컴포넌트, 서버 컴포넌트로 적용

--- components/gnb/Gnb.tsx
+++ components/gnb/Gnb.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import logoImg from '../../public/logo.svg';
+import { getUser } from '@/apis/api';
+import logoImg from '@/public/logo.svg';
 
 import './Gnb.css';
 
@@ -9,7 +10,21 @@ interface GnbProps {
   isFixed: boolean;
 }
 
-const Gnb = ({ isFixed }: GnbProps) => {
+const getProfileData = async () => {
+  try {
+    const { data } = await getUser();
+
+    return data;
+  } catch {
+    throw new Error('error')
+  }
+}
+
+// server component : 상태와 부수 효과, 생명주기를 가질 수 없음.
+const Gnb = async ({ isFixed }: GnbProps) => {
+  const [user] = await getProfileData();
+  console.log(user.image_source);
+
   const gnbStyle: React.CSSProperties = {
     position: isFixed ? 'sticky' : 'static',
     top: 0,
@@ -22,17 +37,13 @@ const Gnb = ({ isFixed }: GnbProps) => {
         <Link href="/">
           <Image className='logo' src={logoImg} alt="홈으로 연결된 Linkbrary 로고" />
         </Link>
-        {/* {isShowProfile ? (
-          <div className='account'>
-            <img className="account-img" src={user.image_source} alt={`${user.email} 프로필`} />
-            <span className='account-email'>{user.email}</span>
-          </div>
-        ) : (
-          <button className='login-button' type='button'>로그인</button>
-        )} */}
+        <div className='account'>
+          <img className="account-img" src={user.image_source} alt={`${user.email} 프로필`} />
+          <span className='account-email'>{user.email}</span>
+        </div>
       </div>
     </nav> 
   )
-}
+} // img 태그 사용 이유 : Image는 최적화가 가능한 대신, public에 존재해야 한다. 즉, 서버에서 다운로드한 이미지는 사용 불가.
 
 export default Gnb;
