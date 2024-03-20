f4e794c8ca77cb2d0a63f6affbc888303747c93d
zoonyoung <9851248@gmail.com>
2024-03-20 09:44:14 +0900

    refactor: Add settings for typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import AppRouter from './AppRouter';
 import { AuthContextProvider } from './context/AuthContext';
-
 import './styles/_reset.scss';
 import './styles/_common.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <>
+  <React.StrictMode>
     <AuthContextProvider>
       <AppRouter />
     </AuthContextProvider>
-  </>
+  </React.StrictMode>
 );
