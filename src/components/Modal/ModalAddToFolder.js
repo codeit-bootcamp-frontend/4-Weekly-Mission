cfa12e2c87cbcbb1b5c61a6ba0804794bd570b20
정기환 <joelkh784@gmail.com>
2024-03-21 11:22:47 +0900

    Refactor: AddFolderContent component outside ModalAddToFolder component

--- src/components/Modal/ModalAddToFolder.js
+++ src/components/Modal/ModalAddToFolder.js
@@ -4,6 +4,33 @@ import { useState } from "react";
 import checkIcon from "../../assets/check.svg";
 import "./ModalAddToFolder.css";
 
+const AddFolderContent = ({ url, folderList, selectedFolder, handleFolderChange, handleAddToFolder }) => (
+  <>
+    <h2>폴더에 추가</h2>
+    <p>{url}</p>
+    <div className="folderOptionsContainer">
+      {folderList.map((folder) => (
+        <div
+          key={folder.id}
+          className={`folderOption ${selectedFolder === folder.id ? "selectedFolder" : ""}`}
+          onClick={() => handleFolderChange(folder.id)}
+        >
+          <h4>{folder.name}</h4>
+          <p>{folder.link.count}개 링크</p>
+          {selectedFolder === folder.id ? (
+            <img src={checkIcon} alt="check" className="checkIcon" />
+          ) : (
+            ""
+          )}
+        </div>
+      ))}
+    </div>
+    <button className="blueButton" onClick={handleAddToFolder}>
+      추가하기
+    </button>
+  </>
+);
+
 const ModalAddToFolder = ({ isOpen, onClose, url }) => {
   const folderList = useFolderList();
   const [selectedFolder, setSelectedFolder] = useState("");
@@ -14,41 +41,18 @@ const ModalAddToFolder = ({ isOpen, onClose, url }) => {
     //implement add to folder logic later
     onClose();
   };
-  const addFolderContent = (
-    <>
-      <h2>폴더에 추가</h2>
-      <p>{url}</p>
-      <div className="folderOptionsContainer">
-        {folderList.map((folder) => (
-          <div
-            key={folder.id}
-            className={`folderOption ${
-              selectedFolder === folder.id ? "selectedFolder" : ""
-            }`}
-            onClick={() => handleFolderChange(folder.id)}
-          >
-            <h4>{folder.name}</h4>
-            <p>{folder.link.count}개 링크</p>
-            {selectedFolder === folder.id ? (
-              <img src={checkIcon} alt="check" className="checkIcon" />
-            ) : (
-              ""
-            )}
-          </div>
-        ))}
-      </div>
-      <button className="blueButton" onClick={handleAddToFolder}>
-        추가하기
-      </button>
-    </>
-  );
 
   return (
-    <ModalContainer
-      isOpen={isOpen}
-      onClose={onClose}
-      children={addFolderContent}
-    />
+    <ModalContainer isOpen={isOpen} onClose={onClose}>
+      <AddFolderContent
+        url={url}
+        folderList={folderList}
+        selectedFolder={selectedFolder}
+        handleFolderChange={handleFolderChange}
+        handleAddToFolder={handleAddToFolder}
+      />
+    </ModalContainer>
   );
 };
+
 export default ModalAddToFolder;
