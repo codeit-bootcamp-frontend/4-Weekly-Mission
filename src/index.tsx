172980032aae55e9549ed82c38f7ad292844cf79
Rhajiit <ksd9638@gmail.com>
2024-03-21 16:56:08 +0900

    💄 Style: 폰트 사이즈 관련 스타일 탐색의 용이성을 위해 분리

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/reset.css";
 import "./index.css";
+import "./styles/font-sizes.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
