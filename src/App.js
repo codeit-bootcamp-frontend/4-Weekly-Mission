fa410fe4b19c1ec1e3dca094e78cfcfeb9212cba
Taeuk471 <asky990223@gmail.com>
2024-03-22 07:45:30 +0900

    :tada: Begin : Week13 Weekly Project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Add from './components/Modal/Add';
 import FolderPage from './pages/FolderPage';
 import Main from './pages/MainPage';
 import SharedPage from './pages/SharedPage';
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Main />} />
       <Route path='folder' element={<FolderPage />} />
       <Route path='shared' element={<SharedPage />} />
+      <Route path='Add' element={<Add />} />
     </Routes>
   );
 };
