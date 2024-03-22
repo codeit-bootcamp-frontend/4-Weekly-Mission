640fbcc630671511f5b115761d1b2223397b6baf
suzinxix <suzinxix@gmail.com>
2024-03-22 13:57:08 +0900

    feat: Home page에 공유와 폴더 페이지 링크 추가

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div>
-      Home
+      Home 페이지
+      <ul>
+        <li>
+          <Link to="folder">폴더 페이지 이동</Link>
+        </li>
+        <li>
+          <Link to="shared">공유 페이지 이동</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default HomePage
-
+export default HomePage;
