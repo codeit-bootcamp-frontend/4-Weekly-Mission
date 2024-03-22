f3ec0b9bdd7948634ce66a308ece702940ada3fe
김경주 <popo505@naver.com>
2024-03-22 13:26:46 +0900

    feat: 폴더에 필요한 링크 들어있는 api 연결

--- src/components/FolderCardList.js
+++ src/components/FolderCardList.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { timeCalculate, getDateText } from "../utils/timeCalc";
 import "../styles/foldercardlist.css";
 
-function FolderCardList({ folderNames }) {
+const defaultCardImg = "img/card_default.png";
+
+function handleImageError(event) {
+  event.target.src = defaultCardImg;
+}
+
+function FolderCardList({ folders }) {
+  const [linkData, setLinkData] = useState([]);
+  const [selectFolderName, setSelectFolderName] = useState("전체");
+
+  async function fetchData(url, folderName = "전체") {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("네트워크 응답이 올바르지 않습니다.");
+      }
+      const data = await response.json();
+      const processedData = data.data.map((link) => ({
+        ...link,
+        timePassed: timeCalculate(link.created_at),
+        getDate: getDateText(new Date(link.created_at)),
+      }));
+      setLinkData(processedData);
+      setSelectFolderName(folderName);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(`https://bootcamp-api.codeit.kr/api/users/1/links`, "전체");
+  }, []);
+
   function handleSubmit(event) {
     event.preventDefault();
   }
 
-  const handleFolderClick = (name) => {
-    console.log(name + " 폴더 클릭");
+  const handleFolderClick = (folderId, folderName) => {
+    const url = folderId ? `https://bootcamp-api.codeit.kr/api/users/1/links?folderId=${folderId}` : `https://bootcamp-api.codeit.kr/api/users/1/links`;
+    fetchData(url, folderName);
   };
 
   return (
@@ -19,18 +53,17 @@ function FolderCardList({ folderNames }) {
       <div className="folder-container">
         <ul className="folderlist">
           <li>
-            <button className="button-folder" onClick={() => handleFolderClick("전체")}>
+            <button className="button-folder" onClick={() => handleFolderClick(null, "전체")}>
               전체
             </button>
           </li>
-          {folderNames &&
-            folderNames.map((name, index) => (
-              <li key={index}>
-                <button className="button-folder" onClick={() => handleFolderClick(name)}>
-                  {name}
-                </button>
-              </li>
-            ))}
+          {folders.map((folder) => (
+            <li key={folder.id}>
+              <button className="button-folder" onClick={() => handleFolderClick(folder.id, folder.name)}>
+                {folder.name}
+              </button>
+            </li>
+          ))}
         </ul>
         <button className="button-folderadd">
           폴더추가
@@ -40,6 +73,21 @@ function FolderCardList({ folderNames }) {
           폴더추가
           <img src="img/add_mobile.svg" alt="addbutton-mobile" />
         </button>
+      </div>
+      <div className="folder-name">{selectFolderName && <p>{selectFolderName}</p>}</div>
+      <div className="card-container">
+        {linkData.map((card) => (
+          <a className="card" key={card.id} href={card.url} target="_blank" rel="noopener noreferrer">
+            <div className="image-container">
+              <img className="card-image" src={card.image_source || defaultCardImg} alt={card.title} onError={handleImageError} />
+            </div>
+            <div className="text-container">
+              <span className="text-time">{card.timePassed}</span>
+              <p className="text-description">{card.title}</p>
+              <p className="text-date">{card.getDate}</p>
+            </div>
+          </a>
+        ))}
       </div>
     </div>
   );
