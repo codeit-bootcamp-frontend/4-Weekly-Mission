016b4a448a67373c365b58542b15059c650c95e5
박지윤 <tkrhkfld123@naver.com>
2024-03-22 15:58:25 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      안ㄴㅇ뇨ㅛㄴ어
     </div>
   );
 }
