c01ec0bbefdb7c14fb57c03d1fa787061f7f2a22
정기환 <joelkh784@gmail.com>
2024-03-22 14:22:42 +0900

    Feat: AddLinkBar becomes visible on bottom using intersection observer

--- src/pages/FolderPage.tsx
+++ src/pages/FolderPage.tsx
@@ -3,18 +3,48 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AddLinkBar from '../components/FolderPage/AddLinkBar';
 import SearchBar from '../components/SearchBar';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const FolderPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [isAddLinkBarVisible, setIsAddLinkBarVisible] = useState(true);
+
+  const addLinkBarRef = useRef(null);
+
+  useEffect(() => {
+    const current = addLinkBarRef.current;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsAddLinkBarVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
+      }
+    );
+
+    if (current) {
+      observer.observe(current);
+    }
+
+    return () => {
+      if (current) {
+        observer.unobserve(current);
+      }
+    };
+  }, []);
 
   return (
     <>
       <Header />
       <main>
-        <AddLinkBar />
+        <div ref={addLinkBarRef}>
+          <AddLinkBar />
+        </div>
         <SearchBar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
         <FolderList searchQuery={searchQuery} />
+        {!isAddLinkBarVisible && <AddLinkBar />}
       </main>
       <Footer />
     </>
