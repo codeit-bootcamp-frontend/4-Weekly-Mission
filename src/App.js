eb3583a232a9bc040f4bbaf9d4eacb21aa219f35
MinJoon_Choi <minjoon1998@gmail.com>
2024-03-22 20:39:36 +0900

    코드 전면 수정

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
-import { useEffect, useState } from 'react';
-import { getUserData, getFolderData } from './api/api';
-import Header from './components/common/header/Header';
-import Main from './components/main/Main';
-import Footer from './components/common/footer/Footer';
-import './common.css';
+import "sharing/styles/reset.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SharedPage } from "pages/SharedPage";
+import { FolderPage } from "pages/FolderPage";
 
 function App() {
-  const [user, setUser] = useState([]);
-  const [folderOwner, setFolderOwner] = useState([]);
-  const [folderName, setFolderName] = useState('');
-  const [cardData, setCardData] = useState([]);
-
-  // get user sample data from api
-  const getUserInformation = async () => {
-    const userInformation = await getUserData();
-    setUser(userInformation);
-  };
-  useEffect(() => {
-    getUserInformation();
-  }, []);
-
-  // get folder sample data from api
-  const getFolderInformation = async () => {
-    const folderInformation = await getFolderData();
-    setFolderOwner(folderInformation.folder.owner);
-    setFolderName(folderInformation.folder.name);
-    setCardData(folderInformation.folder.links);
-  };
-  useEffect(() => {
-    getFolderInformation();
-  }, []);
   return (
-    <>
-      <Header user={user} />
-      <Main
-        user={user}
-        folderOwner={folderOwner}
-        folderName={folderName}
-        cardData={cardData}
-      />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/shared" element={<SharedPage />} />
+        <Route path="/folder" element={<FolderPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
