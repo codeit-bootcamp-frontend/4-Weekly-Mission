052c4df8365cf19a23093bff3f4032d1d304ee58
jinhok96 <jinhok96a@gmail.com>
2024-03-23 18:40:33 +0900

    chore: eslint 설정 추가 	modified:   .eslintrc.js 	modified:   package-lock.json 	modified:   package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'airbnb',
     'plugin:prettier/recommended',
@@ -16,6 +17,7 @@ module.exports = {
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
+      tsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
