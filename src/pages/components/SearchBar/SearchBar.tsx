c284a33bd118c092c59b5ff2819ccca09a2ec0f2
codingaring <sksms2104@naver.com>
2024-03-23 16:50:00 +0900

    feat : 검색창에 입력된 입력 값을 지우는 버튼 기능 추가

--- src/pages/components/SearchBar/SearchBar.tsx
+++ src/pages/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,36 @@
+import { ChangeEvent, useState } from "react";
 import "./SearchBar.css";
 
 export const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputEmpty = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputClean = () => {
+    setInputValue("");
+  };
+
   return (
     <div className="SearchBar">
       <div className="SearchBar-icon" />
       <input
+        value={inputValue}
         className="SearchBar-input"
         type="search"
         placeholder="링크를 검색해 보세요."
+        onChange={handleInputEmpty}
       />
+      {inputValue && (
+        <button
+          type="button"
+          className="delete-button"
+          onClick={handleInputClean}
+        >
+          X
+        </button>
+      )}
     </div>
   );
 };
