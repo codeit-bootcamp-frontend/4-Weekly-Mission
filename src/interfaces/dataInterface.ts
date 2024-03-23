a28911fc49753b9dbda492724a581b74d3d66d6d
sihyonn <sihyeon982@naver.com>
2024-03-24 04:42:56 +0900

    feat: 컴포넌트 및 서버데이터 관련 공통인터페이스 파일 생성

---
+++ src/interfaces/dataInterface.ts
@@ -0,0 +1,37 @@
+export interface Link {
+  id: number;
+  title: string;
+  url: string;
+  description: string;
+  createdAt: string;
+  imageSource: string;
+}
+
+export interface FolderInfoData {
+  id: number;
+  count: number;
+
+  name: string;
+  links: Link[];
+  owner: {
+    name: string;
+    profileImageSource: string;
+  };
+}
+
+export interface UserInfoData {
+  auth_id: string;
+  id: number;
+  email: string;
+  name: string;
+  image_source: string;
+  created_at: string;
+}
+
+export interface UserLinksData {
+  id: number;
+  url: string;
+  description: string;
+  image_source: string | null;
+  created_at: string;
+}
