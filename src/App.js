c6d60770997eac1ec4008a5a3886194b4ba3c197
Harry <yjkang1223@naver.com>
2024-03-23 14:38:01 +0900

    fix: Fix Page not loading issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { SharedPage } from "./page-layout/SharedPage";
-import { FolderPage } from "./page-layout/FolderPage";
+import { SharedPage } from "./page-layout/SharedPage/SharedPage";
+import { FolderPage } from "./page-layout/FolderPage/FolderPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./global.css";
 
