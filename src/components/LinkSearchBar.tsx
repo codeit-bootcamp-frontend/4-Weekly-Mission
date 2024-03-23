44b048ecd31d2e2064e418df2d5da711e16d874c
jae6269 <jae6269@gmail.com>
2024-03-24 02:06:59 +0900

    feat: LinkSearchBar 컴포넌트에서 input에 값을 입력하면 아이콘 랜더링, 값이 없으면 삭제 구현

--- src/components/LinkSearchBar.tsx
+++ src/components/LinkSearchBar.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import searchIcon from '../assets/searchIcon.svg';
+import searchClearIcon from '../assets/searchClearIcon.svg';
 import '../styles/linkSearchBar.css';
+
 function LinkSearchBar() {
+  const [inputValue, setInputValue] = useState<string>();
   const placeholder = '링크를 검색해 보세요.';
+
+  const handleInputValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+  };
+
   return (
-    <form className="cards-container__search">
-      <img className="search--icon" src={searchIcon} alt="searchIcon"></img>
-      <input className="search--input" placeholder={placeholder}></input>
-    </form>
+    <div className="cards-container__search">
+      <img className="search--icon" src={searchIcon} alt="searchIcon" />
+      <input
+        className="search--input"
+        placeholder={placeholder}
+        onChange={handleInputValueChange}
+      />
+      {inputValue && (
+        <img
+          src={searchClearIcon}
+          alt="search-clear"
+          className="search-clear--icon"
+        />
+      )}
+    </div>
   );
 }
 export default LinkSearchBar;
