73b422e17242835e1ccb14d9156dcf1c70de6c2d
sihyonn <sihyeon982@naver.com>
2024-03-23 16:33:43 +0900

    refactor: constants 내 파일 마이그레이션

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,25 +1,28 @@
+type UserId = number;
+type FolderId = number;
+
 const SAMPLE = Object.freeze({
   FOLDER: '/sample/folder',
   USER: '/sample/user',
 });
 
 const USER = Object.freeze({
-  DETAIL: (userId) => {
+  DETAIL: (userId: UserId) => {
     return `/users/${userId}`;
   },
 });
 
 const FOLDER = Object.freeze({
-  USER_FOLDERS: (userId) => {
+  USER_FOLDERS: (userId: UserId) => {
     return `/users/${userId}/folders`;
   },
 });
 
 const LINK = Object.freeze({
-  USER_LINKS: (userId) => {
+  USER_LINKS: (userId: UserId) => {
     return `/users/${userId}/links`;
   },
-  USER_SPECIFIC_LINKS: (userId, folderId) => {
+  USER_SPECIFIC_LINKS: (userId: UserId, folderId: FolderId) => {
     return `/users/${userId}/links?folderId=${folderId}`;
   },
 });
