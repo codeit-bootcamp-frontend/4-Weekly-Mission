f4abd552de2a3f311a5bf65c0b130f42035b76c5
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-03-24 02:23:37 +0900

    Fix: merge 잘못된부분 오류 수정

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Main from './Main';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
