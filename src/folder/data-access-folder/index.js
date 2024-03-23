46bd2c8253c7492dc30f20d72750d565cb49610f
namju <skawn1228@gmail.com>
2024-03-24 00:07:53 +0900

    fix: git 충돌 해결

--- src/folder/data-access-folder/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./useGetFolder";
-export * from "./useGetFolders";
--- src/sharing/ui-modal-content-title/ModalContentTitle.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./ModalContentTitle.module.scss";
-import classNames from "classnames/bind";
-
-const cx = classNames.bind(styles);
-
-export const ModalContentTitle = ({ children }) => {
-  return <h2 className={cx("title")}>{children}</h2>;
-};
