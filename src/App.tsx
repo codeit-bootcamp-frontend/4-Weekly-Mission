db22eb95dcf3c5d28bcbb0616b92e645e13b5727
박지윤 <tkrhkfld123@naver.com>
2024-03-24 00:32:42 +0900

    reractor: BrowserRouter 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Folder from "./pages/folder";
 import Shared from "./pages/shared";
 
