b05d8d3b264841ac86bef1ff7c7bce46ca7414e1
seoyeon <ysynop7@namver.com>
2024-03-24 04:40:14 +0900

    Feat: shared 페이지 검색 기능 구현

--- src/Pages/SharedPage.tsx
+++ src/Pages/SharedPage.tsx
@@ -1,12 +1,42 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import Header from "../components/Header";
 import LinkList from "../components/LinkList";
 import SearchBar from "../components/SearchBar";
 import { getSampleFolderLinks } from "../api/api";
 
+interface Data {
+  url: string;
+  id: number;
+  imageSource: string;
+  createdAt: string;
+  description: string;
+}
+
+interface SearchData extends Data {
+  title: string;
+}
 
 function SharedPage() {
-  const [linkData, setLinkData] = useState([]);
+  const [linkData, setLinkData] = useState<SearchData[]>([]);
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const handleCloseButtonClick = (e: MouseEvent) => {
+    setSearch('');
+  }
+
+  const searchData = linkData.filter((data) => {
+    const SEARCH_WORD = search.toLowerCase();
+    const URL = data.url.toLowerCase();
+    const TITLE = data.title.toLowerCase();
+    const DESCRIPTION = data.description.toLowerCase();
+    
+    return URL.includes(SEARCH_WORD) || TITLE.includes(SEARCH_WORD) || DESCRIPTION.includes(SEARCH_WORD)
+  })
+
 
   const getLinkData = async () => {
     const { folder } = await getSampleFolderLinks();
@@ -23,8 +53,8 @@ function SharedPage() {
   return (
     <>
       <Header />
-      <SearchBar />
-      <LinkList linkData={linkData}/>
+      <SearchBar inputValue={search} onChange={handleChange} onClick={handleCloseButtonClick}/>
+      <LinkList linkData={searchData}/>
     </>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,29 @@
 import styles from '../css/SearchBar.module.css'
+import closeIcon from '../images/Icon_close.svg';
 
-function SearchBar() {
+function SearchBar({ inputValue, onChange, onClick }: any) {
   return(
-    <form>
+    <div className={styles.searchBar}>
       <div className={styles.container}>
         <div className={styles.searchIcon}></div>
-        <input name="search" placeholder='링크를 검색해 보세요.' />
+        <input
+          type="text"
+          name="search"
+          value={inputValue}
+          onChange={onChange}
+          placeholder='링크를 검색해 보세요.'
+        />
+        {inputValue && (
+          <button
+            className={styles.closeBtn}
+            onClick={onClick}
+          >
+            <img className={styles.closeImg} src={closeIcon} alt='closeBtn'/>
+          </button>
+          )
+        }
       </div>
-    </form>
+    </div>
   );
 }
 
