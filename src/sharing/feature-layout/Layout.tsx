81f25498e906c654b877899c8a5acb181cb4be96
minho <kasamin98@naver.com>
2024-03-24 15:42:39 +0900

    refactor: Rename jsx to tsx files 1차 중간커밋

--- src/sharing/feature-layout/Layout.tsx
+++ src/sharing/feature-layout/Layout.tsx
@@ -3,12 +3,20 @@ import classNames from "classnames/bind";
 import { useGetUser } from "user/data-access-user";
 import { Footer } from "sharing/ui-footer";
 import { NavigationBar } from "sharing/ui-navigation-bar";
+import { ReactNode } from "react";
 
 const cx = classNames.bind(styles);
-
-export const Layout = ({ children, isSticky = true }) => {
+interface Props {
+  children: ReactNode;
+  isSticky?: true | false;
+}
+interface DataType {
+  email?: string;
+  profileImageSource?: string;
+}
+export const Layout = ({ children, isSticky = true }: Props) => {
   const { data } = useGetUser();
-  const { email, profileImageSource } = data || {};
+  const { email, profileImageSource }: DataType = data || {};
   const profile = data ? { email, profileImageSource } : null;
 
   return (
--- src/sharing/ui-navigation-bar/NavigationBar.tsx
+++ src/sharing/ui-navigation-bar/NavigationBar.tsx
@@ -4,15 +4,24 @@ import { ROUTE } from "sharing/util";
 import { Cta } from "sharing/ui-cta";
 import { Profile } from "user/ui-profile";
 import { LOGO_IMAGE, TEXT } from "./constant";
-
+interface Props {
+  profile?: {
+    email: string | undefined;
+    profileImageSource: string | undefined;
+  } | null;
+  isSticky?: boolean;
+}
 const cx = classNames.bind(styles);
-
-export const NavigationBar = ({ profile, isSticky }) => {
+export const NavigationBar = ({ profile, isSticky }: Props) => {
   return (
     <nav className={cx("container", { sticky: isSticky })}>
       <div className={cx("items")}>
         <a href={ROUTE.랜딩}>
-          <img className={cx("logo")} src={LOGO_IMAGE} alt="Linkbrary 서비스 로고" />
+          <img
+            className={cx("logo")}
+            src={LOGO_IMAGE}
+            alt="Linkbrary 서비스 로고"
+          />
         </a>
         {profile ? (
           <Profile profile={profile} />
