9d86b43b8c9dfba19952b2d84a113f2ed5dff2f1
zoonyoung <9851248@gmail.com>
2024-03-24 21:25:37 +0900

    refactor: Remove Link context

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,12 @@ import AppRouter from './AppRouter';
 import { AuthContextProvider } from './context/AuthContext';
 import './styles/_reset.scss';
 import './styles/_common.scss';
-import { LinkContextProvider } from './context/LinkContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <LinkContextProvider>
-        <AppRouter />
-      </LinkContextProvider>
+      <AppRouter />
     </AuthContextProvider>
   </React.StrictMode>
 );
