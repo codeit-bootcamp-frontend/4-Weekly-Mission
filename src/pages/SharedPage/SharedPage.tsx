8d63a2f3a1609e038f9a4d9fa1ab4b571fb0d5e2
김보경 <rlaqhrud6263@naver.com>
2024-03-25 00:36:31 +0900

    feat: SharedPage 수정

--- src/pages/SharedPage/SharedPage.tsx
+++ src/pages/SharedPage/SharedPage.tsx
@@ -3,7 +3,7 @@ import { CardList } from "@components/CardList";
 import { Profile } from "@components/Profile";
 import Footer from "@components/Footer/Footer";
 import { END_POINT } from "@/constants";
-// import CardSearchInput from "@components/CardListBox/CardSearchInput/CardSearchInput";
+import CardSearchInput from "@components/CardListBox/CardSearchInput/CardSearchInput";
 import styles from "./SharedPage.module.scss";
 import { CSSProperties, useEffect, useState } from "react";
 import { FolderData, LinkProps } from "@/constants/index.types";
@@ -12,7 +12,9 @@ import useFetchFolder from "@/hooks/useFetchFolder";
 export default function SharedPage() {
   const [fetchData, setFetchData] = useState<FolderData>();
   const [linksData, setLinksData] = useState<LinkProps[] | undefined>([]);
-  // const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [value, setValue] = useState("");
+
   const { data } = useFetchFolder<FolderData>(END_POINT.sharedLinks);
   const headerPosition: CSSProperties = {
     position: "fixed",
@@ -25,29 +27,39 @@ export default function SharedPage() {
     }
   }, [data]);
 
-  //제대로 동작하지 않는것 같습니다
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const inputValue = e.target.value;
-  //   setSearchValue(inputValue);
-  // };
+  const handleSearchInputCancel = () => {
+    setSearchValue("");
+    setLinksData(data?.links);
+    setValue("");
+  };
 
-  // useEffect(() => {
-  //   if (searchValue !== "") {
-  //     const filteredLinks = linksData?.filter((link) => {
-  //       return Object.values(link).join("").includes(searchValue);
-  //     });
-  //     setLinksData(filteredLinks);
-  //   } else {
-  //     setLinksData(data?.links);
-  //   }
-  // }, [searchValue]);
+  //제대로 동작하지 않는것 같습니다
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setSearchValue(inputValue);
+  };
+
+  useEffect(() => {
+    if (searchValue !== "") {
+      const filteredLinks = linksData?.filter((link) => {
+        return Object.values(link).join("").includes(searchValue);
+      });
+      setLinksData(filteredLinks);
+    } else {
+      setLinksData(data?.links);
+    }
+  }, [searchValue]);
 
   return (
     <>
       <Header headerPosition={headerPosition} />
       <Profile fetchData={fetchData} />
       <section className={styles.CardListBox}>
-        {/* <CardSearchInput handleInputChange={handleInputChange} /> */}
+        <CardSearchInput
+          value={value}
+          handleInputChange={handleInputChange}
+          handleSearchInputCancel={handleSearchInputCancel}
+        />
         <CardList links={linksData} />
       </section>
       <Footer />
