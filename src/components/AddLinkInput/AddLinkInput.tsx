f63e9b21ebf4fe28f6baf6c634f7ed45118f2f05
김보경 <rlaqhrud6263@naver.com>
2024-03-25 22:00:47 +0900

    refactor: AddLinkInput - intersectionObserver 적용

--- src/components/AddLinkInput/AddLinkInput.tsx
+++ src/components/AddLinkInput/AddLinkInput.tsx
@@ -1,49 +1,44 @@
-import { CSSProperties, useEffect, useRef, useState } from "react";
 import AddLinkInputGlassIcon from "@/assets/svg/AddLinkInputGlassIcon";
 import styles from "./AddLinkInput.module.scss";
 
 export default function AddLinkInput() {
-  const [isAddLinkInputVisible, setIsAddLinkInputVisible] = useState(true);
-  const addLinkInputRef = useRef<HTMLElement>(null);
+  // const [isAddLinkInputVisible, setIsAddLinkInputVisible] = useState(true);
+  // const addLinkInputRef = useRef<HTMLElement>(null);
 
-  const addLinkInputStyle: CSSProperties = !isAddLinkInputVisible
-    ? {
-        position: "fixed",
-        bottom: "0px",
-        padding: "24px 32px",
-        width: "100%",
-      }
-    : {};
+  // const addLinkInputStyle: CSSProperties = !isAddLinkInputVisible
+  //   ? {
+  //       position: "fixed",
+  //       bottom: "0px",
+  //       padding: "24px 32px",
+  //       width: "100%",
+  //     }
+  //   : {};
 
-  // observer 시작
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          setIsAddLinkInputVisible(entry.isIntersecting);
-        });
-      },
-      { threshold: 0.01 }
-    );
+  // // observer 시작
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         setIsAddLinkInputVisible(entry.isIntersecting);
+  //       });
+  //     },
+  //     { threshold: 0.01 }
+  //   );
 
-    const addLinkInput = addLinkInputRef.current;
-    if (addLinkInput) {
-      observer.observe(addLinkInput);
-    }
+  //   const addLinkInput = addLinkInputRef.current;
+  //   if (addLinkInput) {
+  //     observer.observe(addLinkInput);
+  //   }
 
-    return () => {
-      if (addLinkInput) {
-        observer.unobserve(addLinkInput);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (addLinkInput) {
+  //       observer.unobserve(addLinkInput);
+  //     }
+  //   };
+  // }, []);
 
   return (
-    <section
-      className={styles.AddLinkInput}
-      style={addLinkInputStyle}
-      ref={addLinkInputRef}
-    >
+    <section className={styles.AddLinkInput}>
       <form className={styles.form}>
         <div className={styles.inputBox}>
           <AddLinkInputGlassIcon />
--- src/pages/FolderPage/FolderPage.tsx
+++ src/pages/FolderPage/FolderPage.tsx
@@ -1,19 +1,58 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useRef, useState } from "react";
 import { Header } from "@components/Header";
 import AddLinkInput from "@components/AddLinkInput/AddLinkInput";
 import CardListBox from "@components/CardListBox/CardListBox";
 import Footer from "@components/Footer/Footer";
+import styles from "./FolderPage.module.scss";
 
 export default function FolderPage() {
   const headerPosition: CSSProperties = {
     position: "relative",
   };
 
+  // 작업중
+  const [isAddLinkInputVisible, setIsAddLinkInputVisible] = useState(true);
+  const addLinkInputRef = useRef<HTMLDivElement>(null);
+
+  const addLinkInputStyle: CSSProperties = isAddLinkInputVisible
+    ? {
+        visibility: "hidden",
+      }
+    : {};
+
+  // observer 시작
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          setIsAddLinkInputVisible(entry.isIntersecting);
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    const addLinkInput = addLinkInputRef.current;
+    if (addLinkInput) {
+      observer.observe(addLinkInput);
+    }
+
+    return () => {
+      if (addLinkInput) {
+        observer.unobserve(addLinkInput);
+      }
+    };
+  }, []);
+
   return (
     <>
       <Header headerPosition={headerPosition} />
-      <AddLinkInput />
+      <div className={styles.staticAddLinkInput} ref={addLinkInputRef}>
+        <AddLinkInput />
+      </div>
       <CardListBox />
+      <div className={styles.fixdAddLinkInput} style={addLinkInputStyle}>
+        <AddLinkInput />
+      </div>
       <Footer />
     </>
   );
