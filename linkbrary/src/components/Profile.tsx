16a58165649c5948870eebadce3d09cdd945ceb3
Hooni07 <gkxogns21@gmail.com>
2024-03-26 21:09:22 +0900

    fixed: change jsx->tsx in components

--- linkbrary/src/components/Profile.tsx
+++ linkbrary/src/components/Profile.tsx
@@ -1,13 +1,20 @@
+import React from "react";
 import "../styles/Profile.css";
 
-function Profile({ folder, user, proImg }) {
+interface ProfileProps {
+  folder: string;
+  user: string;
+  proImg: string;
+}
+
+const Profile: React.FC<ProfileProps> = ({ folder, user, proImg }) => {
   return (
-    <profile className="profile">
+    <div className="profile">
       <img src={proImg} alt="프로필 사진" className="img-profile" />
       <p className="user-name">{user}</p>
       <h1 className="folder-name">{folder}</h1>
-    </profile>
+    </div>
   );
-}
+};
 
 export default Profile;
