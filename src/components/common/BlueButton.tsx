b5fb74beaade4a0949c2115ea04d6bc52009d894
DoDOhyeon <118147033+DoDOhyeon@users.noreply.github.com>
2024-03-27 11:10:35 +0900

    js에서 ts변환 test

---
+++ src/components/common/BlueButton.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { styled } from "styled-components";
+import { COLORS } from "../../constants/colors";
+
+export interface ButtonProps {
+  width?: string;
+  height?: string;
+  radius?: string;
+  margin?: string;
+  padding?: string;
+  fontSize?: string;
+}
+
+export const BlueButton = ({
+  text,
+  width,
+  height,
+  margin,
+  padding,
+  fontSize,
+  radius,
+  onBtnHandle,
+}) => {
+  return (
+    <Button
+      width={width}
+      height={height}
+      margin={margin}
+      padding={padding}
+      color={COLORS.White}
+      fontSize={fontSize}
+      radius={radius}
+      onClick={(e) => (onBtnHandle ? onBtnHandle() : null)}
+    >
+      {text}
+    </Button>
+  );
+};
+
+const Button = styled.button<ButtonProps>`
+  display: block;
+  width: ${({ width }) => width || "auto"};
+  min-width: max-content;
+  height: ${({ height }) => height || "auto"};
+  border: 0px;
+  border-radius: ${({ radius }) => radius || "0px"};
+  margin: ${({ margin }) => margin || "auto"};
+  padding: ${({ padding }) => padding || "auto"};
+  background: linear-gradient(91deg, #6d6afe 0.12%, #6ae3fe 101.84%);
+  cursor: pointer;
+
+  color: ${({ color }) => color};
+  font-size: ${({ fontSize }) => fontSize || "14px"};
+  font-weight: 600;
+  line-height: 21.6px;
+`;
---
+++ src/components/home/Headline.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { styled } from "styled-components";
+import { BlueButton } from "../common/BlueButton";
+
+export const Headline = () => {
+  return (
+    <Container>
+      <TextBox>
+        <HeadlineText>
+          <Strong>세상의 모든 정보</Strong>를<br />
+          쉽게 저장하고 <LineBreak />
+          관리해 보세요.
+        </HeadlineText>
+      </TextBox>
+      <BlueButton
+        text="링크 추가하기"
+        width="350px"
+        margin="40px auto"
+        padding="10px 20px"
+        fontSize="18px"
+        radius="8px"
+      />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextBox = styled.p`
+  width: 100%;
+  text-align: center;
+`;
+
+const HeadlineText = styled.span`
+  font-size: 64px;
+  font-weight: 700;
+
+  @media (max-width: 774px) {
+    font-size: 32px;
+  }
+`;
+
+const Strong = styled(HeadlineText)`
+  background: linear-gradient(90deg, #6d6afe, #ff9f9f);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const LineBreak = styled.br`
+  display: none;
+
+  @media (max-width: 1124px) {
+    display: block;
+  }
+`;
