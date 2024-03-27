04021ae2fb987a8091ff1e57b5c0eed58e6bb265
SeungAAA <oblossomo1031@naver.com>
2024-03-28 00:08:27 +0900

    [fix] tscnofig 수정

--- src/sharing/util/axiosInstance.ts
+++ src/sharing/util/axiosInstance.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "https://bootcamp-api.codeit.kr/api/",
+  baseURL: 'https://bootcamp-api.codeit.kr/api/',
 });
--- src/user/ui-profile/Profile.tsx
+++ src/user/ui-profile/Profile.tsx
@@ -1,13 +1,20 @@
-import styles from "./Profile.module.scss";
-import classNames from "classnames/bind";
+import styles from './Profile.module.scss';
+import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-export const Profile = ({ profile }) => {
+interface ProfileProps {
+  profile: {
+    profileImageSource: string;
+    email: string;
+  };
+}
+
+export const Profile = ({ profile }: ProfileProps) => {
   return (
-    <div className={cx("container")}>
-      <img className={cx("image")} src={profile.profileImageSource} alt="프로필 이미지" />
-      <span className={cx("email")}>{profile.email}</span>
+    <div className={cx('container')}>
+      <img className={cx('image')} src={profile.profileImageSource} alt='프로필 이미지' />
+      <span className={cx('email')}>{profile.email}</span>
     </div>
   );
 };
