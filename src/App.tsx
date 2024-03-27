41a70d6eb7d97766b802e424e0c73c7e6e5c57ce
hong <hsw5971@naver.com>
2024-03-27 17:49:22 +0900

    타입스크립트 마이그레이션

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from '../src/components/pages/homePage/HomePage';
-import SharedPage from '../src/components/pages/sharedPage/SharedPage';
+import HomePage from './components/pages/homePage/HomePage';
+import SharedPage from './components/pages/sharedPage/SharedPage';
 import FolderPage from './components/pages/folderPage/FolderPage';
 
 function App() {
