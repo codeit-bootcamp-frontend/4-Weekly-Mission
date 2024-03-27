c836ea1f198f0e5a2e1096d874d3a29887a7d021
Yeojin Yoon <roseiris1001@gmail.com>
2024-03-28 08:12:43 +0900

    단순 주석 제거

--- src/components/area/SearchTopBodyArea.js
+++ src/components/area/SearchTopBodyArea.js
@@ -2,7 +2,6 @@ import AddSearchLink from '../common/AddSearchLink';
 import './SearchTopBodyArea.css';
 
 const SearchTopBodyArea = ({ folderListData }) => {
-  console.log('folderListData:::', folderListData);
   return (
     <div className="search_top_area">
       <AddSearchLink folderList={folderListData} />
