9a465898ced198639c3025e2063b615488e35a02
minbo <smb5174@naver.com>
2024-03-28 00:17:31 +0900

    feat: 닫기 버튼 추가

--- src/componenets/SearchBar.tsx
+++ src/componenets/SearchBar.tsx
@@ -1,11 +1,14 @@
 import searchBar from "../images/searchBar.svg";
 import "./SearchBar.css";
-
+import close from "../images/_close.png";
 function SearchBar() {
   return (
     <form className="SearchBar-Form">
       <img src={searchBar} alt="링크 검색" />
       <input placeholder="링크를 검색해 보세요." />
+      <div className="close-div">
+        <img src={close} alt="닫기" />
+      </div>
     </form>
   );
 }
