d20330f3e73d47b9c31e791a88e21e238ebce749
minbo <smb5174@naver.com>
2024-03-27 21:04:46 +0900

    feat: SharedFolder.tsx 변환

--- src/componenets/modal/SharedFolder.tsx
+++ src/componenets/modal/SharedFolder.tsx
@@ -3,17 +3,22 @@ import "./modal.css";
 import kakao from "../../images/Kakao.svg";
 import facebook from "../../images/modal-Facebook.svg";
 import icon from "../../images/modal-link.svg";
-import { useEffect } from "react";
+import { SyntheticEvent } from "react";
+// import { useEffect } from "react";
 
-function SharedFolder({ toggleModal }) {
-  const { Kakao } = window;
+type SharedFolderType = {
+  toggleModal: (id: number) => void;
+};
+
+function SharedFolder({ toggleModal }: SharedFolderType) {
+  // const { Kakao } = window;
   const shareUrl = `${window.location.origin}/shared/${1}`;
 
-  useEffect(() => {
-    if (window.Kakao) {
-      Kakao.init("YOUR_KAKAO_APP_KEY");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.Kakao) {
+  //     Kakao.init("YOUR_KAKAO_APP_KEY");
+  //   }
+  // }, []);
 
   function copyToClipboard() {
     const link = "https://your-shared-link.com";
@@ -27,20 +32,20 @@ function SharedFolder({ toggleModal }) {
     );
   }
 
-  const shareKakao = () => {
-    if (window.Kakao && Kakao.Share) {
-      Kakao.Share.sendDefault({
-        objectType: "text",
-        text: "세상에 모든 정보를 쉽게 저장하고 관리해보세요",
-        link: {
-          mobileWebUrl: shareUrl,
-          webUrl: shareUrl,
-        },
-      });
-    } else {
-      alert("Kakao SDK가 로드되지 않았거나 초기화되지 않았습니다.");
-    }
-  };
+  // const shareKakao = () => {
+  //   if (window.Kakao && Kakao.Share) {
+  //     Kakao.Share.sendDefault({
+  //       objectType: "text",
+  //       text: "세상에 모든 정보를 쉽게 저장하고 관리해보세요",
+  //       link: {
+  //         mobileWebUrl: shareUrl,
+  //         webUrl: shareUrl,
+  //       },
+  //     });
+  //   } else {
+  //     alert("Kakao SDK가 로드되지 않았거나 초기화되지 않았습니다.");
+  //   }
+  // };
 
   function shareToFacebook() {
     const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
@@ -48,13 +53,17 @@ function SharedFolder({ toggleModal }) {
     )}`;
     window.open(facebookLink, "_blank");
   }
+  function handleImageClick(event: SyntheticEvent) {
+    event.preventDefault();
+    toggleModal(7);
+  }
 
   return (
     <div className="modal">
       <form className="modal-form">
         <div className="modal-form-content">
           <img
-            onClick={toggleModal}
+            onClick={handleImageClick}
             className="modal-form-img"
             src={close}
             alt="닫기"
@@ -64,7 +73,7 @@ function SharedFolder({ toggleModal }) {
             <p className="modal-form-folderName">폴더명</p>
           </div>
           <div className="modal-icon-box">
-            <div className="modal-icon-content" onClick={shareKakao}>
+            <div className="modal-icon-content">
               <div className="modal-kakaoIcon-img">
                 <img src={kakao} alt="카카오톡"></img>
               </div>
