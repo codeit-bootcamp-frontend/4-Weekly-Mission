f6622a3f725fdcea8f8b54d2be69ff027ed9a4d5
jinhok96 <jinhok96a@gmail.com>
2024-03-27 16:11:55 +0900

    chore: FloatingAddFolderButton 클릭 시 모달 열기 	modified:   src/pages/FolderPage/FolderPage.tsx

--- src/pages/FolderPage/FolderPage.tsx
+++ src/pages/FolderPage/FolderPage.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import { useState } from 'react';
 
+import useModal from 'hooks/useModal';
+
 import FloatingAddFolderButton from 'components/Common/FloatingAddFolderButton';
 import Footer from 'components/Footer/Footer';
 import FolderHeaderContent from 'components/Header/FolderHeaderContent';
@@ -10,11 +12,24 @@ import Main from 'components/Main/Main';
 import SearchBar from 'components/Main/SearchBar';
 import SortingSection from 'components/Main/SortingSection';
 
+import { modalList } from 'context/Modal';
+
 import styles from 'pages/FolderPage/FolderPage.module.css';
 
 function Folder() {
   const ALL = { id: 0, name: '전체' };
   const [selectedFolder, setSelectedFolder] = useState(ALL);
+  const { openModal } = useModal();
+
+  const handleAddFolderButtonClick = () => {
+    console.log('AddForderModal');
+
+    const handleAddFolder = () => {
+      console.log('handleAddFolder');
+    };
+
+    openModal(modalList.AddForderModal, { onSubmit: handleAddFolder });
+  };
 
   const floatingAddFolderButtonClasses = classNames(
     styles['floating-add-folder-button'],
@@ -34,7 +49,7 @@ function Folder() {
           <SearchBar />
           <SortingSection selectedFolder={selectedFolder} setSelectedFolder={setSelectedFolder} />
           <CardList folderId={selectedFolder.id} />
-          <FloatingAddFolderButton className={floatingAddFolderButtonClasses} />
+          <FloatingAddFolderButton className={floatingAddFolderButtonClasses} onClick={handleAddFolderButtonClick} />
         </Main>
       </div>
       <Footer />
