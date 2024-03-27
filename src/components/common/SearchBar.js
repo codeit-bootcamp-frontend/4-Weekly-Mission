a53bc0c22adc40f528925f7559772c3adf3e1d19
Yeojin Yoon <roseiris1001@gmail.com>
2024-03-27 19:49:09 +0900

    feat : 검색어 초기화 기능 추가

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,7 +1,15 @@
 import searchIcon from '../../assets/icon-search.svg';
+import closeIcon from '../../assets/icon-close-white.svg';
 import './SearchBar.css';
+import { useState } from 'react';
 
 const SearchBar = () => {
+  const [searchText, setSeachText] = useState('');
+
+  const deleteSeachText = () => {
+    setSeachText('');
+  };
+
   return (
     <form className="search_link">
       <img src={searchIcon} alt="search" />
@@ -9,7 +17,10 @@ const SearchBar = () => {
         type="text"
         className="input_box"
         placeholder="링크를 검색해 보세요."
+        value={searchText}
+        onChange={(e) => setSeachText(e.target.value)}
       />
+      <img src={closeIcon} alt="close" onClick={deleteSeachText} />
     </form>
   );
 };
