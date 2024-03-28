7c57f55e11c3c9eea1cd72b2e4f6a500287e653c
jinhok96 <jinhok96a@gmail.com>
2024-03-28 22:38:51 +0900

    chore: InputStateContextProvider 위치 변경 	modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactModal from 'react-modal';
 
+import { InputStateContextProvider } from 'context/InputStateProvider';
 import ModalProvider from 'context/ModalProvider';
 
 import App from './App';
@@ -33,7 +34,9 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <ModalProvider>
-      <App />
+      <InputStateContextProvider>
+        <App />
+      </InputStateContextProvider>
     </ModalProvider>
   </React.StrictMode>
 );
